368c71e83c9249f005d3dbccd69d7edbb72d3098
op48 <lianne.tan@gmail.com>
2014-09-30 10:49:38 +0100

    Added a simple form for name and age

--- hello_world.php
+++ hello_world.php
@@ -3,6 +3,18 @@
  <title>PHP Test</title>
 </head>
 <body>
-<?php echo '<p>Hello World</p>'; ?> 
+
+<form action="hello_world.php" method="post">
+  <p>Your name: <input type="text" name="name" /></p>
+  <p>Your age: <input type="text" name="age" /></p>
+  <p><input type="submit" value="Submit"/></p>
+</form>
+
+Hi <?php echo ($_POST['name']); ?>.
+You are <?php echo (int)$_POST['age']; ?> years old.
+
+<?php echo '<p>Hello World</p>';
+
+?>
 </body>
 </html>
