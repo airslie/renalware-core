f4f4b881404a961b6441e2d9f57a13160f642fd7
op48 <lianne.tan@gmail.com>
2014-10-01 16:39:38 +0100

    Fixed broken test

--- features/step_definitions/hello_world_steps.rb
+++ features/step_definitions/hello_world_steps.rb
@@ -1,5 +1,5 @@
 Given(/^I am on the hello world page$/) do
-  visit "http://localhost:8000/hello_world.php"
+  visit "http://localhost:8000/php/hello_world.php"
 end
 
 Then(/^I should see "(.*?)"$/) do |hello|
