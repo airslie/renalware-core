c3ed48357a98728d20a8bd024c00e2cd5a329451
op48 <lianne.tan@gmail.com>
2014-10-01 16:41:52 +0100

    Added php for fruits and index

---
+++ php/fruits.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+ <title>PHP Test</title>
+</head>
+<body>
+<p>Fruits Dashboard</p>
+
+<p><a href="index.php">View Fruits</a></p>
+</body>
+</html>
---
+++ php/index.php
@@ -0,0 +1,10 @@
+<h1>All Fruits<h1>
+  
+<p>
+<?php
+
+$query = 'SELECT * FROM fruits';
+mysql_db_query( $fruits , $query );
+
+?>
+<p>
