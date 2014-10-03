903c849003dbd9f121733f42d91256ace286cf11
op48 <lianne.tan@gmail.com>
2014-10-03 16:29:33 +0100

    Testing jquery

--- php/hello_world.php
+++ php/hello_world.php
@@ -1,8 +1,11 @@
 <html>
 <head>
  <title>PHP Test</title>
+ <script type="text/javascript" src="../js/jquery-1.11.1.min.js"></script>
 </head>
 <body>
+
+<p onclick="$(this).hide(1000);">click to hide</p>
 
 <form action="hello_world.php" method="post">
   <p>Your name: <input type="text" name="name" /></p>
