39dccde7d4d9a9cb2bf6cadcd4ee5b789e39b49b
op48 <lianne.tan@gmail.com>
2014-10-03 13:17:55 +0100

    Connect php to mysql

--- php/index.php
+++ php/index.php
@@ -1,10 +1,13 @@
 <h1>All Fruits<h1>
-  
+
 <p>
 <?php
+$link = mysqli_connect("localhost","CHANGE ME to username","","cuke_php_test") or die("Error " . mysqli_error($link));
+$query = "SELECT * FROM fruits" or die("Error in the consult.." . mysqli_error($link));
+$result = $link->query($query);
 
-$query = 'SELECT * FROM fruits';
-mysql_db_query( $fruits , $query );
-
+while($row = mysqli_fetch_array($result)) {
+  echo $row["name"] . "<br>";
+}
 ?>
 <p>
