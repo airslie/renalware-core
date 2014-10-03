68201bbbc96f4ef61bfedd6d6f48ad590a8e028e
op48 <lianne.tan@gmail.com>
2014-10-03 13:17:06 +0100

    Added comment on whats happening

--- features/step_definitions/legacy_database_steps.rb
+++ features/step_definitions/legacy_database_steps.rb
@@ -1,3 +1,4 @@
+# Takes a database backup from Paul and loads into the test database (DB_NAME)
 Given(/^the legacy database data "(.*?)"$/) do |fixture_ddl|
   # TODO make this nicer to work with?
   `cat test/db/#{fixture_ddl} | mysql #{DB_NAME} -u #{ENV['USER']}`
