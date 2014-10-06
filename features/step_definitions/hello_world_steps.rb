615f77ba19e383d3564f5540292feeddd0cf3e1d
Dan Garland <dan@dangarland.co.uk>
2014-10-06 11:27:58 +0100

    Losing development test files

--- features/step_definitions/hello_world_steps.rb
+++
@@ -1,8 +0,0 @@
-Given(/^I am on the hello world page$/) do
-  visit "http://localhost:8000/hello_world.php"
-end
-
-Then(/^I should see "(.*?)"$/) do |hello|
-  expect(page.has_content?(hello)).to be true
-end
-
