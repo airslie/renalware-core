43702141e6914ebd3c1d6629f61adb865b12e640
op48 <lianne.tan@gmail.com>
2014-10-09 16:55:27 +0100

    filled in missing field

--- features/step_definitions/sign_in_steps.rb
+++ features/step_definitions/sign_in_steps.rb
@@ -1,10 +1,11 @@
 Given(/^I have a user in the database$/) do
-  $client.query("INSERT INTO userdata VALUES (1,'daniel','*668425423DB5193AF921380129F465A6425216D0', \
+  $client.query("INSERT INTO userdata VALUES (1097,'daniel','*668425423DB5193AF921380129F465A6425216D0', \
     'Garland','MR',0,1,1,1,1,1,1,1,'2014-10-06',NULL,'2014-10-06 17:13:33', \
-    'IT Staff','dan@dangarland.co.uk','','Renal','United Kingdom','','', \
+    'IT Staff','dan@dangarland.co.uk','kings','Renal','United Kingdom','','', \
     '447979770053','','DMG','Dan','GURU','2014-10-06 16:29:23', \
     '2014-10-06 17:13:13',1,'2014-10-06 17:13:13','2014-10-06 17:14:03',1,0, \
     NULL,0,'0',0,0)")
+  #binding.pry
 end
 
 Given(/^I am on the signin page$/) do
@@ -18,6 +19,5 @@ When(/^I sign in$/) do
 end
 
 Then(/^I should see my dashboard$/) do
-  pending
-  expect(page.has_content? "My Amazing Dashboard?").to be_true
+  expect(page.has_content? "Home Screen").to be true  
 end
