cb8290bfbb0ab6892704e01694249c4a3210f3f0
op48 <lianne.tan@gmail.com>
2014-10-20 16:22:09 +0100

    removed unused step

--- features/php/step_definitions/search_patient_steps.rb
+++ features/php/step_definitions/search_patient_steps.rb
@@ -1,3 +1,12 @@
-Then(/^he will see a list of matching results$/) do
+Given(/^some patients who need renal treatment$/) do
+  $client.query File.read('test/fixtures/patient.sql')
+end
+
+Given(/^I've searched for a patient$/) do
+  find('#findpatinput').set('RABBIT')
+  click_button "Find!"
+end
+
+Then(/^they will see a list of matching results$/) do
   expect(page.has_content? "RABBIT").to be true
 end
--- features/stories/patient_search.feature
+++ features/stories/patient_search.feature
@@ -5,5 +5,4 @@ Scenario: A doctor searches for a patient
     And some patients who need renal treatment
     #quicksearch by hospital number, lastname, firstname or NHS number
     And I've searched for a patient
-    And I've selected the patient from the search results
-  Then he will see a list of matching results
+  Then they will see a list of matching results
