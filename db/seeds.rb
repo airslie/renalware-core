a0c45293486a56f843b7a6a1fd3eba0d3408e3a0
op48 <lianne.tan@gmail.com>
2014-10-21 17:04:32 +0100

    seeded a patient in to dev database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Patient.find_or_create_by!(:nhs_number => "1000124502") do |patient|
+  patient.local_patient_id = "Z999999" 
+  patient.surname = "RABBIT" 
+  patient.forename = "R"
+  patient.dob = "01/01/1947"
+  patient.paediatric_patient_indicator = "1"
+  patient.sex = "1"
+  patient.ethnic_category = "A"
+end
