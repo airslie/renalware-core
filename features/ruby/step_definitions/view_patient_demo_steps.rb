f03a19c4e0035a3e15570dd4c23bc1db8dcd6d27
op48 <lianne.tan@gmail.com>
2014-10-21 17:06:05 +0100

    Made corrections to column references and modified expectations

--- features/ruby/step_definitions/view_patient_demo_steps.rb
+++ features/ruby/step_definitions/view_patient_demo_steps.rb
@@ -1,12 +1,12 @@
 Given(/^I have a patient in the database$/) do
   @patient = Patient.create!(
     :nhs_number => "1000124502", 
-    :local_patient_identifier => "Z999999", 
+    :local_patient_id => "Z999999", 
     :surname => "RABBIT", 
     :forename => "R",
-    :date_of_birth => "01/01/1947",
-    :paediatric_patient_flag => 1,
-    :sex => 1,
+    :dob => "01/01/1947",
+    :paediatric_patient_indicator => "1",
+    :sex => "1",
     :ethnic_category => "A"
     ) 
 end
@@ -18,15 +18,12 @@ Given(/^I've searched for a patient$/) do
 end
 
 Given(/^I've selected the patient from the search results$/) do
-  visit "patients/#{@patient.id}"
+  visit patient_path(@patient)
+  save_and_open_page
 end
 
 Then(/^I should see the patient's demographics on their profile page$/) do
-  expect(page.has_content? "PATIENT DEMOGRAPHICS").to be true
   expect(page.has_content? "1000124502").to be true
-  expect(page.has_content? "Mr").to be true
   expect(page.has_content? "RABBIT").to be true
   expect(page.has_content? "R").to be true
-  expect(page.has_content? "M").to be true
-  expect(page.has_content? "01/01/1947").to be true
 end
