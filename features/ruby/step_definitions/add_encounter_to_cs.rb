a0cdbae78469bf8e2b196e584072d6c1ab590392
op48 <lianne.tan@gmail.com>
2014-10-24 17:47:08 +0100

    Created steps for adding an encounter

---
+++ features/ruby/step_definitions/add_encounter_to_cs.rb
@@ -0,0 +1,15 @@
+Given(/^they are on a patient's clinical summary$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^they add an event$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^complete the encounter form$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^they should see the new event on the clinical summary$/) do
+  pending # express the regexp above with the code you wish you had
+end
