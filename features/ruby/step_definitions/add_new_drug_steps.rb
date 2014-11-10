e4c0c0c7181fcff14ea55457795f0c9a55f13298
op48 <lianne.tan@gmail.com>
2014-11-10 18:23:21 +0000

    Fixed test after removing Standard drug type as an STI model

--- features/ruby/step_definitions/add_new_drug_steps.rb
+++ features/ruby/step_definitions/add_new_drug_steps.rb
@@ -4,10 +4,11 @@ end
 
 When(/^I complete the form for a new drug$/) do
   fill_in "New Drug Name", with: "I am a new drug"
-  fill_in "Type", with: "Standard"
+  select "Immunosuppressant", from: "Type"
   click_on "Save New Drug"
 end
 
 Then(/^I should see the new drug on the drugs list$/) do
   expect(page.has_content? "I am a new drug").to be true
+  expect(page.has_content? "Immunosuppressant").to be true
 end
