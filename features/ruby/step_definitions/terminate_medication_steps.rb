7ade47e3e747d8e5140989797cea58c527726066
op48 <lianne.tan@gmail.com>
2014-11-19 18:16:59 +0000

    Created tests for terminating a medication

---
+++ features/ruby/step_definitions/terminate_medication_steps.rb
@@ -0,0 +1,24 @@
+Given(/^a patient has a medication$/) do
+  @patient_medication = @patient.patient_medications.build(medication_id: 2, 
+    medication_type: "Esa", 
+    dose: "10mg", 
+    route: "PO",
+    frequency: "Daily", 
+    notes: "Must take with food", 
+    date: "2014-11-01",
+    provider: 1
+    )
+end
+
+When(/^they terminate a medication$/) do
+  visit medications_patient_path(@patient)
+end
+
+Then(/^they should no longer see this medication in their clinical summary$/) do
+  expect(page.has_content? "Blue").to be false
+end
+
+# Then(/^should see this terminated medication in their medications history$/) do
+#   visit medications_index_patient_path(@patient)
+#   expect(page.has_content? "Blue" ).to be true
+# end
