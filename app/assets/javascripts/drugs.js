dd5b6032c964fc7ee6117898139a6eef7e91ef76
op48 <lianne.tan@gmail.com>
2014-11-20 17:04:08 +0000

    Fixed form for changing patient medication type for each form (saved medication and creating new meds)

--- app/assets/javascripts/drugs.js
+++ app/assets/javascripts/drugs.js
@@ -1,29 +1,31 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(document).ready(function(){
-    
-  $('#medication_type').change(function(e) {
+
+  $('.medication-type-select').change(function(e) {
     var select_box = $(e.currentTarget);
     var selected_medication_type = select_box.val();
 
+    var med_form = select_box.closest('.med-form');
+    
     switch (selected_medication_type) {
-    case "":
-      $("#patient_patient_medications_attributes_0_provider_gp").prop("checked", true);
-    break;
-    case "Esa":
-      $("#patient_patient_medications_attributes_0_provider_hospital").prop("checked", true);
-    break;
-    case "Immunosuppressant":
-      $("#patient_patient_medications_attributes_0_provider_gp").prop("checked", true);
-    break;
-  }
+      case "":
+        med_form.find('.provider-gp').prop("checked", true);
+      break;
+      case "Esa":
+        med_form.find('.provider-hospital').prop("checked", true);
+      break;
+      case "Immunosuppressant":
+        med_form.find('.provider-home-delivery').prop("checked", true);
+      break;
+    }
 
     $.ajax({
       url: '/drugs.json',
       data: { medication_type: selected_medication_type },
       success: function(json) {
         console.log(json);
-        var drug_select_box = $("#patient_patient_medications_attributes_0_medication_id");
+        var drug_select_box = med_form.find('.drug-select');
         drug_select_box.html('');
 
         for (var i = 0; i < json.length; i++) {
