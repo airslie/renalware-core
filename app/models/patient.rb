3b9dea0bcb6f73e6d387547d36b056f06dd2c757
op48 <lianne.tan@gmail.com>
2014-11-28 12:11:58 +0000

    Made age more accurate

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -28,7 +28,8 @@ class Patient < ActiveRecord::Base
   end
 
   def age
-    Time.now.year - dob.year
+    now = Time.now.utc.to_date
+    now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
   end
 
 end
