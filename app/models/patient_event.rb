ce0e448ecc1ee243a04b2c668eac5c9a6629fff8
op48 <lianne.tan@gmail.com>
2014-12-04 12:22:35 +0000

    removed validation for user_id(field in form now removed)

--- app/models/patient_event.rb
+++ app/models/patient_event.rb
@@ -2,6 +2,6 @@ class PatientEvent < ActiveRecord::Base
   belongs_to :patients
   belongs_to :patient_event_type
 
-  validates :patient_event_type_id, :date_time, :user_id,
+  validates :patient_event_type_id, :date_time,
       :description, :notes, :presence => true 
 end
