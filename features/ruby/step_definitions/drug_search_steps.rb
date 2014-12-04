d5f591c1f769280ee2db455fc473fb857b2c1535
op48 <lianne.tan@gmail.com>
2014-12-04 12:34:57 +0000

    Edited test/view to find correct search field

--- features/ruby/step_definitions/drug_search_steps.rb
+++ features/ruby/step_definitions/drug_search_steps.rb
@@ -1,12 +1,11 @@
 When(/^I search for a drug by name$/) do
-  click_on "Add a new medication"
-  fill_in "Drug Name", :with => "Red"
-  
+  save_and_open_page
+  fill_in "Drug Search", :with => "Red"  
 end
 
 Then(/^they should see the list of drugs listed in the dropdown$/) do
   # binding.pry
   within('.drug-select') do
-    assert page.has_css?("option", :text => "Red") # Don't quote me!!
+    page.has_css?("option", :text => "Red")
   end
 end
