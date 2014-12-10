06d450cc8726a5cf961cff6c48d04867cc2a769b
op48 <lianne.tan@gmail.com>
2014-12-10 16:53:27 +0000

    Created function for adding multiple problems

--- app/assets/javascripts/problems.js
+++ app/assets/javascripts/problems.js
@@ -1,10 +1,18 @@
-// $(document).ready(function() {
+$(document).ready(function() {
   
-//   $("a.add-problem").click(function() {
-//     var add_form = $(".probs-form").clone();
+  $("a.add-problem").click(function(e) {
+    e.preventDefault();
+    var next_number =  $("#new-probs-template").data("index") + 1;
+    $("#new-probs-template").data("index", next_number);
 
-//     // add_form.append(".additional-form");
+    var new_problem = $("#new-probs-template").html();
 
-//   });
+    var update_markup = new_problem.replace(/\d/g, next_number);
 
-// });
+    $("#additional-form").append(update_markup);
+    
+  
+  });
+
+  
+});
