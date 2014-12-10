3052eccece96f0496ff79f1d4ffa810ad3e3fee2
Dan Garland <dan@dangarland.co.uk>
2014-12-10 16:51:11 +0000

    Structure changes

--- db/structure.sql
+++ db/structure.sql
@@ -32,22 +32,122 @@ CREATE TABLE `addresses` (
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `encounter_events`
+-- Table structure for table `drugs`
 --
 
-DROP TABLE IF EXISTS `encounter_events`;
+DROP TABLE IF EXISTS `drugs`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `encounter_events` (
+CREATE TABLE `drugs` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `type` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1874 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `drugs_patients`
+--
+
+DROP TABLE IF EXISTS `drugs_patients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `drugs_patients` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `drug_id` int(11) DEFAULT NULL,
+  `patient_id` int(11) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ethnicities`
+--
+
+DROP TABLE IF EXISTS `ethnicities`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ethnicities` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `patient_event_types`
+--
+
+DROP TABLE IF EXISTS `patient_event_types`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `patient_event_types` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `patient_events`
+--
+
+DROP TABLE IF EXISTS `patient_events`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `patient_events` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date_time` datetime DEFAULT NULL,
+  `user_id` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `description` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `notes` text COLLATE utf8_unicode_ci,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `patient_event_type_id` int(11) DEFAULT NULL,
+  `patient_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `patient_medications`
+--
+
+DROP TABLE IF EXISTS `patient_medications`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `patient_medications` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `patient_id` int(11) DEFAULT NULL,
+  `medication_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `medication_type` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `dose` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `route` int(11) DEFAULT NULL,
+  `frequency` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `notes` text COLLATE utf8_unicode_ci,
+  `date` date DEFAULT NULL,
+  `provider` int(11) DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -59,22 +159,37 @@ DROP TABLE IF EXISTS `patients`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `patients` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `created_at` datetime DEFAULT NULL,
-  `updated_at` datetime DEFAULT NULL,
   `nhs_number` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `local_patient_id` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `surname` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `forename` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `dob` date DEFAULT NULL,
   `paediatric_patient_indicator` tinyint(1) DEFAULT NULL,
-  `sex` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `ethnic_category` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `sex` int(11) DEFAULT '9',
   `current_address_id` int(11) DEFAULT NULL,
   `address_at_diagnosis_id` int(11) DEFAULT NULL,
   `gp_practice_code` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `pct_org_code` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `hosp_centre_code` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `primary_esrf_centre` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `ethnicity_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `problems`
+--
+
+DROP TABLE IF EXISTS `problems`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `problems` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -101,18 +216,32 @@ CREATE TABLE `schema_migrations` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-10-28 15:12:04
+-- Dump completed on 2014-12-10 16:50:02
 INSERT INTO schema_migrations (version) VALUES ('20141020170329');
-
-INSERT INTO schema_migrations (version) VALUES ('20141020171819');
 
 INSERT INTO schema_migrations (version) VALUES ('20141023111038');
 
-INSERT INTO schema_migrations (version) VALUES ('20141023111249');
-
-INSERT INTO schema_migrations (version) VALUES ('20141023123657');
-
-INSERT INTO schema_migrations (version) VALUES ('20141024091931');
-
 INSERT INTO schema_migrations (version) VALUES ('20141024111716');
+
+INSERT INTO schema_migrations (version) VALUES ('20141030174624');
+
+INSERT INTO schema_migrations (version) VALUES ('20141030180107');
+
+INSERT INTO schema_migrations (version) VALUES ('20141030183859');
+
+INSERT INTO schema_migrations (version) VALUES ('20141104150240');
+
+INSERT INTO schema_migrations (version) VALUES ('20141104152805');
+
+INSERT INTO schema_migrations (version) VALUES ('20141105153315');
+
+INSERT INTO schema_migrations (version) VALUES ('20141106100842');
+
+INSERT INTO schema_migrations (version) VALUES ('20141107145549');
+
+INSERT INTO schema_migrations (version) VALUES ('20141107145615');
+
+INSERT INTO schema_migrations (version) VALUES ('20141124152845');
+
+INSERT INTO schema_migrations (version) VALUES ('20141208160813');
 
