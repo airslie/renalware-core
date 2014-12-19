ac2379c430cbce5fac4807e25d3c350aeeaf200f
op48 <lianne.tan@gmail.com>
2014-12-19 19:30:42 +0000

    Fixed test to include id of snomed term

--- spec/models/snomed_spec.rb
+++ spec/models/snomed_spec.rb
@@ -6,14 +6,14 @@ RSpec.describe Snomed, :type => :model do
     before do
       stub_request(:get, "http://snomed.com/").
       with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
-      to_return(:status => 200, :body => '[{"concept": "cool beans"}]', :headers => {})
+      to_return(:status => 200, :body => '[{ "id": "12345", "concept": "cool beans"}]', :headers => {})
       @results = Snomed.lookup "cool beans"
     end
 
     it "should give us snomed results" do
       expect(@results.is_a? Array).to be true
       expect(@results.length).to eq(1)
-      expect(@results[0]["concept"]).to eq("cool beans")
+      expect(@results[0].values[1]).to eq("cool beans")
     end
   end
 
