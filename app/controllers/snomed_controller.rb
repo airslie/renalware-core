62e01ee0b7ba765eca6cb98369944cc89347ea77
op48 <lianne.tan@gmail.com>
2014-12-19 11:55:22 +0000

    Prelim index method for snomed controller

--- app/controllers/snomed_controller.rb
+++ app/controllers/snomed_controller.rb
@@ -1,11 +1,8 @@
 class SnomedController < ApplicationController
-
+  
   def index
-    @results = Snomed.lookup("")
-    respond_to do |format|
-      format.html { redirect_to(problems_patient_url) }
-      format.json { render :json => @results }
-    end
+    @results = Snomed.lookup("cool beans")
+    render nothing: true
   end
 
 end
