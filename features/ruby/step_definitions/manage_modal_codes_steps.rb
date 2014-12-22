6d806c62ada86ca05be33d6f334a9b13ce31b195
op48 <lianne.tan@gmail.com>
2014-12-22 14:54:30 +0000

    Updated tests to with correct fields

--- features/ruby/step_definitions/manage_modal_codes_steps.rb
+++ features/ruby/step_definitions/manage_modal_codes_steps.rb
@@ -1,11 +1,15 @@
 Given(/^that I'm on the add a new modal page$/) do
-  visit new_modal_path
+  visit new_modality_code_path
 end
 
 When(/^I complete the form for a new modal$/) do
   fill_in "Modal Name",:with => "New Modal" 
+  fill_in "Modal Code",:with => "newmodels"
+  click_on "Save" 
 end
 
 Then(/^I should see the new modal on the modalities list$/) do
-   expect(page.has_content? "New Modal").to be true
+  save_and_open_page
+  expect(page.has_content? "New Modal").to be true
+  expect(page.has_content? "newmodel").to be true
 end
