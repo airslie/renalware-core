4b8a337cf026fcf0278c965bf8c9ff183094f566
op48 <lianne.tan@gmail.com>
2014-12-22 15:33:01 +0000

    Added tests for edit/update actions in modality_codes

--- features/ruby/step_definitions/manage_modal_codes_steps.rb
+++ features/ruby/step_definitions/manage_modal_codes_steps.rb
@@ -1,3 +1,8 @@
+Given(/^there are modalities in the database$/) do
+  @modal_codes = [["Modal One", "modelone"], ["Modal Two", "modeltwo"], ["Modal Three", "Modalthree"]]
+  @modal_codes.map! {|d| @modal_code = ModalityCode.create!(:name => d[0], :code => d[1])}
+end
+
 Given(/^that I'm on the add a new modal page$/) do
   visit new_modality_code_path
 end
@@ -9,7 +14,19 @@ When(/^I complete the form for a new modal$/) do
 end
 
 Then(/^I should see the new modal on the modalities list$/) do
-  save_and_open_page
   expect(page.has_content? "New Modal").to be true
   expect(page.has_content? "newmodel").to be true
+end
+
+Given(/^that I choose to edit a modality$/) do
+  visit edit_modality_code_path(@modal_code)
+end
+
+When(/^I complete the form for editing a modality$/) do
+  fill_in "Modal Name",:with => "This is an edited modal"
+  click_on "Update Modal" 
+end
+
+Then(/^I should see the updated drug on the modality list$/) do
+  expect(page.has_content? "This is an edited modal").to be true
 end
--- features/stories/manage_modal_codes.feature
+++ features/stories/manage_modal_codes.feature
@@ -3,10 +3,16 @@ Feature: An admin adds a new modality to existing modality list
 
 Background:
   Given that I'm logged in
+    And there are modalities in the database
   
-@wip
 Scenario: An admin adds a new modal to modality list
   Given that I'm on the add a new modal page
   When I complete the form for a new modal
   Then I should see the new modal on the modalities list
+
+@wip
+Scenario: An admin edits a modal in the modalities list
+  Given that I choose to edit a modality
+  When I complete the form for editing a modality
+  Then I should see the updated drug on the modality list
 
