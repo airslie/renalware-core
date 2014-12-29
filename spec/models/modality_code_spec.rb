9ac4a538faf7573b2c0482adee78aefc0a56ba32
op48 <lianne.tan@gmail.com>
2014-12-29 11:04:29 +0000

    Set up rspec tests for associations

--- spec/models/modality_code_spec.rb
+++ spec/models/modality_code_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe ModalityCode, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many :patient_modality }
+  it { should have_many(:patients).through(:patient_modality)}
 end
--- spec/models/patient_modality_spec.rb
+++ spec/models/patient_modality_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe PatientModality, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:modality_code) }
+  it { should belong_to(:patient) }
 end
