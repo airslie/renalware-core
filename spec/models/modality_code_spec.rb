404e8d854b7c2e9dee61db5be30f53d6c975f873
op48 <lianne.tan@gmail.com>
2014-12-30 18:58:39 +0000

    Edited modality code rspec associations

--- spec/models/modality_code_spec.rb
+++ spec/models/modality_code_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe ModalityCode, :type => :model do
-  it { should have_many :patient_modality }
-  it { should have_many(:patients).through(:patient_modality)}
+  it { should have_many :patient_modalities }
+  it { should have_many(:patients).through(:patient_modalities)}
 end
