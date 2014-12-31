a3586710cf728f2f7d6c860c1b9f5934f6588ca2
op48 <lianne.tan@gmail.com>
2014-12-31 11:59:46 +0000

    Added look up table for Renal Reg Reasons for Change Codes with link in patient_modality form

--- app/controllers/modality_reasons_controller.rb
+++ app/controllers/modality_reasons_controller.rb
@@ -1,6 +1,8 @@
 class ModalityReasonsController < ApplicationController
 
   def index
+    @reason_hd_pd = HaemodialysisToPd.all
+    @reason_pd_hd = PdToHaemodialysis.all
     @modality_reason_select = ModalityReason.where(:type => params[:modal_change_type])
     respond_to do |format|
       format.html
