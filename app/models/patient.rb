82c4cbaffe3e3dee95d2233a8cdfa501d909ab15
op48 <lianne.tan@gmail.com>
2015-01-02 13:22:15 +0000

    Added edta_code model and set up associations

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -4,6 +4,8 @@ class Patient < ActiveRecord::Base
   belongs_to :current_address, :class_name => "Address", :foreign_key => :current_address_id
   belongs_to :address_at_diagnosis, :class_name => "Address", :foreign_key => :address_at_diagnosis_id
   belongs_to :ethnicity
+  belongs_to :first_edta_code, :class_name => "EdtaCode", :foreign_key => :first_edta_code_id
+  belongs_to :second_edta_code, :class_name => "EdtaCode", :foreign_key => :second_edta_code_id
 
   has_many :patient_events
   has_many :patient_medications
