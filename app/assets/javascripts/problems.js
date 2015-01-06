1e6ebff66bfcb0678b7e0170681506494c52a732
op48 <lianne.tan@gmail.com>
2015-01-06 15:45:11 +0000

    Fixed issue with adding another problem to problems feature

--- app/assets/javascripts/problems.js
+++ app/assets/javascripts/problems.js
@@ -2,10 +2,10 @@ $(document).ready(function() {
   
   $("a.add-problem").click(function(e) {
     e.preventDefault();
-    var next_number =  $("#new-probs-template").data("index") + 1;
-    $("#new-probs-template").data("index", next_number);
+    var next_number =  $(".problem-form").data("index") + 1;
+    $(".problem-form").data("index", next_number);
 
-    var new_problem = $("#new-probs-template").html();
+    var new_problem = $(".problem-form").html();
 
     var update_markup = new_problem.replace(/\d/g, next_number);
 
