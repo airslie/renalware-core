64be095dcbdefab01d5c7e0f286612b9c92d42a1
op48 <lianne.tan@gmail.com>
2015-01-06 13:30:31 +0000

    Added info to modal/pop up for rr codes lookup link

--- app/controllers/modality_reasons_controller.rb
+++ app/controllers/modality_reasons_controller.rb
@@ -6,7 +6,7 @@ class ModalityReasonsController < ApplicationController
     @modality_reason_select = ModalityReason.where(:type => params[:modal_change_type])
     respond_to do |format|
       format.html
-      format.json { render :json => @modality_reason_select.as_json(:only => [:id, :description]) }
+      format.json { render :json => @modality_reason_select.as_json(:only => [:id, :rr_code, :description]) }
     end
   end
 
