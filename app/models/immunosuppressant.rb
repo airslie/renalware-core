ebd5d5b38a58f1f0f68a12fa15780c9c653f7fe3
op48 <lianne.tan@gmail.com>
2015-01-19 11:52:39 +0000

    Fixed drug search so that it picks up esa/immuno as well as standard drugs

--- app/models/immunosuppressant.rb
+++ app/models/immunosuppressant.rb
@@ -1,5 +1,14 @@
 class Immunosuppressant < Drug
+
+  index_name "drugs"
+
+  document_type "drug"
+
   def display_type
     "Immunosuppressant"
+  end
+
+  def as_indexed_json(options={})
+    as_json(only: %i(name type))
   end
 end
