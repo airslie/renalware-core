46ff14cf4095ec528b377a506827ee92a43e6ab3
op48 <lianne.tan@gmail.com>
2015-01-28 18:28:46 +0000

    Preparing test for Problem.history to display historical versions of a problem

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,9 +1,15 @@
 class Problem < ActiveRecord::Base
-
   belongs_to :patient
-
-  # has_paper_trail
-
+ 
   has_paper_trail :class_name => 'ProblemVersion'
+  
+  def history
+    problem = self
+    history = [problem]
+    while (problem = problem.previous_version) != nil
+      history << problem
+    end
+    history
+  end
 
 end
--- spec/models/problem_spec.rb
+++ spec/models/problem_spec.rb
@@ -2,4 +2,33 @@ require 'rails_helper'
 
 RSpec.describe Problem, :type => :model do
   it { should belong_to :patient }
+
+  describe "history", :versioning => true do
+    before do
+      binding.pry
+      @patient = Patient.find_or_create_by!(
+        nhs_number: "1000124505", 
+        local_patient_id: "Z999988", 
+        surname: "Jones", 
+        forename: "Jack",
+        dob: "01/01/1988",
+        paediatric_patient_indicator: "0",
+        sex: 1,
+        ethnicity_id: 1
+        )
+    end
+
+    context "display all history of problems " do
+      before do
+        p = @patient.problems.create!(description: "I have a problem")
+        p.update!(description: "I have another problem")
+      end
+
+      it "should record a patient's history of a problem" do
+        expect(@patient.problems.length).to eq(1)
+      end 
+    end
+
+  end
+
 end
