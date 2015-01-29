ad31d912cc866dc8bd7a82348ead97ab0d2a4d04
op48 <lianne.tan@gmail.com>
2015-01-29 14:11:01 +0000

    Adjusted Problem.history method to exclude the current problem version, updated rspec test

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -2,13 +2,14 @@ class Problem < ActiveRecord::Base
   belongs_to :patient
  
   has_paper_trail :class_name => 'ProblemVersion'
-  
+
   def history
     problem = self
     history = [problem]
     while (problem = problem.previous_version) != nil
       history << problem
     end
+    history.shift
     history
   end
 
