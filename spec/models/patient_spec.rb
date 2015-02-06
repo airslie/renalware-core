e0b631a1e5f6def9c566dc2905138f917dbd42da
op48 <lianne.tan@gmail.com>
2015-02-06 17:29:45 +0000

    Rspec unit test for patient_medication model using soft delete on update(_destroy) via paranoia gem

--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -7,5 +7,19 @@ RSpec.describe Patient, :type => :model do
   it { should have_one(:modality_code).through(:patient_modality)}
   it { should accept_nested_attributes_for(:patient_modality)}
   it { should have_one :esrf_info }
-  it { should accept_nested_attributes_for(:esrf_info)}  
+  it { should accept_nested_attributes_for(:esrf_info)} 
+
+  describe "updating with nested attributes containing _destroy" do
+    it "should soft delete the associated record" do
+      patient = FactoryGirl.create(:patient)
+      patient_medication = FactoryGirl.create(:patient_medication)
+      patient.patient_medications << patient_medication
+      
+      patient.update(patient_medications_attributes: {
+        "0" => { id: patient_medication.id, dose: "a lot", _destroy: "1" } })
+      
+      expect(patient.patient_medications.with_deleted.first).to eq(patient_medication)
+      expect(patient.patient_medications.with_deleted.first.deleted_at).not_to be nil
+    end
+  end 
 end
