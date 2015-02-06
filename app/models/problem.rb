7b4df328ce92d9ad0dd4f48c5915de03cf735e85
op48 <lianne.tan@gmail.com>
2015-02-06 17:46:06 +0000

    Added acts_as_paranoid to Problems model

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,6 +1,10 @@
 class Problem < ActiveRecord::Base
+  
+  acts_as_paranoid
+
+  has_paper_trail :class_name => 'ProblemVersion'
+  
   belongs_to :patient
  
-  has_paper_trail :class_name => 'ProblemVersion'
 
 end
