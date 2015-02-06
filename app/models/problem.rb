0874d9de189f4cb25200da92266bb4d274954930
op48 <lianne.tan@gmail.com>
2015-02-06 10:24:11 +0000

    Removed 'history' method from problem and patient_medication models (Using paper_trail 'versions' and 'reify' methods instead)

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -3,14 +3,4 @@ class Problem < ActiveRecord::Base
  
   has_paper_trail :class_name => 'ProblemVersion'
 
-  def history
-    problem = self
-    history = [problem]
-    while (problem = problem.previous_version) != nil
-      history << problem
-    end
-    history.shift
-    history
-  end
-
 end
