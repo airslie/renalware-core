a1e946f89b7c5e4801abcb6e71bd7fbec23153e0
op48 <lianne.tan@gmail.com>
2015-02-06 17:25:24 +0000

    Rspec unit test for patient update

--- spec/controllers/patients_controller_spec.rb
+++ spec/controllers/patients_controller_spec.rb
@@ -9,4 +9,25 @@ RSpec.describe PatientsController, :type => :controller do
     end
   end
 
+  # When a doctor checks a terminate box a soft delete is triggered.
+  # And the deleted_at value is not nil.
+
+  describe "PATCH update" do
+
+    before do
+      @patient = FactoryGirl.create(:patient)     
+    end
+
+    it "should redirect when the patient is updated" do
+      patch :update, id: @patient.id, patient: { patient_medications_attributes: {} }
+      expect(response).to have_http_status(:found)     
+    end
+
+    it "should render the form when update fails" do
+      patch :update, id: @patient.id, patient: { forename: " " }
+      expect(response).to have_http_status(:success)     
+    end
+  
+  end
+
 end
