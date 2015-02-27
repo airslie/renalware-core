a696dc7cc0badab1d18796e3851de50537da3947
op48 <lianne.tan@gmail.com>
2015-02-27 22:38:30 +0000

    Displayed saved antibiotic routes in peritonitis view(table) as string(not integer)

--- app/models/peritonitis_episode.rb
+++ app/models/peritonitis_episode.rb
@@ -6,5 +6,17 @@ class PeritonitisEpisode < ActiveRecord::Base
   def self.medication_routes
     @medication_routes ||= YAML.load_file(Rails.root.join("data", "medication_routes.yml"))
   end
+
+  def antibiotic_routes
+    [].tap do |ary|
+      (1..5).each do |num|
+        ary << medication_route(send(:"antibiotic_#{num}_route"))
+      end
+    end
+  end
+
+  def medication_route(id)
+    self.class.medication_routes.find { |r| r.id == id }
+  end
  
 end
