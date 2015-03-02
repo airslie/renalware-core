0b7c4a45142ec785840ece4bbb71c35822411b4c
op48 <lianne.tan@gmail.com>
2015-03-02 15:51:24 +0000

    Updated exit site infections antibiotic route fields with dropdown options, with correctly displayed string in view table on pd_info page.

--- app/models/exit_site_infection.rb
+++ app/models/exit_site_infection.rb
@@ -1,3 +1,21 @@
 class ExitSiteInfection < ActiveRecord::Base
   belongs_to :patient
+
+  def self.medication_routes
+    @medication_routes ||= YAML.load_file(Rails.root.join("data", "medication_routes.yml"))
+  end
+
+  def antibiotic_routes
+    [].tap do |ary|
+      (1..3).each do |num|
+        ary << medication_route(send(:"antibiotic_#{num}_route"))
+      end
+    end
+  end
+
+  def medication_route(id)
+    self.class.medication_routes.find { |r| r.id == id }
+  end
+
+
 end
