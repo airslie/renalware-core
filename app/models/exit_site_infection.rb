3ba96e862f6e07580b30832088fb55d63822c515
op48 <lianne.tan@gmail.com>
2015-03-04 16:19:20 +0000

    Added antibiotic drop downs for exit site infection form/update exit site view table on PD_info page.

--- app/models/exit_site_infection.rb
+++ app/models/exit_site_infection.rb
@@ -4,6 +4,10 @@ class ExitSiteInfection < ActiveRecord::Base
   belongs_to :organism_1, :class_name => "OrganismCode", :foreign_key => :organism_1_id
   belongs_to :organism_2, :class_name => "OrganismCode", :foreign_key => :organism_2_id
 
+  belongs_to :antibiotic_1, :class_name => "Antibiotic", :foreign_key => :antibiotic_1_id
+  belongs_to :antibiotic_2, :class_name => "Antibiotic", :foreign_key => :antibiotic_2_id
+  belongs_to :antibiotic_3, :class_name => "Antibiotic", :foreign_key => :antibiotic_3_id
+ 
   def self.medication_routes
     @medication_routes ||= YAML.load_file(Rails.root.join("data", "medication_routes.yml"))
   end
