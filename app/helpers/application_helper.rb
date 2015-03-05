d88ae69380c28166d07dd25b2b27a12cabefc25e
op48 <lianne.tan@gmail.com>
2015-03-05 17:49:44 +0000

    Removed old unused medication route methods.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,8 +6,4 @@ module ApplicationHelper
     options_for_select Patient.sexes.keys.map { |g| [I18n.t("enums.gender.#{g}"), g] }
   end
 
-  def medication_routes(model)
-    model.medication_routes.map { |r| [ r.name, r.id ] }
-  end 
-
 end
