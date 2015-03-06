303f5a97cec5ed0e28b4e623ca80d1c6d0f5cf25
op48 <lianne.tan@gmail.com>
2015-03-06 10:39:14 +0000

    Created factory for a peritonitis episode.

--- spec/factories/peritonitis_episodes.rb
+++ spec/factories/peritonitis_episodes.rb
@@ -1,6 +1,25 @@
 FactoryGirl.define do
   factory :peritonitis_episode do
-    
+    patient_id 1                     
+    diagnosis_date "30/01/2015"
+    start_treatment_date "1/02/2015"
+    end_treatment_date "28/02/2015"
+    episode_type_id 1    
+    catheter_removed true   
+    line_break false 
+    exit_site_infection true
+    diarrhoea false          
+    abdominal_pain false     
+    fluid_description_id 2
+    white_cell_total 2000   
+    white_cell_neutro 25  
+    white_cell_lympho 25 
+    white_cell_degen 20  
+    white_cell_other 30   
+    organism_1_id 5      
+    organism_2_id 12     
+    notes "Needs review in 6 weeks"                  
+    sensitivities "Need to monitor antibiotic 1"                        
   end
 
 end
