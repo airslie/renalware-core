8cd05b43c02022a037f77f3e628095dfd67b88cd
op48 <lianne.tan@gmail.com>
2015-03-18 11:05:48 +0000

    Made corrections to medication route_associations with exit_site_infection and peritonitis_episode models.

--- app/models/exit_site_infection.rb
+++ app/models/exit_site_infection.rb
@@ -1,7 +1,7 @@
 class ExitSiteInfection < ActiveRecord::Base
   
   has_many :medications, as: :treatable
-  has_many :medication_routes, through: :medications, as: :treatable
+  has_many :medication_routes, through: :medications
   has_many :patients, through: :medications, as: :treatable
   has_many :infection_organisms, as: :infectable
   has_many :organism_codes, through: :infection_organisms, as: :infectable
--- app/models/peritonitis_episode.rb
+++ app/models/peritonitis_episode.rb
@@ -4,7 +4,7 @@ class PeritonitisEpisode < ActiveRecord::Base
   belongs_to :fluid_description
  
   has_many :medications, as: :treatable
-  has_many :medication_routes, through: :medications, as: :treatable  
+  has_many :medication_routes, through: :medications
   has_many :patients, through: :medications, as: :treatable
   has_many :infection_organisms, as: :infectable
   has_many :organism_codes, -> { uniq }, through: :infection_organisms, as: :infectable
