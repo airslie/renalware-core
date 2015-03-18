04b7c0e0762a8984c9a4652239a1468fd300484f
op48 <lianne.tan@gmail.com>
2015-03-18 11:43:42 +0000

    Made corrections to infection_organism associations with peritonitis_episode and exit_site_infection models.

--- app/models/peritonitis_episode.rb
+++ app/models/peritonitis_episode.rb
@@ -6,7 +6,7 @@ class PeritonitisEpisode < ActiveRecord::Base
   has_many :medications, as: :treatable
   has_many :medication_routes, through: :medications
   has_many :patients, through: :medications, as: :treatable
-  has_many :infection_organisms, as: :infectable
+  has_many :infection_organisms
   has_many :organism_codes, -> { uniq }, through: :infection_organisms, as: :infectable
 
 
