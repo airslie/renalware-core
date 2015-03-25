429e0bf573709d3e7984afa8495f343cd6a1185b
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2015-03-25 16:08:37 +0000

    Fix failing test for unavailable route

--- spec/controllers/patients_controller_spec.rb
+++ spec/controllers/patients_controller_spec.rb
@@ -2,32 +2,25 @@ require 'rails_helper'
 
 RSpec.describe PatientsController, :type => :controller do
 
-  describe "GET show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
-    end
-  end
-
   # When a doctor checks a terminate box a soft delete is triggered.
   # And the deleted_at value is not nil.
 
   describe "PATCH update" do
 
     before do
-      @patient = FactoryGirl.create(:patient)     
+      @patient = FactoryGirl.create(:patient)
     end
 
     it "should redirect when the patient is updated" do
       patch :update, id: @patient.id, patient: { patient_medications_attributes: {} }
-      expect(response).to have_http_status(:found)     
+      expect(response).to have_http_status(:found)
     end
 
     it "should render the form when update fails" do
       patch :update, id: @patient.id, patient: { forename: " " }
-      expect(response).to have_http_status(:success)     
+      expect(response).to have_http_status(:success)
     end
-  
+
   end
 
 end
