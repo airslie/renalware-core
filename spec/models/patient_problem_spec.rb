7ce04582439fe483e85fae4b5faa2249f2418626
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2015-03-25 10:59:22 +0000

    Remove outdated test

--- spec/models/patient_problem_spec.rb
+++ spec/models/patient_problem_spec.rb
@@ -2,25 +2,4 @@ require 'rails_helper'
 
 RSpec.describe PatientProblem, :type => :model do
   it { should belong_to :patient }
-
-  describe "history", :versioning => true do
-    before do
-      # binding.pry
-      @patient = FactoryGirl.create(:patient)
-    end
-
-    context "display all history of problems " do
-      before do
-        @problem = @patient.patient_problems.create!(description: "I have a problem")
-        @problem.update!(description: "I have another problem") 
-        @problem.update!(description: "I have more problems")   
-      end
-
-      it "should record a patient's history of a problem" do
-        expect(@problem.history.map { |x| x.description }).to eq(["I have another problem", "I have a problem"])
-      end 
-    end
-
-  end
-
 end
