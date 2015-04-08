1e90da191b32d4fa42273993fc4251bf2eae43e7
op48 <lianne.tan@gmail.com>
2015-04-08 15:53:54 +0100

    Removed unused comment.

--- app/helpers/drugs_helper.rb
+++ app/helpers/drugs_helper.rb
@@ -9,7 +9,6 @@ module DrugsHelper
   end
 
   def drug_select_options(selected = nil)
-    options_for_select DrugType.all.map { |dt| [dt.name, dt.name.downcase] }, selected   
-    # options_for_select DrugType.all, selected   
+    options_for_select DrugType.all.map { |dt| [dt.name, dt.name.downcase] }, selected    
   end
 end
