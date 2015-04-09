9ac617aca1c463162856fabcee8044a754b6ee00
op48 <lianne.tan@gmail.com>
2015-04-09 17:01:38 +0100

    Created factories for drug_drug_types and medication.

--- spec/factories/drug_drug_types.rb
+++ spec/factories/drug_drug_types.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :drug_drug_type do
-    
+    drug_id 1
+    drug_type_id 1
   end
 
 end
--- spec/factories/medication.rb
+++ spec/factories/medication.rb
@@ -1,14 +1,16 @@
 FactoryGirl.define do
   factory :medication do
-    patient_id 1
+    patient
     user_id "1"
     medicatable_id 1
     medicatable_type "Drug"
+    treatable_id nil
+    treatable_type nil
     dose "20mg"
     medication_route_id 1
     frequency "daily"
     notes "with food"
-    date "2015"
+    date "25/02/2015"
     deleted_at "NULL"
     created_at "2015-02-03 18:21:04"
     updated_at "2015-02-05 18:21:04"
