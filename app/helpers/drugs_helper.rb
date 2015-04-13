e981487f0fc6d1fd0b101b5054594b62583ea2d0
op48 <lianne.tan@gmail.com>
2015-04-13 12:54:19 +0100

    Edited the way medications are colour tagged to be more consistent.

--- app/helpers/drugs_helper.rb
+++ app/helpers/drugs_helper.rb
@@ -11,4 +11,15 @@ module DrugsHelper
   def drug_select_options(selected = nil)
     options_for_select DrugType.all.reject{ |r| r.name == "Peritonitis" }.map { |dt| [dt.name, dt.name.downcase] }, selected    
   end
+
+  def drug_types_colour_tag(drug_type)  
+    if drug_type.map(&:name).include?("ESA")
+      return "esa"
+    elsif drug_type.map(&:name).include?("Immunosuppressant")
+      return "immunosuppressant"
+    else
+      return "drug"
+    end
+  end
+
 end
