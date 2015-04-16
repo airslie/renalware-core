ba45c283c2e62837f64efe1f118397368e51359a
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2015-04-16 15:43:44 +0100

    Add modalities controller action specs

--- app/controllers/patient_modalities_controller.rb
+++ app/controllers/patient_modalities_controller.rb
@@ -1,24 +1,32 @@
 class PatientModalitiesController < ApplicationController
+
+  before_filter :find_patient
+
   def new
-    @modality = PatientModality.new(patient: patient)
+    @modality = PatientModality.new(patient: @patient)
   end
 
   def index
-    @modalities = patient.patient_modalities
+    # TODO: Ordering is incorrect here as an arbitrary date
+    # of termination can be set.
+    @modalities = @patient.patient_modalities.with_deleted.order('deleted_at DESC')
   end
 
   def create
-    if patient.patient_modality.present?
-      @modality = patient.patient_modality.supersede!(params[:patient_modality])
-    else
-      @modality = PatientModality.create!(params[:patient_modality])
-    end
-    redirect_to patient_modalities_path
+    @patient.set_modality(modality_params)
+
+    redirect_to patient_patient_modalities_path(@patient)
   end
 
   private
 
-  def patient
-    @patient ||= Patient.find(params[:patient_id])
+  def modality_params
+    params.require(:modality).permit(
+      :patient_id, :user_id, :modality_code_id,
+      :modality_change_type, :modality_reason_id, :date)
+  end
+
+  def find_patient
+    @patient = Patient.find(params[:patient_id])
   end
 end
--- spec/controllers/patient_modalities_controller_spec.rb
+++ spec/controllers/patient_modalities_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe PatientModalitiesController, :type => :controller do
 
   before do
-    @patient = FactoryGirl.create(:patient)
+    @patient = create(:patient)
   end
 
   describe 'new' do
@@ -12,24 +12,59 @@ describe PatientModalitiesController, :type => :controller do
         expect { get :new }.to raise_error(ActionController::UrlGenerationError)
       end
     end
+
     context 'with a patient' do
       before do
         get :new, patient_id: @patient.to_param
       end
+
       it 'succeeds' do
         expect(response).to be_success
       end
       it 'assigns a new PatientModality' do
-        modality = assigns(:modality)
-        expect(modality).to be_a(PatientModality)
+        expect(assigns(:modality)).to be_a(PatientModality)
       end
     end
   end
 
   describe 'index' do
+    context 'without a patient' do
+      it 'errors' do
+        expect { get :index }.to raise_error(ActionController::UrlGenerationError)
+      end
+    end
+
+    context 'with a patient' do
+      before do
+        @patient.patient_modalities << create(:patient_modality)
+        get :index, patient_id: @patient.to_param
+      end
+
+      it 'succeeds' do
+        expect(response).to be_success
+      end
+      it 'assigns modalities' do
+        expect(assigns(:modalities)).not_to be_empty
+      end
+    end
   end
 
   describe 'create' do
+    context 'without a patient' do
+      it 'errors' do
+        expect { post :create }.to raise_error(ActionController::UrlGenerationError)
+      end
+    end
+
+    context 'with a patient' do
+      before do
+        post :create, patient_id: @patient.to_param, modality: { notes: 'Notes' }
+      end
+
+      it 'succeeds' do
+        expect(response).to redirect_to(patient_patient_modalities_path(@patient))
+      end
+    end
   end
 
 end
