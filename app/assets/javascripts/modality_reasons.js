c07f9bc1bc4173b3f03aa3368ed2b7b2eac54979
op48 <lianne.tan@gmail.com>
2015-04-17 10:33:23 +0100

    Fixed js syntax for modality_reasons.js

--- app/assets/javascripts/modality_reasons.js
+++ app/assets/javascripts/modality_reasons.js
@@ -4,23 +4,23 @@ $(document).ready(function(){
 
   $('.modal-change-type').change(function(e) {
 
-    var select_box = $(e.currentTarget);
-    var selected_change_type = select_box.val();
-    var modality_form = select_box.closest('.modality-form');
+    var $selectBox = $(e.currentTarget);
+    var selectedChangeType = $selectBox.val();
+    var $modalityForm = $selectBox.closest('.modality-form');
 
     $.ajax({
         url: '/modality_reasons.json',
-        data: { modal_change_type: selected_change_type },
+        data: { modal_change_type: selectedChangeType },
         success: function(json) {
           console.log(json);
-          var reason_select_box = modality_form.find('.modality-reason-select');
-          reason_select_box.html('');
+          var $reasonSelectBox = $modalityForm.find('.modality-reason-select');
+          $reasonSelectBox.html('');
 
           for (var i = 0; i < json.length; i++) {
-            var reason_id = json[i].id;
-            var reason_description = json[i].description;
-            var option_html = _.template("<option value=<%=id%>><%=description%></option>")({ id: reason_id, description: reason_description });
-            reason_select_box.append(option_html);
+            var reasonId = json[i].id;
+            var reasonDescription = json[i].description;
+            var optionHtml = _.template("<option value=<%=id%>><%=description%></option>")({ id: reasonId, description: reasonDescription });
+            $reasonSelectBox.append(optionHtml);
           }
         },
         error: function(json) {
@@ -34,17 +34,17 @@ $(document).ready(function(){
 
   $('#modal-reason-code').click(function() {
 
-    var bigModal = $('<div></div>').attr('class','modal-show');
-    var smallModal = $('<div><b class="modal-close fi-x"></b><h5 class="g-center ">Renal Registry Reason for Change Codes</h5></div>').attr('class','modal-window ');
-    var modalHolder = $('<div id=modal-holder></div>');
-    var hd_pd_table = $('<table><caption>Haemodialysis to PD</caption><thead><tr><th>RR Code</th><th>Reason for Change</th></tr></thead><tbody id="hd-pd"></tbody></table>');
-    var pd_hd_table = $('<table><caption>PD to Haemodialysis</caption><thead><tr><th>RR Code</th><th>Reason for Change</th></tr></thead><tbody id="pd-hd"></tbody></table>');
+    var $bigModal = $('<div></div>').attr('class','modal-show');
+    var $smallModal = $('<div><b class="modal-close fi-x"></b><h5 class="g-center ">Renal Registry Reason for Change Codes</h5></div>').attr('class','modal-window ');
+    var $modalHolder = $('<div id=modal-holder></div>');
+    var $hdPdTable = $('<table><caption>Haemodialysis to PD</caption><thead><tr><th>RR Code</th><th>Reason for Change</th></tr></thead><tbody id="hd-pd"></tbody></table>');
+    var $pdHdTable = $('<table><caption>PD to Haemodialysis</caption><thead><tr><th>RR Code</th><th>Reason for Change</th></tr></thead><tbody id="pd-hd"></tbody></table>');
 
-    bigModal.attr('id','big-modal').appendTo('div#wrapper');
-    smallModal.attr('id','small-modal').appendTo('div#big-modal');
-    modalHolder.appendTo('div#small-modal');
-    hd_pd_table.appendTo('div#modal-holder');
-    pd_hd_table.appendTo('div#modal-holder');
+    $bigModal.attr('id','big-modal').appendTo('div#wrapper');
+    $smallModal.attr('id','small-modal').appendTo('div#big-modal');
+    $modalHolder.appendTo('div#small-modal');
+    $hdPdTable.appendTo('div#modal-holder');
+    $pdHdTable.appendTo('div#modal-holder');
 
     $('.modal-close').click(function() {
       $('#big-modal').remove();
@@ -57,10 +57,10 @@ $(document).ready(function(){
           console.log(json);
 
           for (var i = 0; i < json.length; i++) {
-            var reason_rr_code = json[i].rr_code;
-            var reason_description = json[i].description;
-            var tr_td_html = _.template("<tr><td><%=rr_code%></td><td><%=description%></td></tr>")({ rr_code: reason_rr_code, description: reason_description });
-            $('#hd-pd').append(tr_td_html);
+            var reasonRrCode = json[i].rr_code;
+            var reasonDescription = json[i].description;
+            var trTdHtml = _.template("<tr><td><%=rr_code%></td><td><%=description%></td></tr>")({ rr_code: reasonRrCode, description: reasonDescription });
+            $('#hd-pd').append(trTdHtml);
           }
         },
         error: function(json) {
@@ -76,10 +76,10 @@ $(document).ready(function(){
           console.log(json);
 
           for (var i = 0; i < json.length; i++) {
-            var reason_rr_code = json[i].rr_code;
-            var reason_description = json[i].description;
-            var tr_td_html = _.template("<tr><td><%=rr_code%></td><td><%=description%></td></tr>")({ rr_code: reason_rr_code, description: reason_description });
-            $('#pd-hd').append(tr_td_html);
+            var reasonRrCode = json[i].rr_code;
+            var reasonDescription = json[i].description;
+            var trTdHtml = _.template("<tr><td><%=rr_code%></td><td><%=description%></td></tr>")({ rr_code: reasonRrCode, description: reasonDescription });
+            $('#pd-hd').append(trTdHtml);
           }
         },
         error: function(json) {
