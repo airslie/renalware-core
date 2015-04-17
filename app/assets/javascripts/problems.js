b71f472f86ef163ba696bc662f0a645248bbc07d
op48 <lianne.tan@gmail.com>
2015-04-17 10:46:26 +0100

    Fixed js syntax for problems.js

--- app/assets/javascripts/problems.js
+++ app/assets/javascripts/problems.js
@@ -2,16 +2,16 @@ $(document).ready(function() {
 
   $("a.add-problem").click(function(e) {
     e.preventDefault();
-    var next_number =  $(".problem-form").data("index") + 1;
-    $(".problem-form").data("index", next_number);
+    var nextNumber =  $(".problem-form").data("index") + 1;
+    $(".problem-form").data("index", nextNumber);
 
-    var new_problem = $(".problem-form").html();
+    var newProblem = $(".problem-form").html();
 
-    var update_number_markup = new_problem.replace(/\d/g, next_number);
+    var updateNumberMarkup = newProblem.replace(/\d/g, nextNumber);
 
-    var updated_markup = update_number_markup.replace("add-x", "fi-x");
+    var updatedMarkup = updateNumberMarkup.replace("add-x", "fi-x");
 
-    $("#additional-form").append(updated_markup);
+    $("#additional-form").append(updatedMarkup);
 
     $(".fi-x").click(function(e) {
       $(e.currentTarget).closest(".problem-form").remove();
@@ -22,15 +22,15 @@ $(document).ready(function() {
 
   $('.snomed-lookup').change(function(e) {
 
-    var entered_text = e.currentTarget.value;
+    var enteredText = e.currentTarget.value;
 
     $.ajax({
-      url: '/snomed.json?snomed_term=' + entered_text,
+      url: '/snomed.json?snomedTerm=' + enteredText,
       success: function(json) {
         for(var i = 0; i < json.length; i++) {
-          var snomed_id = json[i].id;
-          var snomed_term = json[i].concept;
-          var term = _.template("<li class='snomed-select-link' data-snomed-id=<%= id %>><%= term %></li>")({ id: snomed_id, term: snomed_term });
+          var snomedId = json[i].id;
+          var snomedTerm = json[i].concept;
+          var term = _.template("<li class='snomed-select-link' data-snomed-id=<%= id %>><%= term %></li>")({ id: snomedId, term: snomedTerm });
           $('.snomed-results').append(term);
         }
       }
@@ -38,15 +38,15 @@ $(document).ready(function() {
   });
 
   $('body').on('click', '.snomed-select-link', function(e) {
-    var bullet = $(e.currentTarget);
-    var problem_form = bullet.closest('.problem-form');
+    var $bullet = $(e.currentTarget);
+    var $problemForm = $bullet.closest('.problem-form');
     var snomedId = bullet.data('snomed-id');
     console.log("clicked on a snomed term" + snomedId);
-    problem_form.find('.selected-snomed-id').val(snomedId);
+    problemForm.find('.selected-snomed-id').val(snomedId);
 
     // Show the selected drug
-    problem_form.find('.snomed-lookup').val(bullet.html());
-    problem_form.find('.snomed-results').hide();
+    problemForm.find('.snomed-lookup').val(bullet.html());
+    problemForm.find('.snomed-results').hide();
 
   });
 
