ffa252c0f4e7faf48a6a59f36d38213385db4a38
op48 <lianne.tan@gmail.com>
2015-05-06 16:59:16 +0100

    Edited medications helper for 'other' option.

--- app/helpers/peritonitis_episodes_helper.rb
+++ app/helpers/peritonitis_episodes_helper.rb
@@ -1,7 +1,7 @@
 module PeritonitisEpisodesHelper
 
   def medication_and_route(med)
-    safe_join(med.map { |m| "<li>#{m.medicatable.name} - #{m.medication_route.name}</li>".html_safe })
+    safe_join(med.map { |m| "<li>#{m.medicatable.name} - #{m.medication_route.name == 'Other (Please specify in notes)' ? m.medication_route.full_name : m.medication_route.name }</li>".html_safe })
   end
 
   def organism_and_sensitivity(organism)
