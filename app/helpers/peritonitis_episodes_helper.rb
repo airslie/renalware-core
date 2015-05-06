3973290069e3e9ca6054a7c305febd6557c507b9
op48 <lianne.tan@gmail.com>
2015-05-06 14:05:16 +0100

    Reworked how organisms and medications are displayed.

--- app/helpers/peritonitis_episodes_helper.rb
+++ app/helpers/peritonitis_episodes_helper.rb
@@ -1,11 +1,11 @@
 module PeritonitisEpisodesHelper
 
   def medication_and_route(med)
-    med.map {|m| "#{m.medicatable.name} - #{m.medication_route.name}" }.join(", ")
+    safe_join(med.map { |m| "<li>#{m.medicatable.name} - #{m.medication_route.name}</li>".html_safe })
   end
 
   def organism_and_sensitivity(organism)
-    organism.map {|o| "#{o.organism_code.name} - #{o.sensitivity}" }.join(", ")
+    safe_join(organism.map { |o| "<li>#{o.organism_code.name}</li>".html_safe })
   end
 
 end
