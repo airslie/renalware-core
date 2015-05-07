42ab6dd613f571a0f4d9631c99ce0e92f8160421
Steve Laing <developer@laingsolutions.com>
2015-05-07 12:37:01 +0100

    Add a feature spec for adding a patient problem with select2

---
+++ spec/features/adding_patient_problem_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+feature 'Patient problems' do
+  include Select2SpecHelper
+
+  background do
+    @patient = create(:patient)
+  end
+
+  scenario 'A clinician saves a new problem', js: true do
+    visit problems_patient_path(@patient)
+
+    click_on 'Add a new problem'
+    select2 'Anemia of diabetes', '#snomed_term'
+    fill_in 'Description', with: 'a description'
+    click_on 'Save Problems'
+
+    expect(page).to have_css("dl.accordion dd.accordion-navigation a", text: 'a description')
+    expect(@patient.reload.patient_problems.last.snomed_id).to eq('824516018')
+  end
+end
