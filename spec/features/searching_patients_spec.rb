205b093d085e4a177abaac34d02717e6cfb1acd0
Steve Laing <developer@laingsolutions.com>
2015-05-07 12:57:56 +0100

    Use correct 'scenario' keyword

--- spec/features/searching_patients_spec.rb
+++ spec/features/searching_patients_spec.rb
@@ -27,66 +27,63 @@ feature 'Searching for a patient' do
     create(:patient, surname: 'Walker', forename: 'Johnny',  local_patient_id: '0987654321')
   end
 
-  feature 'with comma delimited terms' do
-    it 'ignores commas' do
-      search_for_patient('Jones, J')
-      expect_patient_in_results('Jones, J')
-    end
+  scenario 'with comma delimited terms it ignores commas' do
+    search_for_patient('Jones, J')
+    expect_patient_in_results('Jones, J')
   end
 
-  feature 'by first name' do
-    it 'matches the full first name' do
-      search_for_patient('Jenny')
-      expect_patient_in_results('Jones, J')
-    end
-    it 'matches a partial first name' do
-      search_for_patient('John')
-      expect_patient_in_results('Walker, J')
-    end
-    it 'matches a first name in any case' do
-      search_for_patient('joHN')
-      expect_patient_in_results('Walker, J')
-    end
+  scenario 'by first name it matches the full first name' do
+    search_for_patient('Jenny')
+    expect_patient_in_results('Jones, J')
   end
 
-  feature 'by surname' do
-    it 'matches the full surname' do
-      search_for_patient('Jones')
-      expect_patient_in_results('Jones, J')
-    end
-    it 'matches a partial surname' do
-      search_for_patient('mith')
-      expect_patient_in_results('Smith, W')
-    end
-    it 'matches a surname in any case' do
-      search_for_patient('wALk')
-      expect_patient_in_results('Walker, J')
-    end
-    it 'matches a surname and initial' do
-      search_for_patient('Walker J')
-      expect_patient_in_results('Walker, J')
-    end
+  scenario 'by first name it matches a partial first name' do
+    search_for_patient('John')
+    expect_patient_in_results('Walker, J')
   end
 
-  feature 'by nhs number' do
-    it 'matches the exactly' do
-      search_for_patient('Z111111119')
-      expect_patient_in_results('Smith, W')
-    end
-    it 'does not match partially' do
-      search_for_patient('Z1111')
-      dont_expect_patient_in_results('Smith, W')
-    end
+  scenario 'scenario by first name it matches a first name in any case' do
+    search_for_patient('joHN')
+    expect_patient_in_results('Walker, J')
   end
 
-  feature 'by local id' do
-    it 'matches exactly' do
-      search_for_patient('0987654321')
-      expect_patient_in_results('Walker, J')
-    end
-    it 'does not match partially' do
-      search_for_patient('1234')
-      dont_expect_patient_in_results('Walker, J')
-    end
+  scenario 'by surname it matches the full surname' do
+    search_for_patient('Jones')
+    expect_patient_in_results('Jones, J')
+  end
+
+  scenario 'by surname it matches a partial surname' do
+    search_for_patient('mith')
+    expect_patient_in_results('Smith, W')
+  end
+
+  scenario 'by surname it matches a surname in any case' do
+    search_for_patient('wALk')
+    expect_patient_in_results('Walker, J')
+  end
+
+  scenario 'by surname it matches a surname and initial' do
+    search_for_patient('Walker J')
+    expect_patient_in_results('Walker, J')
+  end
+
+  scenario 'by nhs number it matches the exactly' do
+    search_for_patient('Z111111119')
+    expect_patient_in_results('Smith, W')
+  end
+
+  scenario 'by partial nhs number it does not match' do
+    search_for_patient('Z1111')
+    dont_expect_patient_in_results('Smith, W')
+  end
+
+  scenario 'by local id it matches exactly' do
+    search_for_patient('0987654321')
+    expect_patient_in_results('Walker, J')
+  end
+
+  scenario 'by partial local id it does not match' do
+    search_for_patient('1234')
+    dont_expect_patient_in_results('Walker, J')
   end
 end
