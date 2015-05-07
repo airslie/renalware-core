363421d7f6416cb3de0ca716fe4d7a578b061a37
op48 <lianne.tan@gmail.com>
2015-05-07 17:53:54 +0100

    Added method to just show peritonitis episode organisms.

--- app/helpers/peritonitis_episodes_helper.rb
+++ app/helpers/peritonitis_episodes_helper.rb
@@ -1,11 +1,15 @@
 module PeritonitisEpisodesHelper
 
-  def medication_and_route(med)
-    safe_join(med.map { |m| "<li>#{m.medicatable.name} - #{m.medication_route.name == 'Other (Please specify in notes)' ? m.medication_route.full_name : m.medication_route.name }</li>".html_safe })
+  def medication_and_route(med_route)
+    safe_join(med_route.map { |m| "<li>#{m.medicatable.name} - #{m.medication_route.name == 'Other (Please specify in notes)' ? m.medication_route.full_name : m.medication_route.name }</li>".html_safe })
   end
 
-  def organism_and_sensitivity(organism)
-    safe_join(organism.map { |o| "<li>#{o.organism_code.name}</li>".html_safe })
+  def organisms_and_sensitivities(infection_organisms)
+    safe_join(infection_organisms.map { |io| "<li>#{io.organism_code.name} - #{io.sensitivity}</li>".html_safe })
+  end
+
+  def organisms(infection_organisms)
+    safe_join(infection_organisms.map { |io| "<li>#{io.organism_code.name}</li>".html_safe })
   end
 
 end
