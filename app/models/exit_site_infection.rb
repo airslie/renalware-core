b2e274711a9e52de126b7f979ffb1a99a2d95e60
op48 <lianne.tan@gmail.com>
2015-05-08 12:18:58 +0100

    Removed redundant methods for medicationroutes.

--- app/models/exit_site_infection.rb
+++ app/models/exit_site_infection.rb
@@ -1,28 +1,12 @@
 class ExitSiteInfection < ActiveRecord::Base
 
   belongs_to :patient
-  
+
   has_many :medications, as: :treatable
   has_many :medication_routes, through: :medications
   has_many :patients, through: :medications, as: :treatable
   has_many :infection_organisms, as: :infectable
   has_many :organism_codes, through: :infection_organisms, as: :infectable
- 
-  def self.medication_routes
-    @medication_routes ||= YAML.load_file(Rails.root.join("data", "medication_routes.yml"))
-  end
-
-  def antibiotic_routes
-    [].tap do |ary|
-      (1..3).each do |num|
-        ary << medication_route(send(:"antibiotic_#{num}_route"))
-      end
-    end
-  end
-
-  def medication_route(id)
-    self.class.medication_routes.find { |r| r.id == id }
-  end
 
 
 end
