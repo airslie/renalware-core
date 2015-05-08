c05571ee80df7693fa69bdcfc226926c9352051e
Steve Laing <developer@laingsolutions.com>
2015-05-08 14:28:23 +0100

    Make Drug compatible with select2

--- spec/models/drug_spec.rb
+++ spec/models/drug_spec.rb
@@ -7,21 +7,26 @@ RSpec.describe Drug, :type => :model do
   it { should have_many(:drug_drug_types) }
   it { should have_many(:drug_types).through(:drug_drug_types) }
 
+  context 'as json' do
+    it 'serializes Drug compatible with select2' do
+      expect(build(:drug).as_json.keys).to match_array([:id, :text])
+    end
+  end
+
   context "assign drug types to a drug" do
-    
     before do
-      @drug = FactoryGirl.build(:drug)
+      @drug = build(:drug)
     end
 
     it "can be assigned many unique drug types" do
 
-      @antibiotic = FactoryGirl.create(:drug_type, name: 'Antibiotic')
-      @esa = FactoryGirl.create(:drug_type, name: 'ESA') 
-     
+      @antibiotic = create(:drug_type, name: 'Antibiotic')
+      @esa = create(:drug_type, name: 'ESA')
+
       @drug.drug_types << @antibiotic
       @drug.drug_types << @antibiotic
       @drug.drug_types << @esa
-      
+
       @drug.save!
       @drug.reload
 
