1ccfb62781121466b928358c9067d6a3126ac75a
op48 <lianne.tan@gmail.com>
2015-05-08 16:40:53 +0100

    Setup associations for medications and organisms with spec test.

--- app/models/exit_site_infection.rb
+++ app/models/exit_site_infection.rb
@@ -8,5 +8,10 @@ class ExitSiteInfection < ActiveRecord::Base
   has_many :infection_organisms, as: :infectable
   has_many :organism_codes, through: :infection_organisms, as: :infectable
 
+  accepts_nested_attributes_for :medications, allow_destroy: true,
+  reject_if: proc { |attrs| attrs[:dose].blank? && attrs[:notes].blank? && attrs[:frequency].blank? }
+
+  accepts_nested_attributes_for :infection_organisms, allow_destroy: true,
+  reject_if: proc { |attrs| attrs[:sensitvity].blank? && attrs[:organism_code_id].blank? }
 
 end
--- spec/models/exit_site_infection_spec.rb
+++ spec/models/exit_site_infection_spec.rb
@@ -3,11 +3,14 @@ require 'rails_helper'
 RSpec.describe ExitSiteInfection, :type => :model do
 
   it { should belong_to(:patient) }
- 
+
   it { should have_many(:medications) }
   it { should have_many(:medication_routes).through(:medications) }
-  it { should have_many(:patients).through(:medications) }    
+  it { should have_many(:patients).through(:medications) }
   it { should have_many(:infection_organisms) }
   it { should have_many(:organism_codes).through(:infection_organisms) }
+
+  it { should accept_nested_attributes_for(:medications) }
+  it { should accept_nested_attributes_for(:infection_organisms) }
 
 end
