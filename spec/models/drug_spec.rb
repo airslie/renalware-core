3c3b11f7f53947f67073233dc538a51962fedcc9
Steve Laing <developer@laingsolutions.com>
2015-05-12 16:53:54 +0100

    Test soft deletion of Drug

--- spec/models/drug_spec.rb
+++ spec/models/drug_spec.rb
@@ -9,6 +9,16 @@ RSpec.describe Drug, :type => :model do
 
   subject { build(:drug) }
 
+  describe 'destroy' do
+    it 'soft deletes the drug' do
+      subject.save!
+      expect{ subject.destroy! }.to change(Drug,:count).by(-1)
+      soft_deleted = Drug.with_deleted.find(subject.id)
+      expect(soft_deleted).to eq(subject)
+      expect(soft_deleted.deleted_at).not_to be_nil
+    end
+  end
+
   context 'assign drug types to a drug' do
     it 'can be assigned many unique drug types' do
       @antibiotic = create(:drug_type, name: 'Antibiotic')
