40b8ec0d81ffe5f95b7c28b79f6286ead20d711a
op48 <lianne.tan@gmail.com>
2015-05-13 11:49:16 +0100

    Moved medications/routes and organisms/sensitivities helpers to a more generic helper.

--- app/helpers/peritonitis_episodes_helper.rb
+++ app/helpers/peritonitis_episodes_helper.rb
@@ -1,15 +1,2 @@
 module PeritonitisEpisodesHelper
-
-  def medication_and_route(med_route)
-    safe_join(med_route.map { |m| "<li>#{m.medicatable.name} - #{m.medication_route.name == 'Other (Please specify in notes)' ? m.medication_route.full_name : m.medication_route.name }</li>".html_safe })
-  end
-
-  def organisms_and_sensitivities(infection_organisms)
-    safe_join(infection_organisms.map { |io| "<li>#{io.organism_code.name} - #{io.sensitivity}</li>".html_safe })
-  end
-
-  def organisms(infection_organisms)
-    safe_join(infection_organisms.map { |io| "<li>#{io.organism_code.name}</li>".html_safe })
-  end
-
 end
