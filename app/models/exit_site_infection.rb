e6fc7448e5c922b018738296a0ea469e260f91e1
op48 <lianne.tan@gmail.com>
2015-05-14 15:47:15 +0100

    Added uniq case for organism codes per exit site infection.

--- app/models/exit_site_infection.rb
+++ app/models/exit_site_infection.rb
@@ -6,7 +6,7 @@ class ExitSiteInfection < ActiveRecord::Base
   has_many :medication_routes, through: :medications
   has_many :patients, through: :medications, as: :treatable
   has_many :infection_organisms, as: :infectable
-  has_many :organism_codes, through: :infection_organisms, as: :infectable
+  has_many :organism_codes, -> { uniq }, through: :infection_organisms, as: :infectable
 
   accepts_nested_attributes_for :medications, allow_destroy: true,
   reject_if: proc { |attrs| attrs[:dose].blank? && attrs[:notes].blank? && attrs[:frequency].blank? }
