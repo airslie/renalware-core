4a6e59e0472336dc9d35ff0127090340c6d6d6c6
Steve Laing <developer@laingsolutions.com>
2015-05-14 11:08:51 +0100

    Ensure multiple medications work with drugs search

--- app/assets/javascripts/drugs.js
+++ app/assets/javascripts/drugs.js
@@ -60,14 +60,15 @@ $(document).ready(function(){
 
   // drug search
   var timer,
-      $drugResults = $('#drug-results');
       // TODO: Get rid of this.
       drugAdminTemplate = "<li class='row drug-list' data-drug-id=<%=id%>><div class='large-4 columns'></div><div class='large-4 columns'><a href='/drugs/<%=id%>/drug_drug_types'><%= name %></a></div><div class='large-2 columns'><a href='/drugs/<%=id%>/edit'>Edit</a></div><div class='large-2 columns'><a class='delete' data-confirm='Are you sure you want to delete this drug?' data-method='delete' href='/drugs/<%=id%>'>Delete</a></div></li>",
       medicationsTemplate = "<li id=drug-<%=id%> class='drug-select-link' data-drug-id=<%=id%>><%= name %></li>";
 
-  $('#drug_search').keydown(function(e) {
-    var $searchInput = $(e.currentTarget),
-        query = $searchInput.val();
+  $('.find_drug').keydown(function(e) {
+    var $drugSearchInput = $(e.currentTarget),
+        query = $drugSearchInput.val();
+        $medForm = $drugSearchInput.closest('.med-form');
+        $drugResults = $drugSearchInput.siblings('.drug-results');
 
     if(timer) clearTimeout(timer);
 
@@ -79,16 +80,10 @@ $(document).ready(function(){
           success: function(json) {
             $drugResults.html('').show();
             for (var i = 0; i < json.length; i++) {
-              var drugId = json[i].id,
-                  drugName = json[i].name,
-                  htmlTemplate = location.pathname === '/drugs' ? drugAdminTemplate : medicationsTemplate,
-                  resultsHtml = _.template(htmlTemplate)({ id: drugId, name: drugName });
+              var htmlTemplate = location.pathname === '/drugs' ? drugAdminTemplate : medicationsTemplate,
+                  resultsHtml = _.template(htmlTemplate)(json[i]);
               $drugResults.append(resultsHtml);
             }
-          },
-          error: function(json) {
-            console.log("Drug list failed to load");
-            console.log(json);
           }
         });
       }
@@ -96,7 +91,7 @@ $(document).ready(function(){
   });
 
   // set hidden value of chosen medication via search
-  $drugResults.on('click', '.drug-select-link', function(e) {
+  $('body').on('click', '.drug-select-link', function(e) {
     var $bullet = $(e.currentTarget);
     var $medForm = $bullet.closest('.med-form');
     var drugId = $bullet.data('drug-id');
@@ -104,7 +99,7 @@ $(document).ready(function(){
 
     // Show the selected drug
     $medForm.find('.find_drug').val($bullet.html());
-    $drugResults.hide();
+    $medForm.find('.drug-results').hide();
   });
 
   // set hidden value of chosen medication via select dropdown
