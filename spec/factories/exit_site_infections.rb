ee604d67501edc5256520015783f04f3f7ae6325
op48 <lianne.tan@gmail.com>
2015-05-14 15:41:16 +0100

    Created factory for an exit site infection.

--- spec/factories/exit_site_infections.rb
+++ spec/factories/exit_site_infections.rb
@@ -1,6 +1,9 @@
 FactoryGirl.define do
   factory :exit_site_infection do
-    
+    patient
+    diagnosis_date "01/04/2015"
+    treatment "Having treatment for exit site infection."
+    outcome "Outcome is satisfactory."
+    notes "Review in 2 weeks time, observe for temperature."
   end
-
 end
