87872a67cd1f7a25fa6fd1c8f0dbc5edf50e6071
op48 <lianne.tan@gmail.com>
2015-05-18 17:39:31 +0100

    Updated/reworked cuke tests(pd_infections) for exit site infections.

--- features/ruby/step_definitions/pd_infection_steps.rb
+++ features/ruby/step_definitions/pd_infection_steps.rb
@@ -37,6 +37,10 @@ Given(/^there are fluid descriptions in the database$/) do
   @pea_soup = @fluid_descriptions[3]
 end
 
+Given(/^a patient has PD$/) do
+  visit pd_info_patient_path(@patient_1)
+end
+
 Given(/^a patient has a recently recorded episode of peritonitis$/) do
 
   @peritonitis_episode_1 = FactoryGirl.create(:peritonitis_episode,
@@ -61,8 +65,14 @@ Given(/^a patient has a recently recorded episode of peritonitis$/) do
 
 end
 
-Given(/^a patient has PD$/) do
-  visit pd_info_patient_path(@patient_1)
+Given(/^a patient has a recently recorded exit site infection$/) do
+  @exit_site_1 = FactoryGirl.create(:exit_site_infection,
+    patient: @patient_1,
+    diagnosis_date: "01/01/2015",
+    treatment: "Typical treatment.",
+    outcome: "Ok outcome.",
+    notes: "review treatment in a 6 weeks."
+  )
 end
 
 Given(/^a patient has episodes of peritonitis$/) do
@@ -84,8 +94,31 @@ Given(/^a patient has episodes of peritonitis$/) do
     white_cell_lympho: 25,
     white_cell_degen: 25,
     white_cell_other: 25,
-    notes: "Has problems getting rid of infection."
+    notes: "Has problems getting rid of infection.",
+    medications_attributes: [
+      patient: @patient_1,
+      medicatable: @amoxicillin,
+      treatable: @peritonitis_episode_2,
+      dose: "20mg",
+      medication_route: @po,
+      frequency: "daily",
+      notes: "with food",
+      date: "02/03/2015",
+      provider: 0,
+      deleted_at: "NULL",
+      created_at: "2015-02-03 18:21:04",
+      updated_at: "2015-02-05 18:21:04"
+    ]
   )
+
+  @infection_organism_1 = FactoryGirl.create(:infection_organism,
+    organism_code: @mrsa,
+    sensitivity: "Sensitive to MRSA.",
+    created_at: "2015-03-03 15:30:00",
+    updated_at: "2015-03-05 15:30:00"
+  )
+
+  @peritonitis_episode_2.infection_organisms << @infection_organism_1
 
   @peritonitis_episode_3 = FactoryGirl.create(:peritonitis_episode,
     patient: @patient_1,
@@ -104,13 +137,98 @@ Given(/^a patient has episodes of peritonitis$/) do
     white_cell_lympho: 25,
     white_cell_degen: 30,
     white_cell_other: 25,
-    notes: "Needs pain management."
+    notes: "Needs pain management.",
+    medications_attributes: [
+      patient: @patient_1,
+      medicatable: @penicillin,
+      treatable: @peritonitis_episode_3,
+      dose: "50mg",
+      medication_route: @sc,
+      frequency: "PID",
+      notes: "with water",
+      date: "02/03/2015",
+      provider: 0,
+      deleted_at: "NULL",
+      created_at: "2015-02-03 18:21:04",
+      updated_at: "2015-02-05 18:21:04"
+    ]
   )
+
+  @infection_organism_2 = FactoryGirl.create(:infection_organism,
+    organism_code: @strep,
+    sensitivity: "Sensitive to strep.",
+    created_at: "2015-03-03 15:30:00",
+    updated_at: "2015-03-05 15:30:00"
+  )
+
+  @peritonitis_episode_3.infection_organisms << @infection_organism_2
 
 end
 
-Then(/^an episode of peritonitis can be viewed in more detail from the PD info page$/) do
-  visit pd_info_patient_path(@patient_1)
+Given(/^a patient has exit site infections$/) do
+
+  @exit_site_2 = FactoryGirl.create(:exit_site_infection,
+    patient: @patient_1,
+    diagnosis_date: "25/01/2015",
+    treatment: "Received treatment for this exit site infection.",
+    outcome: "Outcome for this exit site infection is not good.",
+    notes: "Needs time for review.",
+    medications_attributes: [
+      patient: @patient_1,
+      medicatable: @dicloxacillin,
+      treatable: @exit_site_2,
+      dose: "10mg",
+      medication_route: @im,
+      frequency: "Twice a month",
+      notes: "Has a cold.",
+      date: "02/03/2015",
+      provider: 0,
+      deleted_at: "NULL",
+      created_at: "2015-02-03 18:21:04",
+      updated_at: "2015-02-05 18:21:04"
+    ]
+  )
+
+  @infection_organism_3 = FactoryGirl.create(:infection_organism,
+    organism_code: @bacillis,
+    sensitivity: "Very sensitive to bacillis.",
+    created_at: "2015-03-03 15:30:00",
+    updated_at: "2015-03-05 15:30:00"
+  )
+
+  @exit_site_2.infection_organisms <<  @infection_organism_3
+
+  @exit_site_3 = FactoryGirl.create(:exit_site_infection,
+    patient: @patient_1,
+    diagnosis_date: "26/01/2015",
+    treatment: "Received treatment for this second exit site infection.",
+    outcome: "Outcome for this second exit site infection is not good.",
+    notes: "Needs more time for review.",
+    medications_attributes: [
+      patient: @patient_1,
+      medicatable: @rifampin,
+      treatable: @exit_site_3,
+      dose: "30mg",
+      medication_route: @iv,
+      frequency: "BD",
+      notes: "Review after 2 weeks",
+      date: "02/03/2015",
+      provider: 0,
+      deleted_at: "NULL",
+      created_at: "2015-02-03 18:21:04",
+      updated_at: "2015-02-05 18:21:04"
+    ]
+  )
+
+  @infection_organism_4 = FactoryGirl.create(:infection_organism,
+    organism_code: @e_coli,
+    sensitivity: "Very sensitive to E.Coli.",
+    created_at: "2015-03-03 15:30:00",
+    updated_at: "2015-03-05 15:30:00"
+  )
+
+  @exit_site_3.infection_organisms << @infection_organism_4
+
 end
 
 When(/^the Clinician records the episode of peritonitis$/) do
@@ -195,105 +313,6 @@ When(/^the Clinician records the episode of peritonitis$/) do
   click_on "Save Peritonitis Episode"
 end
 
-Then(/^the recorded episode should be displayed on PD info page$/) do
-
-  #dates
-  expect(page).to have_content("25/12/2014")
-  expect(page).to have_content("30/12/2014")
-  expect(page).to have_content("31/01/2015")
-
-  #outcome
-  expect(page).to have_content("De novo")
-  expect(page).to have_content("Catheter Removed: Yes")
-
-  #organism
-  expect(page).to have_content("E.Coli")
-
-  #causes/symptoms
-  expect(page).to have_content("Line Break: No")
-  expect(page).to have_content("Exit Site Infection: Yes")
-  expect(page).to have_content("Diarrhoea: Yes")
-  expect(page).to have_content("Abdominal Pain: Yes")
-  expect(page).to have_content("Misty")
-
-  #whitecells
-  expect(page).to have_content("1000")
-  expect(page).to have_content("Neutro: 20%")
-  expect(page).to have_content("Lympho: 30%")
-  expect(page).to have_content("Degen: 25%")
-  expect(page).to have_content("Other: 25%")
-
-  #medication/route
-  expect(page).to have_content("Rifampin")
-  expect(page).to have_content("SC")
-
-end
-
-When(/^the Clinician updates the episode of peritonitis$/) do
-  visit edit_patient_peritonitis_episode_path(@patient_1, @peritonitis_episode_1.id)
-
-  fill_in "Episode notes", :with => "On review, needs stronger antibiotics."
-
-  click_on "Update Peritonitis Episode"
-end
-
-When(/^they add a medication to this episode of peritonitis$/) do
-  visit edit_patient_peritonitis_episode_path(@patient_1, @peritonitis_episode_1.id)
-
-  click_on "Add a new medication"
-
-  select "Penicillin", from: "Select Drug (peritonitis drugs only)"
-  fill_in "Dose", with: "5mg"
-  select "IV", from: "Route"
-  fill_in "Frequency & Duration", with: "PID"
-  fill_in "Notes", with: "Review in 1 month."
-
-  within "#peritonitis_episode_medications_attributes_0_date_3i" do
-    select '28'
-  end
-  within "#peritonitis_episode_medications_attributes_0_date_2i" do
-    select 'February'
-  end
-  within "#peritonitis_episode_medications_attributes_0_date_1i" do
-    select '2015'
-  end
-
-  click_on "Update Peritonitis Episode"
-end
-
-When(/^they record an organism and sensitivity to this episode of peritonitis$/) do
-  visit edit_patient_peritonitis_episode_path(@patient_1, @peritonitis_episode_1.id)
-
-  click_on "Record a new organism and sensitivity"
-
-  select "Bacillis", from: "Organism"
-  fill_in "Sensitivity", with: "Very sensitive to Bacillis."
-
-  click_on "Update Peritonitis Episode"
-end
-
-Then(/^the updated episode should be displayed on PD info page$/) do
-  @peritonitis_episode_1.reload
-  expect(page).to have_content("On review, needs stronger antibiotics.")
-end
-
-Then(/^the new medication should be displayed on the updated peritonitis form$/) do
-  visit edit_patient_peritonitis_episode_path(@patient_1, @peritonitis_episode_1.id)
-
-  expect(page).to have_content("Penicillin")
-  expect(page).to have_content("5mg")
-  expect(page).to have_content("IV")
-  expect(page).to have_content("PID")
-  expect(page).to have_content("2015-02-28")
-end
-
-Then(/^the recorded organism and sensitivity should be displayed on the updated peritonitis form$/) do
-  visit edit_patient_peritonitis_episode_path(@patient_1, @peritonitis_episode_1.id)
-
-  expect(page).to have_content("Bacillis")
-  expect(page).to have_content("Very sensitive to Bacillis.")
-end
-
 When(/^the Clinician records an exit site infection$/) do
   visit new_patient_exit_site_infection_path(@patient_1)
 
@@ -341,6 +360,138 @@ When(/^the Clinician records an exit site infection$/) do
   click_on "Save Exit Site Infection"
 end
 
+When(/^a patient selects an episode of peritonitis view$/) do
+  visit pd_info_patient_path(@patient_1)
+
+  find("#view-pe-2").click
+end
+
+When(/^a patient selects an exit site infection to view$/) do
+  visit pd_info_patient_path(@patient_1)
+
+  find("#view-es-infection-2").click
+end
+
+When(/^the Clinician updates the episode of peritonitis$/) do
+  visit edit_patient_peritonitis_episode_path(@patient_1, @peritonitis_episode_1.id)
+
+  fill_in "Episode notes", :with => "On review, needs stronger antibiotics."
+
+  click_on "Update Peritonitis Episode"
+end
+
+When(/^the Clinician updates an exit site infection$/) do
+  visit edit_patient_exit_site_infection_path(@patient_1, @exit_site_1.id)
+
+  fill_in "General notes about this infection", :with => "Needs a review in 2 weeks time."
+
+  click_on "Update Exit Site Infection"
+end
+
+When(/^they add a medication to this episode of peritonitis$/) do
+  visit edit_patient_peritonitis_episode_path(@patient_1, @peritonitis_episode_1.id)
+
+  click_on "Add a new medication"
+
+  select "Penicillin", from: "Select Drug (peritonitis drugs only)"
+  fill_in "Dose", with: "5mg"
+  select "IV", from: "Route"
+  fill_in "Frequency & Duration", with: "PID"
+  fill_in "Notes", with: "Review in 1 month."
+
+  within "#peritonitis_episode_medications_attributes_0_date_3i" do
+    select '28'
+  end
+  within "#peritonitis_episode_medications_attributes_0_date_2i" do
+    select 'February'
+  end
+  within "#peritonitis_episode_medications_attributes_0_date_1i" do
+    select '2015'
+  end
+
+  click_on "Update Peritonitis Episode"
+end
+
+When(/^they add a medication to this exit site infection$/) do
+  visit edit_patient_exit_site_infection_path(@patient_1, @exit_site_1.id)
+
+  click_on "Add a new medication"
+
+  select "Tobramycin", from: "Select Drug (exit site drugs only)"
+  fill_in "Dose", with: "10mg"
+  select "PO", from: "Route"
+  fill_in "Frequency & Duration", with: "Twice a week"
+  fill_in "Notes", with: "Watch for improvement."
+
+  within "#exit_site_infection_medications_attributes_0_date_3i" do
+    select '10'
+  end
+  within "#exit_site_infection_medications_attributes_0_date_2i" do
+    select 'April'
+  end
+  within "#exit_site_infection_medications_attributes_0_date_1i" do
+    select '2015'
+  end
+
+  click_on "Update Exit Site Infection"
+end
+
+When(/^they record an organism and sensitivity to this episode of peritonitis$/) do
+  visit edit_patient_peritonitis_episode_path(@patient_1, @peritonitis_episode_1.id)
+
+  click_on "Record a new organism and sensitivity"
+
+  select "Bacillis", from: "Organism"
+  fill_in "Sensitivity", with: "Very sensitive to Bacillis."
+
+  click_on "Update Peritonitis Episode"
+end
+
+When(/^they record an organism and sensitivity to this exit site infection$/) do
+  visit edit_patient_exit_site_infection_path(@patient_1, @exit_site_1.id)
+
+  click_on "Record a new organism and sensitivity"
+
+  select "E.Coli", from: "Organism"
+  fill_in "Sensitivity", with: "High sensitivity to E.Coli."
+
+  click_on "Update Exit Site Infection"
+end
+
+Then(/^the recorded episode should be displayed on PD info page$/) do
+
+  #dates
+  expect(page).to have_content("25/12/2014")
+  expect(page).to have_content("30/12/2014")
+  expect(page).to have_content("31/01/2015")
+
+  #outcome
+  expect(page).to have_content("De novo")
+  expect(page).to have_content("Catheter Removed: Yes")
+
+  #organism
+  expect(page).to have_content("E.Coli")
+
+  #causes/symptoms
+  expect(page).to have_content("Line Break: No")
+  expect(page).to have_content("Exit Site Infection: Yes")
+  expect(page).to have_content("Diarrhoea: Yes")
+  expect(page).to have_content("Abdominal Pain: Yes")
+  expect(page).to have_content("Misty")
+
+  #whitecells
+  expect(page).to have_content("1000")
+  expect(page).to have_content("Neutro: 20%")
+  expect(page).to have_content("Lympho: 30%")
+  expect(page).to have_content("Degen: 25%")
+  expect(page).to have_content("Other: 25%")
+
+  #medication/route
+  expect(page).to have_content("Rifampin")
+  expect(page).to have_content("SC")
+
+end
+
 Then(/^the recorded exit site infection should be displayed on PD info page$/) do
 
   #dates
@@ -364,67 +515,68 @@ Then(/^the recorded exit site infection should be displayed on PD info page$/) d
 
 end
 
-Given(/^a patient has a recently recorded exit site infection$/) do
-  @exit_site_infection = FactoryGirl.create(:exit_site_infection,
-    patient: @patient_1,
-    diagnosis_date: "01/01/2015",
-    treatment: "Typical treatment.",
-    outcome: "Ok outcome.",
-    notes: "review treatment in a 6 weeks."
-  )
+Then(/^an episode of peritonitis can be viewed in more detail from the PD info page$/) do
+  expect(page).to have_content("24/01/2015")
+  expect(page).to have_content("25/01/2015")
+  expect(page).to have_content("27/02/2015")
+  expect(page).to have_content("Misty")
+  expect(page).to have_content("3000")
+  expect(page).to have_content("25")
+  expect(page).to have_content("20")
+  expect(page).to have_content("30")
+  expect(page).to have_content("3000")
+  expect(page).to have_content("Needs pain management.")
+
+  #medication/route
+  expect(page).to have_content("Penicillin")
+  expect(page).to have_content("SC")
+
+  #organism/sensitivity
+  expect(page).to have_content("Strep")
+  expect(page).to have_content("Sensitive to strep")
+
 end
 
-When(/^the Clinician updates an exit site infection$/) do
-  visit edit_patient_exit_site_infection_path(@patient_1, @exit_site_infection.id)
+Then(/^an exit site infection can be viewed in more detail from the PD info page$/) do
+  expect(page).to have_content("26/01/2015")
+  expect(page).to have_content("Received treatment for this second exit site infection.")
+  expect(page).to have_content("Outcome for this second exit site infection is not good.")
+  expect(page).to have_content("Needs more time for review.")
 
-  fill_in "General notes about this infection", :with => "Needs a review in 2 weeks time."
+  #medication/route
+  expect(page).to have_content("Rifampin")
+  expect(page).to have_content("IV")
 
-  click_on "Update Exit Site Infection"
+  #organism/sensitivity
+  expect(page).to have_content("E.Coli")
+  expect(page).to have_content("Very sensitive to E.Coli.")
 end
 
-When(/^they add a medication to this exit site infection$/) do
-  visit edit_patient_exit_site_infection_path(@patient_1, @exit_site_infection.id)
+Then(/^the updated peritonitis episode should be displayed on PD info page$/) do
+  @peritonitis_episode_1.reload
 
-  click_on "Add a new medication"
-
-  select "Tobramycin", from: "Select Drug (exit site drugs only)"
-  fill_in "Dose", with: "10mg"
-  select "PO", from: "Route"
-  fill_in "Frequency & Duration", with: "Twice a week"
-  fill_in "Notes", with: "Watch for improvement."
-
-  within "#exit_site_infection_medications_attributes_0_date_3i" do
-    select '10'
-  end
-  within "#exit_site_infection_medications_attributes_0_date_2i" do
-    select 'April'
-  end
-  within "#exit_site_infection_medications_attributes_0_date_1i" do
-    select '2015'
-  end
-
-  click_on "Update Exit Site Infection"
-end
-
-When(/^they record an organism and sensitivity to this exit site infection$/) do
-  visit edit_patient_exit_site_infection_path(@patient_1, @exit_site_infection.id)
-
-  click_on "Record a new organism and sensitivity"
-
-  select "E.Coli", from: "Organism"
-  fill_in "Sensitivity", with: "High sensitivity to E.Coli."
-
-  click_on "Update Exit Site Infection"
+  expect(page).to have_content("On review, needs stronger antibiotics.")
 end
 
 Then(/^the updated exit site infection should be displayed on PD info page$/) do
-  @exit_site_infection.reload
+  @exit_site_1.reload
 
   expect(page).to have_content("Needs a review in 2 weeks time.")
 end
 
+Then(/^the new medication should be displayed on the updated peritonitis form$/) do
+  visit edit_patient_peritonitis_episode_path(@patient_1, @peritonitis_episode_1.id)
+
+  expect(page).to have_content("Penicillin")
+  expect(page).to have_content("5mg")
+  expect(page).to have_content("IV")
+  expect(page).to have_content("PID")
+  expect(page).to have_content("2015-02-28")
+end
+
 Then(/^the new medication should be displayed on the updated exit site form$/) do
-  visit edit_patient_exit_site_infection_path(@patient_1, @exit_site_infection.id)
+  visit edit_patient_exit_site_infection_path(@patient_1, @exit_site_1.id)
+
   expect(page).to have_content("Tobramycin")
   expect(page).to have_content("10mg")
   expect(page).to have_content("PO")
@@ -432,8 +584,15 @@ Then(/^the new medication should be displayed on the updated exit site form$/) d
   expect(page).to have_content("2015-04-10")
 end
 
+Then(/^the recorded organism and sensitivity should be displayed on the updated peritonitis form$/) do
+  visit edit_patient_peritonitis_episode_path(@patient_1, @peritonitis_episode_1.id)
+
+  expect(page).to have_content("Bacillis")
+  expect(page).to have_content("Very sensitive to Bacillis.")
+end
+
 Then(/^the recorded organism and sensitivity should be displayed on the updated exit site form$/) do
-  visit edit_patient_exit_site_infection_path(@patient_1, @exit_site_infection.id)
+  visit edit_patient_exit_site_infection_path(@patient_1, @exit_site_1.id)
 
   expect(page).to have_content("E.Coli")
   expect(page).to have_content("High sensitivity to E.Coli.")
--- features/stories/pd_infection.feature
+++ features/stories/pd_infection.feature
@@ -14,35 +14,37 @@ Background:
     And there are episode types in the database
     And there are fluid descriptions in the database
     And some patients who need renal treatment
+    And a patient has PD
 
 Scenario: Clinician records an episode of peritonitis
-  Given a patient has PD
   When the Clinician records the episode of peritonitis
   Then the recorded episode should be displayed on PD info page
 
 Scenario: Clinician views an episode of peritonitis
-  Given a patient has PD
-    And a patient has episodes of peritonitis
+  Given a patient has episodes of peritonitis
+  When a patient selects an episode of peritonitis view
   Then an episode of peritonitis can be viewed in more detail from the PD info page
 
 Scenario: Clinician updates an episode of peritonitis
-  Given a patient has PD
-    And a patient has a recently recorded episode of peritonitis
+  Given a patient has a recently recorded episode of peritonitis
   When the Clinician updates the episode of peritonitis
     And they add a medication to this episode of peritonitis
     And they record an organism and sensitivity to this episode of peritonitis
-  Then the updated episode should be displayed on PD info page
+  Then the updated peritonitis episode should be displayed on PD info page
     And the new medication should be displayed on the updated peritonitis form
     And the recorded organism and sensitivity should be displayed on the updated peritonitis form
 
 Scenario: Clinician records an exit site infection
-  Given a patient has PD
   When the Clinician records an exit site infection
   Then the recorded exit site infection should be displayed on PD info page
 
+Scenario: Clinician views an exit site infection
+  Given a patient has exit site infections
+  When a patient selects an exit site infection to view
+  Then an exit site infection can be viewed in more detail from the PD info page
+
 Scenario: Clinician updates an exit site infection
-  Given a patient has PD
-    And a patient has a recently recorded exit site infection
+  Given a patient has a recently recorded exit site infection
   When the Clinician updates an exit site infection
     And they add a medication to this exit site infection
     And they record an organism and sensitivity to this exit site infection
