f5b8f5785b79fa11f4b6c7f0655b1ad25cc2aea9
op48 <lianne.tan@gmail.com>
2015-05-18 17:35:41 +0100

    Created factory for infection_organism.

--- spec/factories/infection_organisms.rb
+++ spec/factories/infection_organisms.rb
@@ -1,6 +1,11 @@
 FactoryGirl.define do
   factory :infection_organism do
-    
+    organism_code
+    sensitivity "Sensitive to MRSA."
+    infectable_id 1
+    infectable_type "PeritonitisEpisode"
+    created_at "2015-03-03 15:30:00"
+    updated_at "2015-03-05 15:30:00"
   end
 
 end
