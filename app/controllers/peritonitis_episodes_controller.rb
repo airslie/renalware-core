d7c1011dadaec92675e162ec20613cb49fe7708b
op48 <lianne.tan@gmail.com>
2015-05-18 17:45:04 +0100

    Rearranged whitelist layout in code to be more readable.

--- app/controllers/peritonitis_episodes_controller.rb
+++ app/controllers/peritonitis_episodes_controller.rb
@@ -42,8 +42,8 @@ class PeritonitisEpisodesController < ApplicationController
   def allowed_params
     params.require(:peritonitis_episode).permit(:diagnosis_date, :treatment_start_date, :treatment_end_date,
       :episode_type_id, :catheter_removed, :line_break, :exit_site_infection, :diarrhoea, :abdominal_pain, :fluid_description_id,
-      :white_cell_total, :white_cell_neutro, :white_cell_lympho, :white_cell_degen, :white_cell_other, :notes, :infection_organisms_attributes =>
-      [:id, :organism_code_id, :sensitivity, :infectable_id, :infectable_type ],
+      :white_cell_total, :white_cell_neutro, :white_cell_lympho, :white_cell_degen, :white_cell_other, :notes,
+      :infection_organisms_attributes => [:id, :organism_code_id, :sensitivity, :infectable_id, :infectable_type ],
       :medications_attributes => [:id, :patient_id, :treatable_id, :treatable_type, :medicatable_id, :medicatable_type, :dose, :medication_route_id,
       :frequency, :notes, :date, :provider, :_destroy]
       )
