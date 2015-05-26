52cf7f18e9c64ed896a326ca5429086b48a6a848
op48 <lianne.tan@gmail.com>
2015-05-26 11:26:52 +0100

    Added rspec test for checking if a patient has had any history of being on a PD modality to influence menu options.

--- spec/helpers/patients_helper_spec.rb
+++ spec/helpers/patients_helper_spec.rb
@@ -1,15 +1,29 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the PatientsHelper. For example:
-#
-# describe PatientsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe PatientsHelper, :type => :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "display_pd_menu" do
+    before do
+      load_modalities(["diabetic", "Renal/Diabetic"], ["livedonor", "Live Donor"], ["PD_CAPD", "PD-CAPD"], ["heartfailure", "Heart Failure"])
+
+      @patient = create(:patient)
+      @non_pd_modality = create(:modality, patient: @patient, modality_code: @diabetic, deleted_at: nil)
+      @patient.modalities << @non_pd_modality
+    end
+
+    context "patient has history of PD" do
+      it "should detect PD" do
+        @pd_modality = create(:modality, patient: @patient, modality_code: @pd_capd, deleted_at: "2015-05-22 10:58:25.428869")
+        @patient.modalities << @pd_modality
+
+        expect(display_pd_menu(@patient.modalities.with_deleted)).to eq(true)
+      end
+    end
+
+    context "patient has no history of PD" do
+      it "should detect no PD" do
+        expect(display_pd_menu(@patient.modalities.with_deleted)).to eq(false)
+      end
+    end
+
+  end
 end
