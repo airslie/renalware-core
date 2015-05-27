346fdecc49bab7ce3bc74a31b3a890e83f84c7c0
Paul Nordstrom August <paul@nuagist.es>
2015-05-27 12:04:11 -0400

    Add RABBIT sample events to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -251,4 +251,33 @@ end
 
 log "...#{logcount} problems seeded!"
 
+log 'Adding RABBIT events...'
+
+PatientEvent.create(
+  patient_id: 1,
+  patient_event_type_id: 19,
+  description: "meeting with family in clinic",
+  notes: "anxious about medication changes",
+  date_time: Time.now - 2.weeks
+)
+
+
+PatientEvent.create(
+  patient_id: 1,
+  patient_event_type_id: 25,
+  description: "call regarding meds",
+  notes: "told patient to get other drug info from GP",
+  date_time: Time.now - 12.days
+)
+
+PatientEvent.create(
+  patient_id: 1,
+  patient_event_type_id: 8,
+  description: "email re next clinic visit",
+  notes: "reminded patient to bring complete drug list to clinic",
+  date_time: Time.now - 5.days
+)
+
+log "...3 events seeded!"
+
 log '-----------Database seeding complete!----------'
