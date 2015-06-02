9d9ddb0ea1b504040dc845615d1e3a92c39f4224
op48 <lianne.tan@gmail.com>
2015-06-02 18:06:19 +0100

    Added actions/routes/views for creating a new bag_type.

--- app/controllers/bag_types_controller.rb
+++ app/controllers/bag_types_controller.rb
@@ -1,2 +1,21 @@
 class BagTypesController < ApplicationController
+
+  def new
+    @bag_type = BagType.new
+  end
+
+  def create
+    @bag_type = BagType.new(bag_type_params)
+    if @bag_type.save
+      redirect_to patients_path, :notice => "You have successfully created a new bag type."
+    else
+      render :new
+    end
+  end
+
+  private
+  def bag_type_params
+    params.require(:bag_type).permit(:manufacturer, :bag_description, :amount_1_36_glucose, :amount_2_27_glucose,
+      :amount_3_86_glucose, :amount_amino_acid, :amount_icodextrin_acid, :uses_low_gdp, :uses_low_sodium)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -43,4 +43,6 @@ Rails.application.routes.draw do
 
   resources :modality_reasons, only: [:index]
 
+  resources :bag_types, only: [:new, :create]
+
 end
