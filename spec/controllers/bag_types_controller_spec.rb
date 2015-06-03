a05ced4ef04819cca829afad3709c8a4a92323f4
op48 <lianne.tan@gmail.com>
2015-06-03 15:36:57 +0100

    Set up rspec controller tests for new/create action.

--- spec/controllers/bag_types_controller_spec.rb
+++ spec/controllers/bag_types_controller_spec.rb
@@ -2,4 +2,27 @@ require 'rails_helper'
 
 RSpec.describe BagTypesController, :type => :controller do
 
+  describe 'GET #new' do
+    it 'renders the new template' do
+      get :new
+      expect(response).to render_template('new')
+    end
+  end
+
+  describe 'POST #create' do
+    context "with valid attributes" do
+      it 'creates a new bag type' do
+        expect { post :create, bag_type: { description: "Green Bag" } }.to change(BagType, :count).by(1)
+        expect(response).to redirect_to(bag_types_path)
+      end
+    end
+
+    context "with invalid attributes" do
+      it 'creates a new bag type' do
+        expect { post :create, bag_type: { description: nil } }.to change(BagType, :count).by(0)
+        expect(response).to render_template(:new)
+      end
+    end
+  end
+
 end
