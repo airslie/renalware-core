644a461a31935b8626062e8bcb84ce5d0eec95fd
op48 <lianne.tan@gmail.com>
2015-06-03 10:29:53 +0100

    Created factory for 'bag_type'.

--- spec/factories/bag_types.rb
+++ spec/factories/bag_types.rb
@@ -1,6 +1,13 @@
 FactoryGirl.define do
   factory :bag_type do
-    
+    manufacturer "Brand Star, Brand Lucky"
+    bag_description "Green–2.34"
+    amount_1_36_glucose "10"
+    amount_2_27_glucose "20"
+    amount_3_86_glucose "30"
+    amount_amino_acid "40"
+    amount_icodextrin_acid "50"
+    uses_low_gdp true
+    uses_low_sodium false
   end
-
 end
