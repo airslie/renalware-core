9b2ec8075dc3fca2ca29536f8d428895feeb41eb
op48 <lianne.tan@gmail.com>
2015-06-04 11:26:15 +0100

    Added feature for editing/updating a bag type.

--- app/controllers/bag_types_controller.rb
+++ app/controllers/bag_types_controller.rb
@@ -17,6 +17,19 @@ class BagTypesController < ApplicationController
     @bag_types = BagType.all
   end
 
+  def edit
+    @bag_type = BagType.find(params[:id])
+  end
+
+  def update
+    @bag_type = BagType.find(params[:id])
+    if @bag_type.update(bag_type_params)
+      redirect_to bag_types_path, :notice => "You have successfully updated a bag type"
+    else
+      render :edit
+    end
+  end
+
   private
   def bag_type_params
     params.require(:bag_type).permit(:manufacturer, :description, :glucose_ml_percent_1_36, :glucose_ml_percent_2_27,
