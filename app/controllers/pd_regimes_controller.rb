b6adbc418f7ba93412610a748fdee851def1035d
Steve Laing <developer@laingsolutions.com>
2015-06-09 17:39:34 +0100

    Nest regime bags in PD Regime form

--- app/controllers/pd_regimes_controller.rb
+++ app/controllers/pd_regimes_controller.rb
@@ -6,14 +6,13 @@ class PdRegimesController < ApplicationController
   before_filter :find_patient
 
   def new
-    @pd_regime = PdRegime.new
+    @pd_regime = PdRegime.new(patient: @patient)
   end
 
   def create
     @pd_regime = PdRegime.new(pd_regime_params)
-    @pd_regime.patient_id = @patient.id
-    if @pd_regime.save
-      redirect_to pd_info_patient_path(@patient), :notice => "You have successfully added a PD Regime."
+    if perform_action
+      redirect_to pd_info_patient_path(@patient), notice:"You have successfully added a PD Regime."
     else
       render :new
     end
@@ -22,13 +21,23 @@ class PdRegimesController < ApplicationController
   private
 
   def pd_regime_params
-    params.require(:pd_regime).permit(:user_id, :patient_id, :start_date, :end_date,
+    params.require(:pd_regime).permit(:patient_id, :start_date, :end_date,
       :glucose_ml_percent_1_36, :glucose_ml_percent_2_27, :glucose_ml_percent_3_86, :amino_acid_ml,
-      :icodextrin_ml, :low_glucose_degradation, :low_sodium, :additional_hd)
+      :icodextrin_ml, :low_glucose_degradation, :low_sodium, :additional_hd,
+      pd_regime_bags_attributes: [:bag_type_id, :volume, :per_week, :monday, :tuesday, :wednesday,
+                                  :thursday, :friday, :saturday, :sunday])
   end
 
   def find_patient
     @patient = Patient.find(params[:patient_id])
   end
 
+  def perform_action
+    case params[:commit]
+    when 'Add Bag'
+      @pd_regime.pd_regime_bags.build; false
+    else
+      @pd_regime.save
+    end
+  end
 end
--- app/models/pd_regime.rb
+++ app/models/pd_regime.rb
@@ -2,7 +2,8 @@ class PdRegime < ActiveRecord::Base
 
   belongs_to :patient
 
-  has_many :pd_regime_bag
+  has_many :pd_regime_bags
   has_many :bag_types, through: :pd_regime_bag
 
+  accepts_nested_attributes_for :pd_regime_bags
 end
