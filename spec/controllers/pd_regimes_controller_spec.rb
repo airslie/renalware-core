a1a2f802e48bbaf4085750fc0a91e1b8734e9495
op48 <lianne.tan@gmail.com>
2015-06-09 16:50:47 +0100

    Set up spec tests for new/create actions for pd_regimes.

--- spec/controllers/pd_regimes_controller_spec.rb
+++ spec/controllers/pd_regimes_controller_spec.rb
@@ -2,9 +2,13 @@ require 'rails_helper'
 
 RSpec.describe PdRegimesController, :type => :controller do
 
+  before do
+    @patient = create(:patient)
+  end
+
   describe 'GET #new' do
     it 'renders the new template' do
-      get :new
+      get :new, patient_id: @patient.id
       expect(response).to render_template('new')
     end
   end
@@ -12,14 +16,14 @@ RSpec.describe PdRegimesController, :type => :controller do
   describe 'POST #create' do
     context "with valid attributes" do
       it 'creates a new PD Regime' do
-        expect { post :create, pd_regime: { start_date: '01/02/2015' } }.to change(PdRegime, :count).by(1)
+        expect { post :create, patient_id: @patient.id, pd_regime: { start_date: '01/02/2015' } }.to change(PdRegime, :count).by(1)
         expect(response).to redirect_to(pd_info_patient_path(@patient))
       end
     end
 
     context "with invalid attributes" do
       it 'creates a new PD Regime' do
-        expect { post :create, pd_regime: { start_date: nil } }.to change(PdRegime, :count).by(0)
+        expect { post :create, patient_id: @patient.id, pd_regime: { start_date: nil } }.to change(PdRegime, :count).by(0)
         expect(response).to render_template(:new)
       end
     end
