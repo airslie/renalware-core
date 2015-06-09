57148643822609eb9d83560355fd7cb45975c4c2
op48 <lianne.tan@gmail.com>
2015-06-09 15:03:08 +0100

    Added assocation between patient and pd_regime with rspec.

--- app/models/pd_regime.rb
+++ app/models/pd_regime.rb
@@ -1,5 +1,7 @@
 class PdRegime < ActiveRecord::Base
 
+  belongs_to :patient
+
   has_many :pd_regime_bag
   has_many :bag_types, through: :pd_regime_bag
 
--- spec/models/pd_regime_spec.rb
+++ spec/models/pd_regime_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe PdRegime, :type => :model do
 
+  it { should belong_to :patient }
+
   it { should have_many :pd_regime_bag }
   it { should have_many(:bag_types).through(:pd_regime_bag) }
 
