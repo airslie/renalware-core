ada4834e0584a5acb0fd71ec6496f6d0f084f684
op48 <lianne.tan@gmail.com>
2015-06-09 18:26:44 +0100

    Added spec tests for editing/updating a pd_regime.

--- spec/controllers/pd_regimes_controller_spec.rb
+++ spec/controllers/pd_regimes_controller_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe PdRegimesController, :type => :controller do
 
   before do
     @patient = create(:patient)
+    @pd_regime = create(:pd_regime)
   end
 
   describe 'GET #new' do
@@ -25,6 +26,22 @@ RSpec.describe PdRegimesController, :type => :controller do
       it 'creates a new PD Regime' do
         expect { post :create, patient_id: @patient.id, pd_regime: { start_date: nil } }.to change(PdRegime, :count).by(0)
         expect(response).to render_template(:new)
+      end
+    end
+  end
+
+  describe 'PUT #update' do
+    context "with valid attributes" do
+      it 'updates a PD Regime' do
+        put :update, id: @pd_regime.id, patient_id: @patient.id, pd_regime: { start_date: '15/02/2015' }
+        expect(response).to redirect_to(pd_info_patient_path(@patient))
+      end
+    end
+
+    context "with invalid attributes" do
+      it 'update a PD Regime' do
+        put :update, id: @pd_regime.id, patient_id: @patient.id, pd_regime: { start_date: nil }
+        expect(response).to render_template(:edit)
       end
     end
   end
