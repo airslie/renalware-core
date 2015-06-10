8c546d8ad84ffa4a9da4da4e9fa591fb45c1513b
op48 <lianne.tan@gmail.com>
2015-06-10 10:24:39 +0100

    Corrected associations for pd regime.

--- spec/models/pd_regime_spec.rb
+++ spec/models/pd_regime_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe PdRegime, :type => :model do
 
   it { should belong_to :patient }
 
-  it { should have_many :pd_regime_bag }
-  it { should have_many(:bag_types).through(:pd_regime_bag) }
+  it { should have_many :pd_regime_bags }
+  it { should have_many(:bag_types).through(:pd_regime_bags) }
 
 end
