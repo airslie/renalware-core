e371136f9f1abb7cd2a68dcc77f32e1e2579112f
Steve Laing <developer@laingsolutions.com>
2015-06-10 13:06:30 +0100

    Fix PdRegime pd_regime_bags association names

--- app/models/pd_regime.rb
+++ app/models/pd_regime.rb
@@ -3,7 +3,7 @@ class PdRegime < ActiveRecord::Base
   belongs_to :patient
 
   has_many :pd_regime_bags
-  has_many :bag_types, through: :pd_regime_bag
+  has_many :bag_types, through: :pd_regime_bags
 
   accepts_nested_attributes_for :pd_regime_bags
 
--- spec/models/pd_regime_spec.rb
+++ spec/models/pd_regime_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe PdRegime, :type => :model do
+RSpec.describe PdRegime, type: :model do
 
   it { should belong_to :patient }
 
-  it { should have_many :pd_regime_bag }
-  it { should have_many(:bag_types).through(:pd_regime_bag) }
+  it { should have_many :pd_regime_bags }
+  it { should have_many(:bag_types).through(:pd_regime_bags) }
 
 end
