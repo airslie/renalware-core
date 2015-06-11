a8a8f791269629707d768146755d247f6a2bdecc
Steve Laing <developer@laingsolutions.com>
2015-06-11 11:11:16 +0100

    Add current regime display to PD Info

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -16,7 +16,9 @@ class PatientsController < ApplicationController
   end
 
   def pd_info
-    @pd_regimes = PdRegime.where(patient_id: @patient)
+    @pd_regimes = PdRegime.where(patient_id: @patient).order(:created_at)
+    @current_regime = @pd_regimes.last if @pd_regimes.any?
+
     @peritonitis_episodes = PeritonitisEpisode.where(patient_id: @patient)
     @exit_site_infections = ExitSiteInfection.where(patient_id: @patient)
   end
