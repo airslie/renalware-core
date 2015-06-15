c83b938fd27184c1014393ad0583a5a7b23a1160
op48 <lianne.tan@gmail.com>
2015-06-15 17:05:32 +0100

    Adding validations to certain fields for medications.

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -10,5 +10,10 @@ class Medication < ActiveRecord::Base
   belongs_to :treatable, :polymorphic => true
   belongs_to :medication_route
 
+  validates :medicatable, presence: true
+  validates :dose, presence: true
+  validates :medication_route_id, presence: true
+  validates :frequency, presence: true
+
   enum provider: %i(gp hospital home_delivery)
 end
--- spec/models/medication_spec.rb
+++ spec/models/medication_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe Medication, :type => :model do
   it { should belong_to(:medicatable) }
   it { should belong_to(:treatable) }
   it { should belong_to(:medication_route) }
+
+  it { should validate_presence_of :medicatable }
+  it { should validate_presence_of :dose }
+  it { should validate_presence_of :medication_route_id }
+  it { should validate_presence_of :frequency }
 end
