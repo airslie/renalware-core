dd023051b311a3929470953bc1f078191fc9f29b
Steve Laing <developer@laingsolutions.com>
2015-06-19 16:20:38 +0100

    Add User#signature

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150619134623) do
+ActiveRecord::Schema.define(version: 20150619150714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -361,6 +361,7 @@ ActiveRecord::Schema.define(version: 20150619134623) do
     t.datetime "last_activity_at"
     t.datetime "expired_at"
     t.string   "professional_position"
+    t.string   "signature"
   end
 
   add_index "users", ["approved"], name: "index_users_on_approved", using: :btree
