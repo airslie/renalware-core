1d5e21022bd2f24518ee43990e17cafe0c8298f7
Steve Laing <developer@laingsolutions.com>
2015-06-26 11:39:07 +0100

    Toggle address form for Doctor

--- app/assets/javascripts/doctors.js
+++ app/assets/javascripts/doctors.js
@@ -6,10 +6,18 @@ Renalware.Doctors = (function () {
     $('#doctor_practice_ids').select2();
   };
 
+  var bindAddressFormToggle = function () {
+    $('#address-form-toggle').click(function (e) {
+      var button = $(e.target);
+      $(button.attr('href')).toggle();
+    });
+  };
+
   return {
     // public interface
     init : function () {
       initSelect2();
+      bindAddressFormToggle();
     }
   }
 })();
