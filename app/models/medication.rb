28d5c628c1f2947e75ab7ad80148c46c65576980
op48 <lianne.tan@gmail.com>
2015-06-29 15:26:23 +0100

    Replaced medicatable with medicatable_id.

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -12,7 +12,7 @@ class Medication < ActiveRecord::Base
 
   validates :patient, presence: true
 
-  validates :medicatable, presence: { message: "Medication to be administered can't be blank" }
+  validates :medicatable_id, presence: { message: "Medication to be administered can't be blank" }
   validates :dose, presence: { message: "Dose can't be blank" }
   validates :medication_route_id, presence: { message: "Route can't be blank" }
   validates :frequency, presence: { message: "Frequency & Duration can't be blank" }
