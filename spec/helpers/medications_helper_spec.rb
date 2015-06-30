0d6b3088c9e0e5e8269bd004b05e846b244920aa
op48 <lianne.tan@gmail.com>
2015-06-30 13:00:23 +0100

    Revised medications helper for 'medicatable_name', added test for 'validation_fail'.

--- spec/helpers/medications_helper_spec.rb
+++ spec/helpers/medications_helper_spec.rb
@@ -3,85 +3,88 @@ require 'rails_helper'
 RSpec.describe MedicationsHelper, :type => :helper do
   before do
     @patient = FactoryGirl.create(:patient)
+    @med_route = FactoryGirl.create(:medication_route)
     @blue_pill = FactoryGirl.create(:drug)
     @amoxicillin = FactoryGirl.create(:drug, name: 'Amoxicillin')
+    @penicillin = FactoryGirl.create(:drug, name: 'Penicillin')
+    @invalid_patient_med = FactoryGirl.build(
+                            :medication,
+                            patient: @patient,
+                            medicatable_id: nil,
+                            dose: nil,
+                            medication_route_id: nil,
+                            frequency: nil,
+                            start_date: nil,
+                            provider: nil)
+
+    @valid_patient_med = FactoryGirl.build(
+                          :medication,
+                          patient: @patient,
+                          medicatable: @amoxicillin,
+                          medicatable_type: 'Drug',
+                          dose: '23mg',
+                          medication_route: @med_route,
+                          frequency: 'PID',
+                          start_date: '02/10/2014',
+                          provider: 0)
   end
 
-  describe 'display_med_field' do
-    context 'value submitted' do
-      it 'should return changed/persisted value.' do
-        @patient_medication = FactoryGirl.build(
+  describe 'medicatable_name' do
+    context 'medicatable present' do
+      it 'should return name' do
+        patient_medication = FactoryGirl.build_stubbed(
           :medication,
           patient: @patient,
-          medicatable: @blue_pill,
-          medicatable_type: 'Drug',
-          dose: nil,
-          medication_route_id: nil,
-          frequency: nil,
-          start_date: nil,
-          provider: nil)
+          medicatable: @blue_pill
+          )
 
-        @patient_medication.save
-        expect(display_med_field(@patient_medication, @patient_medication.medicatable, :name)).to eq(@patient_medication.medicatable.send(:name))
+        expect(medicatable_name(patient_medication)).to eq(patient_medication.medicatable.name)
       end
     end
 
-    context 'value not submitted' do
-      it 'should not return changed/persisted value.' do
-        @patient_medication = FactoryGirl.build(
+    context 'medicatable not present' do
+      it 'should not return name' do
+        patient_medication = FactoryGirl.build_stubbed(
           :medication,
           patient: @patient,
-          medicatable: nil,
-          medicatable_type: nil,
-          dose: nil,
-          medication_route_id: nil,
-          frequency: nil,
-          start_date: nil,
-          provider: nil)
+          medicatable: nil
+          )
 
-        @patient_medication.save
-        expect(display_med_field(@patient_medication, @patient_medication.medicatable, :name)).to eq(nil)
+        expect(medicatable_name(patient_medication)).to eq(nil)
       end
     end
   end
 
   describe 'highlight_validation_fail' do
-
     context 'with errors' do
       it 'should apply hightlight' do
-        @patient_medication = FactoryGirl.build(
-          :medication,
-          patient: @patient,
-          medicatable_id: nil,
-          dose: nil,
-          medication_route_id: nil,
-          frequency: nil,
-          start_date: nil,
-          provider: nil)
-
-        @patient_medication.save
-        expect(highlight_validation_fail(@patient_medication, :medicatable_id)).to eq('field_with_errors')
+        @invalid_patient_med.save
+        expect(highlight_validation_fail(@invalid_patient_med, :medicatable_id)).to eq('field_with_errors')
       end
     end
 
     context 'with no errors' do
       it 'should not apply highlight' do
-        @patient_medication = FactoryGirl.build(
-          :medication,
-          patient: @patient,
-          medicatable: @amoxicillin,
-          medicatable_type: 'Drug',
-          dose: '23mg',
-          medication_route_id: 1,
-          frequency: 'PID',
-          start_date: '02/10/2014',
-          provider: 0)
+        @valid_patient_med.save
+        expect(highlight_validation_fail(@valid_patient_med, :medicatable)).to eq(nil)
+      end
+    end
+  end
 
-        @patient_medication.save
-        expect(highlight_validation_fail(@patient_medication, :medicatable)).to eq(nil)
+  describe 'validation_fail' do
+    context 'with errors' do
+      it 'should apply class "show-form"' do
+        @invalid_patient_med.save
+        expect(validation_fail(@invalid_patient_med)).to eq('show-form')
       end
     end
 
+    context 'with no errors' do
+      it 'should apply class "content"' do
+        @valid_patient_med.save
+        expect(validation_fail(@valid_patient_med)).to eq('content')
+      end
+    end
   end
 
 end
