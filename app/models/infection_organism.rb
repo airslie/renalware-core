ecc25b865170089f96c71693097806652e92e8f2
op48 <lianne.tan@gmail.com>
2015-06-30 18:01:29 +0100

    Added validations for organism_code within infection_organisms, with rspec tests.

--- app/models/infection_organism.rb
+++ app/models/infection_organism.rb
@@ -1,8 +1,9 @@
 class InfectionOrganism < ActiveRecord::Base
 
   belongs_to :organism_code
-  belongs_to :infectable, :polymorphic => true 
+  belongs_to :infectable, :polymorphic => true
 
-  validates :organism_code, uniqueness: { scope: :infectable } 
+  validates :infectable_id, :infectable_type, :presence => true
+  validates :organism_code_id, uniqueness: { scope: [:infectable_id, :infectable_type] }, presence: { message: "Organism can't be blank" }
 
 end
