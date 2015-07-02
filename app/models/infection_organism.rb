8a178d3f74c133b5fc3c61ae504e07fbbfbb46d9
op48 <lianne.tan@gmail.com>
2015-07-02 16:24:49 +0100

    Removed validation for presence of infectable.

--- app/models/infection_organism.rb
+++ app/models/infection_organism.rb
@@ -3,7 +3,6 @@ class InfectionOrganism < ActiveRecord::Base
   belongs_to :organism_code
   belongs_to :infectable, :polymorphic => true
 
-  validates :infectable_id, :infectable_type, :presence => true
   validates :organism_code_id, uniqueness: { scope: [:infectable_id, :infectable_type] }, presence: { message: "Organism can't be blank" }
 
 end
