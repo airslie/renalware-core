c05a7ddda1b598a8f56c3d26ffc21c00a6cb257d
Steve Laing <developer@laingsolutions.com>
2015-07-02 09:53:15 +0100

    Add Personable#full_name

--- app/models/concerns/personable.rb
+++ app/models/concerns/personable.rb
@@ -6,6 +6,10 @@ module Personable
       validates_presence_of :first_name, :last_name
     end
 
+    def full_name
+      to_s(:full_name)
+    end
+
     alias_method :orig_to_s, :to_s
 
     def to_s(format=nil)
