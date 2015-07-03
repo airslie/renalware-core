8e405c0adf9128c05bf5ad48f6d7b5fafd488f26
op48 <lianne.tan@gmail.com>
2015-07-03 16:32:47 +0100

    Removed reject_if for nested infection_organisms from exit site infections.

--- app/models/exit_site_infection.rb
+++ app/models/exit_site_infection.rb
@@ -11,8 +11,7 @@ class ExitSiteInfection < ActiveRecord::Base
   accepts_nested_attributes_for :medications, allow_destroy: true,
   reject_if: proc { |attrs| attrs[:dose].blank? && attrs[:notes].blank? && attrs[:frequency].blank? }
 
-  accepts_nested_attributes_for :infection_organisms, allow_destroy: true,
-  reject_if: proc { |attrs| attrs[:sensitvity].blank? && attrs[:organism_code_id].blank? }
+  accepts_nested_attributes_for :infection_organisms, allow_destroy: true
 
   validates :diagnosis_date, presence: true
 
