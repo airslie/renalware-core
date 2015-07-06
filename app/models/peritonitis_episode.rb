2e2fd9eac55eb38394e367c2d3bdfe3b4acd6bda
op48 <lianne.tan@gmail.com>
2015-07-06 09:50:14 +0100

    Removed reject_if statement for peritonitis medications to allow validations.

--- app/models/peritonitis_episode.rb
+++ app/models/peritonitis_episode.rb
@@ -10,8 +10,7 @@ class PeritonitisEpisode < ActiveRecord::Base
   has_many :infection_organisms, as: :infectable
   has_many :organism_codes, -> { uniq }, through: :infection_organisms, as: :infectable
 
-  accepts_nested_attributes_for :medications, allow_destroy: true,
-  reject_if: proc { |attrs| attrs[:dose].blank? && attrs[:notes].blank? && attrs[:frequency].blank? }
+  accepts_nested_attributes_for :medications, allow_destroy: true
 
   accepts_nested_attributes_for :infection_organisms, allow_destroy: true
 
