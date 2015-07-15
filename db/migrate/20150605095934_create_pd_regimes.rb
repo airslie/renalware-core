0e834568dee1e3e65c1534c7c39510fb876bfe12
op48 <lianne.tan@gmail.com>
2015-07-15 16:06:56 +0100

    Added APD specific fields, revised naming of additional_hd, removed glucose/sodium columns (recorded in bag types).

--- db/migrate/20150605095934_create_pd_regimes.rb
+++ db/migrate/20150605095934_create_pd_regimes.rb
@@ -4,15 +4,20 @@ class CreatePdRegimes < ActiveRecord::Migration
       t.integer :patient_id
       t.date :start_date
       t.date :end_date
+      t.string :treatment_type
       t.string :type
       t.integer :glucose_ml_percent_1_36
       t.integer :glucose_ml_percent_2_27
       t.integer :glucose_ml_percent_3_86
       t.integer :amino_acid_ml
       t.integer :icodextrin_ml
-      t.boolean :low_glucose_degradation
-      t.boolean :low_sodium
-      t.boolean :additional_hd
+      t.boolean :add_hd
+      t.integer :last_fill_ml
+      t.boolean :add_manual_exchange
+      t.boolean :tidal_indicator
+      t.integer :tidal_percentage
+      t.integer :no_cycles_per_apd
+      t.integer :overnight_pd_ml
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -341,15 +341,20 @@ ActiveRecord::Schema.define(version: 20150717093153) do
     t.integer  "patient_id"
     t.date     "start_date"
     t.date     "end_date"
+    t.string   "treatment_type"
     t.string   "type"
     t.integer  "glucose_ml_percent_1_36"
     t.integer  "glucose_ml_percent_2_27"
     t.integer  "glucose_ml_percent_3_86"
     t.integer  "amino_acid_ml"
     t.integer  "icodextrin_ml"
-    t.boolean  "low_glucose_degradation"
-    t.boolean  "low_sodium"
-    t.boolean  "additional_hd"
+    t.boolean  "add_hd"
+    t.integer  "last_fill_ml"
+    t.boolean  "add_manual_exchange"
+    t.boolean  "tidal_indicator"
+    t.integer  "tidal_percentage"
+    t.integer  "no_cycles_per_apd"
+    t.integer  "overnight_pd_ml"
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
   end
