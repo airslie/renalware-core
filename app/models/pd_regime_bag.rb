6de19b96d0bc9fe78f9af31236b8bd828675aaa8
op48 <lianne.tan@gmail.com>
2015-07-20 13:17:29 +0100

    Added validation to pd_regime_bag for volume max and min values with test and errors display.

--- app/models/pd_regime_bag.rb
+++ app/models/pd_regime_bag.rb
@@ -1,6 +1,10 @@
 class PdRegimeBag < ActiveRecord::Base
 
+  before_save :assign_days_per_week
+
   belongs_to :bag_type
   belongs_to :pd_regime
+
+  validates :volume, numericality: { greater_than_or_equal_to: 100, less_than_or_equal_to: 10000 }
 
 end
--- spec/models/pd_regime_bag_spec.rb
+++ spec/models/pd_regime_bag_spec.rb
@@ -5,4 +5,6 @@ RSpec.describe PdRegimeBag, :type => :model do
   it { should belong_to :bag_type }
   it { should belong_to :pd_regime }
 
+  it { should validate_numericality_of(:volume).is_greater_than_or_equal_to(100).is_less_than_or_equal_to(10000) }
+
 end
