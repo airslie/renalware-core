73a6c7d55886e6b86c1f20648b7470f7ff25b608
op48 <lianne.tan@gmail.com>
2015-07-23 17:03:28 +0100

    Added validations pd regime's 'treatment' field, also for apd regime: last_fill_ml, tidal_percentage, no_cycles_per_apd, overnight_pd_ml fields with rspec tests.

--- app/models/pd_regime.rb
+++ app/models/pd_regime.rb
@@ -8,4 +8,19 @@ class PdRegime < ActiveRecord::Base
   accepts_nested_attributes_for :pd_regime_bags, allow_destroy: true
 
   validates :start_date, presence: true
+  validates :treatment, presence: true
+
+  with_options if: :type_apd? do |apd|
+    apd.validates :last_fill_ml, allow_nil: true, numericality: { greater_than_or_equal_to: 500, less_than_or_equal_to: 5000 }
+    apd.validates :tidal_percentage, allow_nil: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
+    apd.validates :no_cycles_per_apd, allow_nil: true, numericality: { greater_than_or_equal_to: 2, less_than_or_equal_to: 20 }
+    apd.validates :overnight_pd_ml, allow_nil: true, numericality: { greater_than_or_equal_to: 3000, less_than_or_equal_to: 25000 }
+  end
+
+  def type_apd?
+    if self.type.present?
+      self.type == 'ApdRegime'
+    end
+  end
+
 end
--- spec/models/pd_regime_spec.rb
+++ spec/models/pd_regime_spec.rb
@@ -7,4 +7,31 @@ RSpec.describe PdRegime, type: :model do
   it { should have_many :pd_regime_bags }
   it { should have_many(:bag_types).through(:pd_regime_bags) }
 
+  it { should validate_presence_of :start_date }
+  it { should validate_presence_of :treatment }
+
+
+  describe "type_apd?" do
+    before do
+      @capd_regime = create(:capd_regime)
+      @apd_regime = create(:apd_regime)
+    end
+
+    context "if PD type is ApdRegime" do
+      before { allow(@apd_regime).to receive(:type_apd?).and_return(true) }
+      it { expect(@apd_regime).to validate_numericality_of(:last_fill_ml).is_greater_than_or_equal_to(500).is_less_than_or_equal_to(5000) }
+      it { expect(@apd_regime).to validate_numericality_of(:tidal_percentage).is_greater_than_or_equal_to(0).is_less_than_or_equal_to(100) }
+      it { expect(@apd_regime).to validate_numericality_of(:no_cycles_per_apd).is_greater_than_or_equal_to(2).is_less_than_or_equal_to(20) }
+      it { expect(@apd_regime).to validate_numericality_of(:overnight_pd_ml).is_greater_than_or_equal_to(3000).is_less_than_or_equal_to(25000) }
+    end
+
+    context "if PD type is CapdRegime" do
+      before { allow(@capd_regime).to receive(:type_apd?).and_return(false) }
+      it { expect(@capd_regime).to_not validate_numericality_of(:last_fill_ml).is_greater_than_or_equal_to(500).is_less_than_or_equal_to(5000) }
+      it { expect(@capd_regime).to_not validate_numericality_of(:tidal_percentage).is_greater_than_or_equal_to(0).is_less_than_or_equal_to(100) }
+      it { expect(@capd_regime).to_not validate_numericality_of(:no_cycles_per_apd).is_greater_than_or_equal_to(2).is_less_than_or_equal_to(20) }
+      it { expect(@capd_regime).to_not validate_numericality_of(:overnight_pd_ml).is_greater_than_or_equal_to(3000).is_less_than_or_equal_to(25000) }
+    end
+  end
+
 end
