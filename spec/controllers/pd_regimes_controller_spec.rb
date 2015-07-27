5ce842745576a85f3efb9208a5db19fa8a0dfd27
op48 <lianne.tan@gmail.com>
2015-07-27 16:50:45 +0100

    Updated pd_regimes_controller_spec with now validated bag min bag per regime.

--- spec/controllers/pd_regimes_controller_spec.rb
+++ spec/controllers/pd_regimes_controller_spec.rb
@@ -4,12 +4,26 @@ RSpec.describe PdRegimesController, :type => :controller do
 
   before do
     @patient = create(:patient)
-    @capd_regime = create(:capd_regime)
+    @bag_type = create(:bag_type)
+    @capd_regime = create(:capd_regime,
+                    pd_regime_bags_attributes: [
+                      bag_type_id: @bag_type.id,
+                      volume: 600,
+                      sunday: true,
+                      monday: true,
+                      tuesday: true,
+                      wednesday: true,
+                      thursday: true,
+                      friday: true,
+                      saturday: true
+                    ]
+                  )
   end
 
   describe 'GET #new' do
     it 'renders the new template' do
       get :new, patient_id: @patient.id
+
       expect(response).to render_template('new')
     end
   end
@@ -17,10 +31,24 @@ RSpec.describe PdRegimesController, :type => :controller do
   describe 'POST #create' do
     context "with valid attributes" do
       it 'creates a new CAPD Regime' do
-        expect {
-          post :create,
+        expect { post :create,
           patient_id: @patient.id,
-          pd_regime: { type: 'CapdRegime', start_date: '01/02/2015', treatment: 'CAPD 3 exchanges per day' }
+          pd_regime: {
+            type: 'CapdRegime',
+            start_date: '01/02/2015',
+            treatment: 'CAPD 3 exchanges per day',
+            pd_regime_bags_attributes: [
+              bag_type_id: @bag_type.id,
+              volume: 600,
+              sunday: true,
+              monday: true,
+              tuesday: true,
+              wednesday: true,
+              thursday: true,
+              friday: true,
+              saturday: true
+            ]
+          }
         }.to change(PdRegime, :count).by(1)
 
         expect(response).to redirect_to(pd_info_patient_path(@patient))
@@ -32,7 +60,10 @@ RSpec.describe PdRegimesController, :type => :controller do
         expect {
           post :create,
           patient_id: @patient.id,
-          pd_regime: { type: 'CapdRegime', start_date: nil, treatment: nil }
+          pd_regime: { type: 'CapdRegime',
+            start_date: nil,
+            treatment: nil
+          }
         }.to change(PdRegime, :count).by(0)
 
         expect(response).to render_template(:new)
@@ -45,7 +76,9 @@ RSpec.describe PdRegimesController, :type => :controller do
           post :create,
           patient_id: @patient.id,
           actions: { add_bag: 'Add Bag' },
-          pd_regime: { type: 'CapdRegime', start_date: Date.today, treatment: 'CAPD 3 exchanges per day' }
+          pd_regime: { type: 'CapdRegime',
+            start_date: Date.today,
+            treatment: 'CAPD 3 exchanges per day' }
         }.to change(PdRegime, :count).by(0)
 
         expect(assigns(:pd_regime).pd_regime_bags.size).to eq(1)
@@ -57,7 +90,12 @@ RSpec.describe PdRegimesController, :type => :controller do
         post :create,
         patient_id: @patient.id,
         actions: { remove: { '0' => 'Remove' } },
-        pd_regime: { type: 'CapdRegime', start_date: Date.today, treatment: 'CAPD 4 exchanges per day', pd_regime_bags_attributes: [{ bag_type_id:'100',volume:'2',per_week:'1',monday:true }] }
+        pd_regime: { type: 'CapdRegime',
+          start_date: Date.today,
+          treatment: 'CAPD 4 exchanges per day',
+          pd_regime_bags_attributes: [{ bag_type_id:'100', volume:'2', per_week:'1', monday:true }]
+        }
+
         expect(assigns(:pd_regime).pd_regime_bags.size).to eq(0)
       end
     end
@@ -69,7 +107,11 @@ RSpec.describe PdRegimesController, :type => :controller do
         put :update,
         id: @capd_regime.id,
         patient_id: @patient.id,
-        pd_regime: { type: 'CapdRegime', start_date: '15/02/2015', treatment: ' CAPD 5 exchanges per day' }
+        pd_regime: { type: 'CapdRegime',
+          start_date: '15/02/2015',
+          treatment: ' CAPD 5 exchanges per day'
+        }
+
         expect(response).to redirect_to(pd_info_patient_path(@patient))
       end
     end
@@ -79,7 +121,11 @@ RSpec.describe PdRegimesController, :type => :controller do
         put :update,
         id: @capd_regime.id,
         patient_id: @patient.id,
-        pd_regime: { type: 'CapdRegime', start_date: nil, treatment: nil }
+        pd_regime: { type: 'CapdRegime',
+          start_date: nil,
+          treatment: nil
+        }
+
         expect(response).to render_template(:edit)
       end
     end
@@ -91,10 +137,13 @@ RSpec.describe PdRegimesController, :type => :controller do
           id: @capd_regime.id,
           patient_id: @patient.id,
           actions: { add_bag: 'Add Bag' },
-          pd_regime: { type: 'CapdRegime', start_date: Date.today, treatment: 'CAPD 3 exchanges per day' }
+          pd_regime: { type: 'CapdRegime',
+            start_date: Date.today,
+            treatment: 'CAPD 3 exchanges per day'
+          }
         }.to change(PdRegime, :count).by(0)
 
-        expect(assigns(:pd_regime).pd_regime_bags.size).to eq(1)
+        expect(assigns(:pd_regime).pd_regime_bags.size).to eq(2)
       end
     end
 
@@ -105,8 +154,12 @@ RSpec.describe PdRegimesController, :type => :controller do
         id: @capd_regime.id,
         patient_id: @patient.id,
         actions: { remove: { '0' => 'Remove' } },
-        pd_regime: { type: 'CapdRegime', start_date: Date.today, treatment: 'CAPD 3 exchanges per day' }
-        expect(assigns(:pd_regime).pd_regime_bags.size).to eq(0)
+        pd_regime: { type: 'CapdRegime',
+          start_date: Date.today,
+          treatment: 'CAPD 3 exchanges per day'
+        }
+
+        expect(assigns(:pd_regime).pd_regime_bags.size).to eq(1)
       end
     end
   end
