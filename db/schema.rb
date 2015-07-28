86276723984241763dabd034aa79ef554c63f3f6
op48 <lianne.tan@gmail.com>
2015-07-28 11:05:51 +0100

    Revised bag types table columns.

--- db/schema.rb
+++ db/schema.rb
@@ -30,16 +30,18 @@ ActiveRecord::Schema.define(version: 20150717093153) do
   create_table "bag_types", force: :cascade do |t|
     t.string   "manufacturer"
     t.string   "description"
-    t.integer  "glucose_ml_percent_1_36"
-    t.integer  "glucose_ml_percent_2_27"
-    t.integer  "glucose_ml_percent_3_86"
-    t.integer  "amino_acid_ml"
-    t.integer  "icodextrin_ml"
+    t.decimal  "glucose_grams_per_litre", precision: 4, scale: 1
+    t.boolean  "amino_acid"
+    t.boolean  "icodextrin"
     t.boolean  "low_glucose_degradation"
     t.boolean  "low_sodium"
+    t.integer  "sodium_content"
+    t.integer  "lactate_content"
+    t.decimal  "calcium_content",         precision: 3, scale: 2
+    t.decimal  "magnesium_content",       precision: 3, scale: 2
     t.datetime "deleted_at"
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
   end
 
   add_index "bag_types", ["deleted_at"], name: "index_bag_types_on_deleted_at", using: :btree
