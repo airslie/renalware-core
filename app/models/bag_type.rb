c57250af49d27b53d735db7fa47983515289b7e0
op48 <lianne.tan@gmail.com>
2015-07-28 14:53:57 +0100

    Added validation criteria for 'glucose_grams_per_litre' for bag_type.

--- app/models/bag_type.rb
+++ app/models/bag_type.rb
@@ -4,6 +4,9 @@ class BagType < ActiveRecord::Base
   has_many :pd_regime_bags
 
   validates :description, presence: true
+  validates :glucose_grams_per_litre, presence: true
+
+  validates :glucose_grams_per_litre, numericality: { allow_nil: true, greater_than_or_equal_to: 10, less_than_or_equal_to: 50 }
 
   def full_description
     [manufacturer, description].join(' ')
