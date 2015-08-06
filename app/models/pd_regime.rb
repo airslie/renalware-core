8c50dd52d660927c10adf635ee482300d346c61d
op48 <lianne.tan@gmail.com>
2015-08-06 10:41:35 +0100

    Added methods to calculate average daily glucose use over duration of a week, with rspec test.

--- app/models/pd_regime.rb
+++ app/models/pd_regime.rb
@@ -1,5 +1,9 @@
 class PdRegime < ActiveRecord::Base
 
+  before_save :set_glucose_ml_percent_1_36
+  before_save :set_glucose_ml_percent_2_27
+  before_save :set_glucose_ml_percent_3_86
+
   belongs_to :patient
 
   has_many :pd_regime_bags
@@ -28,6 +32,55 @@ class PdRegime < ActiveRecord::Base
   private
   def min_one_pd_regime_bag
     errors.add(:pd_regime_bags, "must be assigned at least one bag") if pd_regime_bags.size < 1
+  end
+
+  def match_bag_type
+    glucose_types = [[],[],[]]
+
+    self.pd_regime_bags.each do |bag|
+      case bag.bag_type.glucose_grams_per_litre.to_f
+
+      when 13.6
+        glucose_types[0] << bag.weekly_total_glucose_ml_per_bag
+      when 22.7
+        glucose_types[1] << bag.weekly_total_glucose_ml_per_bag
+      when 38.6
+        glucose_types[2] << bag.weekly_total_glucose_ml_per_bag
+      else
+        glucose_types
+      end
+    end
+    glucose_types
+  end
+
+  def set_glucose_ml_percent_1_36
+    if match_bag_type[0].empty?
+      0
+    else
+      per_week_total = match_bag_type[0].inject{ |sum, v| sum + v }
+      glucose_daily_average = per_week_total/7.to_f
+      self.glucose_ml_percent_1_36 = glucose_daily_average.round
+    end
+  end
+
+  def set_glucose_ml_percent_2_27
+    if match_bag_type[1].empty?
+      0
+    else
+      per_week_total = match_bag_type[1].inject{ |sum, v| sum + v }
+      glucose_daily_average = per_week_total/7.to_f
+      self.glucose_ml_percent_2_27 = glucose_daily_average.round
+    end
+  end
+
+  def set_glucose_ml_percent_3_86
+    if match_bag_type[2].empty?
+      0
+    else
+      per_week_total = match_bag_type[2].inject{ |sum, v| sum + v }
+      glucose_daily_average = per_week_total/7.to_f
+      self.glucose_ml_percent_3_86 = glucose_daily_average.round
+    end
   end
 
 end
