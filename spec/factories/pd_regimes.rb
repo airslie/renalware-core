245a21262c55cce84aeace671b6bb6547c0bca63
op48 <lianne.tan@gmail.com>
2015-08-10 10:36:24 +0100

    Removed preset values for average glucose strengths within pd regime factories.

--- spec/factories/pd_regimes.rb
+++ spec/factories/pd_regimes.rb
@@ -8,8 +8,6 @@ FactoryGirl.define do
     start_date "01/02/2015"
     end_date "01/02/2015"
     treatment "CAPD 3 exchanges per day"
-    glucose_ml_percent_1_36 10
-    glucose_ml_percent_2_27 20
     amino_acid_ml 40
     icodextrin_ml 50
     add_hd false
@@ -20,9 +18,6 @@ FactoryGirl.define do
     start_date "01/03/2015"
     end_date "02/04/2015"
     treatment "APD Wet day with additional exchange"
-    glucose_ml_percent_1_36 13
-    glucose_ml_percent_2_27 23
-    glucose_ml_percent_3_86 33
     amino_acid_ml 43
     icodextrin_ml 53
     add_hd true
