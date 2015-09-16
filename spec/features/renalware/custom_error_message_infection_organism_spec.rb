a7ae41222b84d6f9b53c13c380f010c1987d87ef
op48 <lianne.tan@gmail.com>
2015-09-16 14:43:15 +0100

    Relocated feature test from infection_organism model spec.

---
+++ spec/features/renalware/custom_error_message_infection_organism_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+module Renalware
+  feature 'able to view custom error messages on failed validation for an infection organism', js: true do
+    background do
+      @patient = create(:patient)
+      login_as_clinician
+      visit new_patient_peritonitis_episode_path(@patient)
+    end
+
+    scenario 'a peritonitis episode is saved with invalid values for an infection organism' do
+      click_link 'Record a new organism and sensitivity'
+      click_on 'Save Peritonitis Episode'
+
+      expect(page).to have_content("Organism can't be blank")
+    end
+  end
+
+end
--- spec/models/renalware/infection_organism_spec.rb
+++ spec/models/renalware/infection_organism_spec.rb
@@ -7,21 +7,5 @@ module Renalware
 
     it { should validate_uniqueness_of(:organism_code_id).scoped_to([:infectable_id, :infectable_type]) }
     it { should validate_presence_of(:organism_code_id) }
-
-    describe 'organism validation custom error message', :type => :feature, :js => true do
-      context 'peritonitis episode' do
-        it 'should display custom error message when an infection organism fails validation' do
-          @patient = create(:patient)
-          login_as_clinician
-          visit new_patient_peritonitis_episode_path(@patient)
-
-          click_link 'Record a new organism and sensitivity'
-          click_on 'Save Peritonitis Episode'
-
-          expect(page).to have_content("Organism can't be blank")
-        end
-      end
-    end
-
   end
 end
