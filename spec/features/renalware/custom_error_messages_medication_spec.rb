28e678a05fdf67e45eefa4455e6799ad82ee7e27
op48 <lianne.tan@gmail.com>
2015-09-16 15:03:21 +0100

    Relocated feature test from medication model spec.

---
+++ spec/features/renalware/custom_error_messages_medication_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+module Renalware
+  feature 'Able to view custom error messages on failed validation for a medication', js: true do
+    background do
+      @patient = create(:patient)
+      login_as_clinician
+      visit manage_medications_patient_path(@patient)
+    end
+
+    scenario 'a medication is saved with invalid values' do
+      click_link 'Add a new medication'
+      click_on 'Save Medication'
+
+      expect(page).to have_content("Medication to be administered can't be blank")
+      expect(page).to have_content("Medication's dose can't be blank")
+      expect(page).to have_content("Medication's route can't be blank")
+      expect(page).to have_content("Medication's frequency & duration can't be blank")
+      expect(page).to have_content("Medication's prescribed date can't be blank")
+    end
+  end
+end
--- spec/models/renalware/medication_spec.rb
+++ spec/models/renalware/medication_spec.rb
@@ -17,23 +17,6 @@ module Renalware
     it { should validate_presence_of(:start_date) }
     it { should validate_presence_of(:provider) }
 
-    describe 'medication validation custom error messages', :type => :feature, :js => true do
-      it 'should display custom error messages when a medication fails validation' do
-        @patient = create(:patient)
-        login_as_clinician
-        visit manage_medications_patient_path(@patient)
-
-        click_link 'Add a new medication'
-        click_on 'Save Medication'
-
-        expect(page).to have_content("Medication to be administered can't be blank")
-        expect(page).to have_content("Medication's dose can't be blank")
-        expect(page).to have_content("Medication's route can't be blank")
-        expect(page).to have_content("Medication's frequency & duration can't be blank")
-        expect(page).to have_content("Medication's prescribed date can't be blank")
-      end
-    end
-
     describe 'self.peritonitis' do
       it "should set 'treatable_type' as 'PeritonitisEpisode' for a medication relating to peritonitis" do
         expect(Medication.peritonitis.treatable_type).to eq('Renalware::PeritonitisEpisode')
