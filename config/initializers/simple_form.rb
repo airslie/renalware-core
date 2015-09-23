f7f0d7c74fa64f0be7f55893e8c42d020431199c
Hugo Frappier <hugo@cekoya.com>
2015-09-23 11:34:00 -0400

    simple_form: don't make all fields required by default

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -110,7 +110,7 @@ SimpleForm.setup do |config|
   # config.generate_additional_classes_for = [:wrapper, :label, :input]
 
   # Whether attributes are required by default (or not). Default is true.
-  # config.required_by_default = true
+  config.required_by_default = false
 
   # Tell browsers whether to use the native HTML5 validations (novalidate form option).
   # These validations are enabled in SimpleForm's internal config but disabled by default
