b9e699bed83b04e986250b3a770f6c31ad3fd735
Hugo Frappier <hugo@cekoya.com>
2015-09-23 17:54:08 -0400

    Load nested localisation files

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,8 @@ module Renalware
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+
     # config.i18n.default_locale = :de
     # Custom directories with classes and modules you want to be autoloadable.
     config.autoload_paths += %W(#{config.root}/lib #{config.root}/app/validators #{config.root}/app/documents)
