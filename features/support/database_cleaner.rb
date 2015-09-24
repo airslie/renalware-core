01780a0425b03e3ba7f8cc443ecf542b4e66ad9c
Hugo Frappier <hugo@cekoya.com>
2015-09-24 13:59:16 -0400

    Upgrade database_cleaner gem

--- features/support/database_cleaner.rb
+++ features/support/database_cleaner.rb
@@ -1,11 +1,12 @@
-# Remove/comment out the lines below if your app doesn't have a database.
-# For some databases (like MongoDB and CouchDB) you may need to use :truncation instead.
 begin
+  require 'database_cleaner'
+  require 'database_cleaner/cucumber'
+
   DatabaseCleaner.strategy = :truncation
 rescue NameError
   raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
 end
 
-Before do
-  DatabaseCleaner.clean_with(:truncation)
+Around do |scenario, block|
+  DatabaseCleaner.cleaning(&block)
 end
