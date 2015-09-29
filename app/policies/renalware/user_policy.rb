50e57f8e5b346125d99ae9dbc2cfd9246e51e08c
op48 <lianne.tan@gmail.com>
2015-09-29 13:14:44 +0100

    Set up user policy and user policy spec test.

--- app/policies/renalware/user_policy.rb
+++ app/policies/renalware/user_policy.rb
@@ -1,9 +1,9 @@
 module Renalware
 
   class UserPolicy < ApplicationPolicy
-    def index?  ; has_privilege? end
     def create? ; has_privilege? end
-    def update? ; has_privilege? end
+    def edit? ; has_privilege? end
+    def index? ; has_privilege? end
 
     private
     def has_privilege?
---
+++ spec/policies/renalware/user_policy_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+module Renalware
+
+  describe UserPolicy, type: :policy do
+    subject { UserPolicy.new(current_user, user) }
+
+    let(:current_user) { build_stubbed :user }
+
+    context "for a superadmin" do
+      let(:user) { User.new }
+
+    end
+  end
+
+end
