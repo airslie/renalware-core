d4aaf37e25f1d177ae02327ef4f16f4e6515efa4
op48 <lianne.tan@gmail.com>
2015-10-01 13:20:00 +0100

    Setup pundit policy for event types.

---
+++ app/policies/renalware/event_type_policy.rb
@@ -0,0 +1,14 @@
+module Renalware
+  class EventTypePolicy < ApplicationPolicy
+    def new? ; has_privilege? end
+    def create? ; has_privilege? end
+    def index? ; has_privilege? end
+    def edit? ; has_privilege? end
+    def destroy? ; has_privilege? end
+
+    private
+    def has_privilege?
+      user.has_role?(:admin) || user.has_role?(:super_admin)
+    end
+  end
+end
