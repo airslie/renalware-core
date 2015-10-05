f1bd9c85853eaf792900325309cb2ff538fc190d
Hugo Frappier <hugo@cekoya.com>
2015-10-05 11:33:03 -0400

    Ability to specify old attributes
    Otherwise, we get a method missing exception when loading data from the database

--- lib/document/base.rb
+++ lib/document/base.rb
@@ -15,5 +15,31 @@ module Document
     def self.fields
       attribute_set.entries.map(&:name)
     end
+
+    # Flag an old attribute to be ignored
+    # when the document is deserialized from the database
+    #
+    #   class RecipientWorkupDocument < Document::Base
+    #     old_attribute :hx_tb
+    #   end
+    def self.old_attribute(attribute)
+      @@methods_to_ignore ||= []
+      @@methods_to_ignore << attribute
+      @@methods_to_ignore << "#{attribute}=".to_sym
+    end
+
+    # Flag a list of old attribtues to be ignored
+    # when the document is deserialized from the database
+    #
+    #   class RecipientWorkupDocument < Document::Base
+    #     old_attributes :hx_tb, :hx_tb_date, :foo_bar
+    #   end
+    def self.old_attributes(*list)
+      list.each { |item| old_attribute(item) }
+    end
+
+    def method_missing(method_sym, *arguments, &block)
+      super unless @@methods_to_ignore.include? method_sym
+    end
   end
 end
