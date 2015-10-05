39b78875d513f19cd5ba3b6eeeafe244d6980953
Hugo Frappier <hugo@cekoya.com>
2015-10-05 14:35:34 -0400

    Improve scrolling to error feature

--- app/assets/javascripts/forms.js
+++ app/assets/javascripts/forms.js
@@ -1,7 +1,7 @@
 $(function() {
   if ($('small.error').length > 0) {
     $('html, body').animate({
-      scrollTop: ($('small.error').first().offset().top)
+      scrollTop: ($('small.error').first().offset().top-100)
     },500);
   }
 });
