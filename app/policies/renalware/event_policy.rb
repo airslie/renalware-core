22b6e19b2126ba328ef03c9c0f24dab6d0572ab8
op48 <lianne.tan@gmail.com>
2015-10-05 12:12:30 +0100

    Created pundit policy for events, with spec tests.

---
+++ app/policies/renalware/event_policy.rb
@@ -0,0 +1,12 @@
+module Renalware
+  class EventPolicy < ApplicationPolicy
+    def new? ; has_privilege? end
+    def create? ; has_privilege? end
+    def index? ; true end
+
+    private
+    def has_privilege?
+      user.has_role?(:clinician) || user.has_role?(:admin) || user.has_role?(:super_admin)
+    end
+  end
+end
