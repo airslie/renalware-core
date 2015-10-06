9ca0f62b070a69ae9ccb10c29a6909360c278a8b
op48 <lianne.tan@gmail.com>
2015-10-06 12:21:56 +0100

    Created pundit policy for letter with spec tests.

---
+++ app/policies/renalware/letter_policy.rb
@@ -0,0 +1,15 @@
+module Renalware
+  class LetterPolicy < ApplicationPolicy
+    def new? ; has_privilege? end
+    def create? ; has_privilege? end
+    def index? ; has_privilege? end
+    def edit? ; has_privilege? end
+    def update? ; has_privilege? end
+    def author? ; has_privilege? end
+
+    private
+    def has_privilege?
+      user.has_role?(:clinician) || user.has_role?(:admin) || user.has_role?(:super_admin)
+    end
+  end
+end
