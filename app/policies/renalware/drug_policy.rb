5c305f3d50949ea6ce6dfb00149f2ba15172a25b
op48 <lianne.tan@gmail.com>
2015-10-06 14:55:08 +0100

    Updated drug policy, created drug policy spec.

--- app/policies/renalware/drug_policy.rb
+++ app/policies/renalware/drug_policy.rb
@@ -4,6 +4,7 @@ module Renalware
     def create? ; has_privilege? end
     def index? ; has_privilege? end
     def edit? ; has_privilege? end
+    def update? ; has_privilege? end
     def destroy? ; has_privilege? end
 
     private
