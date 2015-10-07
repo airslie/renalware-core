f96bc4e95a8e95b5d6bd7d589ab9cedeeaa0c739
Hugo Frappier <hugo@cekoya.com>
2015-10-07 12:20:19 -0400

    Do not use FactoryGirl

--- features/step_definitions/renalware/users_steps.rb
+++ features/step_definitions/renalware/users_steps.rb
@@ -1,6 +1,11 @@
 Given(/^Clyde is a clinician$/) do
-  @clyde = FactoryGirl.create(
-    :user, :approved, :clinician,
-    first_name: "Clyde", last_name: "TheClinician"
+  @clyde = Renalware::User.create!(
+    first_name: "Clyde",
+    last_name: "The Clinician",
+    username: "clyde",
+    email: "clyde@renalware.com",
+    password: "supersecret",
+    approved: true,
   )
+  @clyde.roles << Renalware::Role.find_or_create_by(name: "clinician")
 end
