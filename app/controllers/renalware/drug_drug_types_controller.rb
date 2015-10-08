26df3ffc6de13cd8ceb03f12304a342f95bf3394
op48 <lianne.tan@gmail.com>
2015-10-08 14:12:16 +0100

    Removed cancancan method, added pundit 'authorize' methods to drugdrugtype actions.

--- app/controllers/renalware/drug_drug_types_controller.rb
+++ app/controllers/renalware/drug_drug_types_controller.rb
@@ -1,21 +1,23 @@
 module Renalware
   class DrugDrugTypesController < BaseController
-    load_and_authorize_resource
 
     #TODO: Refactor
     def index
       @drug = Drug.find(params[:drug_id])
       @drug_drug_types = @drug.drug_drug_types
       @assign_drug_types = DrugDrugType.new(:drug => @drug)
+      authorize @assign_drug_types
     end
 
     def create
       @drug_drug_type = DrugDrugType.create!(allowed_params)
+      authorize @drug_drug_type
       redirect_to drug_drug_drug_types_path
     end
 
     def destroy
       @drug_drug_type = DrugDrugType.find(params[:id])
+      authorize @drug_drug_type
       @drug_drug_type.destroy!
     end
 
