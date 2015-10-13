a6efd727854cba0ca2af29354a30fe95af35cbc9
op48 <lianne.tan@gmail.com>
2015-10-13 12:51:09 +0100

    Added missing association spec tests for event model.

--- spec/models/renalware/event_spec.rb
+++ spec/models/renalware/event_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 module Renalware
   describe Event, :type => :model do
+
+    it { should belong_to :patient }
+    it { should belong_to :event_type }
+
     it { should validate_presence_of :event_type_id }
     it { should validate_presence_of :date_time }
     it { should validate_presence_of :description }
