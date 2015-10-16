12dc94c6b6101d5a8b6bc475a1cfb92037d3dd16
op48 <lianne.tan@gmail.com>
2015-10-16 16:55:45 +0100

    Fixing rebase - adding refactored migrations.

--- db/migrate/20151006135256_rename_prd_codes_to_prd_descriptions.rb
+++
@@ -1,6 +0,0 @@
-class RenamePRDCodesToPRDDescriptions < ActiveRecord::Migration
-  def change
-    rename_table :prd_codes, :prd_descriptions
-    rename_column :esrf, :prd_code_id, :prd_description_id
-  end
-end
