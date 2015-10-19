5f6ace92ae77ac8cca3f0bc58255cead411b496e
op48 <lianne.tan@gmail.com>
2015-10-19 10:17:30 +0100

    Removed cancancan method, added pundit 'authorize' to clinical summaries controller.

--- app/controllers/renalware/clinical_summaries_controller.rb
+++ app/controllers/renalware/clinical_summaries_controller.rb
@@ -1,9 +1,9 @@
 module Renalware
   class ClinicalSummariesController < BaseController
-    skip_authorize_resource only: :show
 
     def show
       @patient = Patient.find(params[:patient_id])
+      authorize @patient
     end
   end
 end
