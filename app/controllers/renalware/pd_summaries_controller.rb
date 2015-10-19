545bbef763448104ce0f3035afcc30b82627f6b1
op48 <lianne.tan@gmail.com>
2015-10-19 10:26:15 +0100

    Added pundit 'authorize' to pd_summaries_controller's show action.

--- app/controllers/renalware/pd_summaries_controller.rb
+++ app/controllers/renalware/pd_summaries_controller.rb
@@ -1,14 +1,13 @@
 module Renalware
   class PDSummariesController < BaseController
-    skip_authorize_resource only: :show
 
     def show
       @patient = Patient.find(params[:patient_id])
-
       @current_regime = @patient.pd_regimes.current if @patient.pd_regimes.any?
       @capd_regimes = CAPDRegime.for_patient(@patient).ordered
+      authorize @capd_regimes
       @apd_regimes = APDRegime.for_patient(@patient).ordered
-
+      authorize @apd_regimes
       @peritonitis_episodes = PeritonitisEpisode.for_patient(@patient)
       @exit_site_infections = ExitSiteInfection.for_patient(@patient)
     end
