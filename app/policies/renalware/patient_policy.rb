b03fb1b21dc60c4e7df17e8fd359958f011b5609
op48 <lianne.tan@gmail.com>
2015-10-19 10:37:27 +0100

    Amended Patient Policy to remove redundant patient policies/tests, covered by BasePolicy.

--- app/policies/renalware/patient_policy.rb
+++ app/policies/renalware/patient_policy.rb
@@ -1,16 +1,10 @@
 module Renalware
   class PatientPolicy < BasePolicy
-    def new? ; has_privilege? end
-    def create? ; has_privilege? end
-    def edit? ; has_privilege? end
-    def update? ; has_privilege? end
-    def index? ; has_privilege? end
     def death_update? ; has_privilege? end
     def death? ; true end
     def esrf_info? ; true end
     def show? ; true end
     def manage_medications? ; true end
-    def pd_info? ; true end
     def problems? ; true end
 
     private
