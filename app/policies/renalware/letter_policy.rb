29c2a2654ef66cb3449091d712c5f136b8a9a051
op48 <lianne.tan@gmail.com>
2015-10-19 17:46:26 +0100

    Removed redundant privileges method, using method from base policy for letter model.

--- app/policies/renalware/letter_policy.rb
+++ app/policies/renalware/letter_policy.rb
@@ -1,12 +1,7 @@
 module Renalware
   class LetterPolicy < BasePolicy
 
-    def author? ; has_privilege? end
-
-    private
-    def has_privilege?
-      user.has_role?(:clinician) || user.has_role?(:admin) || user.has_role?(:super_admin)
-    end
+    def author? ; has_write_privileges? end
 
   end
 end
