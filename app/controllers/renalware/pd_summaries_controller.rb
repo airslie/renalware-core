7ac29f0b7f62b7671d0a125a002836511afb3a63
op48 <lianne.tan@gmail.com>
2015-10-20 12:57:37 +0100

    Removed pundit 'authorize' from pd_summaries_controller's patient actions.

--- app/controllers/renalware/pd_summaries_controller.rb
+++ app/controllers/renalware/pd_summaries_controller.rb
@@ -7,9 +7,7 @@ module Renalware
       @patient = Patient.find(params[:patient_id])
       @current_regime = @patient.pd_regimes.current if @patient.pd_regimes.any?
       @capd_regimes = CAPDRegime.for_patient(@patient).ordered
-      authorize @capd_regimes
       @apd_regimes = APDRegime.for_patient(@patient).ordered
-      authorize @apd_regimes
       @peritonitis_episodes = PeritonitisEpisode.for_patient(@patient)
       @exit_site_infections = ExitSiteInfection.for_patient(@patient)
     end
