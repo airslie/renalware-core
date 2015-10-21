91cf589fc59ba72f62444c786c7233ce826b4b84
Nicholas Henry <nicholas.henry@gmail.com>
2015-10-21 17:58:07 -0400

    Fix style guide violations

--- app/controllers/renalware/modalities_controller.rb
+++ app/controllers/renalware/modalities_controller.rb
@@ -15,7 +15,8 @@ module Renalware
       @patient.set_modality(modality_params)
 
       if @patient.modality_code.death?
-        redirect_to edit_patient_death_path(@patient), notice: "Please make sure to update patient date of death and cause of death!"
+        redirect_to edit_patient_death_path(@patient),
+          notice: "Please make sure to update patient date of death and cause of death!"
       else
         redirect_to patient_modalities_path(@patient)
       end
--- spec/controllers/renalware/deaths_controller_spec.rb
+++ spec/controllers/renalware/deaths_controller_spec.rb
@@ -1,9 +1,9 @@
-require 'rails_helper'
+require "rails_helper"
 
 module Renalware
-  RSpec.describe DeathsController, :type => :controller do
-    describe 'index' do
-      it 'responds with success' do
+  RSpec.describe DeathsController, type: :controller do
+    describe "index" do
+      it "responds with success" do
         get :index
         expect(response).to have_http_status(:success)
       end
