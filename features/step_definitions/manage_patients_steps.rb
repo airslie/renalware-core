9bf921882bf33596370c0458a2ff42a52ff75eca
op48 <lianne.tan@gmail.com>
2015-10-23 16:34:06 +0100

    Fixed ruby old to new hash syntax.

--- features/step_definitions/manage_patients_steps.rb
+++ features/step_definitions/manage_patients_steps.rb
@@ -4,44 +4,44 @@ end
 
 Given(/^there are ethnicities in the database$/) do
   @ethnicities = ["White", "Black", "Asian"]
-  @ethnicities.map! { |e| Renalware::Ethnicity.create!(:name => e) }
+  @ethnicities.map! { |e| Renalware::Ethnicity.create!(name: e) }
 end
 
 Given(/^some patients who need renal treatment$/) do
   @patient_1 = FactoryGirl.create(:patient,
-    :nhs_number => "1000124501",
-    :local_patient_id => "Z999991",
-    :surname => "RABBIT",
-    :forename => "Roger",
-    :birth_date => "01/01/1947",
-    :paediatric_patient_indicator => "1",
-    :sex => "Male",
-    :ethnicity_id => Renalware::Ethnicity.first.id,
-    :hosp_centre_code => "888"
+    nhs_number: "1000124501",
+    local_patient_id: "Z999991",
+    surname: "RABBIT",
+    forename: "Roger",
+    birth_date: "01/01/1947",
+    paediatric_patient_indicator: "1",
+    sex: "Male",
+    ethnicity_id: Renalware::Ethnicity.first.id,
+    hosp_centre_code: "888"
   )
 
   @patient_2 = FactoryGirl.create(:patient,
-    :nhs_number => "1000124502",
-    :local_patient_id => "Z999992",
-    :surname => "DAY",
-    :forename => "Doris",
-    :birth_date => "24/06/1970",
-    :paediatric_patient_indicator => "1",
-    :sex => "Female",
-    :ethnicity_id => Renalware::Ethnicity.second.id,
-    :hosp_centre_code => "888"
+    nhs_number: "1000124502",
+    local_patient_id: "Z999992",
+    surname: "DAY",
+    forename: "Doris",
+    birth_date: "24/06/1970",
+    paediatric_patient_indicator: "1",
+    sex: "Female",
+    ethnicity_id: Renalware::Ethnicity.second.id,
+    hosp_centre_code: "888"
   )
 
   @patient_3 = FactoryGirl.create(:patient,
-    :nhs_number => "1000124503",
-    :local_patient_id => "Z999993",
-    :surname => "CASPER",
-    :forename => "Ghost",
-    :birth_date => "28/02/1930",
-    :paediatric_patient_indicator => "1",
-    :sex => "Male",
-    :ethnicity_id => Renalware::Ethnicity.third.id,
-    :hosp_centre_code => "999"
+    nhs_number: "1000124503",
+    local_patient_id: "Z999993",
+    surname: "CASPER",
+    forename: "Ghost",
+    birth_date: "28/02/1930",
+    paediatric_patient_indicator: "1",
+    sex: "Male",
+    ethnicity_id: Renalware::Ethnicity.third.id,
+    hosp_centre_code: "999"
   )
 end
 
@@ -54,10 +54,10 @@ Given(/^I've selected the patient from the search results$/) do
 end
 
 When(/^I complete the add a new patient form$/) do
-  fill_in "NHS Number", :with => "1000124504"
-  fill_in "Local Patient ID", :with => "Z999994"
-  fill_in "Surname", :with => "Smith"
-  fill_in "Forename", :with => "Ian"
+  fill_in "NHS Number", with: "1000124504"
+  fill_in "Local Patient ID", with: "Z999994"
+  fill_in "Surname", with: "Smith"
+  fill_in "Forename", with: "Ian"
 
   select "Male", from: "Sex"
 
@@ -77,20 +77,20 @@ When(/^I complete the add a new patient form$/) do
 
   within "#current_address" do
     @current_street_1 = Faker::Address.street_address
-    fill_in "Street 1", :with => @current_street_1
-    fill_in "Street 2", :with => Faker::Address.secondary_address
-    fill_in "City", :with => Faker::Address.city
-    fill_in "County", :with => Faker::AddressUK.county
-    fill_in "Postcode", :with => Faker::AddressUK.postcode
+    fill_in "Street 1", with: @current_street_1
+    fill_in "Street 2", with: Faker::Address.secondary_address
+    fill_in "City", with: Faker::Address.city
+    fill_in "County", with: Faker::AddressUK.county
+    fill_in "Postcode", with: Faker::AddressUK.postcode
   end
 
   within "#address_at_diagnosis" do
     @address_diagnosis_street_1 = Faker::Address.street_address
-    fill_in "Street 1", :with => @address_diagnosis_street_1
-    fill_in "Street 2", :with => Faker::Address.secondary_address
-    fill_in "City", :with => Faker::Address.city
-    fill_in "County", :with => Faker::AddressUK.county
-    fill_in "Postcode", :with => Faker::AddressUK.postcode
+    fill_in "Street 1", with: @address_diagnosis_street_1
+    fill_in "Street 2", with: Faker::Address.secondary_address
+    fill_in "City", with: Faker::Address.city
+    fill_in "County", with: Faker::AddressUK.county
+    fill_in "Postcode", with: Faker::AddressUK.postcode
   end
 
   click_on "Save"
@@ -98,7 +98,7 @@ end
 
 When(/^I update the patient's demographics$/) do
   click_on "Edit"
-  fill_in "Forename", :with => "Roger"
+  fill_in "Forename", with: "Roger"
 end
 
 When(/^submit the update form$/) do
