ce3923aa71ace744a8e1bd8ff794b8bc4dac3be1
Nicholas Henry <nicholas.henry@gmail.com>
2015-10-26 14:32:42 -0400

    Remove old artifacts of managing nested attributes in PatientController
    - also includes a code reformatting of the patients controller
    - also reformats patient related views

--- app/controllers/renalware/patients_controller.rb
+++ app/controllers/renalware/patients_controller.rb
@@ -3,19 +3,19 @@ module Renalware
     include Renalware::Concerns::Pageable
 
     skip_after_action :verify_authorized, only: :show
-
-    before_filter :prepare_paging, only: [:index]
-
+    before_action :prepare_paging, only: [:index]
     before_action :find_patient, only: [:show, :edit, :update]
 
     def new
       @patient = Patient.new
+
       authorize @patient
     end
 
     def create
       @patient = Patient.new(patient_params)
       authorize @patient
+
       if @patient.save
         redirect_to patient_path(@patient), notice: "You have successfully added a new patient."
       else
@@ -29,28 +29,35 @@ module Renalware
 
     def update
       authorize @patient
+
       if @patient.update(patient_params)
-        redirect_to params[:redirect_url] || patient_clinical_summary_path(@patient), notice: params[:message]
+        redirect_to patient_clinical_summary_path(@patient),
+          notice: "You have successfully updated a patient's demographics"
       else
-        render params[:template] || :edit
+        render :edit
       end
     end
 
     def index
       @patients = @patient_search.result.page(@page).per(@per_page)
+
       authorize @patients
     end
 
     private
+
     def patient_params
-      params.require(:patient).permit(:nhs_number, :local_patient_id, :surname,
-        :forename, :sex, :ethnicity_id, :birth_date, :paediatric_patient_indicator,
+      params.require(:patient).permit(
+        :nhs_number, :local_patient_id, :surname, :forename, :sex,
+        :ethnicity_id, :birth_date, :paediatric_patient_indicator,
         :gp_practice_code, :pct_org_code, :hosp_centre_code, :primary_esrf_centre,
-        current_address_attributes: [:street_1, :street_2, :county, :country, :city, :postcode],
-        address_at_diagnosis_attributes: [:street_1, :street_2, :county, :country, :city, :postcode],
-        medications_attributes: [:id, :medicatable_id, :medicatable_type, :dose, :medication_route_id,
-        :frequency, :notes, :start_date, :end_date, :provider, :_destroy]
-        )
+        current_address_attributes: [
+          :street_1, :street_2, :county, :country, :city, :postcode
+        ],
+        address_at_diagnosis_attributes: [
+          :street_1, :street_2, :county, :country, :city, :postcode
+        ]
+      )
     end
 
     def find_patient
