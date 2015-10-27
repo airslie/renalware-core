dc88c5d8cd25bb6382c1d41faaf80d5c77672df2
op48 <lianne.tan@gmail.com>
2015-10-27 17:05:36 +0000

    Simplified listing patients feature spec, replaced single quotes with double quotes.

--- spec/features/renalware/listing_patients_spec.rb
+++ spec/features/renalware/listing_patients_spec.rb
@@ -1,25 +1,25 @@
-require 'rails_helper'
+require "rails_helper"
 
 module Renalware
-  feature 'Listing patients' do
+  feature "Listing patients" do
     background do
-      create(:patient, surname: 'Alpha', forename: 'Johnny')
-      create(:patient, surname: 'Bravo', forename: 'Betty')
-      create(:patient, surname: 'Bravo', forename: 'Juliette')
-      create(:patient, surname: 'Delta', forename: 'Dave')
-      create(:patient, surname: 'Zebra', forename: 'Zoe')
+      create(:patient, surname: "Alpha", forename: "Johnny")
+      create(:patient, surname: "Bravo", forename: "Betty")
+      create(:patient, surname: "Bravo", forename: "Juliette")
+      create(:patient, surname: "Delta", forename: "Dave")
+      create(:patient, surname: "Zebra", forename: "Zoe")
 
       login_as_clinician
     end
 
     scenario 'Viewing the first page of patients' do
-      visit '/patients?per_page=4'
-      expect(page).to have_css('.pagination')
+      visit "/patients?per_page=4"
+      expect(page).to have_css(".pagination")
 
-      within('table.patients tbody') do
-        expect(page).to have_css('tr:first-child td', text: 'Alpha, Johnny')
-        expect(page).to have_css('tr:nth-child(2) td', text: 'Bravo, Betty')
-        expect(page).to have_css('tr:nth-child(3) td', text: 'Bravo, Juliette')
+      within("#patients") do
+        expect(page).to have_content("Alpha, Johnny")
+        expect(page).to have_content("Bravo, Betty")
+        expect(page).to have_content("Bravo, Juliette")
       end
     end
   end
