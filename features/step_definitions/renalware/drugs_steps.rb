ec2799efcc91d625814ddd116aac1f1e5e3fc9d1
Nicholas Henry <nicholas.henry@gmail.com>
2015-10-27 15:25:03 -0400

    Move drug management features into correct name spaces

--- features/step_definitions/renalware/drugs_steps.rb
+++ features/step_definitions/renalware/drugs_steps.rb
@@ -74,4 +74,14 @@ Then(/^I should see the drug removed from the drugs list$/) do
   expect(page).to have_no_content("Vancomycin")
 end
 
+When(/^I search for a drug by name$/) do
+  fill_in "Drug", :with => "Red"
 
+  page.execute_script %Q($('.find_drug').trigger('keyup'))
+end
+
+Then(/^they should see the list of drugs listed in the dropdown$/) do
+  within('#new-form .drug-results') do
+    expect(page).to have_css("li", :text => "Red")
+  end
+end
