c9f43bea651004f2fbf0af9c38656eb0abf7efe3
op48 <lianne.tan@gmail.com>
2015-10-28 10:52:21 +0000

    Fixed rebase conflict for modality attribute name.

--- app/models/renalware/modality.rb
+++ app/models/renalware/modality.rb
@@ -9,7 +9,7 @@ module Renalware
 
     validates :start_date, presence: true
 
-    scope :ordered, -> { order(termination_date: :desc) }
+    scope :ordered, -> { order(end_date: :desc) }
 
     def transfer!(attrs)
       transaction do
