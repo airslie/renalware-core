6e97d9270579021f0f19942f0a8d7a1fbce4d641
Hugo Frappier <hugo@cekoya.com>
2015-11-02 17:44:10 -0500

    Add validation on name

--- app/models/renalware/transplants/registration_status_description.rb
+++ app/models/renalware/transplants/registration_status_description.rb
@@ -3,6 +3,7 @@ require_dependency "renalware/transplants"
 module Renalware
   module Transplants
     class RegistrationStatusDescription < ActiveRecord::Base
+      validates :name, presence: true
     end
   end
 end
