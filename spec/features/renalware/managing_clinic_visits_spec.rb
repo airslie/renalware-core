1e752796f94bfadd623d2d720228467610794894
Nicholas Henry <nicholas.henry@gmail.com>
2015-11-03 16:02:05 -0500

    Fix style violations

--- spec/features/renalware/managing_clinic_visits_spec.rb
+++ spec/features/renalware/managing_clinic_visits_spec.rb
@@ -1,49 +1,50 @@
-require 'rails_helper'
+require "rails_helper"
 
 module Renalware
-  feature 'Managing clinic visits' do
+  feature "Managing clinic visits" do
     background do
       @patient = create(:patient)
       login_as_clinician
     end
 
-    scenario 'Adding a clinic visit' do
+    scenario "Adding a clinic visit" do
       visit new_patient_clinic_visit_path(@patient)
 
-      fill_in 'Height', with: '1.78'
-      fill_in 'Weight', with: '82.5'
-      fill_in 'Blood Pressure', with: '110/75'
+      fill_in "Height", with: "1.78"
+      fill_in "Weight", with: "82.5"
+      fill_in "Blood Pressure", with: "110/75"
 
-      click_on 'Save'
+      click_on "Save"
 
-      within('.clinics tbody tr:first-child') do
-        within('.bmi') do
-          expect(page).to have_content('26.04')
+      within(".clinics tbody tr:first-child") do
+        within(".bmi") do
+          expect(page).to have_content("26.04")
         end
-        within('.bp') do
-          expect(page).to have_content('110/75')
+        within(".bp") do
+          expect(page).to have_content("110/75")
         end
       end
     end
 
-    scenario 'Editing a clinic visit' do
+    scenario "Editing a clinic visit" do
       clinic_visit = create(:clinic_visit)
       @patient.clinic_visits << clinic_visit
 
-      visit edit_patient_clinic_visit_path(patient_id: @patient.to_param, id: clinic_visit.to_param)
+      visit edit_patient_clinic_visit_path(patient_id: @patient.to_param,
+                                           id: clinic_visit.to_param)
 
-      fill_in 'Height', with: '1.62'
-      fill_in 'Weight', with: '95'
-      fill_in 'Blood Pressure', with: '128/95'
+      fill_in "Height", with: "1.62"
+      fill_in "Weight", with: "95"
+      fill_in "Blood Pressure", with: "128/95"
 
-      click_on 'Update'
+      click_on "Update"
 
-      within('.clinics tbody tr:first-child') do
-        within('.bmi') do
-          expect(page).to have_content('36.2')
+      within(".clinics tbody tr:first-child") do
+        within(".bmi") do
+          expect(page).to have_content("36.2")
         end
-        within('.bp') do
-          expect(page).to have_content('128/95')
+        within(".bp") do
+          expect(page).to have_content("128/95")
         end
       end
 
