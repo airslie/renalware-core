370dce2d102e984c65d6b9fffa7655a42daa9511
Nicholas Henry <nicholas.henry@gmail.com>
2015-11-03 15:22:13 -0500

    Enable antibiotics drugs for peritonitis episodes

--- app/models/renalware/drugs/type.rb
+++ app/models/renalware/drugs/type.rb
@@ -5,7 +5,14 @@ module Renalware
     class Type < ActiveRecord::Base
       self.table_name = "drug_types"
 
-      has_and_belongs_to_many :drugs
+      has_and_belongs_to_many :drugs, foreign_key: :drug_type_id
+
+      include OrderedSetScope
+
+      def self.for(*codes)
+        includes(:drugs)
+          .ordered_set(:code, codes)
+      end
     end
   end
 end
