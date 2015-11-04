ff6d4089269a589d02d0553c46ef42076d27deb7
Nicholas Henry <nicholas.henry@gmail.com>
2015-11-04 14:27:47 -0500

    Skip authorization on viewing modality reasons, not required

--- app/controllers/renalware/modalities/reasons_controller.rb
+++ app/controllers/renalware/modalities/reasons_controller.rb
@@ -3,17 +3,14 @@ require_dependency "renalware/modalities"
 module Renalware
   module Modalities
     class ReasonsController < BaseController
+      skip_after_action :verify_authorized
 
       def index
         @reason_hd_pd = HaemodialysisToPD.all
-        authorize @reason_hd_pd
-
         @reason_pd_hd = PDToHaemodialysis.all
-        authorize @reason_pd_hd
 
         type = params[:modal_change_type] ? "Renalware::Modalities::#{params[:modal_change_type]}" : nil
         @modality_reason_select = Reason.where(type: type)
-        authorize @modality_reason_select
 
         respond_to do |format|
           format.html
