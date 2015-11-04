d7666a0aa662e94adfcec4c0677471f466cc7acf
Hugo Frappier <hugo@cekoya.com>
2015-11-04 17:44:55 -0500

    Remove unneeded association

--- app/models/renalware/transplants/registration_status.rb
+++ app/models/renalware/transplants/registration_status.rb
@@ -3,7 +3,6 @@ require_dependency "renalware/transplants"
 module Renalware
   module Transplants
     class RegistrationStatus < ActiveRecord::Base
-      belongs_to :registration
       belongs_to :description, class_name: "RegistrationStatusDescription"
 
       scope :ordered, -> (direction=:desc) { order(started_on: direction) }
