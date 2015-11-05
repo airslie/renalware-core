3482e756a7fca65c05863fd8d9dd5e62fc8889cb
Nicholas Henry <nicholas.henry@gmail.com>
2015-11-05 17:45:13 -0500

    Add `Blameable` concern to track creators and updaters of a model

---
+++ app/models/concerns/renalware/blameable.rb
@@ -0,0 +1,30 @@
+require_dependency 'renalware'
+
+module Renalware
+  module Blameable
+    extend ActiveSupport::Concern
+
+    included do
+      belongs_to :created_by, class_name: "Renalware::User"
+      belongs_to :updated_by, class_name: "Renalware::User"
+
+      before_create :assign_creator
+      before_update :assign_updater
+    end
+
+    private
+
+    def assign_creator
+      self.created_by_id ||= PaperTrail.whodunnit
+      self.updated_by_id = created_by_id
+    end
+
+    def assign_updater
+      return unless persisted?
+      return if updated_by_id_changed?
+      return if PaperTrail.whodunnit.blank?
+
+      self.updated_by_id = PaperTrail.whodunnit
+    end
+  end
+end
