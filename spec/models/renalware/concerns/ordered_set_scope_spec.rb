5306860e1bc5da964b03171aa97b702f5a59142b
Nicholas Henry <nicholas.henry@gmail.com>
2015-11-05 18:16:41 -0500

    Fix failing test due to common test class name
    This requires a better fix by storing the test class in a local variable.

--- spec/models/renalware/concerns/ordered_set_scope_spec.rb
+++ spec/models/renalware/concerns/ordered_set_scope_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 module Renalware
   describe "Ordered Set scope" do
-    class ::Qux < ActiveRecord::Base
+    class ::Baz < ActiveRecord::Base
       include OrderedSetScope
     end
 
@@ -11,7 +11,7 @@ module Renalware
         create_relation
 
         %w(baz bar foo).each do |value|
-          Qux.create(code: value)
+          Baz.create(code: value)
         end
       end
 
@@ -20,7 +20,7 @@ module Renalware
 
         it "returns an ordered scope with the attribute and values specified" do
           attribute = :code
-          actual_scope = Qux.ordered_set(attribute, ordered_values)
+          actual_scope = Baz.ordered_set(attribute, ordered_values)
 
           expect(actual_scope).to be_a ActiveRecord::Relation
           expect(actual_scope.map(&:code)).to eq(ordered_values)
@@ -32,7 +32,7 @@ module Renalware
 
         it "returns an ordered scope with the values that exist in the relation"  do
           attribute = :code
-          actual_scope = Qux.ordered_set(attribute, ordered_values)
+          actual_scope = Baz.ordered_set(attribute, ordered_values)
 
           expect(actual_scope).to be_a ActiveRecord::Relation
           expect(actual_scope.map(&:code)).to eq(%w(foo bar))
@@ -44,7 +44,7 @@ module Renalware
       ActiveRecord::Schema.define do
         self.verbose = false
 
-        create_table :quxes, force: true do |t|
+        create_table :bazs, force: true do |t|
           t.string :code
         end
       end
