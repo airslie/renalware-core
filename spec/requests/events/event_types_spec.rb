3378e2d93afaefb9a61a966f4bcf78c08c8df489
op48 <lianne.tan@gmail.com>
2015-11-06 15:14:04 +0000

    Added tests for edit/update and destroy within event types request spec.

--- spec/requests/events/event_types_spec.rb
+++ spec/requests/events/event_types_spec.rb
@@ -1,6 +1,9 @@
 require "rails_helper"
 
 RSpec.describe "Configuring Event Types", type: :request do
+  before do
+    @event_type = create(:events_type)
+  end
 
   it "creates an event type" do
     get new_events_type_path
@@ -16,4 +19,31 @@ RSpec.describe "Configuring Event Types", type: :request do
     follow_redirect!
     expect(response).to have_http_status(:success)
   end
+
+  it "updates an event type" do
+    get edit_events_type_path(@event_type.id)
+    expect(response).to have_http_status(:success)
+
+    attributes = {name: "My Edited Event"}
+    put events_type_path, events_type: attributes
+    expect(response).to have_http_status(:redirect)
+
+    event_type = Renalware::Events::Type.find_by(attributes)
+    expect(event_type).to be_present
+
+    follow_redirect!
+    expect(response).to have_http_status(:success)
+  end
+
+  it "destroys an event type" do
+    delete events_type_path(@event_type.id)
+    expect(response).to have_http_status(:redirect)
+
+    event_type = Renalware::Events::Type.find_by(id: @event_type.id)
+    expect(event_type).to_not be_present
+
+    follow_redirect!
+    expect(response).to have_http_status(:success)
+  end
+
 end
