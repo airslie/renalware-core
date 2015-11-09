27d489bb4212e7afe21b065f030b9c6f3715a18d
Nicholas Henry <nicholas.henry@gmail.com>
2015-11-09 11:11:11 -0500

    Extract specs for the seven controller actions, testing error path as well

--- spec/requests/events/event_types_spec.rb
+++ spec/requests/events/event_types_spec.rb
@@ -1,49 +1,83 @@
 require "rails_helper"
 
 RSpec.describe "Configuring Event Types", type: :request do
-  before do
-    @event_type = create(:events_type)
+  let(:event_type) { create(:events_type) }
+
+  describe "GET new" do
+    it "responds with a form" do
+      get new_events_type_path
+
+      expect(response).to have_http_status(:success)
+    end
   end
 
-  it "creates an event type" do
-    get new_events_type_path
-    expect(response).to have_http_status(:success)
+  describe "POST create" do
+    context "given valid attributes" do
+      it "creates a new record" do
+        attributes = {name: "My Event"}
+        post events_types_path, events_type: attributes
+        expect(response).to have_http_status(:redirect)
 
-    attributes = {name: "My Event"}
-    post events_types_path, events_type: attributes
-    expect(response).to have_http_status(:redirect)
+        event_type = Renalware::Events::Type.find_by(attributes)
+        expect(event_type).to be_present
 
-    event_type = Renalware::Events::Type.find_by(attributes)
-    expect(event_type).to be_present
+        follow_redirect!
+        expect(response).to have_http_status(:success)
+      end
+    end
 
-    follow_redirect!
-    expect(response).to have_http_status(:success)
+    context "given invalid attributes" do
+      it "responds with form" do
+        attributes = {name: ""}
+        post events_types_path, events_type: attributes
+
+        expect(response).to have_http_status(:success)
+      end
+    end
   end
 
-  it "updates an event type" do
-    get edit_events_type_path(@event_type.id)
-    expect(response).to have_http_status(:success)
+  describe "GET edit" do
+    it "responds with a form" do
+      get edit_events_type_path(event_type)
 
-    attributes = {name: "My Edited Event"}
-    put events_type_path, events_type: attributes
-    expect(response).to have_http_status(:redirect)
-
-    event_type = Renalware::Events::Type.find_by(attributes)
-    expect(event_type).to be_present
-
-    follow_redirect!
-    expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:success)
+    end
   end
 
-  it "destroys an event type" do
-    delete events_type_path(@event_type.id)
-    expect(response).to have_http_status(:redirect)
+  describe "PATCH update" do
+    context "given valid attributes" do
+      it "updates a record" do
+        attributes = {name: "My Edited Event"}
+        patch events_type_path(event_type), events_type: attributes
+        expect(response).to have_http_status(:redirect)
 
-    event_type = Renalware::Events::Type.find_by(id: @event_type.id)
-    expect(event_type).to_not be_present
+        event_type = Renalware::Events::Type.find_by(attributes)
+        expect(event_type).to be_present
 
-    follow_redirect!
-    expect(response).to have_http_status(:success)
+        follow_redirect!
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context "given invalid attributes" do
+      it "responds with a form" do
+        attributes = {name: ""}
+        patch events_type_path(event_type), events_type: attributes
+        expect(response).to have_http_status(:success)
+      end
+    end
   end
 
+  describe "DELETE destroy" do
+    it "deletes the event type" do
+      delete events_type_path(event_type)
+      expect(response).to have_http_status(:redirect)
+
+      deleted_event_type = Renalware::Events::Type.find_by(id: event_type.id)
+      expect(deleted_event_type).to_not be_present
+
+      follow_redirect!
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
