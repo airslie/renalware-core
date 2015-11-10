83b216e4ee737e775c8363ade1457001b70b079e
Nicholas Henry <nicholas.henry@gmail.com>
2015-11-10 16:10:37 -0500

    Extract death steps

--- features/step_definitions/clinical_summary_steps.rb
+++ features/step_definitions/clinical_summary_steps.rb
@@ -1,7 +1,3 @@
 Given(/^they are on a patient's clinical summary$/) do
   visit patient_clinical_summary_path(@patient_1)
 end
-
-Then(/^I should see a patient's modality on their clinical summary$/) do
-   expect(page).to have_content("Modal One")
-end
--- features/step_definitions/renalware/death_steps.rb
+++ features/step_definitions/renalware/death_steps.rb
@@ -3,6 +3,25 @@ Given(/^there are edta causes of death in the database$/) do
   FactoryGirl.create(:edta_code, code: 200, :death_cause => "Death cause two")
 end
 
+When(/^I select death modality$/) do
+  within "#modality-description-select" do
+    select "Death"
+  end
+
+  select "2015", from: "modality_started_on_1i"
+  select "April", from: "modality_started_on_2i"
+  select "1", from: "modality_started_on_3i"
+
+  click_on "Save"
+end
+
+Then(/^I should see the patient's current modality set as death with set date$/) do
+  visit patient_modalities_path(@patient_1)
+
+  expect(page).to have_content("Death")
+  expect(page).to have_content("01/04/2015")
+end
+
 Then(/^I should see the date of death and causes of death in the patient's demographics$/) do
   visit patient_path(@patient_1)
   expect(page).to have_content("22/09/2014")
