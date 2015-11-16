f7e3925ab310776cd4e9db86b44a82f6f8eb978d
Hugo Frappier <hugo@cekoya.com>
2015-11-16 15:50:34 -0500

    Add workup summary to dashboards

--- app/controllers/renalware/transplants/recipient_dashboards_controller.rb
+++ app/controllers/renalware/transplants/recipient_dashboards_controller.rb
@@ -4,6 +4,7 @@ module Renalware
       before_filter :load_patient
 
       def show
+        @recipient_workup = RecipientWorkup.for_patient(@patient).first_or_initialize
         @registration = Registration.for_patient(@patient).first_or_initialize
         @recipient_operations = RecipientOperation.for_patient(@patient).ordered(:desc)
       end
