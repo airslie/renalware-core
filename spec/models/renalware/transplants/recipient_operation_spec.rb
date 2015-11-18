21a4a167bbd02508cd9040d6d2cf13745cfa9f96
Hugo Frappier <hugo@cekoya.com>
2015-11-18 17:02:23 -0500

    Use is_expected.to instead of should

--- spec/models/renalware/transplants/recipient_operation_spec.rb
+++ spec/models/renalware/transplants/recipient_operation_spec.rb
@@ -5,14 +5,13 @@ module Renalware
     describe RecipientOperation do
       let(:clinician) { create(:user, :clinician) }
 
-      it { should belong_to :patient }
-      it { should validate_presence_of :performed_on }
-      it { should validate_presence_of :theatre_case_start_time }
-      it { should validate_presence_of :donor_kidney_removed_from_ice_at }
-      it { should validate_presence_of :kidney_perfused_with_blood_at }
-      it { should validate_presence_of :operation_type }
-      it { should validate_presence_of :transplant_site }
-      it { should validate_presence_of :cold_ischaemic_time }
+      it { is_expected.to validate_presence_of(:performed_on) }
+      it { is_expected.to validate_presence_of(:theatre_case_start_time) }
+      it { is_expected.to validate_presence_of(:donor_kidney_removed_from_ice_at) }
+      it { is_expected.to validate_presence_of(:kidney_perfused_with_blood_at) }
+      it { is_expected.to validate_presence_of(:operation_type) }
+      it { is_expected.to validate_presence_of(:transplant_site) }
+      it { is_expected.to validate_presence_of(:cold_ischaemic_time) }
     end
   end
 end
