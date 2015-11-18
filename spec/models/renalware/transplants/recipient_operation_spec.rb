a5db735b65c8866bce770c9c233e652eb29d673b
Hugo Frappier <hugo@cekoya.com>
2015-11-18 17:24:06 -0500

    Add specs for validations in AR models

--- spec/models/renalware/transplants/recipient_operation_spec.rb
+++ spec/models/renalware/transplants/recipient_operation_spec.rb
@@ -12,6 +12,12 @@ module Renalware
       it { is_expected.to validate_presence_of(:operation_type) }
       it { is_expected.to validate_presence_of(:transplant_site) }
       it { is_expected.to validate_presence_of(:cold_ischaemic_time) }
+
+      it { is_expected.to validate_timeliness_of(:donor_kidney_removed_from_ice_at) }
+      it { is_expected.to validate_timeliness_of(:donor_kidney_removed_from_ice_at) }
+      it { is_expected.to validate_timeliness_of(:kidney_perfused_with_blood_at) }
+      it { is_expected.to validate_timeliness_of(:theatre_case_start_time) }
+      it { is_expected.to validate_timeliness_of(:cold_ischaemic_time) }
     end
   end
 end
