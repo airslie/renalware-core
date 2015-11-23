47bc9c284f9de44109e513224a321ec7551f9143
Hugo Frappier <hugo@cekoya.com>
2015-11-23 13:00:42 -0500

    Fix violations

--- features/support/autocomplete.rb
+++ features/support/autocomplete.rb
@@ -1,5 +1,4 @@
 module AutocompleteHelpers
-
   def fill_autocomplete(field, options = {})
     fill_in field, with: options[:with]
 
@@ -7,10 +6,9 @@ module AutocompleteHelpers
     page.execute_script %Q{ $('##{field}').trigger('keydown') }
     selector = %Q{ul.ui-autocomplete li.ui-menu-item:contains("#{options[:select]}")}
 
-    page.should have_selector('ul.ui-autocomplete li.ui-menu-item')
+    page.should have_selector("ul.ui-autocomplete li.ui-menu-item")
     page.execute_script %Q{ $('#{selector}').trigger('mouseenter').click() }
   end
-
 end
 
 World(AutocompleteHelpers)
--- features/support/javascript_driver.rb
+++ features/support/javascript_driver.rb
@@ -4,7 +4,8 @@ require 'capybara/poltergeist'
 # to pause the test and launch a browser which gives you the WebKit inspector
 # to view your test run with.
 Capybara.register_driver :poltergeist_debug do |app|
-  Capybara::Poltergeist::Driver.new(app,
+  Capybara::Poltergeist::Driver.new(
+    app,
     inspector: "open",
     debug: true
   )
