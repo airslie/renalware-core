780da3e1854a280d295690d85c8c13320d46c70a
op48 <lianne.tan@gmail.com>
2015-11-23 10:30:03 +0000

    Created change_type.rb to store reason for change modality type options. Applied to modalities new form.

---
+++ app/models/renalware/modalities/change_type.rb
@@ -0,0 +1,7 @@
+module Renalware
+  module Modalities
+    class ChangeType
+      OPTIONS = [["Other", "Other"], ["Haemodialysis To PD", "HaemodialysisToPD"], ["PD To Haemodialysis", "PDToHaemodialysis"]]
+    end
+  end
+end
