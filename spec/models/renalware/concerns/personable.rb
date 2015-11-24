f060c79e97d4bca086b7abe7e4b8939d08a3a8f8
op48 <lianne.tan@gmail.com>
2015-11-24 17:43:34 +0000

    Updated personable.rb to use given/family name, renamed :last_first to use :reversed_full_name - updated related files.

--- spec/models/renalware/concerns/personable.rb
+++ spec/models/renalware/concerns/personable.rb
@@ -10,21 +10,21 @@ shared_examples_for 'Personable' do
     end
     it 'requires the presence of first_name' do
       expect(@instance.valid?).to be false
-      expect(@instance.errors[:first_name]).not_to be_empty
+      expect(@instance.errors[:given_name]).not_to be_empty
     end
     it 'requires the presence of last_name' do
       expect(@instance.valid?).to be false
-      expect(@instance.errors[:last_name]).not_to be_empty
+      expect(@instance.errors[:family_name]).not_to be_empty
     end
   end
 
   describe 'to_s' do
-    let(:instance) { subject.new(first_name: 'Aneurin', last_name: 'Bevan') }
+    let(:instance) { subject.new(given_name: 'Aneurin', family_name: 'Bevan') }
     it 'accepts the :full_name format' do
       expect(instance.to_s(:full_name)).to eq('Aneurin Bevan')
     end
-    it 'accepts the :last_first format' do
-      expect(instance.to_s(:last_first)).to eq('Bevan, Aneurin')
+    it 'accepts the :reversed_full_name format' do
+      expect(instance.to_s(:reversed_full_name)).to eq('Bevan, Aneurin')
     end
     it 'defaults to Class#to_s' do
       expect(instance.to_s).to eq(instance.orig_to_s)
