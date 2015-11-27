aced895d76ad02c9b5c845bb2b833f3dc6434b26
op48 <lianne.tan@gmail.com>
2015-11-27 17:58:40 +0000

    Added null: false to drugs migration required fields.

--- db/migrate/20141107145549_create_drugs.rb
+++ db/migrate/20141107145549_create_drugs.rb
@@ -1,7 +1,7 @@
 class CreateDrugs < ActiveRecord::Migration
   def change
     create_table :drugs do |t|
-      t.string :name
+      t.string :name, null: false
       t.datetime :deleted_at
       t.timestamps
     end
