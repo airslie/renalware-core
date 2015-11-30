30e8bb721fbb30c11647816b1f0e4d1032b914fa
op48 <lianne.tan@gmail.com>
2015-11-30 17:35:52 +0000

    Added missing validation for patient in problems.

--- spec/models/renalware/problems/problem_spec.rb
+++ spec/models/renalware/problems/problem_spec.rb
@@ -3,5 +3,7 @@ require 'rails_helper'
 module Renalware::Problems
   describe Problem, :type => :model do
     it { should belong_to :patient }
+
+    it { should validate_presence_of :patient }
   end
 end
