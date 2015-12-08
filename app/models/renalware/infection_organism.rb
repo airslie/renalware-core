70d0971423744a0674473348ffa6a9a85a5af708
op48 <lianne.tan@gmail.com>
2015-12-08 18:50:23 +0000

    Shortened line.

--- app/models/renalware/infection_organism.rb
+++ app/models/renalware/infection_organism.rb
@@ -4,7 +4,9 @@ module Renalware
     belongs_to :organism_code
     belongs_to :infectable, :polymorphic => true
 
-    validates :organism_code, uniqueness: { scope: [:infectable_id, :infectable_type] }, presence: true
+    validates :organism_code, uniqueness: {
+      scope: [:infectable_id, :infectable_type]
+    }, presence: true
 
   end
 end
