5774755367b30caf6f0e766b2eb8a7dac950f310
Hugo Frappier <hugo@cekoya.com>
2015-12-09 14:27:01 -0500

    Improve readability

--- spec/support/translations.rb
+++ spec/support/translations.rb
@@ -2,8 +2,8 @@ module TranslationsHelper
   def with_translations(locale, translations)
     original_backend = I18n.backend
 
-    I18n.backend = I18n::Backend::KeyValue.new Hash.new, true
-    I18n.backend.store_translations locale, translations
+    I18n.backend = I18n::Backend::KeyValue.new(Hash.new, true)
+    I18n.backend.store_translations(locale, translations)
 
     yield
   ensure
