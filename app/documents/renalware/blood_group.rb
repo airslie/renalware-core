94c80a0f2438e94b83e0717357dda8e65207bdad
Hugo Frappier <hugo@cekoya.com>
2015-12-10 14:40:22 -0500

    Use class methods to get list of valid values for a DDL

--- app/documents/renalware/blood_group.rb
+++ app/documents/renalware/blood_group.rb
@@ -1,8 +1,10 @@
 module Renalware
   class BloodGroup < NestedAttribute
-    OPTIONS = %w(A A+ A- B B+ B- AB AB+ AB- O O+ O-)
-
     attribute :value, String
+
+    def self.valid_values
+      %w(A A+ A- B B+ B- AB AB+ AB- O O+ O-)
+    end
 
     def to_s
       value
--- app/documents/renalware/year_dated_confirmation.rb
+++ app/documents/renalware/year_dated_confirmation.rb
@@ -6,6 +6,10 @@ module Renalware
     validates :confirmed_on_year, numericality: { allow_blank: true, only_integer: true }
     validates :confirmed_on_year, presence: true, if: ->(o) { o.status.try(:yes?) }
 
+    def self.valid_years
+      (1900..Time.zone.today.year).to_a
+    end
+
     def to_s
       datestamp = confirmed_on_year.present? ? "(#{confirmed_on_year})" : nil
       [status.try(:text), datestamp].compact.join(" ")
