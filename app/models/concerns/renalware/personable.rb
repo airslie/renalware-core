4e37650e6555ba39870b12ac7b65ed3c179b426d
op48 <lianne.tan@gmail.com>
2015-12-18 16:45:02 +0000

    Removed alias orig_to_s method and related test from personable, updated same test to use changed 'full_name' method.

--- app/models/concerns/renalware/personable.rb
+++ app/models/concerns/renalware/personable.rb
@@ -11,16 +11,12 @@ module Renalware
         "#{given_name} #{family_name}"
       end
 
-      alias_method :orig_to_s, :to_s
-
       def to_s(format=:default)
         case format
         when :default
           "#{family_name}, #{given_name}"
         when :long
           "#{family_name}, #{given_name} (#{nhs_number})"
-        else
-          orig_to_s
         end
       end
     end
