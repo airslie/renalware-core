5eb6e5e21155745196087214162d1efb429d5532
Hugo Frappier <hugo@cekoya.com>
2016-01-07 13:51:18 -0500

    Allow adding a modality on the same day

--- spec/models/renalware/modalities/modality_spec.rb
+++ spec/models/renalware/modalities/modality_spec.rb
@@ -39,6 +39,20 @@ module Renalware
           end
         end
 
+        context "given start date is the same as the previous start date" do
+          subject do
+            build(:modality,
+              patient: patients_modality.patient,
+              started_on: Date.parse("2015-04-01")
+            )
+          end
+
+          it "has a valid start date" do
+            subject.valid?
+            expect(subject.errors[:started_on]).to be_empty
+          end
+        end
+
         context "given start date is not later than previous start date" do
           subject do
             build(:modality,
