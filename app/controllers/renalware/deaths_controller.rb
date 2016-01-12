ca76e4513b6685734e78f811e1a0eb447a7a2884
op48 <lianne.tan@gmail.com>
2016-01-12 18:02:31 +0000

    Added missing notices for deaths_controller.

--- app/controllers/renalware/deaths_controller.rb
+++ app/controllers/renalware/deaths_controller.rb
@@ -12,6 +12,7 @@ module Renalware
         redirect_to patient_path(@patient),
           notice: t(".success", model_name: "patient")
       else
+        flash[:error] = t(".failed", model_name: "patient")
         render :edit
       end
     end
