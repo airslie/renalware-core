a4bfd86ab2318badd5b9d25e1ba3377d7a4e1dc3
Hugo Frappier <hugo@cekoya.com>
2016-01-18 11:40:34 -0500

    Move comment and add link to Ransack doc

--- app/queries/renalware/transplants/registrations/wait_list_query.rb
+++ app/queries/renalware/transplants/registrations/wait_list_query.rb
@@ -28,17 +28,19 @@ module Renalware
 
         def query_for_filter(filter)
           case filter
+          when :nhb_consent
+            { nhb_consent_eq: "yes" }
           when :active
             { current_status_in: :active }
           when :suspended
             { current_status_in: :suspended }
           when :active_and_suspended
+            # note: array must be embedded in another array as mentionned here:
+            # https://github.com/activerecord-hackery/ransack#using-scopesclass-methods
             { current_status_in: [%w(active suspended)] }
           when :working_up
             { current_status_in: [%w(working_up working_up_lrf)] }
-          when :nhb_consent
-            { nhb_consent_eq: "yes" }
-          end # note: array must be embedded in another array as mentionned in the Ransack readme
+          end
         end
 
         class QueryableRegistration < ActiveType::Record[Registration]
