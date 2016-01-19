06d48e190f9e6b482e00405d6311961bbb11cc7e
op48 <lianne.tan@gmail.com>
2016-01-19 11:43:05 +0000

    Replaced single quotes with double quotes.

--- spec/helpers/renalware/medications_helper_spec.rb
+++ spec/helpers/renalware/medications_helper_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require "rails_helper"
 
 module Renalware
   RSpec.describe MedicationsHelper, :type => :helper do
@@ -6,8 +6,8 @@ module Renalware
       @patient = FactoryGirl.create(:patient)
       @med_route = FactoryGirl.create(:medication_route)
       @blue_pill = FactoryGirl.create(:drug)
-      @amoxicillin = FactoryGirl.create(:drug, name: 'Amoxicillin')
-      @penicillin = FactoryGirl.create(:drug, name: 'Penicillin')
+      @amoxicillin = FactoryGirl.create(:drug, name: "Amoxicillin")
+      @penicillin = FactoryGirl.create(:drug, name: "Penicillin")
       @invalid_patient_med = FactoryGirl.build(
                               :medication,
                               patient: @patient,
@@ -22,16 +22,16 @@ module Renalware
                             :medication,
                             patient: @patient,
                             drug: @amoxicillin,
-                            dose: '23mg',
+                            dose: "23mg",
                             medication_route: @med_route,
-                            frequency: 'PID',
-                            start_date: '02/10/2014',
+                            frequency: "PID",
+                            start_date: "02/10/2014",
                             provider: 0)
     end
 
-    describe 'drug_name' do
-      context 'drug present' do
-        it 'should return name' do
+    describe "drug_name" do
+      context "drug present" do
+        it "should return name" do
           patient_medication = FactoryGirl.build_stubbed(
             :medication,
             patient: @patient,
@@ -42,8 +42,8 @@ module Renalware
         end
       end
 
-      context 'drug not present' do
-        it 'should not return name' do
+      context "drug not present" do
+        it "should not return name" do
           patient_medication = FactoryGirl.build_stubbed(
             :medication,
             patient: @patient,
@@ -55,52 +55,52 @@ module Renalware
       end
     end
 
-    describe 'highlight_validation_fail' do
-      context 'with errors' do
-        it 'should apply hightlight' do
+    describe "highlight_validation_fail" do
+      context "with errors" do
+        it "should apply hightlight" do
           @invalid_patient_med.save
-          expect(highlight_validation_fail(@invalid_patient_med, :drug)).to eq('field_with_errors')
+          expect(highlight_validation_fail(@invalid_patient_med, :drug)).to eq("field_with_errors")
         end
       end
 
-      context 'with no errors' do
-        it 'should not apply highlight' do
+      context "with no errors" do
+        it "should not apply highlight" do
           @valid_patient_med.save
           expect(highlight_validation_fail(@valid_patient_med, :drug)).to eq(nil)
         end
       end
     end
 
-    describe 'validation_fail' do
-      context 'with errors' do
-        it 'should apply class "show-form"' do
+    describe "validation_fail" do
+      context "with errors" do
+        it "should apply class 'show-form'" do
           @invalid_patient_med.save
-          expect(validation_fail(@invalid_patient_med)).to eq('show-form')
+          expect(validation_fail(@invalid_patient_med)).to eq("show-form")
         end
       end
 
-      context 'with no errors' do
-        it 'should apply class "content"' do
+      context "with no errors" do
+        it "should apply class 'content'" do
           @valid_patient_med.save
-          expect(validation_fail(@valid_patient_med)).to eq('content')
+          expect(validation_fail(@valid_patient_med)).to eq("content")
         end
       end
     end
 
-    describe 'default_provider' do
+    describe "default_provider" do
       before do
         @gp = Medication.providers.keys[0]
         @hospital = Medication.providers.keys[1]
       end
 
-      context 'is gp' do
-        it 'should return "checked"' do
-          expect(default_provider(@gp)).to eq('checked')
+      context "is gp" do
+        it "should return 'checked'" do
+          expect(default_provider(@gp)).to eq("checked")
         end
       end
 
-      context 'is not gp' do
-        it 'should return nil' do
+      context "is not gp" do
+        it "should return nil" do
           expect(default_provider(@hospital)).to eq(nil)
         end
       end
