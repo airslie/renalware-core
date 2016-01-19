1be0bc9438ac3b17391282ed47e0187be47ad9af
Nicholas Henry <nicholas.henry@gmail.com>
2016-01-19 16:54:15 -0500

    Add missing organism and medication to episode
    - these are required when the episode is revised

--- features/step_definitions/renalware/peritonitis_episodes_steps.rb
+++ features/step_definitions/renalware/peritonitis_episodes_steps.rb
@@ -4,6 +4,21 @@ Given(/^Patty is being treated for a peritonitis episode$/) do
     user: @clyde,
     diagnosed_on: "10-10-2016"
   )
+
+  record_organism_for(
+    infectable: @patty.peritonitis_episodes.last!,
+    organism_name: "Acineobactor"
+  )
+
+  record_medication_for(
+    treatable: @patty.peritonitis_episodes.last!,
+    drug_name: "Ciprofloxacin Infusion",
+    dose: "100 ml",
+    route_name: "PO",
+    frequency: "once a day",
+    starts_on: "10-10-2015",
+    provider: "GP"
+  )
 end
 
 When(/^Clyde records a peritonitis episode for Patty$/) do
