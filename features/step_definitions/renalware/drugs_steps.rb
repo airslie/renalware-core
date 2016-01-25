27721a965f832d60aaafc66581e442a346830680
Nicholas Henry <nicholas.henry@gmail.com>
2016-01-25 16:37:59 -0500

    Remove explicit innovation of script
    This is handled by the poltergeist driver.

--- features/step_definitions/renalware/drugs_steps.rb
+++ features/step_definitions/renalware/drugs_steps.rb
@@ -80,8 +80,6 @@ end
 
 When(/^I search for a drug by name$/) do
   fill_in "Drug", :with => "Red"
-
-  page.execute_script %Q($('.find_drug').trigger('keyup'))
 end
 
 Then(/^they should see the list of drugs listed in the dropdown$/) do
