f7394cfdcdf403d6f2f126959d1e06155e845e17
Hugo Frappier <hugo@cekoya.com>
2016-02-01 11:42:19 -0500

    Remove preferences changes (other issue)

--- app/models/renalware/hd/preference_set.rb
+++
@@ -1,9 +0,0 @@
-require_dependency "renalware/hd"
-
-module Renalware
-  module HD
-    class PreferenceSet < ActiveRecord::Base
-      belongs_to :hospital_unit
-    end
-  end
-end
--- features/step_definitions/hd_steps.rb
+++ features/step_definitions/hd_steps.rb
@@ -1,15 +1,7 @@
-Given(/^Patty has HD preferences$/) do
-  set_up_hd_preferences_for(@patty)
-end
-
 Given(/^Patty has an HD profile$/) do
   pending # express the regexp above with the code you wish you had
 end
 
-
-When(/^Clyde records the HD preferences of Patty$/) do
-  pending # express the regexp above with the code you wish you had
-end
 
 When(/^Clyde records an HD profile for Patty$/) do
   pending # express the regexp above with the code you wish you had
@@ -19,14 +11,6 @@ When(/^Clyde submits an erroneous HD profile$/) do
   pending # express the regexp above with the code you wish you had
 end
 
-
-Then(/^Patty has new HD preferences$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-Then(/^Clyde can update Patty's HD preferences$/) do
-  pending # express the regexp above with the code you wish you had
-end
 
 Then(/^Patty has a new HD profile$/) do
   pending # express the regexp above with the code you wish you had
