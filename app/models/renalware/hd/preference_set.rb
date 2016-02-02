46fddd581b2aa90f2f9b530dd1e6ac42728aa78e
Hugo Frappier <hugo@cekoya.com>
2016-02-02 09:38:35 -0500

    Make validation condition more obvious

--- app/models/renalware/hd/preference_set.rb
+++ app/models/renalware/hd/preference_set.rb
@@ -15,9 +15,14 @@ module Renalware
       has_paper_trail class_name: "Renalware::HD::Version"
 
       validates :patient, presence: true
-      validates :other_schedule, presence: true,
-          if: ->(o) { o.schedule.try(:text) =~ /specify/ }
+      validates :other_schedule, presence: true, if: :other_schedule_required?
       validates :entered_on, timeliness: { type: :date, allow_blank: true }
+
+      private
+
+      def other_schedule_required?
+        schedule.try(:text) =~ /specify/
+      end
     end
   end
 end
