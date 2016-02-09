7d409c9285d4bf2fc7b124ce021e9f2b746d8892
Hugo Frappier <hugo@cekoya.com>
2016-02-09 15:25:22 -0500

    Dry weight recording feature

--- features/step_definitions/hd_steps.rb
+++ features/step_definitions/hd_steps.rb
@@ -6,6 +6,10 @@ Given(/^Patty has an HD profile$/) do
   set_up_hd_profile_for(@patty, @clyde)
 end
 
+Given(/^Patty has a dry weight entry$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
 
 When(/^Clyde records the HD preferences of Patty$/) do
   create_hd_preferences(patient: @patty, user: @clyde)
@@ -17,6 +21,14 @@ end
 
 When(/^Clyde submits an erroneous HD profile$/) do
   create_hd_profile(patient: @patty, user: @clyde, prescriber: nil)
+end
+
+When(/^Clyde records the dry weight for Patty$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^Clyde submits an erroneous dry weight$/) do
+  pending # express the regexp above with the code you wish you had
 end
 
 
@@ -38,4 +50,16 @@ end
 
 Then(/^the HD profile is not accepted$/) do
   expect_hd_profile_to_be_refused
+end
+
+Then(/^Patty has a new dry weight$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^Clyde can update Patty's dry weight entry$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the dry weight is not accepted$/) do
+  pending # express the regexp above with the code you wish you had
 end
