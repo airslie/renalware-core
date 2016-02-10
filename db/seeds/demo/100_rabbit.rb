fd17809a84df45298f3ea543081c3e2bbb13cf51
Hugo Frappier <hugo@cekoya.com>
2016-02-10 16:54:31 -0500

    Seed HD sessions

--- db/seeds/demo/100_rabbit.rb
+++ db/seeds/demo/100_rabbit.rb
@@ -204,4 +204,27 @@ module Renalware
     }
   }
   profile.save!
+
+  log '--------------------Assign HD sessions to Francois RABBIT-------------------'
+  units = Hospitals::Unit.hd_sites.limit(3).to_a
+  users = User.limit(3).to_a
+  start_times = ["13:00", "13:15", "13:30"]
+  end_times = ["15:30", "15:45", "16:00"]
+  20.times do |i|
+    session = HD::Session.new(
+      patient: patient,
+      hospital_unit: units.sample,
+      performed_on: (i*2).days.ago,
+      start_time: start_times.sample,
+      end_time: end_times.sample,
+      signed_on_by: users.sample,
+      signed_off_by: users.sample,
+      by: users.sample
+    )
+    if i == 0
+      session.end_time = nil
+      session.signed_off_by = nil
+    end
+    session.save!
+  end
 end
