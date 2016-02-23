17bb5a4c6f4b4e1024841fefb8b977cb24615a30
Nicholas Henry <nicholas.henry@gmail.com>
2016-02-23 16:26:36 -0500

    Extract route name into presenter

--- app/presenters/renalware/medications/medication_presenter.rb
+++ app/presenters/renalware/medications/medication_presenter.rb
@@ -3,6 +3,14 @@ require_dependency "renalware/medications"
 module Renalware
   module Medications
     class MedicationPresenter < DumbDelegator
+      def route_name
+        if medication_route.name == 'Route: Other (Please specify in notes)'
+          medication_route.full_name
+        else
+          medication_route.name
+        end
+      end
+
       def drug_types
         drug.drug_types.map(&:name).join(", ")
       end
