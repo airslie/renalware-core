a7fca5afef816cb02235949a2dc3ffc149c53562
Nicholas Henry <nicholas.henry@gmail.com>
2016-02-23 16:23:27 -0500

    Extract provider translation into presenter

--- app/presenters/renalware/medications/medication_presenter.rb
+++ app/presenters/renalware/medications/medication_presenter.rb
@@ -6,6 +6,10 @@ module Renalware
       def drug_types
         drug.drug_types.map(&:name).join(", ")
       end
+
+      def provider
+        ::I18n.t(super, scope: "enums.provider")
+      end
     end
   end
 end
