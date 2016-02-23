2bdb40ab43553f75c4cefec2238831f94bd5d10a
Nicholas Henry <nicholas.henry@gmail.com>
2016-02-23 13:08:22 -0500

    Formatting

--- app/controllers/renalware/medications_controller.rb
+++ app/controllers/renalware/medications_controller.rb
@@ -106,9 +106,8 @@ module Renalware
 
     def medication_params
       params.require(:medication).permit(
-        :drug_id,
-        :dose, :medication_route_id, :frequency, :notes,
-        :start_date, :end_date, :provider
+        :drug_id, :dose, :medication_route_id, :frequency,
+        :notes, :start_date, :end_date, :provider
       )
     end
 
