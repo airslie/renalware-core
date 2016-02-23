3b1574f872291c0ae6d20322eaaeed4b9c227aca
Nicholas Henry <nicholas.henry@gmail.com>
2016-02-23 16:37:49 -0500

    Formatting

--- app/models/renalware/medication_route.rb
+++ app/models/renalware/medication_route.rb
@@ -1,10 +1,11 @@
 module Renalware
   class MedicationRoute < ActiveRecord::Base
-
     has_many :medications
     has_many :patients, through: :medications
-    has_many :exit_site_infections, through: :medications, :source => :treatable, :source_type => "ExitSiteInfection"
-    has_many :peritonitis_episodes, through: :medications, :source => :treatable, :source_type => "PeritonitisEpisode"
+    has_many :exit_site_infections, through: :medications,
+      source: :treatable, source_type: "ExitSiteInfection"
+    has_many :peritonitis_episodes, through: :medications,
+      source: :treatable, source_type: "PeritonitisEpisode"
 
   end
 end
