31bb7e19bec86334f712588fca33dcc3344abdbe
Nicholas Henry <nicholas.henry@gmail.com>
2016-03-03 13:32:20 -0500

    Fix style violations

--- spec/models/renalware/feeds/message_parser_spec.rb
+++ spec/models/renalware/feeds/message_parser_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require "rails_helper"
 
 module Renalware::Feeds
   RSpec.describe MessageParser do
--- spec/models/renalware/feeds/message_spec.rb
+++ spec/models/renalware/feeds/message_spec.rb
@@ -1,7 +1,7 @@
-require 'rails_helper'
+require "rails_helper"
 
 module Renalware
-  RSpec.describe Feeds::Message, :type => :model do
+  RSpec.describe Feeds::Message, type: :model do
     it { should validate_presence_of :event_code }
     it { should validate_presence_of :body }
   end
