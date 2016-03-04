74e250b0d69e5806f5ae767365a3d4ad2b02ef80
Nicholas Henry <nicholas.henry@gmail.com>
2016-03-04 11:20:23 -0500

    Add job for delayed job

---
+++ app/jobs/feed_job.rb
@@ -0,0 +1,17 @@
+require_dependency 'renalware/feeds'
+
+# To create outside of Rails, insert into the `delayed_jobs` table:
+#
+#     handler: {see example.yml}
+#     run_at: {current_time}
+#
+# Note: raw_message for the handler must have line endings "\n"
+# To run delayed_jobs see e.g.
+# http://blog.andolasoft.com/2013/04/4-simple-steps-to-implement-delayed-job-in-rails.html
+# in devel use rake jobs:work
+#
+PathologyFeedJob = Struct.new(:raw_message) do
+  def perform
+    Renalware::Feeds::MessageProcessor.new.call(raw_message)
+  end
+end
