5c803bbb353985d3d644b616e8f951aa7aec0744
Nicholas Henry <nicholas.henry@gmail.com>
2016-03-14 13:32:52 -0400

    Use simplified rescue syntax

--- app/models/renalware/feeds/message_processor.rb
+++ app/models/renalware/feeds/message_processor.rb
@@ -4,17 +4,15 @@ module Renalware
   module Feeds
     class MessageProcessor
       def call(raw_message)
-        begin
-           message_payload = parse_message(raw_message)
-           persist_message(message_payload)
+         message_payload = parse_message(raw_message)
+         persist_message(message_payload)
 
-           params = parse_pathology_params(message_payload)
-           create_observations(params)
+         params = parse_pathology_params(message_payload)
+         create_observations(params)
 
-        rescue Exception => error
-          notify_exception(error)
-          raise error
-        end
+      rescue Exception => error
+        notify_exception(error)
+        raise error
       end
 
       private
