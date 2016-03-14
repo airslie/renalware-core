55dc09b0d350d9dd96a97b326dbc5f4233b09948
Nicholas Henry <nicholas.henry@gmail.com>
2016-03-14 16:51:00 -0400

    Only create patient if it does not exist
    Verifies against the hospital number

--- app/models/renalware/patients/create_patient.rb
+++ app/models/renalware/patients/create_patient.rb
@@ -4,7 +4,15 @@ module Renalware
   module Patients
     class CreatePatient
       def call(params)
-        ::Renalware::Patient.create!(params[:patient].merge(nhs_number: "TO-ADD-123"))
+        patient_params = params.fetch(:patient)
+        local_patient_id = patient_params.fetch(:local_patient_id)
+
+        create_patient_if_not_exist(patient_params, local_patient_id)
+      end
+
+      def create_patient_if_not_exist(patient_params, local_patient_id)
+        ::Renalware::Patient.create_with(patient_params.merge(nhs_number: "TO-ADD-123"))
+                            .find_or_create_by!(local_patient_id: local_patient_id)
       end
     end
   end
--- spec/models/renalware/patients/create_patient_spec.rb
+++ spec/models/renalware/patients/create_patient_spec.rb
@@ -3,10 +3,23 @@ require "rails_helper"
 module Renalware::Patients
   RSpec.describe CreatePatient do
     describe "#call" do
-      it "creates the patient" do
-        params = {patient: attributes_for(:patient)}
+      context "given a patient does not have the same hospital number" do
+        it "creates the patient" do
+          params = {patient: attributes_for(:patient)}
 
-        expect{subject.call(params)}.to change{::Renalware::Patient.count}.by(1)
+          expect{subject.call(params)}.to change{::Renalware::Patient.count}.by(1)
+        end
+      end
+
+      context "give a patient has the same hospital number" do
+        before do
+          create(:patient, local_patient_id: "SAME-12345")
+        end
+        it "does not create the patient" do
+          params = {patient: attributes_for(:patient).merge(local_patient_id: "SAME-12345")}
+
+          expect{subject.call(params)}.not_to change{::Renalware::Patient.count}
+        end
       end
     end
   end
