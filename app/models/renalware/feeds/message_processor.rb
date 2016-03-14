2fdeef1e320e90ed58615eb686e63a147423ef95
Nicholas Henry <nicholas.henry@gmail.com>
2016-03-14 13:23:50 -0400

    Extract private methods, makes the main method easier to read

--- app/models/renalware/feeds/message_processor.rb
+++ app/models/renalware/feeds/message_processor.rb
@@ -5,13 +5,37 @@ module Renalware
     class MessageProcessor
       def call(raw_message)
         begin
-           message_payload = MessageParser.new.parse(raw_message)
-           MessagePersister.new.call(message_payload)
-           params = Pathology::MessageParamParser.new.parse(message_payload)
-           Pathology::CreateObservations.new.call(params)
+           message_payload = parse_message(raw_message)
+           persist_message(message_payload)
+
+           params = parse_pathology_params(message_payload)
+           create_observations(params)
+
         rescue Exception => error
-          # TODO: Add notifier
+          notify_exception(error)
           raise error
+        end
+
+        private
+
+        def parse_message(raw_message)
+          MessageParser.new.parse(raw_message)
+        end
+
+        def persist_message(message_payload)
+          MessagePersister.new.call(message_payload)
+        end
+
+        def parse_pathology_params(message_payload)
+          Pathology::MessageParamParser.new.parse(message_payload)
+        end
+
+        def create_observations(params)
+          Pathology::CreateObservations.new.call(params)
+        end
+
+        def notify_exception(error)
+          # TODO: Add notifier
         end
       end
     end
