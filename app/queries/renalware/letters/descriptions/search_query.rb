2e237a48204f319422e674f24b52ba1c684b3647
Hugo Frappier <hugo@cekoya.com>
2016-03-14 11:25:05 -0400

    Refactor Description query

--- app/queries/renalware/letters/descriptions/search_query.rb
+++ app/queries/renalware/letters/descriptions/search_query.rb
@@ -4,33 +4,17 @@ module Renalware
       class SearchQuery
         attr_reader :term, :page, :per_page
 
-        def initialize(term:, page: 1, per_page: 20)
-          @term = term
-          @page = page
-          @per_page = per_page
+        def initialize(term)
+          @search_params = { q: { text_cont: term} }
+          @search_params.reverse_merge!(s: "text asc")
         end
 
         def call
-          search.result.page(page).per(per_page)
-            .select(fields)
-            .map do |description|
-              {
-                id: description.id,
-                label: description.text
-              }
-            end
+          search.result
         end
-
-        private
 
         def search
-          @search ||= Description.search(text_cont: term).tap do |s|
-            s.sorts = ["text"]
-          end
-        end
-
-        def fields
-          %i(id text)
+          @search ||= Description.search(@search_params)
         end
       end
     end
