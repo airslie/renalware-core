a3a07fa8b410e6d3cad6fc46cf118f5bb4dfebeb
Nicholas Henry <nicholas.henry@gmail.com>
2016-03-16 17:02:06 -0400

    Remove validation — causing problems with saving an object graph
    DB constraint will ensure column is required

--- app/models/renalware/pathology/observation.rb
+++ app/models/renalware/pathology/observation.rb
@@ -6,7 +6,6 @@ module Renalware
       belongs_to :request, class_name: "ObservationRequest"
       belongs_to :description, class_name: "ObservationDescription"
 
-      validates :request, presence: true
       validates :description, presence: true
       validates :result, presence: true
       validates :observed_at, presence: true
