f56b16fbcaf61b50c7f4363d4ce8bac255e10f33
Hugo Frappier <hugo@cekoya.com>
2016-03-17 09:25:04 -0400

    Improve smart delegator

--- lib/smart_delegator.rb
+++ lib/smart_delegator.rb
@@ -1,22 +1,28 @@
+# This class delegates method calls to the given object.
+# If the returned value if an instance of the delegated object,
+# the smart delegator will return an instance of itself.
 class SmartDelegator
+
+  attr_reader :object
+
   def initialize(object)
     @object = object
   end
 
   def respond_to?(method, include_private = false)
-    super || @object.respond_to?(method, include_private)
+    super || object.respond_to?(method, include_private)
   end
 
   def inspect
-    "SmartDelegator(#{@object.inspect})"
+    "SmartDelegator(#{object.inspect})"
   end
 
   def method_missing(method, *args, &block)
-    value = @object.public_send(method, *args, &block)
-    if value.class == @object.class
-      self.class.new(value)
+    returned_object = object.public_send(method, *args, &block)
+    if returned_object.class == object.class
+      self.class.new(returned_object)
     else
-      value
+      returned_object
     end
   end
 end
