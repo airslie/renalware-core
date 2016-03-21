9cc5910a462378898ae343dda95e5142d55d63de
Nicholas Henry <nicholas.henry@gmail.com>
2016-03-21 14:43:29 -0400

    Present multiple observations recorded the same day

--- app/presenters/renalware/pathology/archived_results_presenter.rb
+++ app/presenters/renalware/pathology/archived_results_presenter.rb
@@ -7,17 +7,22 @@ module Renalware
         @query = query
       end
 
-      def first
-        @query.first
+      def to_a
+        grouped.map { |observed_at, observation_attrs| present_attrs(observed_at, observation_attrs) }
       end
 
-      def to_a
-        [
-          {
-          "observed_at"=> I18n.l(first.observed_at.to_date),
-          first.description.code => first.result
-          }
-        ]
+      private
+
+      def present_attrs(observed_at, observations)
+        attrs = {"observed_on" => I18n.l(observed_at) }
+
+        observations.each_with_object(attrs) do |observation|
+          attrs[observation.description.code] = observation.result
+        end
+      end
+
+      def grouped
+        @query.group_by {|x| x.observed_at.to_date }
       end
     end
   end
--- features/renalware/pathology/viewing_archived_patient_results.feature
+++ features/renalware/pathology/viewing_archived_patient_results.feature
@@ -9,6 +9,7 @@ Feature: Viewing archived pathology results for a patient
     And the following observations were recorded
       | code | result | observed_at         |
       | WBC  | 6.09   | 2009-11-11 20:26:00 |
+      | RBC  | 4.00   | 2009-11-11 20:26:00 |
     Then the doctor views the following archived pathology result report:
-      | observed_at | WBC  |
-      | 11-11-2009  | 6.09 |
+      | observed_on | WBC  | RBC  |
+      | 11-11-2009  | 6.09 | 4.00 |
