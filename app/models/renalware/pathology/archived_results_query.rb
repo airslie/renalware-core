7aa45e0a8f25a139d9e541ec0dd3499ac8c6e85e
Nicholas Henry <nicholas.henry@gmail.com>
2016-03-22 16:39:25 -0400

    Limit the number of patient observations returned in the query

--- app/models/renalware/pathology/archived_results_query.rb
+++ app/models/renalware/pathology/archived_results_query.rb
@@ -2,13 +2,56 @@ require_dependency "renalware/pathology"
 
 module Renalware
   module Pathology
+    # Responsible for finding a patient's most recent observations results
+    # limited by the number of unique observation dates.
+    #
+    # Example:
+    #
+    # Given a patient has results observed on the following dates:
+    #
+    #     2014/01/01
+    #     2015/01/01
+    #     2015/01/01
+    #     2016/01/01
+    #
+    # If you specify the limit to `2` then expect three observations returned:
+    #
+    #     2015/01/01
+    #     2015/01/01
+    #     2016/01/01
+    #
+    # Although there are three observations returned, there are only 2 unique dates.
+    #
     class ArchivedResultsQuery
-      def initialize(patient:)
+      def initialize(patient:, limit: 20)
         @patient = patient
+        @limit = limit
       end
 
       def call
-        @patient.observations.ordered
+        observed_at_range = determine_date_range_within_limit
+        find_observations_within_range(observed_at_range)
+      end
+
+      private
+
+      def determine_date_range_within_limit
+        values = @patient
+          .observations
+          .ordered
+          .distinct(:observed_at)
+          .limit(@limit)
+          .pluck(:observed_at)
+          .reverse
+
+        Range.new(values.first, values.last)
+      end
+
+      def find_observations_within_range(range)
+        @patient
+          .observations
+          .where(observed_at: range)
+          .ordered
       end
     end
   end
---
+++ spec/models/renalware/pathology/archived_results_query_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper"
+
+module Renalware
+  module Pathology
+    RSpec.describe ArchivedResultsQuery do
+      describe "#call" do
+        it "return the most recent observations for the specified limit" do
+          patient = create_patient
+
+          create_observations_observed_at(patient, "2014-01-01", "2016-01-01", "2015-01-01", "2016-01-01", "2013-01-1")
+
+          query = ArchivedResultsQuery.new(patient: patient, limit: 3)
+          records = query.call
+
+          expect(records.map(&extract_observed_on)).to match(["2016-01-01", "2016-01-01", "2015-01-01", "2014-01-01"])
+        end
+      end
+
+      def extract_observed_on
+        ->(record) { record.observed_at.strftime("%Y-%m-%d") }
+      end
+
+      def create_patient
+        patient = create(:patient)
+        Pathology.cast_patient(patient)
+      end
+
+      def create_observations_observed_at(patient, *observation_dates)
+        observation_dates.each do |observed_on|
+          request = create(:pathology_observation_request, patient: patient)
+          create(:pathology_observation, request: request, observed_at: observed_on)
+        end
+      end
+    end
+  end
+end
