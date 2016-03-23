34e88e630cb5c0291d878ddd0114263918dd0f5d
Nicholas Henry <nicholas.henry@gmail.com>
2016-03-23 18:28:21 -0400

    Fix style violation

--- app/presenters/renalware/pathology/archived_results_presenter.rb
+++ app/presenters/renalware/pathology/archived_results_presenter.rb
@@ -91,7 +91,9 @@ module Renalware
         #                  if none is found, a blank string is returned.
         #
         def find_observation_result_by_description(description)
-          observation = @observations.detect { |observation| observation.description == description }
+          observation = @observations.detect do |observation|
+            observation.description == description
+          end
           observation.try!(:result).to_s
         end
       end
