911ff9cfb986f388508720e8741b8be79c334b95
Nicholas Henry <nicholas.henry@gmail.com>
2016-03-24 10:52:27 -0400

    Fix style violations

--- spec/models/renalware/update_user_spec.rb
+++ spec/models/renalware/update_user_spec.rb
@@ -1,80 +1,80 @@
-require 'rails_helper'
+require "rails_helper"
 
 module Renalware
   describe UpdateUser do
-    describe '#call' do
+    describe "#call" do
       let(:super_admin) { find_or_create_role(:super_admin) }
 
-      context 'given an unapproved user' do
+      context "given an unapproved user" do
         let (:user) { create(:user, :clinician) }
 
         subject { UpdateUser.new(user) }
 
-        it 'approves the user' do
+        it "approves the user" do
           expect(user).to receive(:approved=).with(true)
-          subject.call(approved: 'true')
+          subject.call(approved: "true")
         end
 
-        it 'authorises the user' do
+        it "authorises the user" do
           expect(user).to receive(:roles=).with([super_admin])
-          subject.call(approved: 'true', roles: [super_admin])
+          subject.call(approved: "true", roles: [super_admin])
         end
 
-        it 'notifies the user of approval' do
+        it "notifies the user of approval" do
           expect {
-            subject.call(approved: 'true', roles: [super_admin])
+            subject.call(approved: "true", roles: [super_admin])
           }.to change{
             ActionMailer::Base.deliveries.count
           }.by(1)
         end
 
-        it 'does not approve a user without roles' do
-          actual = subject.call(approved: 'true', roles: [])
+        it "does not approve a user without roles" do
+          actual = subject.call(approved: "true", roles: [])
           expect(actual).to be false
         end
       end
 
-      context 'given an approved user' do
+      context "given an approved user" do
         let (:user) { create(:user, :approved) }
         subject { UpdateUser.new(user) }
 
-        it 'skips approval' do
+        it "skips approval" do
           expect(subject).not_to receive(:approve)
           expect{
-            subject.call(approved: 'true')
+            subject.call(approved: "true")
           }.to change{
             ActionMailer::Base.deliveries.count
           }.by(0)
         end
 
-        it 'authorises the user' do
+        it "authorises the user" do
           expect(user).to receive(:roles=).with([super_admin])
-          subject.call(approved: 'true', roles: [super_admin])
+          subject.call(approved: "true", roles: [super_admin])
         end
       end
 
-      context 'given an expired user' do
+      context "given an expired user" do
         let (:user) { build_stubbed(:user, :expired) }
         subject { UpdateUser.new(user) }
 
-        it 'unexpires the user' do
+        it "unexpires the user" do
           expect(user).to receive(:expired_at=).with(nil)
-          subject.call(unexpire: 'true')
+          subject.call(unexpire: "true")
         end
 
-        it 'notifies the user of account reactivation' do
-          expect{subject.call(unexpire: 'true')}.to change(
+        it "notifies the user of account reactivation" do
+          expect{subject.call(unexpire: "true")}.to change(
             ActionMailer::Base.deliveries, :count).by(1)
         end
       end
 
-      context 'given an unexpired user' do
+      context "given an unexpired user" do
         let (:user) { build_stubbed(:user, :approved) }
         subject { UpdateUser.new(user) }
 
-        it 'skips unexpiry' do
+        it "skips unexpiry" do
           expect(subject).not_to receive(:unexpire)
-          expect{subject.call(unexpire: 'true')}.to change(
+          expect{subject.call(unexpire: "true")}.to change(
             ActionMailer::Base.deliveries, :count).by(0)
         end
       end
