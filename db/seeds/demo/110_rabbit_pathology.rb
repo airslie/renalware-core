d205c5683628c64cf025ac0c46388058fc07c287
Paul Nordstrom August <paul@nuagist.es>
2016-03-28 14:22:09 -0400

    Adding Rabbit Pathology Observations (OBX) CSV script

--- db/seeds/demo/110_rabbit_pathology.rb
+++ db/seeds/demo/110_rabbit_pathology.rb
@@ -1,7 +1,8 @@
 module Renalware
-  log '--------------------Adding Rabbit Pathology Requests (OBR)--------------------'
   rabbit = Patient.find_by(family_name: 'RABBIT', given_name: 'Roger')
   pathology_rabbit = Pathology.cast_patient(rabbit)
+
+  log '--------------------Adding Rabbit Pathology Requests (OBR)--------------------'
 
   logcount=0
   CSV.foreach(File.join(demo_path, 'rabbit_pathology_obr.csv'), headers: true) do |row|
@@ -9,6 +10,7 @@ module Renalware
     logcount += 1
     request_description = Pathology::RequestDescription.find_by!(code: row['description'])
     request = pathology_rabbit.observation_requests.create!(
+      id: row['id'],
       description: request_description,
       requestor_order_number: row['order_no'],
       requested_at: row['requested_at'],
@@ -17,4 +19,22 @@ module Renalware
   end
 
   log "#{logcount} Path Requests seeded"
+
+  log '--------------------Adding Rabbit Pathology Observations (OBX)--------------------'
+
+  logcount=0
+  CSV.foreach(File.join(demo_path, 'rabbit_pathology_obx.csv'), headers: true) do |row|
+  #id,"result","comment","observed_at","created_at","description",request_id
+    logcount += 1
+    observation_description = Pathology::ObservationDescription.find_by!(code: row['description'])
+    request = Pathology::ObservationRequest.find(row['request_id'])
+    request.observations.create!(
+      description: observation_description,
+      result: row['result'],
+      observed_at: request.requested_at - 12.hours
+    )
+  end
+
+  log "#{logcount} Path Observations seeded"
+
 end
