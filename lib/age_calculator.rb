7162ab196e9cbc488888d2e2fbf716fc6c41097e
Nicholas Henry <nicholas.henry@gmail.com>
2016-03-29 15:23:47 -0400

    Extract intention revealing methods

--- lib/age_calculator.rb
+++ lib/age_calculator.rb
@@ -10,32 +10,52 @@ class AgeCalculator
   # @return [Hash] containing the year, month and day
   #
   def compute(birth_date, current_date)
-    calculation = OpenStruct.new(
+    calculation = build_calculation(birth_date, current_date)
+    compute_days(calculation, birth_date, current_date)
+    compute_months(calculation, birth_date, current_date)
+    handle_future_dates(calculation)
+
+    build_hash(calculation)
+  end
+
+  private
+
+  def build_calculation(birth_date, current_date)
+    OpenStruct.new(
       days:   current_date.day - birth_date.day,
       months: current_date.month - birth_date.month,
       years:  current_date.year - birth_date.year,
       borrowed_month: false
     )
+  end
 
-    if calculation.days < 0
-      days_in_month = Time.days_in_month(birth_date.month)
-      calculation.days = days_in_month - birth_date.day + current_date.day
-      calculation.months -= 1
-      calculation.borrowed_month = true
-    end
+  def compute_days(calculation, birth_date, current_date)
+    return unless calculation.days < 0
 
-    if calculation.months < 0
-      months_in_year = 12
-      calculation.months = months_in_year - birth_date.month + current_date.month
-      calculation.months -= 1 if calculation.borrowed_month
-      calculation.years -= 1
-    end
+    days_in_month = Time.days_in_month(birth_date.month)
+    calculation.days = (days_in_month - birth_date.day) + current_date.day
+    calculation.months -= 1
+    calculation.borrowed_month = true
+  end
 
-    # Error-handling for future date
-    if calculation.years < 0
-      calculation.years = calculation.months = calculation.days = 0
-    end
+  def compute_months(calculation, birth_date, current_date)
+    return unless calculation.months < 0
 
+    months_in_year = 12
+    calculation.months = (months_in_year - birth_date.month) + current_date.month
+    calculation.months -= 1 if calculation.borrowed_month
+    calculation.years -= 1
+  end
+
+  # Error-handling for future date
+  #
+  def handle_future_dates(calculation)
+    return unless calculation.years < 0
+
+    calculation.years = calculation.months = calculation.days = 0
+  end
+
+  def build_hash(calculation)
     calculation.to_h.delete_if {|k,v| k == :borrowed_month}
   end
 end
