0afe3a60d400dc6fcb7706bded4eba7801151dae
Hugo Frappier <hugo@cekoya.com>
2016-03-31 17:01:40 -0400

    Break the DraftLetter command

---
+++ app/models/renalware/letters/assign_automatic_recipients.rb
@@ -0,0 +1,40 @@
+require_dependency "renalware/letters"
+
+module Renalware
+  module Letters
+    class AssignAutomaticRecipients
+      attr_reader :letter
+
+      def initialize(letter)
+        @letter = letter
+      end
+
+      def call
+        remove_automatic_ccs
+        add_patient_as_cc
+        add_doctor_as_cc
+      end
+
+      private
+
+      def remove_automatic_ccs
+        letter.cc_recipients = letter.manual_cc_recipients
+      end
+
+      def add_patient_as_cc
+        return if letter.main_recipient.patient?
+        add_source_as_cc(letter.patient) if letter.patient.cc_on_all_letters
+      end
+
+      def add_doctor_as_cc
+        return if letter.main_recipient.doctor?
+        add_source_as_cc(letter.patient.doctor)
+      end
+
+      def add_source_as_cc(source)
+        recipient = letter.cc_recipients.build(source: source)
+        recipient.name = source.full_name
+      end
+    end
+  end
+end
--- app/models/renalware/letters/draft_letter.rb
+++ app/models/renalware/letters/draft_letter.rb
@@ -10,7 +10,8 @@ module Renalware
       end
 
       def call(attributes)
-        if assign_attributes(attributes)
+        assign_letter_attributes(attributes)
+        if valid_letter?
           assign_automatic_cc_recipients
           save_letter
           refresh_dynamic_data_in_letter
@@ -20,42 +21,24 @@ module Renalware
 
       private
 
-      def assign_attributes(attributes)
-        letter.attributes = attributes
-        letter.valid?
+      def assign_letter_attributes(attributes)
+        AssignLetterAttributes.new(letter).call(attributes)
       end
 
       def assign_automatic_cc_recipients
-        remove_automatic_ccs
-        add_patient_as_cc
-        add_doctor_as_cc
+        AssignAutomaticRecipients.new(letter).call
       end
 
       def refresh_dynamic_data_in_letter
         RefreshLetter.new(letter).call
       end
 
+      def valid_letter?
+        letter.valid?
+      end
+
       def save_letter
         letter.save
-      end
-
-      def remove_automatic_ccs
-        letter.cc_recipients = letter.cc_recipients.select { |r| r.source == nil }
-      end
-
-      def add_patient_as_cc
-        return if letter.main_recipient.patient?
-        add_source_as_cc(letter.patient) if letter.patient.cc_on_all_letters
-      end
-
-      def add_doctor_as_cc
-        return if letter.main_recipient.doctor?
-        add_source_as_cc(letter.patient.doctor)
-      end
-
-      def add_source_as_cc(source)
-        recipient ||= letter.cc_recipients.build(source: source)
-        recipient.name = source.full_name
       end
     end
   end
