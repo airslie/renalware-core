8fa794f4bf78aabe0176cadba7724980c5c1c91d
Evan Rolfe <evanrolfe@gmail.com>
2016-04-01 14:25:03 +0100

    move clinics models & controllers into module

---
+++ app/models/renalware/clinics/clinic.rb
@@ -0,0 +1,13 @@
+require_dependency "renalware"
+
+module Renalware
+	module Clinics
+	  class Clinic < ActiveRecord::Base
+	    validates :name, presence: true
+
+	    def to_s
+	      name
+	    end
+	  end
+	end	
+end
