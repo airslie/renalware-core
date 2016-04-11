a57652de2926a2e6433ffec8dde0b5ba77bcbda1
Evan Rolfe <evanrolfe@gmail.com>
2016-04-11 14:50:26 +0100

    move capybara methods to web module in clinic visit world

--- features/support/worlds/clinics/clinic_visits.rb
+++ features/support/worlds/clinics/clinic_visits.rb
@@ -20,6 +20,24 @@ module World
           )
         end
 
+        # @section expectations
+        #
+        def expect_clinic_visit_to_exist(patient)
+          patient = Renalware::Clinics.cast_patient(patient)
+          expect(patient.clinic_visits.length).to eq(1)
+        end
+
+        def expect_clinic_visit_to_be_updated(clinic_visit)
+          clinic_visit = Renalware::Clinics::ClinicVisit.find(clinic_visit.id)
+          expect(clinic_visit.clinic_id).to eq(2)
+          expect(clinic_visit.height).to eq(1.71)
+          expect(clinic_visit.weight).to eq(75)
+        end
+      end
+
+      module Web
+        include Domain
+
         def record_clinic_visit
           fill_in "Date", with: "20-07-2015 10:45"
           select "Access", from: "Clinic"
@@ -38,24 +56,6 @@ module World
 
           click_on "Update"
         end
-
-        # @section expectations
-        #
-        def expect_clinic_visit_to_exist(patient)
-          patient = Renalware::Clinics.cast_patient(patient)
-          expect(patient.clinic_visits.length).to eq(1)
-        end
-
-        def expect_clinic_visit_to_be_updated(clinic_visit)
-          clinic_visit = Renalware::Clinics::ClinicVisit.find(clinic_visit.id)
-          expect(clinic_visit.clinic_id).to eq(2)
-          expect(clinic_visit.height).to eq(1.71)
-          expect(clinic_visit.weight).to eq(75)
-        end
-      end
-
-      module Web
-        include Domain
       end
     end
   end
