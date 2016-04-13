26c7682fefb12ebf0ec8f5b9555634305982e25c
Nicholas Henry <nicholas.henry@gmail.com>
2016-04-13 09:39:54 -0400

    Extract table generation into a class
    This was pretty ugly as template code and makes more sense to extract it into a class

--- app/controllers/renalware/pathology/historical_observations_controller.rb
+++ app/controllers/renalware/pathology/historical_observations_controller.rb
@@ -5,12 +5,56 @@ module Renalware
     class HistoricalObservationsController < Pathology::BaseController
       before_filter :load_patient
 
+      class HTMLHistoricalTableView
+        def initialize(context)
+          @context = context
+        end
+
+        def render(rows)
+          header, *body = rows
+
+          h.content_tag(:table, id: "observations") do
+            h.concat format_header(header)
+            h.concat format_body(body)
+          end
+        end
+
+        private
+
+        def format_header(cells)
+          h.content_tag(:thead) do
+            h.content_tag(:tr) do
+              cells.each do |cell|
+                h.concat(h.content_tag(:th, cell, class: cell.html_class, title: cell.title))
+              end
+            end
+          end
+        end
+
+        def format_body(rows)
+          h.content_tag(:body) do
+            rows.each do |row|
+              h.concat(h.content_tag(:tr) do
+                row.each do |cell|
+                  h.concat(h.content_tag(:td, cell, class: cell.html_class))
+                end
+              end)
+            end
+          end
+        end
+
+        def h
+          @context
+        end
+      end
+
       def index
         presenter = ViewHistoricalObservationsFactory.new.build(@patient).call(params)
 
         render :index, locals: {
           rows: presenter.present,
-          paginator: presenter.paginator
+          paginator: presenter.paginator,
+          table: HTMLHistoricalTableView.new(self.view_context)
         }
       end
     end
