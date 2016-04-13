e9b54c291e954ddb056046fe241e33f5e51d1000
Nicholas Henry <nicholas.henry@gmail.com>
2016-04-13 09:49:31 -0400

    Extract view model for recent pathology results as well

--- app/controllers/renalware/pathology/recent_observations_controller.rb
+++ app/controllers/renalware/pathology/recent_observations_controller.rb
@@ -5,12 +5,45 @@ module Renalware
     class RecentObservationsController < Pathology::BaseController
       before_filter :load_patient
 
+      class HTMLRecentTableView
+        def initialize(context)
+          @context = context
+        end
+
+        def render(rows)
+          h.content_tag(:table, id: "observations") do
+            format_body(rows)
+          end
+        end
+
+        private
+
+        def format_body(rows)
+         rows.each do |row|
+           header, *values = row
+           h.concat(h.content_tag(:tr) do
+              h.content_tag(:td, title: header.title) do
+                h.concat(header)
+                values.each do |cell|
+                  h.concat(h.content_tag(:td, cell, class: cell.html_class))
+                end
+              end
+           end)
+         end
+        end
+
+        def h
+          @context
+        end
+      end
+
       def index
         presenter = ViewRecentObservationsFactory.new.build(@patient).call(params)
 
         render :index, locals: {
           rows: presenter.present,
-          number_of_records: presenter.limit
+          number_of_records: presenter.limit,
+          table: HTMLRecentTableView.new(self.view_context)
         }
       end
     end
