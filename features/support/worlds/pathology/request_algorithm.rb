8bce38733ed4f9a25e2f359a97b9b8efe2f978ba
Evan Rolfe <evanrolfe@gmail.com>
2016-04-18 13:26:19 +0100

    fix failing feature spec

--- features/support/worlds/pathology/request_algorithm.rb
+++ features/support/worlds/pathology/request_algorithm.rb
@@ -2,19 +2,21 @@ require "array_stringifier"
 
 module World
   module Pathology
-    module Domain
-      # @section commands
+    module RequestAlgorithm
+      module Domain
+        # @section commands
 
-      def record_observations(patient:, observations_attributes:)
+        def record_observations(patient:, observations_attributes:)
 
-      end
+        end
 
-      # @section expectations
-      #
-      def expect_observation_request_to_be_created(attrs)
-        observation_request = find_last_observation_request
+        # @section expectations
+        #
+        def expect_observation_request_to_be_created(attrs)
+          observation_request = find_last_observation_request
 
-        expect_attributes_to_match(observation_request, attrs)
+          expect_attributes_to_match(observation_request, attrs)
+        end
       end
     end
   end
