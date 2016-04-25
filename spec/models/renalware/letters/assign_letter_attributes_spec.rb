bae076f24dd24df1b60de41ce52cb42b964063f8
Hugo Frappier <hugo@cekoya.com>
2016-04-25 13:56:01 -0400

    Tests that the source is set onthe main recipient.

--- spec/models/renalware/letters/assign_letter_attributes_spec.rb
+++ spec/models/renalware/letters/assign_letter_attributes_spec.rb
@@ -5,6 +5,7 @@ module Renalware
     RSpec.describe AssignLetterAttributes, type: :model do
       let(:patient) { create(:letter_patient, cc_on_all_letters: false) }
       let(:letter_trait) { :to_patient }
+      let(:letter) { build(:letter, letter_trait) }
 
       subject { DraftLetter.new(letter) }
 
@@ -15,12 +16,17 @@ module Renalware
 
             expect(letter.description).to eq("Foo")
           end
+
+          it "sets the source instance on the main recipient" do
+            subject.call(by: letter.author)
+
+            main_recipient_patient = Letters.cast_patient(letter.main_recipient.source)
+            expect(main_recipient_patient).to match(letter.patient)
+          end
         end
       end
 
       context "with a new letter" do
-        let(:letter) { build(:letter, letter_trait) }
-
         it_behaves_like "Letter"
       end
 
