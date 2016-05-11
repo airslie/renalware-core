26e6733ba38d0b6a203f26df5c27cb6af66bebee
Hugo Frappier <hugo@cekoya.com>
2016-05-11 14:06:22 -0400

    Improve patient spec

--- spec/models/renalware/letters/patient_spec.rb
+++ spec/models/renalware/letters/patient_spec.rb
@@ -6,36 +6,38 @@ module Renalware
     describe Patient, type: :model do
       include LettersSpecHelper
 
-      subject(:patient) { create(:letter_patient) }
+      subject(:patient) { build(:letter_patient) }
 
       describe "#cc_on_letter?" do
-        context "given the letter is for another patient" do
+        context "given the letter belongs to the patient" do
+          context "and the patient is the main recipient" do
+            let(:letter) { build_letter(to: :patient, patient: patient) }
+
+            it { expect(patient.cc_on_letter?(letter)).to be_falsy }
+          end
+
+          context "and the doctor is the main recipient" do
+            let(:letter) { build_letter(to: :doctor, patient: patient) }
+
+            context "and patient requested to be CCd on all letters" do
+              before { patient.cc_on_all_letters = true }
+
+              it { expect(patient.cc_on_letter?(letter)).to be_truthy }
+            end
+
+            context "and patient did not request to be CCd on all letters" do
+              before { patient.cc_on_all_letters = false }
+
+              it { expect(patient.cc_on_letter?(letter)).to be_falsy }
+            end
+          end
+        end
+
+        context "given the letter belongs to another patient" do
           let(:another_patient) { build(:letter_patient) }
           let(:letter) { build_letter(to: :patient, patient: another_patient) }
 
           it { expect(patient.cc_on_letter?(letter)).to be_falsy }
-        end
-
-        context "given the letter is for the patient" do
-          let(:letter) { build_letter(to: :patient, patient: patient) }
-
-          it { expect(patient.cc_on_letter?(letter)).to be_falsy }
-        end
-
-        context "given the letter is for the doctor" do
-          let(:letter) { build_letter(to: :doctor, patient: patient) }
-
-          context "given patient requested to be CCd on all letters" do
-            before { patient.cc_on_all_letters = true }
-
-            it { expect(patient.cc_on_letter?(letter)).to be_truthy }
-          end
-
-          context "given patient did not request to be CCd on all letters" do
-            before { patient.cc_on_all_letters = false }
-
-            it { expect(patient.cc_on_letter?(letter)).to be_falsy }
-          end
         end
       end
     end
