d6a9ae27e734d32bde1d0998901136d14d0fcda0
Hugo Frappier <hugo@cekoya.com>
2016-05-11 13:59:56 -0400

    Improve Doctor specs

--- app/models/renalware/letters/doctor.rb
+++ app/models/renalware/letters/doctor.rb
@@ -4,8 +4,12 @@ module Renalware
   module Letters
     class Doctor < ActiveType::Record[Renalware::Doctor]
       def cc_on_letter?(letter)
-        return false unless letter.patient.doctor == self
+        return false unless assigned_to?(letter.patient)
         letter.main_recipient.patient? || letter.main_recipient.other?
+      end
+
+      def assigned_to?(patient)
+        patient.doctor == self
       end
     end
   end
--- spec/models/renalware/letters/doctor_spec.rb
+++ spec/models/renalware/letters/doctor_spec.rb
@@ -1,40 +1,42 @@
 require "rails_helper"
-require_dependency "models/renalware/concerns/personable"
 
 module Renalware
   module Letters
     describe Doctor, type: :model do
       include LettersSpecHelper
 
-      subject(:doctor) { create(:letter_doctor) }
+      subject(:doctor) { build(:letter_doctor) }
 
       describe "#cc_on_letter?" do
-        let(:patient) { create(:letter_patient, doctor: doctor) }
+        let(:patient) { build(:letter_patient, doctor: doctor) }
 
-        context "given the letter is for another doctor" do
-          let(:other_doctor) { build(:letter_doctor) }
-          let(:other_patient) { build(:letter_patient, doctor: other_doctor) }
-          let(:letter) { build_letter(to: :patient, patient: other_patient) }
+        context "given the doctor is assigned to the patient" do
+          context "and the patient is the main recipient" do
+            let(:letter) { build_letter(to: :patient, patient: patient) }
 
-          it { expect(doctor.cc_on_letter?(letter)).to be_falsy }
+            it { expect(doctor.cc_on_letter?(letter)).to be_truthy }
+          end
+
+          context "and the doctor is the main recipient" do
+            let(:letter) { build_letter(to: :doctor, patient: patient) }
+
+            it { expect(doctor.cc_on_letter?(letter)).to be_falsy }
+          end
+
+          context "and someone else is the main recipient" do
+            let(:letter) { build_letter(to: :other, patient: patient) }
+
+            it { expect(doctor.cc_on_letter?(letter)).to be_truthy }
+          end
         end
 
-        context "given the letter is for the patient" do
+        context "given the patient is assigned to another doctor" do
+          let(:other_doctor) { build(:letter_doctor) }
           let(:letter) { build_letter(to: :patient, patient: patient) }
 
-          it { expect(doctor.cc_on_letter?(letter)).to be_truthy }
-        end
-
-        context "given the letter is for the doctor" do
-          let(:letter) { build_letter(to: :doctor, patient: patient) }
+          before { patient.doctor = other_doctor }
 
           it { expect(doctor.cc_on_letter?(letter)).to be_falsy }
-        end
-
-        context "given the letter is for someone else" do
-          let(:letter) { build_letter(to: :other, patient: patient) }
-
-          it { expect(doctor.cc_on_letter?(letter)).to be_truthy }
         end
       end
     end
