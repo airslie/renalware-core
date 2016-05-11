ea912e227aa5e6e9f1f6176ffe6f45e2f1cbe0cf
Hugo Frappier <hugo@cekoya.com>
2016-05-11 15:17:53 -0400

    Remove #create_letter

--- spec/support/letters_spec_helper.rb
+++ spec/support/letters_spec_helper.rb
@@ -17,30 +17,4 @@ module LettersSpecHelper
 
     letter
   end
-
-  def create_letter(to:, **args)
-    letter = build_letter(to: to, **args)
-
-    case to
-    when :patient
-      letter.cc_recipients = [build(:letter_recipient, :cc, person_role: "doctor")]
-    when :doctor
-      letter.cc_recipients = [build(:letter_recipient, :cc, person_role: "patient")]
-    else
-      letter.cc_recipients = [
-        build(:letter_recipient, :cc, person_role: "doctor"),
-        build(:letter_recipient, :cc, person_role: "patient")
-      ]
-    end
-
-    letter.save!
-
-    if letter.state.archived?
-      letter.recipients.each do |r|
-        r.create_address attributes_for(:address)
-      end
-    end
-
-    letter
-  end
 end
