4f0cb4e2d1151033035be7440e7b13243c042fe5
Nicholas Henry <nicholas.henry@gmail.com>
2016-05-12 15:04:16 -0400

    Correct spec

--- spec/requests/renalware/pathology/patient_observations_spec.rb
+++ spec/requests/renalware/pathology/patient_observations_spec.rb
@@ -12,9 +12,9 @@ RSpec.describe "Patient's Observations", type: :request do
         request: observation_request, description: observation_description)
     end
 
-    it "responds with details" do
+    it "responds with a list" do
       get patient_pathology_observations_path(
-        patient_id: patient.id, description_id: observation_request.id)
+        patient_id: patient.id, description_id: observation_description.id)
 
       expect(response).to have_http_status(:success)
     end
