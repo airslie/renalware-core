2eb97df24843a2751ca133831fbe688180e83050
Evan Rolfe <evanrolfe@gmail.com>
2016-05-24 16:41:47 +0100

    fix request spec

--- spec/requests/renalware/dashboard_spec.rb
+++ spec/requests/renalware/dashboard_spec.rb
@@ -8,8 +8,6 @@ RSpec.describe "User's Dashboard", type: :request do
       get dashboard_path
 
       expect(response).to have_http_status(:success)
-      # TODO: Populate the dashboard page with something
-      expect(response.body).to include("<h4>TODO:</h4>")
     end
   end
 end
