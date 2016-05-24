5bd767721ec33d1437dff05ee8c9a83e22fe4cba
Nicholas Henry <nicholas.henry@gmail.com>
2016-05-24 13:43:41 -0400

    Remove unnecessary  method

--- app/presenters/renalware/doctor_presenter.rb
+++ app/presenters/renalware/doctor_presenter.rb
@@ -3,7 +3,7 @@ require_dependency "renalware"
 module Renalware
   class DoctorPresenter < SimpleDelegator
     def address
-      Renalware::AddressPresenter.new(super || practice_address).to_s
+      Renalware::AddressPresenter.new(super || practice_address)
     end
   end
 end
