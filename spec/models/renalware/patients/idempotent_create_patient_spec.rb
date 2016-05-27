387aefb98945630bfd79514c9e268ecc08ce098c
Nicholas Henry <nicholas.henry@gmail.com>
2016-05-27 11:12:03 -0400

    Injects the user into the Command
    The command should not have the responsibility of knowing what user is required,
    we can give that responsibility to the message listener to inject the
    required user, a co-ordination task.

--- spec/models/renalware/patients/idempotent_create_patient_spec.rb
+++ spec/models/renalware/patients/idempotent_create_patient_spec.rb
@@ -3,20 +3,21 @@ require "rails_helper"
 module Renalware::Patients
   RSpec.describe IdempotentCreatePatient do
     describe "#call" do
-      let!(:system_user) { create(:user, username: ::Renalware::SystemUser.username) }
+      let(:user) { create(:user) }
+
+      subject(:command) { IdempotentCreatePatient.new(user) }
 
       context "given a patient does not have the same hospital number" do
         let(:params) { { patient: attributes_for(:patient) } }
 
-        subject(:command) { IdempotentCreatePatient.new(params) }
-
         it "creates the patient" do
-          expect{command.call}.to change{::Renalware::Patient.count}.by(1)
+          expect{command.call(params)}.to change{::Renalware::Patient.count}.by(1)
         end
       end
 
       context "give a patient has the same hospital number" do
         let!(:existing_patient) { create(:patient, local_patient_id: "SAME-12345") }
+
         let(:params) do
           {
             patient: attributes_for(:patient).merge(
@@ -25,10 +26,8 @@ module Renalware::Patients
           }
         end
 
-        subject(:command) { IdempotentCreatePatient.new(params) }
-
         it "does not create the patient" do
-          expect{command.call}.not_to change{::Renalware::Patient.count}
+          expect{command.call(params)}.not_to change{::Renalware::Patient.count}
         end
       end
     end
