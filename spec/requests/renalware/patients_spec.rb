5fe5b88f3882049ee9649505599677add54e7018
Evan Rolfe <evanrolfe@gmail.com>
2016-05-27 16:53:17 +0100

    fix variable name in request spec

--- spec/requests/renalware/patients_spec.rb
+++ spec/requests/renalware/patients_spec.rb
@@ -57,8 +57,8 @@ RSpec.describe "Managing patients", type: :request do
         expect(response).to have_http_status(:redirect)
         expect(Renalware::Patient.exists?(attributes)).to be_truthy
 
-        created_patient = Renalware::Patient.find_by(attributes)
-        expect(created_patient.updated_by).to eq(@current_user)
+        updated_patient = Renalware::Patient.find_by(attributes)
+        expect(updated_patient.updated_by).to eq(@current_user)
 
         follow_redirect!
 
