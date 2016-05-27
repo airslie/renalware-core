f7913b293069932288842ba939c56d0be04dee48
Nicholas Henry <nicholas.henry@gmail.com>
2016-05-27 12:29:24 -0400

    Display draft letters on the dashboard

--- app/controllers/renalware/dashboard/dashboards_controller.rb
+++ app/controllers/renalware/dashboard/dashboards_controller.rb
@@ -3,9 +3,25 @@ require_dependency "renalware/dashboard"
 module Renalware
   module Dashboard
     class DashboardsController < BaseController
+      skip_after_action :verify_authorized
+
       def show
-        authorize current_user
-        render :show, locals: { user: current_user }
+        letters = find_letters
+        render :show, locals: { user: current_user, letters: present_letters(letters) }
+      end
+
+      private
+
+      def find_letters
+        author.letters
+      end
+
+      def present_letters(letters)
+        CollectionPresenter.new(letters, Letters::LetterPresenterFactory)
+      end
+
+      def author
+        Letters.cast_author(current_user)
       end
     end
   end
