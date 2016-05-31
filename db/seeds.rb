59ecccc5a3a73037dcf863166c0bf5b4295ce3c2
Evan Rolfe <evanrolfe@gmail.com>
2016-05-31 19:24:54 +0100

    add test database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,13 @@
-require 'csv'
+require "csv"
 
 def log(msg)
   puts msg
 end
 
-log '--------------------Seeding Database--------------'
+log "--------------------Seeding Database--------------"
 
-require_relative './seeds/default/seeds.rb'
-require_relative './seeds/demo/seeds.rb' if ENV['DEMO'] || Rails.env.development?
+require_relative "./seeds/default/seeds.rb"
+require_relative "./seeds/demo/seeds.rb" if ENV["DEMO"] || Rails.env.development?
+require_relative './seeds/test/seeds.rb' if Rails.env.test?
 
-log '--------------------Database seeding complete!----------'
+log "--------------------Database seeding complete!----------"
