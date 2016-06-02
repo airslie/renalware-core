36672eeecdbed11df760a3c7d45f560ff20dede0
Evan Rolfe <evanrolfe@gmail.com>
2016-06-02 17:36:02 +0100

    import the remaining database fixtures from the default seeds directory

--- features/support/env.rb
+++ features/support/env.rb
@@ -34,16 +34,45 @@ Capybara.default_wait_time = 5 # in seconds
 #
 ActionController::Base.allow_rescue = false
 
-Cucumber::Rails::World.use_transactional_fixtures
-
+# Load the test fixtures to the database
 table_model_map = {
   drugs: Renalware::Drugs::Drug,
   drug_types: Renalware::Drugs::Type,
   roles: Renalware::Role,
+  event_types: Renalware::Events::Type,
+  ethnicities: Renalware::Ethnicity,
+  modality_descriptions: Renalware::Modalities::Description,
+  edta_codes: Renalware::EdtaCode,
+  prd_descriptions: Renalware::PRDDescription,
+  organism_codes: Renalware::OrganismCode,
+  modality_reasons: Renalware::Modalities::Reason,
+  medication_routes: Renalware::MedicationRoute,
+  episode_types: Renalware::EpisodeType,
+  fluid_descriptions: Renalware::FluidDescription,
+  bag_types: Renalware::BagType,
+  clinics: Renalware::Clinics::Clinic,
+  transplant_registration_status_descriptions: Renalware::Transplants::RegistrationStatusDescription,
+  transplant_failure_cause_description_groups: Renalware::Transplants::FailureCauseDescriptionGroup,
+  transplant_failure_cause_descriptions: Renalware::Transplants::FailureCauseDescription,
+  access_sites: Renalware::Accesses::Site,
+  access_types: Renalware::Accesses::Type,
+  access_plans: Renalware::Accesses::Plan,
+  hospital_centres: Renalware::Hospitals::Centre,
+  hospital_units: Renalware::Hospitals::Unit,
+  hd_cannulation_types: Renalware::HD::CannulationType,
+  hd_dialysers: Renalware::HD::Dialyser,
+  letter_letterheads: Renalware::Letters::Letterhead,
+  pathology_observation_descriptions: Renalware::Pathology::ObservationDescription,
+  pathology_lab: Renalware::Pathology::Lab,
+  pathology_request_descriptions: Renalware::Pathology::RequestDescription,
+  users: Renalware::User,
+  patient_religions: Renalware::Patients::Religion,
+  patient_languages: Renalware::Patients::Language
 }
 
-#Seed the DB
+Cucumber::Rails::World.use_transactional_fixtures
 ActiveRecord::FixtureSet.reset_cache
 fixtures_folder = File.join(Rails.root, 'features', 'support', 'fixtures')
 fixtures = Dir[File.join(fixtures_folder, '*.yml')].map {|f| File.basename(f, '.yml') }
 ActiveRecord::FixtureSet.create_fixtures(fixtures_folder, fixtures, table_model_map)
+puts "Database fixtures loaded."
