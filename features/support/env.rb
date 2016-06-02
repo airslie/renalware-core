eb472bda70922188375e88e4a8c6ce500ca016a4
Evan Rolfe <evanrolfe@gmail.com>
2016-06-02 17:58:16 +0100

    disable transactional fixtures for the meantime

--- features/support/env.rb
+++ features/support/env.rb
@@ -70,9 +70,11 @@ table_model_map = {
   patient_languages: Renalware::Patients::Language
 }
 
-Cucumber::Rails::World.use_transactional_fixtures
-ActiveRecord::FixtureSet.reset_cache
-fixtures_folder = File.join(Rails.root, 'features', 'support', 'fixtures')
-fixtures = Dir[File.join(fixtures_folder, '*.yml')].map {|f| File.basename(f, '.yml') }
-ActiveRecord::FixtureSet.create_fixtures(fixtures_folder, fixtures, table_model_map)
+Before do
+  ActiveRecord::FixtureSet.reset_cache
+  fixtures_folder = File.join(Rails.root, 'features', 'support', 'fixtures')
+  fixtures = Dir[File.join(fixtures_folder, '*.yml')].map {|f| File.basename(f, '.yml') }
+  ActiveRecord::FixtureSet.create_fixtures(fixtures_folder, fixtures, table_model_map)
+end
+
 puts "Database fixtures loaded."
