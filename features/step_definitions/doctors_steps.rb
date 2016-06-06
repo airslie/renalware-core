45dc9e86d9d84fe5c65c7c43d3647606d670b8ce
Evan Rolfe <evanrolfe@gmail.com>
2016-06-06 16:30:22 +0100

    remove unnecessary files

--- features/step_definitions/doctors_steps.rb
+++
@@ -1,9 +0,0 @@
-Given(/^([A-Za-z]+) ([A-Za-z]+) is a doctor with telephone number (\d+)$/) do |given_name, family_name, telephone|
-  Renalware::Doctor.create!(
-    email: "john.merrill@nhs.net",
-    given_name: given_name,
-    family_name: family_name,
-    practitioner_type: 'GP',
-    telephone: telephone
-  )
-end
