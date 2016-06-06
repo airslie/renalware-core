ddc3c7af1729dc59dfb8fa34372e06db5bda3cc9
Nicholas Henry <nicholas.henry@gmail.com>
2016-06-06 12:22:13 -0400

    Remove logging message as fixtures are actually loaded at that point

--- features/support/env.rb
+++ features/support/env.rb
@@ -74,7 +74,5 @@ Before do
   ActiveRecord::FixtureSet.reset_cache
   fixtures_folder = File.join(Rails.root, "features", "support", "fixtures")
   fixtures = Dir[File.join(fixtures_folder, "*.yml")].map {|f| File.basename(f, ".yml") }
-  ActiveRecord::FixtureSet.create_fixtures(fixtures_folder, fixtures, table_model_map)
+  ActiveRecord::FixtureSet.create_fixtures(fixtures_folder, fixtures)
 end
-
-puts "Database fixtures loaded."
