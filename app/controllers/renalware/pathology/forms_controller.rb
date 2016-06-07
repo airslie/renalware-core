c508552358985b9272d21dc981f07cd677f47d7d
Evan Rolfe <evanrolfe@gmail.com>
2016-06-07 13:37:30 +0100

    fix namespacing and use AR find method

--- app/controllers/renalware/pathology/forms_controller.rb
+++ app/controllers/renalware/pathology/forms_controller.rb
@@ -7,23 +7,21 @@ module Renalware
       before_filter :load_patients
 
       def index
-        form_params =
-          Renalware::Pathology::FormParamsDecorator.new(params)
-        patients =
-          Renalware::Pathology::RequestFormsDecorator.wrap(@patients, form_params.clinic)
+        form_params = FormParamsDecorator.new(params)
+        patients = RequestFormsDecorator.wrap(@patients, form_params.clinic)
 
         render :index, locals: {
           form_params: form_params,
           patients: patients,
-          doctors: Renalware::Doctor.ordered,
-          clinics: Renalware::Clinics::Clinic.ordered
+          doctors: Doctor.ordered,
+          clinics: Clinics::Clinic.ordered
         }
       end
 
       private
 
       def load_patients
-        @patients = Renalware::Pathology::Patient.find_by_patient_ids(
+        @patients = Patient.find(
           params[:patient_ids].split(",")
         )
         authorize @patients
