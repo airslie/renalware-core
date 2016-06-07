1b0bab2b66c346b0217b3e553fcde258c81d5392
Evan Rolfe <evanrolfe@gmail.com>
2016-06-07 14:02:24 +0100

    fix namespacing in controller

--- app/controllers/renalware/pathology/forms_controller.rb
+++ app/controllers/renalware/pathology/forms_controller.rb
@@ -7,24 +7,22 @@ module Renalware
       before_filter :load_patients
 
       def index
-        form_params =
-          Renalware::Pathology::FormParamsDecorator.new(params)
-        patients =
-          Renalware::Pathology::RequestFormsDecorator.wrap(@patients, form_params.clinic)
+        form_params = FormParamsDecorator.new(params)
+        patients = RequestFormsDecorator.wrap(@patients, form_params.clinic)
 
         render :index, locals: {
           form_params: form_params,
           patients: patients,
-          doctors: Renalware::Doctor.ordered,
-          clinics: Renalware::Clinics::Clinic.ordered
+          doctors: Doctor.ordered,
+          clinics: Clinics::Clinic.ordered
         }
       end
 
       private
 
       def load_patients
-        @patients = Renalware::Pathology::Patient.find_by_patient_ids(
-          params[:patient_ids].split(",")
+        @patients = Pathology::Patient.where(
+          id: params[:patient_ids].split(",")
         )
         authorize @patients
       end
