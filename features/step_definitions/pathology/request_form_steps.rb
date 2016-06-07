f58e9acf3222b35530a919892242f0d399700705
Evan Rolfe <evanrolfe@gmail.com>
2016-06-07 11:10:43 +0100

    use world for request form feature

--- features/step_definitions/pathology/request_form_steps.rb
+++ features/step_definitions/pathology/request_form_steps.rb
@@ -1,50 +1,23 @@
 When(/^Clyde enters clinic (.*)$/) do |clinic_name|
-  @url_params = {} unless @url_params.present?
   clinic = Renalware::Clinics::Clinic.find_by!(name: clinic_name)
-  @url_params.merge!(clinic_id: clinic.id)
+  @url_params = set_url_params(@url_params, clinic_id: clinic.id)
 end
 
 When(/^Clyde enters doctor ([A-Za-z]+) ([A-Za-z]+)$/) do |given_name, family_name|
-  @url_params = {} unless @url_params.present?
   doctor = Renalware::Doctor.find_by!(given_name: given_name, family_name: family_name)
-  @url_params.merge!(doctor_id: doctor.id)
+  @url_params = set_url_params(@url_params, doctor_id: doctor.id)
 end
 
 When(/^Clyde enters telephone number (\d*)$/) do |telephone|
-  @url_params = {} unless @url_params.present?
-  @url_params.merge!(telephone: telephone)
+  @url_params = set_url_params(@url_params, telephone: telephone)
 end
 
 When(/^Clyde views the pathology request form for Patty$/) do
-  login_as @clyde
-
-  @url_params = {} unless @url_params.present?
-
-  @url_params.merge!(patient_ids: @patty.id)
-  url = pathology_forms_path(@url_params)
-
-  visit url
+  get_pathology_request_form(@clyde, @url_params, @patty.id)
 end
 
 Then(/^Clyde sees these details at the top of the form$/) do |table|
-  html_table =
-    find_by_id("patient_#{@patty.id}_summary")
-      .all("tr")
-      .map do |row|
-        row.all("th, td").map { |cell| cell.text.strip }
-      end
-
-  expected_table = table.raw.map do |row|
-    row.map do |cell|
-      if cell == "TODAYS_DATE"
-        Date.current.strftime("%d/%m/%Y")
-      else
-        cell
-      end
-    end
-  end
-
-  expect(html_table).to eq(expected_table)
+  expect_patient_summary_to_match_table(@patty.id, table)
 end
 
 Then(/^Clyde sees this patient specific test: (.*)$/) do |string|
@@ -56,7 +29,5 @@ Then(/^Clyde sees no global tests required$/) do
 end
 
 Then(/^Clyde sees the request description ([A-Z0-9]+) required$/) do |request_description_code|
-  request_description =
-    Renalware::Pathology::RequestDescription.find_by(code: request_description_code)
-  expect(page.text.downcase).to include(request_description.name.downcase)
+  expect_request_description_required(request_description_code)
 end
---
+++ features/support/worlds/pathology/request_form.rb
@@ -0,0 +1,54 @@
+module World
+  module Pathology
+    module GlobalAlgorithm
+      module Web
+        # @section commands
+        #
+        def set_url_params(url_params, new_param)
+          url_params = {} unless url_params.present?
+          url_params.merge(new_param)
+        end
+
+        def get_pathology_request_form(clinician, url_params, patient_ids)
+          login_as clinician
+
+          url_params = {} unless url_params.present?
+
+          url_params.merge!(patient_ids: patient_ids)
+          url = pathology_forms_path(url_params)
+
+          visit url
+        end
+
+        # @section expectations
+        #
+        def expect_patient_summary_to_match_table(patient_id, expected_table)
+          table_from_html =
+            find_by_id("patient_#{patient_id}_summary")
+              .all("tr")
+              .map do |row|
+                row.all("th, td").map { |cell| cell.text.strip }
+              end
+
+          expected_table = expected_table.raw.map do |row|
+            row.map do |cell|
+              if cell == "TODAYS_DATE"
+                Date.current.strftime("%d/%m/%Y")
+              else
+                cell
+              end
+            end
+          end
+
+          expect(table_from_html).to eq(expected_table)
+        end
+
+        def expect_request_description_required(request_description_code)
+          request_description =
+            Renalware::Pathology::RequestDescription.find_by(code: request_description_code)
+          expect(page.text.downcase).to include(request_description.name.downcase)
+        end
+      end
+    end
+  end
+end
