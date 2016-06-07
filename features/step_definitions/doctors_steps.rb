5e11c972385e691dd125535f1b7ccf18da8f20b1
Evan Rolfe <evanrolfe@gmail.com>
2016-06-07 09:45:13 +0100

    rubocop: line length

--- features/step_definitions/doctors_steps.rb
+++ features/step_definitions/doctors_steps.rb
@@ -1,4 +1,5 @@
-Given(/^([A-Za-z]+) ([A-Za-z]+) is a doctor with telephone number: (\d+)$/) do |given_name, family_name, telephone|
+regex = /^([A-Za-z]+) ([A-Za-z]+) is a doctor with telephone number: (\d+)$/
+Given(regex) do |given_name, family_name, telephone|
   Renalware::Doctor.create!(
     email: "john.merrill@nhs.net",
     given_name: given_name,
