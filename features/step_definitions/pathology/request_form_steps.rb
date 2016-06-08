38817b1fef5f46ee661b624407c0003763a231b5
Nicholas Henry <nicholas.henry@gmail.com>
2016-06-08 12:42:35 -0400

    Explicitly pass presenter to helper methods

--- features/step_definitions/pathology/request_form_steps.rb
+++ features/step_definitions/pathology/request_form_steps.rb
@@ -1,19 +1,19 @@
 When(/^Clyde generates a request form with the following:$/) do |table|
-  generate_pathology_request_form(table.rows_hash)
+  @presenter = generate_pathology_request_form(table.rows_hash)
 end
 
 Then(/^Clyde sees these details at the top of the form$/) do |table|
-  expect_patient_summary_to_match_table(@patty.id, table)
+  expect_patient_summary_to_match_table(@presenter, @patty.id, table)
 end
 
 Then(/^Clyde sees this patient specific test: (.*)$/) do |test_description|
-  expect_patient_specific_test(test_description)
+  expect_patient_specific_test(@presenter, test_description)
 end
 
 Then(/^Clyde sees no global tests required$/) do
-  expect_no_request_descriptions_required
+  expect_no_request_descriptions_required(@presenter)
 end
 
 Then(/^Clyde sees the request description ([A-Z0-9]+) required$/) do |request_description_code|
-  expect_request_description_required(request_description_code)
+  expect_request_description_required(@presenter, request_description_code)
 end
--- features/support/worlds/pathology/request_form.rb
+++ features/support/worlds/pathology/request_form.rb
@@ -19,30 +19,36 @@ module World
           clinics = Renalware::Clinics::Clinic.ordered
 
           form_params = Renalware::Pathology::Forms::ParamsPresenter.new({clinic_id: clinic.id, doctor_id: doctor.id}, doctors, clinics)
-          @patient = Renalware::Pathology::Forms::PatientPresenter.new(patient, form_params.clinic)
+          Renalware::Pathology::Forms::PatientPresenter.new(patient, form_params.clinic)
         end
 
         # @section expectations
         #
-        def expect_patient_summary_to_match_table(patient_id, expected_table)
+        def expect_patient_summary_to_match_table(presenter, patient_id, expected_table)
+          # TODO: We refactor presenters to make this work, it seems we need the following a single presenter representing the form:
+          # presenter = Forms::RequestFormPresenter.new(patient, clinic, doctor, telephone_number: telephone_number)
+          # expect(presenter.patient_name).to eq(table.rows_hash["patient_name"])
+          #
+          # This would replace the ParamsPresenter and the PatientPresenter.
         end
 
-        def expect_patient_specific_test(test_description)
-          @patient.patient_requests_by_lab.each do |lab_name, patient_rules|
+        def expect_patient_specific_test(presenter, test_description)
+          presenter.patient_requests_by_lab.each do |lab_name, patient_rules|
             patient_rules.each do |patient_rule|
               expect(patient_rule.test_description).to eq(test_description)
             end
           end
         end
 
-        def expect_no_request_descriptions_required
-          expect(@patient).not_to have_patient_requests
+        def expect_no_request_descriptions_required(presenter)
+          expect(presenter).not_to have_patient_requests
         end
 
-        def expect_request_description_required(request_description_code)
+        def expect_request_description_required(presenter, request_description_code)
           expected_request_description =
             Renalware::Pathology::RequestDescription.find_by(code: request_description_code)
-          @patient.global_requests_by_lab.each do |lab_name, request_descriptions|
+
+          presenter.global_requests_by_lab.each do |lab_name, request_descriptions|
             expect(expected_request_description).to eq(expected_request_description)
           end
         end
@@ -71,7 +77,7 @@ module World
 
         # @section expectations
         #
-        def expect_patient_summary_to_match_table(patient_id, expected_table)
+        def expect_patient_summary_to_match_table(_presenter, patient_id, expected_table)
           table_from_html =
             find_by_id("patient_#{patient_id}_summary")
               .all("tr")
@@ -92,15 +98,15 @@ module World
           expect(table_from_html).to eq(expected_table)
         end
 
-        def expect_patient_specific_test(test_description)
+        def expect_patient_specific_test(_presenter, test_description)
           expect(page).to have_content(test_description)
         end
 
-        def expect_no_request_descriptions_required
+        def expect_no_request_descriptions_required(_presenter)
           expect(page).to have_content("No tests required.")
         end
 
-        def expect_request_description_required(request_description_code)
+        def expect_request_description_required(_presenter, request_description_code)
           request_description =
             Renalware::Pathology::RequestDescription.find_by(code: request_description_code)
           expect(page.text.downcase).to include(request_description.name.downcase)
