a397c1a33a6fd685f0b4d7a5d2bef48c289d2235
Paul Nordstrom August <paul@nuagist.es>
2016-06-09 11:52:43 -0400

    Add initial JSONB seeding

--- db/seeds/demo/100_rabbit_demographics.rb
+++ db/seeds/demo/100_rabbit_demographics.rb
@@ -1,6 +1,6 @@
 module Renalware
   log '--------------------Adding Demographics for Roger RABBIT--------------------'
-  system_user = SystemUser.find
+  user = User.find_by(username: "bartsdoc")
   rabbit = Patient.find_by(family_name: 'RABBIT', given_name: 'Roger')
 
   rabbit.title = "Mr"
@@ -10,6 +10,14 @@ module Renalware
   rabbit.email = "rogerrabbit@rmail.co.uk"
   rabbit.religion_id = 1
   rabbit.language_id = 1
-  rabbit.by = system_user
+  rabbit.by = user
+  rabbit.document = {referral:
+    {referral_date: 1.week.ago,
+      referral_type: "Urgent",
+      referral_notes: "NB family friend of referrer",
+      referring_physician_name: "Dr Jack L N Hyde"},
+    admin_notes: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua."
+  }
   rabbit.save!
 end
