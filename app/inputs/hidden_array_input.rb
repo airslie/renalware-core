617ba641fee96340109463ed91258c74eb3665f2
Evan Rolfe <evanrolfe@gmail.com>
2016-06-15 15:13:13 +0100

    rubocop: line length

--- app/inputs/hidden_array_input.rb
+++ app/inputs/hidden_array_input.rb
@@ -3,7 +3,13 @@ class HiddenArrayInput < SimpleForm::Inputs::StringInput
     input_html_options[:type] ||= input_type
 
     Array(object.public_send(attribute_name)).map do |array_el|
-      @builder.hidden_field(nil, input_html_options.merge(value: array_el, name: "#{object_name}[#{attribute_name}][]"))
+      @builder.hidden_field(
+        nil,
+        input_html_options.merge(
+          value: array_el,
+          name: "#{object_name}[#{attribute_name}][]"
+        )
+      )
     end.join.html_safe
   end
 
