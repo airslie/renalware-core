aab51a8d4bf81c0c55d2fda85fd5925e9471e723
Evan Rolfe <evanrolfe@gmail.com>
2016-06-15 12:34:22 +0100

    extract code into more private methods & call uniq on the patients ids array

--- app/controllers/renalware/pathology/forms_controller.rb
+++ app/controllers/renalware/pathology/forms_controller.rb
@@ -5,7 +5,8 @@ module Renalware
     class FormsController < Pathology::BaseController
       layout "renalware/layouts/printable"
       before_filter :load_patients
-      before_filter :load_users_and_clinics, only: :index
+      before_filter :load_users, only: :index
+      before_filter :load_clinics, only: :index
 
       def index
         request_forms = RequestFormPresenter.wrap(
@@ -19,29 +20,36 @@ module Renalware
           telephone: request_forms.first.telephone,
           users: @users,
           clinics: @clinics,
-          patient_ids: @patients.map(&:id)
+          patient_ids: @patients.map(&:id).uniq
         }
       end
 
       private
 
-      def load_users_and_clinics
+      def load_users
         @users = User.ordered
+        @user = find_user
+      end
+
+      def find_user
+        if params[:user_id].present?
+          User.find(params[:user_id])
+        else
+          @users.first
+        end
+      end
+
+      def load_clinics
         @clinics = Clinics::Clinic.ordered
+        @clinic = find_clinic
+      end
 
-        @user =
-          if params[:user_id].present?
-            User.find(params[:user_id])
-          else
-            @users.first
-          end
-
-        @clinic =
-          if params[:clinic_id].present?
-            @clinic = Clinics::Clinic.find(params[:clinic_id])
-          else
-            @clinics.first
-          end
+      def find_clinic
+        if params[:clinic_id].present?
+          @clinic = Clinics::Clinic.find(params[:clinic_id])
+        else
+          @clinics.first
+        end
       end
 
       def load_patients
