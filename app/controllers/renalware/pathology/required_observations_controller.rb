4167dece0e78c66e3ffb3a9dee9dbf374bebfbd3
Evan Rolfe <evanrolfe@gmail.com>
2016-06-16 11:06:27 +0100

    add button to generate request form on patient required observations page

--- app/controllers/renalware/pathology/required_observations_controller.rb
+++ app/controllers/renalware/pathology/required_observations_controller.rb
@@ -9,12 +9,17 @@ module Renalware
           global_pathology = @patient.required_observation_requests(clinic)
           patient_pathology = @patient.required_patient_pathology
           clinics = Renalware::Clinics::Clinic.ordered
+          request_form_options = RequestAlgorithm::RequestFormOptions.new(
+            patient_ids: [@patient.id],
+            clinic_id: clinic.id
+          )
 
           render :index, locals: {
             global_pathology: global_pathology,
             patient_pathology: patient_pathology,
             clinics: clinics,
-            clinic: clinic
+            clinic: clinic,
+            request_form_options: request_form_options
           }
       end
 
