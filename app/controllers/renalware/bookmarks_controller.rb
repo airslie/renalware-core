115e4f1e9f841fcfc3725e62575f2ad5b1f89f39
Evan Rolfe <evanrolfe@gmail.com>
2016-06-20 18:51:35 +0100

    rubocop: get rid of useless var assignment

--- app/controllers/renalware/bookmarks_controller.rb
+++ app/controllers/renalware/bookmarks_controller.rb
@@ -4,7 +4,7 @@ module Renalware
 
     def create
       patient = Renalware::Patient.find(params[:patient_id])
-      bookmark = Renalware::Patients::Bookmark.new(user: current_user, patient: patient)
+      Renalware::Patients::Bookmark.new(user: current_user, patient: patient)
 
       redirect_to patient_path(id: patient.id), notice: t(".success", model_name: "bookmark")
     end
