94bf5f7c55313bf294b9124519de697672300a6e
Nicholas Henry <nicholas.henry@gmail.com>
2016-06-20 14:24:05 -0400

    Correct the spec

--- spec/documents/renalware/year_dated_confirmation_spec.rb
+++ spec/documents/renalware/year_dated_confirmation_spec.rb
@@ -8,21 +8,23 @@ module Renalware
 
     describe "#to_s" do
       context "given a completed confirmation" do
-        it { expect(year_dated_confirmation.to_s).to eq("Yes (2015)") }
-      end
-
-      context "given year is missing" do
-        before { year_dated_confirmation.confirmed_on_year = nil }
-
-        it "returns blank" do
-          expect(year_dated_confirmation.to_s).to eq("yes")
+        it "returns the year and status as a string" do 
+          expect(year_dated_confirmation.to_s).to eq("Yes (2015)")
         end
       end
 
-      context "given status is missing" do
+      context "given the year is missing" do
+        before { year_dated_confirmation.confirmed_on_year = nil }
+
+        it "returns the status only as a string" do
+          expect(year_dated_confirmation.to_s).to eq("Yes")
+        end
+      end
+
+      context "given the status is missing" do
         before { year_dated_confirmation.status = nil }
 
-        it "returns blank" do
+        it "returns the year only as a string" do
           expect(year_dated_confirmation.to_s).to eq("(2015)")
         end
       end
