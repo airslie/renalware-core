a0e9f6636f60590effe063a704f78a006c65d470
Hugo Frappier <hugo@cekoya.com>
2016-06-21 15:14:41 -0400

    Use parentheses

--- app/helpers/renalware/clinic_helper.rb
+++ app/helpers/renalware/clinic_helper.rb
@@ -3,13 +3,12 @@ module Renalware
     def link_to_clinic_visit_letter(patient, clinic_visit)
       visit = Letters.cast_clinic_visit(clinic_visit)
       if visit.letter.present?
-        link_to "Preview Letter", patient_letters_letter_path(patient, visit.letter)
+        link_to("Preview Letter", patient_letters_letter_path(patient, visit.letter))
       else
-        path = new_patient_letters_letter_path(
+        link_to("Draft Letter", new_patient_letters_letter_path(
           @patient,
           event_type: clinic_visit.class.name.to_s, event_id: clinic_visit.id
-        )
-        link_to "Draft Letter", path
+        ))
       end
     end
   end
