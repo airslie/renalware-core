a73078caaee369bee1ebab1f6a392ec8c835a5e7
Hugo Frappier <hugo@cekoya.com>
2016-06-21 11:56:15 -0400

    Rename link

--- app/helpers/renalware/clinic_helper.rb
+++ app/helpers/renalware/clinic_helper.rb
@@ -1,6 +1,6 @@
 module Renalware
   module ClinicHelper
-    def link_to_create_or_edit_clinic_visit_letter(patient, clinic_visit)
+    def link_to_clinic_visit_letter(patient, clinic_visit)
       visit = Letters.cast_clinic_visit(clinic_visit)
       if visit.letter.present?
         link_to "Preview Letter", patient_letters_letter_path(patient, visit.letter)
