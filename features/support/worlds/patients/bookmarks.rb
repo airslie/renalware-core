76de6c276b72f18e24fb5daa4859241fac4e31cc
Evan Rolfe <evanrolfe@gmail.com>
2016-06-22 17:56:10 +0100

    rename method to find_patient_by_name and move to patients world module

--- features/support/worlds/patients/bookmarks.rb
+++ features/support/worlds/patients/bookmarks.rb
@@ -2,19 +2,11 @@ module World
   module Patients
     module Bookmarks
       module Domain
-        # @section helpers
-        #
-        def find_patient(given_name, family_name)
-          Renalware::Patient.find_by!(
-            given_name: given_name, family_name: family_name
-          )
-        end
-
         # @section commands
         #
         def bookmark_patient(user, patient_given_name, patient_family_name)
           user = Renalware::Patients.cast_user(user)
-          patient = find_patient(patient_given_name, patient_family_name)
+          patient = find_patient_by_name(patient_given_name, patient_family_name)
 
           Renalware::Patients::Bookmark.create!(user: user, patient: patient)
         end
@@ -33,7 +25,7 @@ module World
         def bookmark_patient(user, patient_given_name, patient_family_name)
           login_as user
 
-          patient = find_patient(patient_given_name, patient_family_name)
+          patient = find_patient_by_name(patient_given_name, patient_family_name)
 
           visit patient_path(id: patient.id)
 
