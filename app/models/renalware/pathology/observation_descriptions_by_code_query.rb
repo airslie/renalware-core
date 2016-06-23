bf0f74e0dd3fac9496b599873b83b75325212803
Nicholas Henry <nicholas.henry@gmail.com>
2016-06-23 16:05:36 -0400

    Simplify logic of diff codes

--- app/models/renalware/pathology/observation_descriptions_by_code_query.rb
+++ app/models/renalware/pathology/observation_descriptions_by_code_query.rb
@@ -21,7 +21,7 @@ module Renalware
       def verify_all_records_found(records)
         found_codes = records.map(&:code)
 
-        missing_records = found_codes - @codes | @codes - found_codes
+        missing_records = found_codes - @codes
         if missing_records.present?
           raise ActiveRecord::RecordNotFound.new("Missing records for #{missing_records}")
         end
