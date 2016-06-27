2a0590842a1d90cda93e7539595a47dc668985f8
Paul Nordstrom August <paul@nuagist.es>
2016-06-27 17:42:35 +0200

    Add 'Jessica RABBIT' bookmarks to each user

--- db/seeds/demo/160_patients_bookmarks.rb
+++ db/seeds/demo/160_patients_bookmarks.rb
@@ -5,10 +5,20 @@ module Renalware
   roger_rabbit = Patient.find_by(family_name: 'RABBIT', given_name: 'Roger')
   User.all.each do |user|
     logcount += 1
-
     patients_user = Patients.cast_user(user)
     patients_user.bookmarks.create!(patient: roger_rabbit)
   end
 
   log "#{logcount} Roger RABBIT Bookmarks seeded"
+
+  logcount=0
+  jessica_rabbit = Patient.find_by(family_name: 'RABBIT', given_name: 'Jessica')
+  User.all.each do |user|
+    logcount += 1
+    patients_user = Patients.cast_user(user)
+    patients_user.bookmarks.create!(patient: jessica_rabbit)
+  end
+
+  log "#{logcount} Jessica RABBIT Bookmarks seeded"
+
 end
