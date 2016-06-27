8c3eed333e5041ec5462ae656a5922210d857ed8
Paul Nordstrom August <paul@nuagist.es>
2016-06-27 17:40:20 +0200

    Remove unused .csv file and change log title

--- db/seeds/demo/160_patients_bookmarks.rb
+++ db/seeds/demo/160_patients_bookmarks.rb
@@ -1,8 +1,6 @@
 module Renalware
   log "--------------------Adding Bookmarks --------------------"
 
-  file_path = File.join(demo_path, "patients_bookmarks.csv")
-
   logcount=0
   roger_rabbit = Patient.find_by(family_name: 'RABBIT', given_name: 'Roger')
   User.all.each do |user|
@@ -12,5 +10,5 @@ module Renalware
     patients_user.bookmarks.create!(patient: roger_rabbit)
   end
 
-  log "#{logcount} Bookmarks seeded"
+  log "#{logcount} Roger RABBIT Bookmarks seeded"
 end
