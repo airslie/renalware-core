d78a3ff98ec2d72f05dabc06edffb6f478ebcd8f
Evan Rolfe <evanrolfe@gmail.com>
2016-06-29 11:24:49 +0100

    update the appointments demo data for stan

--- db/seeds/demo/150_appointments.rb
+++ db/seeds/demo/150_appointments.rb
@@ -6,7 +6,8 @@ module Renalware
   logcount=0
   CSV.foreach(file_path, headers: true) do |row|
     logcount += 1
-    starts_at = Time.now + rand(10).days
+    days_ahead = logcount.even? ? 30 : 40
+    starts_at = Time.now + days_ahead.days
     starts_at_array = row["starts_at"].split(":")
 
     Clinics::Appointment.find_or_create_by!(
