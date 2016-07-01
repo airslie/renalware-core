69c7bbde05e4d1320c4b3b03e05f0ec124a1f54b
Evan Rolfe <evanrolfe@gmail.com>
2016-07-01 18:59:57 +0100

    fix web spec

--- features/support/worlds/pathology/patient_algorithm.rb
+++ features/support/worlds/pathology/patient_algorithm.rb
@@ -97,6 +97,10 @@ module World
         end
 
         def expect_observations_from_patient(algorithm, observations_table)
+          algorithm.map! do |row|
+            row[0..-3]  # NOTE: Remove the last two columns (Edit/Delete)
+          end
+
           expect(algorithm).to eq(observations_table.raw)
         end
       end
