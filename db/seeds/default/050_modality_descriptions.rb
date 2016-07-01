32867b875640f78af560c68bd42a7d0cc60e3a5b
Paul Nordstrom August <paul@nuagist.es>
2016-07-01 12:16:49 +0200

    Use Barts .csv for modality seeding; enhance logging
    ultimately do via environment variable perhaps

--- db/seeds/default/050_modality_descriptions.rb
+++ db/seeds/default/050_modality_descriptions.rb
@@ -1,14 +1,18 @@
 module Renalware
-  log '--------------------Adding Modality Codes & Descriptions--------------------'
+  sitecode = 'BARTS'
+  log "--------------------Adding #{sitecode} Modality Codes & Descriptions--------------------"
 
-  file_path = File.join(default_path, 'modality_descriptions.csv')
+  file_path = File.join(default_path, 'modality_descriptions_barts.csv')
 
   logcount=0
   CSV.foreach(file_path, headers: true) do |row|
     logcount += 1
-    Modalities::Description.find_or_create_by!(code: row['code']) do |code|
-      code.name = row['name']
+    modalcode = row['code']
+    modalname = row['name']
+    Modalities::Description.find_or_create_by!(code: modalcode) do |code|
+      code.name = modalname
     end
+    log "--#{modalcode}: #{modalname}"
   end
 
   log "#{logcount} Modality Codes seeded"
