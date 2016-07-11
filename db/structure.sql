52ee9a81f76c72ce8e61738dd40f3182dfe44070
Nicholas Henry <nicholas.henry@gmail.com>
2016-07-11 19:16:15 -0400

    Fix bad migration file

--- db/structure.sql
+++ db/structure.sql
@@ -1931,8 +1931,8 @@ CREATE TABLE patient_bookmarks (
     id integer NOT NULL,
     patient_id integer NOT NULL,
     user_id integer NOT NULL,
-    created_at timestamp without time zone,
-    updated_at timestamp without time zone
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
 );
 
 
