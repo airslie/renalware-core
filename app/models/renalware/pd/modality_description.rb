f01b41517adcecc19b79acf0fba50d15c86f2811
Nicholas Henry <nicholas.henry@gmail.com>
2016-07-11 14:10:52 -0400

    Extract PD modality description

---
+++ app/models/renalware/pd/modality_description.rb
@@ -0,0 +1,17 @@
+require_dependency "renalware/pd"
+
+module Renalware
+  module PD
+    class ModalityDescription < ActiveType::Record[Renalware::Modalities::Description]
+      NAMES = ["PD-APD", "PD-CAPD", "PD Rest on HD", "PD-Assisted APD", "PD-PrePD"].freeze
+
+      def self.include?(record)
+        filtered.map(&:id).include?(record.id)
+      end
+
+      def self.filtered
+        where(name: NAMES)
+      end
+    end
+  end
+end
--- app/models/renalware/pd/patient.rb
+++ app/models/renalware/pd/patient.rb
@@ -3,14 +3,8 @@ require_dependency "renalware/pd"
 module Renalware
   module PD
     class Patient < ActiveType::Record[Renalware::Patient]
-      MODALITY_NAMES = ["PD-APD", "PD-CAPD", "PD Rest on HD", "PD-Assisted APD", "PD-PrePD"].freeze
-
       def treated?
-        modalities.with_deleted.any? { |modality| pd_modality?(modality.description) }
-      end
-
-      def pd_modality?(description)
-        MODALITY_NAMES.include?(description.name)
+        modalities.with_deleted.any? { |modality| PD::ModalityDescription.include?(modality.description) }
       end
     end
   end
