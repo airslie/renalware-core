4dab9da6e2b1c76dd1df50ed426fc4d6afcbe649
Nicholas Henry <nicholas.henry@gmail.com>
2016-07-12 12:58:45 -0400

    Extract SideNav helper

--- app/helpers/renalware/patients_helper.rb
+++ app/helpers/renalware/patients_helper.rb
@@ -3,20 +3,5 @@ module Renalware
     def med_color_tag(med_type)
       med_type.blank? ? "drug" : med_type
     end
-
-    def display_pd_menu?(patient)
-      pd_patient = Renalware::PD.cast_patient(patient)
-      pd_patient.treated?
-    end
-
-    def display_hd_menu?(patient)
-      hd_patient = Renalware::HD.cast_patient(patient)
-      hd_patient.treated?
-    end
-
-    def find_user_bookmark_for_patient(patient)
-      user = Renalware::Patients.cast_user(current_user)
-      user.bookmark_for_patient(patient)
-    end
   end
 end
---
+++ app/helpers/renalware/side_nav_helper.rb
@@ -0,0 +1,18 @@
+module Renalware
+  module SideNavHelper
+    def display_pd_menu?(patient)
+      pd_patient = Renalware::PD.cast_patient(patient)
+      pd_patient.treated?
+    end
+
+    def display_hd_menu?(patient)
+      hd_patient = Renalware::HD.cast_patient(patient)
+      hd_patient.treated?
+    end
+
+    def find_user_bookmark_for_patient(patient)
+      user = Renalware::Patients.cast_user(current_user)
+      user.bookmark_for_patient(patient)
+    end
+  end
+end
