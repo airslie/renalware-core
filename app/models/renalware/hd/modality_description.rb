41d35b976b9256c3a3210dcf2d7c05dea992263c
Nicholas Henry <nicholas.henry@gmail.com>
2016-07-12 16:29:58 -0400

    Remove unused methods

--- app/models/renalware/hd/modality_description.rb
+++ app/models/renalware/hd/modality_description.rb
@@ -3,9 +3,6 @@ require_dependency "renalware/hd"
 module Renalware
   module HD
     class ModalityDescription < Modalities::Description
-      def self.include?(record)
-        exists?(description: record)
-      end
     end
   end
 end
--- app/models/renalware/pd/modality_description.rb
+++ app/models/renalware/pd/modality_description.rb
@@ -3,9 +3,6 @@ require_dependency "renalware/pd"
 module Renalware
   module PD
     class ModalityDescription < Modalities::Description
-      def self.include?(record)
-        exists?(description: record)
-      end
     end
   end
 end
