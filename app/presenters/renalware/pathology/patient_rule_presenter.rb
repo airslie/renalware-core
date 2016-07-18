ba780eda2a5ff9e148617c210dec21f72d1a011d
Evan Rolfe <evanrolfe@gmail.com>
2016-07-18 14:02:35 +0100

    use pluralize and move patient_rule string generation to presenter from template

--- app/presenters/renalware/pathology/patient_rule_presenter.rb
+++ app/presenters/renalware/pathology/patient_rule_presenter.rb
@@ -3,10 +3,31 @@ require_dependency "renalware/pathology"
 module Renalware
   module Pathology
     class PatientRulePresenter < SimpleDelegator
-      def sample_number_bottles_string
-        suffix = sample_number_bottles > 1 ? "s" : ""
+      def to_s
+        str = test_description
+        str +=
+          if sample_type.present? && sample_number_bottles.present?
+            " (#{sample_type}, #{sample_number_bottles_string})"
+          elsif sample_type.present?
+            " (#{sample_type})"
+          elsif sample_number_bottles.present?
+            " (#{sample_number_bottles_string})"
+          end
+        str
+      end
 
-        "#{sample_number_bottles} bottle#{suffix}"
+      private
+
+      def sample_number_bottles_string
+        bottle = "bottle"
+        unit =
+          if sample_number_bottles > 1
+            bottle.pluralize
+          else
+            bottle
+          end
+
+        "#{sample_number_bottles} #{unit}"
       end
     end
   end
