9e6852d4f7aa8de1c3f68ff4c88f55321bb66dd4
Hugo Frappier <hugo@cekoya.com>
2016-07-26 16:33:44 -0400

    Render letter partial to get the HTML

--- app/models/renalware/letters/letter/typed.rb
+++ app/models/renalware/letters/letter/typed.rb
@@ -7,11 +7,22 @@ module Renalware
         TypedLetterPolicy
       end
 
-      def archive(by:)
+      def archive(by:, renderer: HTMLRenderer)
         becomes!(Archived).tap do |letter|
           letter.by = by
           letter.build_archive(created_by: by)
-          letter.archive.content = "<div>Archive</div>"
+          letter.archive.content = renderer.new.call(letter)
+        end
+      end
+
+      class HTMLRenderer
+        def call(letter)
+          context = Renalware::Letters::LettersController.new()
+          presented_letter = LetterPresenterFactory.new(letter)
+          context.render_to_string(
+            partial: "/renalware/letters/letters/preview",
+            locals: { letter: presented_letter}
+          )
         end
       end
     end
--- spec/models/renalware/letters/letter/typed_spec.rb
+++ spec/models/renalware/letters/letter/typed_spec.rb
@@ -2,25 +2,43 @@ require "rails_helper"
 
 module Renalware::Letters
   describe Letter::Typed do
-    let(:patient) { double(:patient) }
-    let(:user) { build(:user) }
+    include LettersSpecHelper
 
-    subject(:letter) { Letter::Typed.new() }
+    let(:user) { build(:user) }
+    let(:doctor) { build(:letter_doctor) }
+    let(:patient) { build(:letter_patient, doctor: doctor) }
+    let(:letter) { build_letter(to: :patient, patient: patient) }
+
+    subject(:typed_letter) { letter.becomes(Letter::Typed) }
 
     describe "#archive" do
       it "archives the letter" do
-        archived_letter = letter.archive(by: user)
+        archived_letter = typed_letter.archive(by: user, renderer: DummyHTMLRenderer)
         expect(archived_letter).to be_archived
       end
 
-      it "archives the content" do
-        archived_letter = letter.archive(by: user)
-        expect(archived_letter.content).to include("<div>")
+      it "records who archived the letter" do
+        archived_letter = typed_letter.archive(by: user, renderer: DummyHTMLRenderer)
+        expect(archived_letter.archived_by).to eq(user)
       end
 
-      it "records who archived the letter" do
-        archived_letter = letter.archive(by: user)
-        expect(archived_letter.archived_by).to eq(user)
+      context "with persisted letter" do
+        let(:doctor) { create(:letter_doctor) } # Doctor has to exist before saving a letter
+
+        before do
+          letter.save
+        end
+
+        it "archives the content" do
+          archived_letter = typed_letter.archive(by: user)
+          expect(archived_letter.content).to match(/class="letter-preview"/)
+          expect(archived_letter.content).to include(patient.full_name)
+        end
+      end
+    end
+
+    class DummyHTMLRenderer
+      def call(letter)
       end
     end
   end
