f93f83b5cc2d57d747cebb1470716eb0d95cec46
Hugo Frappier <hugo@cekoya.com>
2016-07-26 09:50:57 -0400

    Remove !

--- spec/models/renalware/letters/letter/typed_spec.rb
+++ spec/models/renalware/letters/letter/typed_spec.rb
@@ -7,14 +7,14 @@ module Renalware::Letters
 
     subject(:letter) { Letter::Typed.new() }
 
-    describe "#archive!" do
+    describe "#archive" do
       it "creates an Archived letter" do
-        letter = subject.archive!(by: user)
+        letter = subject.archive(by: user)
         expect(letter).to be_a(Letter::Archived)
       end
 
       it "archives a copy of the letter" do
-        letter = subject.archive!(by: user)
+        letter = subject.archive(by: user)
         expect(letter.archived_copy).to include(".letter-preview")
       end
     end
