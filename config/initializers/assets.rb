58039663efeb7e67008bc1da7496dc73b867fb71
Hugo Frappier <hugo@cekoya.com>
2016-07-28 16:57:32 -0400

    Precompile pdf.css to see if it solved a problem on Heroku

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,4 +7,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w(print/pathology_request_forms.css )
 Rails.application.config.assets.precompile += %w(iframeResizer.contentWindow.js)
-Rails.application.config.assets.precompile += %w(watermark.css)
+Rails.application.config.assets.precompile += %w(pdf.css watermark.css)
