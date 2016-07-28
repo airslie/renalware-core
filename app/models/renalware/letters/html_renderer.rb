a0274b112bab5c2430edcde0fe84b17571d73c5d
Hugo Frappier <hugo@cekoya.com>
2016-07-28 11:21:47 -0400

    Remove namespace

--- app/models/renalware/letters/html_renderer.rb
+++ app/models/renalware/letters/html_renderer.rb
@@ -2,7 +2,7 @@ module Renalware
   module Letters
     class HTMLRenderer
       def call(letter)
-        context = Renalware::Letters::LettersController.new()
+        context = LettersController.new()
         context.render_to_string(
           partial: "/renalware/letters/formatted_letters/letter",
           locals: { letter: letter }
