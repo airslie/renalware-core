c9d0adbce1035c1240a462afefecf008cf778360
Hugo Frappier <hugo@cekoya.com>
2016-07-28 15:19:53 -0400

    Render PDF of a letter

--- app/controllers/renalware/letters/formatted_letters_controller.rb
+++ app/controllers/renalware/letters/formatted_letters_controller.rb
@@ -10,12 +10,34 @@ module Renalware
       def show
         letter = @patient.letters.find(params[:letter_id])
         @content = present_letter(letter).content
+
+        respond_to do |format|
+          format.html
+          format.pdf { render_pdf }
+        end
       end
 
       private
 
       def present_letter(letter)
         LetterPresenterFactory.new(letter)
+      end
+
+      def render_pdf
+        render pdf_options.merge(pdf: "letter", disposition: "attachment")
+      end
+
+      def pdf_options
+        {
+          page_size: "A4",
+          background: true,
+          print_media_type: false,
+          no_background: false,
+          footer: {
+            font_size: 8,
+            right: 'page [page] of [topage]'
+          }
+        }
       end
     end
   end
