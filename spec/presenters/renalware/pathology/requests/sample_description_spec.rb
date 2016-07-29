810dbfe8482fdd94108daecb325d73107773187b
Evan Rolfe <evanrolfe@gmail.com>
2016-07-29 16:24:25 +0100

    explicitly call .to_s in spec

--- spec/presenters/renalware/pathology/requests/sample_description_spec.rb
+++ spec/presenters/renalware/pathology/requests/sample_description_spec.rb
@@ -9,13 +9,13 @@ describe Renalware::Pathology::Requests::SampleDescription do
   end
 
   context "both sample_type and sample_number_bottles are present" do
-    it { expect(sample_description).to include("#{sample_type}", "#{sample_number_bottles} bottles") }
+    it { expect(sample_description.to_s).to include("#{sample_type}", "#{sample_number_bottles} bottles") }
   end
 
   context "only sample_type is present" do
     let(:sample_number_bottles) { nil }
 
-    it { expect(sample_description).to include("#{sample_type}") }
+    it { expect(sample_description.to_s).to include("#{sample_type}") }
   end
 
   context "only sample_number_bottles is present" do
@@ -23,14 +23,14 @@ describe Renalware::Pathology::Requests::SampleDescription do
       let(:sample_type) { nil }
       let(:sample_number_bottles) { 3 }
 
-      it { expect(sample_description).to include("#{sample_number_bottles} bottles") }
+      it { expect(sample_description.to_s).to include("#{sample_number_bottles} bottles") }
     end
 
     context "sample_number_bottles = 1" do
       let(:sample_type) { nil }
       let(:sample_number_bottles) { 1 }
 
-      it { expect(sample_description).to include("#{sample_number_bottles} bottle") }
+      it { expect(sample_description.to_s).to include("#{sample_number_bottles} bottle") }
     end
   end
 end
