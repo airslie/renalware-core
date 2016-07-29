639824a146eaabce41eed34e53f9d7ee1584fb8a
Nicholas Henry <nicholas.henry@gmail.com>
2016-07-29 14:21:17 -0400

    Fix style violation

--- app/models/renalware/medications/prescription_termination.rb
+++ app/models/renalware/medications/prescription_termination.rb
@@ -7,7 +7,8 @@ module Renalware
 
       belongs_to :prescription
 
-      validates :terminated_on, timeliness: { type: :date, after: ->(record) { record.prescription.prescribed_on} }
+      validates :terminated_on,
+        timeliness: { type: :date, after: ->(record) { record.prescription.prescribed_on} }
     end
   end
 end
