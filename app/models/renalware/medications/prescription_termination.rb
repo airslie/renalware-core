6aba9a02a753693a8c90af60dc8fba99debfcc3e
Nicholas Henry <nicholas.henry@gmail.com>
2016-07-29 12:18:01 -0400

    Add validation on the terminated_on attribute

--- app/models/renalware/medications/prescription_termination.rb
+++ app/models/renalware/medications/prescription_termination.rb
@@ -7,7 +7,7 @@ module Renalware
 
       belongs_to :prescription
 
-      validates :terminated_on, presence: true
+      validates :terminated_on, timeliness: { type: :date, after: ->(record) { record.prescription.prescribed_on} }
     end
   end
 end
--- spec/models/renalware/medications/prescription_termination_spec.rb
+++ spec/models/renalware/medications/prescription_termination_spec.rb
@@ -2,6 +2,29 @@ require "rails_helper"
 
 module Renalware
   RSpec.describe Medications::PrescriptionTermination, type: :model do
-    it { is_expected.to validate_presence_of :terminated_on }
+    describe "validations" do
+      describe "terminated_on" do
+        let(:prescription) { build(:prescription, prescribed_on: "2011-01-01") }
+
+        context "given the date is after prescribed on" do
+          let(:termination) do
+            build(:prescription_termination,
+              terminated_on: "2012-01-01", prescription: prescription)
+          end
+
+          it { expect(termination).to be_valid }
+        end
+
+        context "given the date is before prescribed on" do
+          let(:termination) do
+            build(:prescription_termination,
+              terminated_on: "2010-01-10", prescription: prescription)
+          end
+
+          before { termination.valid? }
+          it { expect(termination.errors[:terminated_on]).to match([/after/]) }
+        end
+      end
+    end
   end
 end
