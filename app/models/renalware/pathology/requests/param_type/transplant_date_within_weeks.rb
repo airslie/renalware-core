73b9b653a2e4192b02dba144a5963d7e89e8f700
Evan Rolfe <evanrolfe@gmail.com>
2016-08-05 08:59:17 +0100

    new ParamType::TransplantDateWithinWeeks

---
+++ app/models/renalware/pathology/requests/param_type/transplant_date_within_weeks.rb
@@ -0,0 +1,27 @@
+require_dependency "renalware/pathology"
+
+module Renalware
+  module Pathology
+    module Requests
+      module ParamType
+        class TransplantDateWithinWeeks
+          def initialize(patient, _param_id, _param_comparison_operator, param_comparison_value)
+            @patient = patient
+            @weeks_ago = param_comparison_value.to_i
+          end
+
+          def required?
+            recipient_operation.performed_on > @weeks_ago.weeks.ago
+          end
+
+          private
+
+          def recipient_operation
+            @operations ||=
+              Renalware::Transplants::RecipientOperation.for_patient(@patient).reversed.first
+          end
+        end
+      end
+    end
+  end
+end
