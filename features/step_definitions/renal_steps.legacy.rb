219b0498026ec2e74a74ec26cb1298ba118e11fc
Nicholas Henry <nicholas.henry@gmail.com>
2016-08-10 14:02:17 -0400

    Extract non-legacy steps

--- features/step_definitions/renal_steps.legacy.rb
+++ features/step_definitions/renal_steps.legacy.rb
@@ -17,15 +17,3 @@ Then(/^Patty's renal profile is updated$/) do
     expect(page).to have_content("Cystinuria")
   end
 end
-
-When(/^Donna reviews Patty's clinical summary$/) do
-  @clinical_summary = review_clinical_summary(patient: @patty, user: @donna)
-end
-
-Then(/^Donna should see these current prescriptions in the clinical summary$/) do |table|
-  expect_current_prescriptions_to_match(@clinical_summary.current_prescriptions, table.hashes)
-end
-
-Then(/^Donna should see these current problems in the clinical summary:$/) do |table|
-  expect_problems_to_match_table(@clinical_summary.current_problems, table)
-end
