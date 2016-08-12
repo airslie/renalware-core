f226338cbc749b2a25754f2667c13bda03c99fc9
Evan Rolfe <evanrolfe@gmail.com>
2016-08-12 16:48:39 +0200

    remove unecessary table_name setting from model

--- app/models/renalware/pathology/requests/drug_category.rb
+++ app/models/renalware/pathology/requests/drug_category.rb
@@ -4,8 +4,6 @@ module Renalware
   module Pathology
     module Requests
       class DrugCategory < ActiveRecord::Base
-        self.table_name = "pathology_requests_drug_categories"
-
         has_and_belongs_to_many :drugs, join_table: "pathology_requests_drugs_drug_categories"
 
         validates :name, presence: true
