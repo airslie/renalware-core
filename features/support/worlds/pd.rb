ef275a724ab7eb268229b59d4b6504dd98582aab
Evan Rolfe <evanrolfe@gmail.com>
2016-08-23 11:20:47 +0100

    fix cucumber world for new RevisePrescription behaviour

--- features/support/worlds/pd.rb
+++ features/support/worlds/pd.rb
@@ -3,10 +3,8 @@ module World
       def expect_exit_site_prescriptions_to_be_revised(patient, exit_site_infection)
         prescriptions = exit_site_infection.prescriptions.order(created_at: :asc)
         current_prescription = prescriptions.current.first
-        terminated_prescription = prescriptions.terminated.first
 
         expect(current_prescription.patient).to eq(patient)
-        expect(terminated_prescription.patient).to eq(patient)
       end
   end
 end
