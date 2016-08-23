123958eb07d1bff9ae0ee4061fb303654871f07b
Hugo Frappier <hugo@cekoya.com>
2016-08-23 10:39:04 -0400

    Reject returns a draft letter

--- app/controllers/renalware/letters/rejected_letters_controller.rb
+++ app/controllers/renalware/letters/rejected_letters_controller.rb
@@ -7,10 +7,10 @@ module Renalware
 
       def create
         letter = @patient.letters.pending.find(params[:letter_id])
-        typed_letter = letter.reject(by: current_user)
-        typed_letter.save!
+        draft_letter = letter.reject(by: current_user)
+        draft_letter.save!
 
-        redirect_to patient_letters_letter_path(@patient, typed_letter), notice: t(".success")
+        redirect_to patient_letters_letter_path(@patient, draft_letter), notice: t(".success")
       end
     end
   end
