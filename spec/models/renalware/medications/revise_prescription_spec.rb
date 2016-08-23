d2160d03a3cce8a1863e2f68168d0ceaefe6b53e
Evan Rolfe <evanrolfe@gmail.com>
2016-08-23 15:49:27 +0100

    refactor RevisePrescription spec

--- spec/models/renalware/medications/revise_prescription_spec.rb
+++ spec/models/renalware/medications/revise_prescription_spec.rb
@@ -2,51 +2,48 @@ require "rails_helper"
 
 module Renalware::Medications
   RSpec.describe RevisePrescription do
-    let!(:patient) { create(:patient) }
-    let(:old_dose_amount) { "100" }
-    let!(:prescription) { create(:prescription, patient: patient, dose_amount: old_dose_amount) }
+    let(:patient) { create(:patient) }
+    let(:original_dose_amount) { "100" }
+    let(:original_prescription) { create(:prescription, patient: patient, dose_amount: original_dose_amount) }
 
     describe "#call" do
-      let!(:user) { create(:user) }
-      let(:params) { { dose_amount: new_dose_amount } }
-      subject(:command) { RevisePrescription.new(prescription).call(params.merge(by: user)) }
+      let(:user) { create(:user) }
 
       context "updating the prescription's dose_amount with a valid value" do
-        let(:new_dose_amount) { "200" }
+        let(:revised_dose_amount) { "200" }
 
         before do
-          command
+          RevisePrescription.new(original_prescription)
+                            .call(dose_amount: revised_dose_amount, by: user)
         end
 
-        it "terminates the existing prescription and creates a new one" do
+        it "terminates the original prescription and creates a new one" do
           expect(patient.prescriptions.count).to eq(2)
         end
 
-        it "updates the dose_amount on the new prescription" do
-          expect(patient.prescriptions.current.first.dose_amount).to eq(new_dose_amount)
+        it "create a new prescription with the specified dose amount" do
+          expect(patient.prescriptions.current.first.dose_amount).to eq(revised_dose_amount)
         end
 
-        it "keeps the old dose_amount on the terminated prescription" do
-          expect(patient.prescriptions.terminated.first.dose_amount).to eq(old_dose_amount)
+        it "retains the original dose_amount on the terminated prescription" do
+          expect(patient.prescriptions.terminated.first.dose_amount).to eq(original_dose_amount)
         end
       end
 
       context "updating the prescription's dose_amount with an invalid value" do
-        let(:new_dose_amount) { nil }
-
         before do
           begin
-            command
+            RevisePrescription.new(original_prescription)
+                              .call(dose_amount: nil, by: user)
+
           rescue ActiveRecord::RecordInvalid
-            nil
+            # noop
           end
         end
 
         it "rolls back the transaction" do
           expect(patient.prescriptions.count).to eq(1)
-          expect(patient.prescriptions.current.count).to eq(1)
-          expect(patient.prescriptions.current.first).to eq(prescription)
-          expect(patient.prescriptions.terminated.count).to eq(0)
+          expect(patient.prescriptions.current.first).to eq(original_prescription)
         end
       end
     end
