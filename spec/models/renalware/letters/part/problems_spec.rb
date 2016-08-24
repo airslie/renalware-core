1f1c3af24ae16620311df560ddb9914e59ca14ef
Hugo Frappier <hugo@cekoya.com>
2016-08-24 17:07:43 -0400

    Fix letter part specs

--- spec/models/renalware/letters/part/problems_spec.rb
+++ spec/models/renalware/letters/part/problems_spec.rb
@@ -2,11 +2,13 @@ require "rails_helper"
 
 module Renalware::Letters
   describe Part::Problems do
-    let(:patient) { double(:patient, problems: [:problem]) }
+    let(:patient) { spy(:patient) }
     subject(:part) { Part::Problems.new(patient) }
 
-    it "delegates to the patient's current prescriptions"do
-      expect(part.to_a).to match([:problem])
+    it "delegates to the patient's current problems" do
+      part.to_a
+
+      expect(patient).to have_received(:problems)
     end
   end
 end
