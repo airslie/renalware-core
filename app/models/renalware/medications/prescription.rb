0d665f76132d5e86f4395862e27bc6228e9aa486
Tim Crowe <tim@woodpigeon.com>
2016-08-25 11:32:55 +0100

    Change default_search_order
    To `prescribed_on asc, drug_name asc`.

--- app/models/renalware/medications/prescription.rb
+++ app/models/renalware/medications/prescription.rb
@@ -50,7 +50,7 @@ module Renalware
       }
 
       def self.default_search_order
-        "prescribed_on desc"
+         ["prescribed_on asc", "drug_name asc"]
       end
 
       # @section attributes
