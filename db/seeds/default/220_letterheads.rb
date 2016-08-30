b3a87ac7b255e5a685dc82a003757da0686ebf25
Paul Nordstrom August <paul@nuagist.es>
2016-08-30 11:54:57 -0400

    Fix letterhead 'name' creation

--- db/seeds/default/220_letterheads.rb
+++ db/seeds/default/220_letterheads.rb
@@ -6,7 +6,7 @@ module Renalware
   logcount=0
   CSV.foreach(file_path, headers: true) do |row|
     logcount += 1
-    name = "(#{row["sitecode"]}) #{row["unitinfo"]}"
+    name = "(#{row["site_code"]}) #{row["unit_info"]}"
     letterhead = Letters::Letterhead.find_or_initialize_by(name: name)
     letterhead.site_code = row["site_code"]
     letterhead.unit_info = row["unit_info"]
