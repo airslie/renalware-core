a03c796481ed007b8296a636ed065d7af009ac9c
Paul Nordstrom August <paul@nuagist.es>
2016-08-30 11:21:31 -0400

    Add dummy dialysis unit names

--- db/seeds/default/190_dialysis_units.rb
+++ db/seeds/default/190_dialysis_units.rb
@@ -8,7 +8,7 @@ module Renalware
     logcount += 1
     hospital = Hospitals::Centre.find_by code: row["hospital_centre_code"]
     hospital.units.find_or_create_by!(unit_code: row["unit_code"]) do |unit|
-      unit.name = row["name"]
+      unit.name = row["unit_name"]
       unit.renal_registry_code = row["renal_registry_code"]
       unit.unit_type = row["unit_type"]
       unit.is_hd_site = true
