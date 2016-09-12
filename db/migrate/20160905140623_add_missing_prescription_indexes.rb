8c8d4a9da67866c734b31a11e0cc5edb225705f5
Tim Crowe <tim@woodpigeon.com>
2016-09-12 14:11:40 +0100

    Removed doctors -> practices index
    This will be added elsewhere

--- db/migrate/20160905140623_add_missing_prescription_indexes.rb
+++ db/migrate/20160905140623_add_missing_prescription_indexes.rb
@@ -7,6 +7,5 @@ class AddMissingPrescriptionIndexes < ActiveRecord::Migration
     add_index :medication_prescriptions, :medication_route_id
     add_index :medication_prescriptions, [:patient_id, :medication_route_id], name: 'idx_mp_patient_id_medication_route_id'
     add_index :medication_prescriptions, [:drug_id, :patient_id]
-    add_index :doctor_doctors_practices, [:practice_id, :doctor_id]
   end
 end
