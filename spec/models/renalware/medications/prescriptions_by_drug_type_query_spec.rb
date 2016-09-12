6c2736c722554207a6158c3c040691d864105d3b
Tim Crowe <tim@woodpigeon.com>
2016-09-12 16:00:53 +0100

    Use a named subject

--- spec/models/renalware/medications/prescriptions_by_drug_type_query_spec.rb
+++ spec/models/renalware/medications/prescriptions_by_drug_type_query_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 module Renalware::Medications
   RSpec.describe PrescriptionsByDrugTypeQuery, type: :model, focus: true do
-    subject { described_class.new(drug_type_name: "ESA") }
+    subject(:esa_presciption) { PrescriptionsByDrugTypeQuery.new(drug_type_name: "ESA") }
 
     describe "#call" do
       it "returns only patients with one or more current ESA prescription" do
