37e41616356601941ce695358b28854229a199c0
Hugo Frappier <hugo@cekoya.com>
2016-09-22 14:36:10 -0400

    Remove generated factory file

--- spec/factories/renalware_directory_people.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-  factory :renalware_directory_person, class: 'Renalware::Directory::Person' do
-    given_name "MyString"
-    family_name "MyString"
-    title "MyString"
-    created_by "MyString"
-    updated_by "MyString"
-  end
-end
