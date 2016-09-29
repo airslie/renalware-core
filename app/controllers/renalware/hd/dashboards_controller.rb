a94fa48f716a7ea8e9e36ace84cecbc7040957de
Tim Crowe <tim@woodpigeon.com>
2016-09-29 16:18:29 +0100

    Removed N+1 query and refactored Dashboards controller
    Moved aggregration of presentional data into an HD:DashboardsPresenter

--- app/controllers/renalware/hd/dashboards_controller.rb
+++ app/controllers/renalware/hd/dashboards_controller.rb
@@ -1,4 +1,3 @@
-require "collection_presenter"
 require_dependency "renalware/hd/base_controller"
 
 module Renalware
@@ -7,18 +6,8 @@ module Renalware
       before_filter :load_patient
 
       def show
-        preference_set = PreferenceSet.for_patient(@patient).first_or_initialize
-        access_profile = Renalware::Accesses.cast_patient(@patient).current_profile
-        profile = Profile.for_patient(@patient).first_or_initialize
-        sessions = Session.for_patient(@patient).includes(:signed_off_by).limit(10).ordered
-        dry_weights = DryWeight.for_patient(@patient).limit(10).includes(:assessor).ordered
-
         render locals: {
-          preference_set: preference_set,
-          profile: ProfilePresenter.new(profile, preference_set: preference_set),
-          access: Accesses::ProfilePresenter.new(access_profile),
-          sessions: CollectionPresenter.new(sessions, SessionPresenter),
-          dry_weights: CollectionPresenter.new(dry_weights, DryWeightPresenter)
+          dashboard: DashboardPresenter.new(@patient)
         }
       end
     end
