673e0820f6c204c55fc7defa89704bd35ebe1206
Hugo Frappier <hugo@cekoya.com>
2016-10-06 14:03:21 -0400

    Rename method and explain why we have a _keep flag.

--- app/models/renalware/letters/recipient_params_processor.rb
+++ app/models/renalware/letters/recipient_params_processor.rb
@@ -14,7 +14,7 @@ module Renalware
         params =
           if can_have_contact_as_addressee?(params)
             params = put_contact_as_addressee(params)
-            process_keep_flag(params)
+            translate_keep_flag_to_nested_attributes_destroy_flag(params)
           else
             clear_addressee(params)
           end
@@ -40,8 +40,13 @@ module Renalware
         params.merge(addressee: nil)
       end
 
-      def process_keep_flag(params)
-        params[:_destroy] =  (params[:_keep] == "1") ? "0" : "1"
+      def translate_keep_flag_to_nested_attributes_destroy_flag(params)
+        # ActiveRecord checks "_destroy" attribute to delete an existing
+        # record when mass assigning nested attributes.
+        # In our form, we need to use to "check" the contacts to be
+        # assigned as CC's (not to destroy them).  So we therefore convert
+        # the "_keep" flag to a "_destroy" one.
+        params[:_destroy] = (params[:_keep] == "1") ? "0" : "1"
         params.except(:_keep)
       end
 
