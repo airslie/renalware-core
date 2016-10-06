1bccae2f071281cf8e99a7ebc308238fd21eb0bf
Nicholas Henry <nicholas.henry@gmail.com>
2016-10-06 09:41:42 -0400

    Fix style violation

--- app/models/renalware/letters/contact.rb
+++ app/models/renalware/letters/contact.rb
@@ -12,7 +12,10 @@ module Renalware
       delegate :name, to: :address, prefix: true
 
       scope :with_person, -> { includes(:person) }
-      scope :ordered, -> { includes(:person).order("directory_people.family_name, directory_people.given_name") }
+      scope :ordered, -> {
+        includes(:person)
+          .order("directory_people.family_name, directory_people.given_name")
+      }
 
       def self.find_by_given_name(name)
         with_person.find_by(directory_people: { given_name: name })
