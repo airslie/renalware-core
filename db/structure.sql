31056088c8a9a991187ec979ade8eda66b5a8598
Tim Crowe <tim@woodpigeon.com>
2016-10-07 14:09:12 +0100

    Updated structure.sql
    For some reason a migration was listed twice and some indexes have been removed
    index_doctor_doctors_on_code
    index_doctor_doctors_practices_on_practice_id_and_doctor_id

--- db/structure.sql
+++ db/structure.sql
@@ -6789,4 +6789,3 @@ INSERT INTO schema_migrations (version) VALUES ('20161003204347');
 
 INSERT INTO schema_migrations (version) VALUES ('20161004185820');
 
-INSERT INTO schema_migrations (version) VALUES ('20160905140623');
