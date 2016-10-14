401c7dcae4f7cf4eee6dd2f9c1aff1b1757c01a3
Tim Crowe <tim@woodpigeon.com>
2016-10-14 13:21:26 +0100

    Scopes for session auditing
    Not tested yet and can’t integrate this til other PR merged

--- app/models/renalware/hd/sessions/auditable_sessions_by_period_query.rb
+++ app/models/renalware/hd/sessions/auditable_sessions_by_period_query.rb
@@ -9,8 +9,8 @@ module Renalware
 
         def call(patient:, starting_on:, ending_on:)
           QueryableSession
-            .not_ongoing
             .for_patient(patient)
+            .not_ongoing
             .within_period(starting_on, ending_on)
         end
 
@@ -18,7 +18,9 @@ module Renalware
           include PatientScope
 
           scope :within_period, ->(starting_on, ending_on) {
-
+             where("performed_on >= ? and performed_on <= ?",
+                   starting_on.beginning_of_day,
+                   ending_on.end_of_day)
           }
 
           # We want Closed and DNA sessions but not open ones
