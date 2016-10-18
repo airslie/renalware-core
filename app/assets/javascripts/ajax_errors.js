e0146551d7123f5b3e835bddc7c106f718932bf9
Nicholas Henry <nicholas.henry@gmail.com>
2016-10-18 09:07:24 -0400

    Move comment to correct locality

--- app/assets/javascripts/ajax_errors.js
+++ app/assets/javascripts/ajax_errors.js
@@ -1,8 +1,8 @@
-// For 401 Unathorised responses, for example trying to making a
-// js request after a session timeout, reloading the current page
-// will redirect the user to the login page.
 $(document).ajaxError(function (e, xhr, settings) {
   switch(xhr.status) {
+    // For 401 Unathorised responses, for example trying to making a
+    // js request after a session timeout, reloading the current page
+    // will redirect the user to the login page.
     case 401:
       location.reload();
       break;
