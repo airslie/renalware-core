b4d00d50e1af853073d34d37c27a1bb6bacbd6ec
Hugo Frappier <hugo@cekoya.com>
2016-10-20 08:51:30 -0400

    Extract form from Modal

--- app/assets/javascripts/contacts.js
+++ app/assets/javascripts/contacts.js
@@ -1,27 +1,25 @@
 var Renalware = typeof Renalware === 'undefined' ? {} : Renalware;
 
 Renalware.Contacts = {
-  Modal: function(el, callback) {
+  Form: function(el, callback) {
     this.el = el,
-    this.form = this.el.find("form"),
-    this.errorsContainer = this.form.find(".errors-container"),
+    this.errorsContainer = this.el.find(".errors-container"),
     this.errorsList = this.errorsContainer.find("ul.error-messages"),
     this.callback = callback,
 
     this.init = function() {
       var self = this;
-      this.form.on("submit", function(event) { self._onSubmit(event) });
+      this.el.on("submit", function(event) { self._onSubmit(event) });
     },
 
-    this.open = function() {
-      this.el.foundation("reveal", "open");
+    this.reset = function() {
       this._clearForm();
       this._clearErrors();
     },
 
     this._clearForm = function() {
-      this.form.trigger("reset");
-      this.form.find(".hidden").val("");
+      this.el.trigger("reset");
+      this.el.find(".hidden").val("");
     },
 
     this._addErrors = function(errors) {
@@ -42,7 +40,71 @@ Renalware.Contacts = {
     this._onSubmit = function(event) {
       event.preventDefault();
 
+      var valuesToSubmit = this.el.serialize();
+      var self = this;
+
+      self._clearErrors();
+      $.ajax({
+        type: "POST",
+        url: self.el.attr("action"), //submits it to the given url of the form
+        data: valuesToSubmit,
+        dataType: "JSON",
+        statusCode: {
+          201: function(contact) {
+            self._onSuccess(contact);
+          },
+          400: function(jqXHR) {
+            var errors = jqXHR.responseJSON;
+            self._onErrors(errors);
+          }
+        }
+      });
+    },
+
+    this._onSuccess = function(contact) {
+      this.callback(contact);
+    },
+
+    this._onErrors = function(errors) {
+      this._clearErrors();
+      this._addErrors(errors);
+    }
+  },
+
+  Modal: function(el, callback) {
+    this.el = el,
+    this.forms = [],
+    this.callback = callback,
+
+    this.init = function() {
+      var self = this;
+      this.forms = this.el.find("form").map(function() {
+        var form = new Renalware.Contacts.Form($(this), function(contact) {
+          self._onContactAdded(contact);
+        });
+        form.init();
+        return(form);
+      });
+    },
+
+    this.open = function() {
+      this.el.foundation("reveal", "open");
+      this._resetForms();
+    },
+
+    this._resetForms = function() {
+      this.forms.each(function() {
+        this.reset();
+      })
+    },
+
+    // event handlers
+
+    this._onSubmit = function(event) {
+      event.preventDefault();
+
       var valuesToSubmit = this.form.serialize();
+      console.log(valuesToSubmit);
       var self = this;
 
       self._clearErrors();
@@ -66,11 +128,6 @@ Renalware.Contacts = {
     this._onContactAdded = function(contact) {
       this.el.foundation('reveal', 'close');
       this.callback(contact);
-    },
-
-    this._onErrors = function(errors) {
-      this._clearErrors();
-      this._addErrors(errors);
     }
   }
 };
