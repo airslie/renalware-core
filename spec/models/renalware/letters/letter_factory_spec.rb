4ccbeb4a4ea7fec8fe0dcdf5583bd9920da1f232
Nicholas Henry <nicholas.henry@gmail.com>
2016-10-21 09:59:25 -0400

    Simplify test assertions
    No need to test if rails is doing it's job by assign the correct constraint
    for the association

--- spec/models/renalware/letters/letter_factory_spec.rb
+++ spec/models/renalware/letters/letter_factory_spec.rb
@@ -15,22 +15,20 @@ module Renalware
         end
 
         context "given the patient has contacts flagged as default CC" do
+          let(:default_cc_contact) { build(:letter_contact, default_cc: true, person: build(:directory_person, family_name: "default CC")) }
+          let(:non_default_cc_contact) { build(:letter_contact, default_cc: false, person: build(:directory_person, family_name: "non default CC")) }
+
           before do
-            @default_cc_contact = build(:letter_contact, default_cc: true, person: build(:directory_person, family_name: "default CC"))
-            @non_default_cc_contact = build(:letter_contact, default_cc: false, person: build(:directory_person, family_name: "non default CC"))
-            patient.contacts = [@non_default_cc_contact, @default_cc_contact]
+            patient.contacts = [non_default_cc_contact, default_cc_contact]
           end
 
           context "with contacts as default ccs" do
             it "sets the patient's default CC's" do
               letter = subject.with_contacts_as_default_ccs.build
 
-              recipients = letter.cc_recipients.map(&:addressee)
-              expect(recipients.map(&:family_name)).to include(@default_cc_contact.family_name)
-              expect(recipients.map(&:family_name)).to_not include(@non_default_cc_contact.family_name)
-              letter.cc_recipients.map(&:role).each do |role|
-                expect(role).to eq("cc")
-              end
+              addressees = letter.cc_recipients.map(&:addressee)
+              expect(addressees).to include(default_cc_contact)
+              expect(addressees).to_not include(non_default_cc_contact)
             end
           end
         end
