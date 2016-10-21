4e6d9e71b4cab112574c3d4689c5e20b936a74e7
Hugo Frappier <hugo@cekoya.com>
2016-10-21 11:13:30 -0400

    Assign CCs when calling `build`

--- app/models/renalware/letters/letter_factory.rb
+++ app/models/renalware/letters/letter_factory.rb
@@ -4,22 +4,21 @@ module Renalware
   module Letters
     class LetterFactory
       def initialize(patient, params={})
-        params = LetterParamsProcessor.new(patient).call(params)
-        @letter = Letter::Draft.new(params)
+        @params = LetterParamsProcessor.new(patient).call(params)
         @patient = patient
+        @default_ccs = []
       end
 
       def build
+        @letter = Letter::Draft.new(@params)
         letter.patient = patient
         include_primary_care_physician_as_default_main_recipient
-
+        assign_default_ccs
         letter
       end
 
       def with_contacts_as_default_ccs
-        contacts_with_default_cc_option.each do |contact|
-          letter.cc_recipients.build(person_role: "contact", addressee: contact)
-        end
+        @default_ccs = contacts_with_default_cc_option
 
         self
       end
@@ -36,6 +35,14 @@ module Renalware
 
       def contacts_with_default_cc_option
         patient.contacts.default_ccs
+      end
+
+      def assign_default_ccs
+        return if @default_ccs.empty?
+
+        @default_ccs.each do |contact|
+          letter.cc_recipients.build(person_role: "contact", addressee: contact)
+        end
       end
     end
   end
