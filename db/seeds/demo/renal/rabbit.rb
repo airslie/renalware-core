4b03816b45a5aaf7186d8b2367e0b9650478fbc0
Paul Nordstrom August <paul@nuagist.es>
2016-10-25 17:21:53 -0400

    Update renal profile seeds

--- db/seeds/demo/renal/rabbit.rb
+++ db/seeds/demo/renal/rabbit.rb
@@ -7,23 +7,25 @@ module Renalware
     weight_at_esrf: 56.7,
     first_seen_on: 18.weeks.ago,
     prd_description_id: 142,
-    smoking_status: "nonsmoker",
+    smoking_status: "ex_smoker",
     comorbidities_updated_on: 1.week.ago,
     document: {
       comorbidities: {
-        angina: {status: "yes", confirmed_on_year: 2003},
-        diabetes: {status: "yes", confirmed_on_year: 2005},
-        malignancy: {status: "yes", confirmed_on_year: 2014},
-        claudication: {status: "no", confirmed_on_year: ""},
-        cvd_or_stroke: {status: "no", confirmed_on_year: ""},
-        heart_failure: {status: "no", confirmed_on_year: ""},
+        diabetes: {status: "yes", confirmed_on_year: 2003},
+        ischaemic_heart_dis: {status: "yes", confirmed_on_year: 2003},
+        coronary_artery_bypass_graft: {status: "yes", confirmed_on_year: 2005},
+        heart_failure: {status: "yes", confirmed_on_year: 2004},
+        atrial_fibrill: {status: "no", confirmed_on_year: ""},
+        malignancy: {status: "no", confirmed_on_year: ""},
+        cerebrovascular_dis: {status: "no", confirmed_on_year: ""},
+        chronic_obstr_pulm_dis: {status: "no", confirmed_on_year: ""},
         liver_disease: {status: "no", confirmed_on_year: ""},
+        periph_vascular_dis: {status: "yes", confirmed_on_year: 2004},
         amputation_for_pvd: {status: "no", confirmed_on_year: ""},
-        myocardial_infarct: {status: "no", confirmed_on_year: ""},
-        chronic_obstr_pulm_dis: {status: "yes", confirmed_on_year: 2000},
-        non_coronary_angioplasty: {status: "no", confirmed_on_year: ""},
-        coronary_artery_bypass_graft: {status: "no", confirmed_on_year: ""},
-        ischaemic_neuropathic_ulcers: {status: "no", confirmed_on_year: ""}
+        claudication: {status: "no", confirmed_on_year: ""},
+        ischaemic_neuropathic_ulcers: {status: "no", confirmed_on_year: ""},
+        non_coronary_intervention: {status: "no", confirmed_on_year: ""},
+        dementia: {status: "no", confirmed_on_year: ""}
       }
     }
   )
