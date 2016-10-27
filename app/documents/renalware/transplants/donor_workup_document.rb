47eed56af38b4cfc69f32c718f76f303edbc99da
Paul Nordstrom August <paul@nuagist.es>
2016-10-27 13:24:51 -0400

    Update Tx Donor Workup 'document' comorbidities

--- app/documents/renalware/transplants/donor_workup_document.rb
+++ app/documents/renalware/transplants/donor_workup_document.rb
@@ -5,20 +5,22 @@ module Renalware
   module Transplants
     class DonorWorkupDocument < Document::Embedded
       class Comorbidities < Document::Embedded
-        attribute :angina, YearDatedConfirmation
-        attribute :myocardial_infarct, YearDatedConfirmation
+        attribute :smoking, YearDatedConfirmation
+        attribute :diabetes, YearDatedConfirmation
+        attribute :ischaemic_heart_dis, YearDatedConfirmation
         attribute :cabg_or_angioplasty, YearDatedConfirmation
         attribute :heart_failure, YearDatedConfirmation
-        attribute :smoking, YearDatedConfirmation
-        attribute :chronic_obstr_pulm_dis, YearDatedConfirmation
-        attribute :cvd_or_stroke, YearDatedConfirmation
-        attribute :diabetes, YearDatedConfirmation
+        attribute :atrial_fibrill, YearDatedConfirmation
         attribute :malignancy, YearDatedConfirmation
+        attribute :cerebrovascular_dis, YearDatedConfirmation
+        attribute :chronic_obstr_pulm_dis, YearDatedConfirmation
         attribute :liver_disease, YearDatedConfirmation
+        attribute :periph_vascular_dis, YearDatedConfirmation
+        attribute :amputation_for_pvd, YearDatedConfirmation
         attribute :claudication, YearDatedConfirmation
         attribute :ischaemic_neuropathic_ulcers, YearDatedConfirmation
-        attribute :non_coronary_angioplasty, YearDatedConfirmation
-        attribute :amputation_for_pvd, YearDatedConfirmation
+        attribute :non_coronary_intervention, YearDatedConfirmation
+        attribute :dementia, YearDatedConfirmation
       end
       attribute :comorbidities, Comorbidities
 
