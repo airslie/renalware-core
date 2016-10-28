98c268a9d69e9a5016632269d8335d793d8786c1
Tim Crowe <tim@woodpigeon.com>
2016-10-28 16:25:32 +0100

    Validate pulse in range on HD Session form

---
+++ app/validators/renalware/patients/pulse_validator.rb
@@ -0,0 +1,14 @@
+module Renalware
+  module Patients
+    class PulseValidator < ActiveModel::EachValidator
+      include NumericRangeValidations
+      MIN_VALUE = 20
+      MAX_VALUE = 200
+
+      def validate_each(record, attribute, value)
+        return if value.blank?
+        validate_number_is_in_range(record, attribute, value, MIN_VALUE, MAX_VALUE)
+      end
+    end
+  end
+end
---
+++ spec/validators/renalware/patients/pulse_validator_spec.rb
@@ -0,0 +1,67 @@
+require "rails_helper"
+
+module Renalware
+  module Patients
+    describe PulseValidator, type: :validator do
+      let(:out_of_range_message) { "Out of range" }
+      let(:invalid_number_message) { "Invalid number" }
+      let(:min_value) { Patients::PulseValidator::MIN_VALUE }
+      let(:max_value) { Patients::PulseValidator::MAX_VALUE }
+      let(:model_class) { PulseValidatable }
+
+      class PulseValidatable
+        include ActiveModel::Validations
+        include Virtus::Model
+        attribute :pulse, Integer
+        validates :pulse, "renalware/patients/pulse" => true
+      end
+
+      describe "#validate" do
+        before do
+          yaml = <<-YAML
+            activemodel:
+              errors:
+                models:
+                  renalware/patients/pulse_validatable:
+                    attributes:
+                      pulse:
+                        out_of_range: #{out_of_range_message}
+                        invalid_number: #{invalid_number_message}
+          YAML
+          I18n.backend.store_translations(:en, YAML.load(yaml))
+        end
+
+        it "accepts whole numeric values" do
+          model = model_class.new(pulse: min_value.ceil.to_i)
+
+          expect(model).to be_valid
+        end
+
+        it "rejects non-numeric values" do
+          model = model_class.new(pulse: "NaN")
+
+          expect_model_to_be_invalid_with_messages(model, out_of_range_message)
+        end
+
+        it "rejects values below the minimum" do
+          model = model_class.new(pulse: min_value - 1.0)
+
+          expect_model_to_be_invalid_with_messages(model, out_of_range_message)
+        end
+
+        it "rejects values above the maximum" do
+          model = model_class.new(pulse: max_value + 1.0)
+
+          expect_model_to_be_invalid_with_messages(model, out_of_range_message)
+        end
+
+        def expect_model_to_be_invalid_with_messages(model, *expected_messages)
+          expect(model).to_not be_valid
+          expect(model.errors.count).to eq(expected_messages.count)
+          messages = model.errors.messages[:pulse]
+          expect(messages & expected_messages).to eq(messages)
+        end
+      end
+    end
+  end
+end
