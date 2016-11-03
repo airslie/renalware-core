e22be0e4d4338c4698cb88ed9300a8e8d7254453
Tim Crowe <tim@woodpigeon.com>
2016-11-03 09:29:11 +0000

    Remove DNA null session document
    Its causing issues elsewhere. Reinstate later,

--- app/models/renalware/hd/session/dna.rb
+++ app/models/renalware/hd/session/dna.rb
@@ -16,14 +16,6 @@ module Renalware
         delay = Renalware.config.delay_after_which_a_finished_session_becomes_immutable
         (Time.zone.now - delay) > created_at
       end
-
-      # DNA sessions have a nil jsonb `document` but to avoid clumsy nil? checks
-      # wherever session.documents are being used, always return a NullSessionDocument
-      # which will even allow you to do eg session.document.objecta.objectb.attribute1 without
-      # issue. Inspired by Avdi Grim's confident ruby approach and using his naught gem.
-      def document
-        super || NullSessionDocument.instance
-      end
     end
   end
 end
