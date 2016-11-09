67cac206cce66f33b1e24c7dd7d640e9c4e6faaf
Tim Crowe <tim@woodpigeon.com>
2016-11-09 09:36:42 +0000

    Fix rubocop Style/RescueEnsureAlignment issues

--- app/presenters/presenter.rb
+++ app/presenters/presenter.rb
@@ -2,7 +2,7 @@ module Presenter
   def self.for(value)
     klass = "#{value.class.name}Presenter".constantize
     klass.send(:new, value)
-    rescue NameError
-      value.to_s
+  rescue NameError
+    value.to_s
   end
 end
