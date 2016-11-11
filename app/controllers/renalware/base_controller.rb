3581ede676e45a7fd3eeac8c8cedc75dcd5f243a
Tim Crowe <tim@woodpigeon.com>
2016-11-11 14:18:56 +0000

    Add protect_from_forgery as prompted by Brakeman

--- app/controllers/renalware/base_controller.rb
+++ app/controllers/renalware/base_controller.rb
@@ -1,15 +1,11 @@
 module Renalware
-  class BaseController < ActionController::Base
+  class BaseController < ApplicationController
     include Concerns::DeviseControllerMethods
     include Pundit
 
     after_action :verify_authorized
 
     rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-
-    # Prevent CSRF attacks by raising an exception.
-    # For APIs, you may want to use :null_session instead.
-    protect_from_forgery with: :exception
 
     layout "renalware/layouts/application"
 
