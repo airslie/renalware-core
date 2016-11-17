eafa1ddaf8b3decab27687f56c3b6b348dd32c20
Tim Crowe <tim@woodpigeon.com>
2016-11-17 12:59:02 +0000

    Converge on load_patient default before_action

--- app/controllers/renalware/pd/dashboards_controller.rb
+++ app/controllers/renalware/pd/dashboards_controller.rb
@@ -3,13 +3,9 @@ require_dependency "renalware/pd"
 module Renalware
   module PD
     class DashboardsController < BaseController
-
-      skip_after_action :verify_authorized
+      before_action :load_patient
 
       def show
-        @patient = Patient.find(params[:patient_id])
-        current_regime = patient.pd_regimes.current if @patient.pd_regimes.any?
-
         render :show, locals: {
           patient: patient,
           dashboard: DashboardPresenter.new(patient)
