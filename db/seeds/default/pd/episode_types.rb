fc1c703af5cbd6c4612015b014e07fd836a3b0af
Tim Crowe <tim@woodpigeon.com>
2016-11-21 12:00:53 +0000

    Improve episode_type seeding

--- db/seeds/default/pd/episode_types.rb
+++ db/seeds/default/pd/episode_types.rb
@@ -1,53 +1,11 @@
 module Renalware
-  log "Adding Peritonitis Episode Types" do
-
-    PD::EpisodeType.find_or_create_by!(
-      term: "De novo",
-      definition: "First infection."
-    )
-
-    PD::EpisodeType.find_or_create_by!(
-      term: "Recurrent",
-      definition: <<-DEF.squish
-        An episode that occurs within 4 weeks of completion of therapy of a prior
-        episode but with a different organism.
-      DEF
-    )
-
-    PD::EpisodeType.find_or_create_by!(
-      term: "Relapsing",
-      definition: <<-DEF.squish
-        An episode that occurs within 4 weeks of completion of therapy of a prior
-        episode with the same organism or 1 sterile episode
-      DEF
-    )
-
-    PD::EpisodeType.find_or_create_by!(
-      term: "Repeat",
-      definition: <<-DEF
-        An episode that occurs more than 4 weeks after completion of
-        therapy of a prior episode with the same organism.
-      DEF
-    )
-
-    PD::EpisodeType.find_or_create_by!(
-      term: "Refractory",
-      definition: <<-DEF
-        Failure of the effluent to clear after 5 days of appropriate antibiotics.
-      DEF
-    )
-
-    PD::EpisodeType.find_or_create_by!(
-      term: "Catheter-related",
-      definition: <<-DEF
-        Peritonitis in conjunction with an exit-site or tunnel infection with
-        the same organism or 1 site.
-      DEF
-    )
-
-    PD::EpisodeType.find_or_create_by!(
-      term: "Other",
-      definition: "Refer to notes."
-    )
+  log "Adding PD Episode Types" do
+    file_path = File.join(File.dirname(__FILE__), "episode_types.csv")
+    CSV.foreach(file_path, headers: true) do |row|
+      PD::EpisodeType.find_or_create_by!(
+        term: row["term"],
+        definition: row["definition"]
+      )
+    end
   end
 end
