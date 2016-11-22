65a84338035732833a39f7b09844030ab7e0815d
Paul Nordstrom August <paul@nuagist.es>
2016-11-22 14:56:10 +0100

    seed file tweaks (read_code to code)

--- db/seeds/default/pd/organisms.rb
+++ db/seeds/default/pd/organisms.rb
@@ -4,7 +4,7 @@ module Renalware
   file_path = File.join(File.dirname(__FILE__), "rr_organisms.csv")
 
   CSV.foreach(file_path, headers: true) do |row|
-    PD::OrganismCode.find_or_create_by!(read_code: row["code"]) do |code|
+    PD::OrganismCode.find_or_create_by!(code: row["code"]) do |code|
       code.name = row["name"]
     end
   end
