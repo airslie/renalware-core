5d406251b12676bcb96f46894bd64d502653ceb5
Tim Crowe <tim@woodpigeon.com>
2016-12-01 11:43:36 +0000

    Increase poltergeist window size
    This wil hopefully remove capybara errors
    caused by (current) layout issues at eg  [1024, 768].

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -81,4 +81,13 @@ Capybara.register_driver :poltergeist_debug do |app|
   )
 end
 
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(
+    app,
+    window_size: [1366, 768],
+    js_errors: true
+  )
+end
+
+Capybara.javascript_driver = :poltergeist
 # Capybara.javascript_driver = :poltergeist_debug
