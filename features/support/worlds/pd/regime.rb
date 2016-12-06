990b07cac051decd8ec576fce2e20ab85c7e909f
Tim Crowe <tim@woodpigeon.com>
2016-12-06 11:00:32 +0000

    Started re-instating PD regime features

--- features/support/worlds/pd/regime.rb
+++ features/support/worlds/pd/regime.rb
@@ -2,13 +2,18 @@ module World
   module PD::Regime
     module Domain
       def regime_for_patient_is_current(patient:, regime:)
-        expect(regime).to eq(patient.pd_regimes.current.first)
+        expect(regime).to be_current
       end
     end
 
     module Web
       include Domain
 
+      def regime_for_patient_is_current(patient:, regime:)
+        visit patient_pd_dashboard_path(patient)
+
+        expect(find(".current-regime")["data-regime-id"].to_i).to eq(regime.id)
+      end
     end
   end
 end
