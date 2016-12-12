3df43fc8eb984c8ba2e9dd8f620a46dc33e8ef1a
Paul Nordstrom August <paul@nuagist.es>
2016-12-12 15:08:42 +0100

    Update events sample seeds for RABBIT

--- db/seeds/demo/events/rabbit.rb
+++ db/seeds/demo/events/rabbit.rb
@@ -8,7 +8,9 @@ module Renalware
       event_type_id: 19,
       description: "meeting with family in clinic",
       notes: "anxious about medication changes",
-      date_time: Time.now - 2.weeks
+      date_time: Time.now - 2.weeks,
+      created_by_id: Renalware::User.first.id,
+      updated_by_id: Renalware::User.first.id
     )
 
     Events::Event.find_or_create_by!(
@@ -16,7 +18,9 @@ module Renalware
       event_type_id: 25,
       description: "call regarding meds",
       notes: "told patient to get other drug info from GP",
-      date_time: Time.now - 12.days
+      date_time: Time.now - 12.days,
+      created_by_id: Renalware::User.second.id,
+      updated_by_id: Renalware::User.second.id
     )
 
     Events::Event.find_or_create_by!(
@@ -24,7 +28,9 @@ module Renalware
       event_type_id: 8,
       description: "email re next clinic visit",
       notes: "reminded patient to bring complete drug list to clinic",
-      date_time: Time.now - 5.days
+      date_time: Time.now - 5.days,
+      created_by_id: Renalware::User.last.id,
+      updated_by_id: Renalware::User.last.id
     )
   end
 end
