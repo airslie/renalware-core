072e4ba64f3c6876c73ddc52dda5a9b637052743
Tim Crowe <tim@woodpigeon.com>
2017-01-11 12:15:30 +0000

    Add helpers to patient header view spec

--- spec/views/renalware/patients/_header.html.slim_spec.rb
+++ spec/views/renalware/patients/_header.html.slim_spec.rb
@@ -1,6 +1,8 @@
 require "rails_helper"
 
 describe "renalware/patients/_header" do
+  helper(Renalware::ApplicationHelper, Renalware::PatientHelper)
+
   it "includes the correctly formatted NHS number" do
     patient = build(:patient, nhs_number: "1234567890")
     render partial: "renalware/patients/header", locals: { patient: patient }
