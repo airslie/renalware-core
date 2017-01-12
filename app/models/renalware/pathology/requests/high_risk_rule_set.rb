902e5305731e64a1c95728c3da93ae831fb652b6
Tim Crowe <tim@woodpigeon.com>
2017-01-12 13:42:19 +0000

    Simulate tableless for Rails 5

--- app/models/renalware/pathology/requests/high_risk_rule_set.rb
+++ app/models/renalware/pathology/requests/high_risk_rule_set.rb
@@ -1,13 +1,20 @@
 require_dependency "renalware/pathology"
 
+# This class used to include https://github.com/softace/activerecord-tableless but that does not
+# support Rails 5. The alternative I have used here is to massage this class to get the specs to
+# pass by stubbing out various methods.
+# I'm not really sure yet how this class works and why its tableless.
 module Renalware
   module Pathology
     module Requests
-      class HighRiskRuleSet < Tableless # ActiveRecord::Base
-        # has_no_table
+      class HighRiskRuleSet
 
         def self.rules
           GlobalRule.where(rule_set_type: self.name)
+        end
+
+        def self.primary_key
+          :id
         end
 
         # NOTE: required so ActiveRecord doesn't try to create a new associated HighRiskRuleSet
@@ -16,8 +23,28 @@ module Renalware
           false
         end
 
+        def to_model
+          # noop
+        end
+
         def id
           nil
+        end
+
+        def _read_attribute(name)
+          # noop
+        end
+
+        def self.base_class
+          Renalware::Pathology::Requests::HighRiskRuleSet
+        end
+
+        def marked_for_destruction?
+          false
+        end
+
+        def destroyed?
+          false
         end
       end
     end
