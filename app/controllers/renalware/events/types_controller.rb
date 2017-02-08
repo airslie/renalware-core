e68fdc3bbf306b0741a25a925fedb5cef2f0730b
Tim Crowe <tim@woodpigeon.com>
2017-02-08 18:04:05 +0000

    Tidied up event_types
    Raise code climate score from D to A

--- app/controllers/renalware/events/types_controller.rb
+++ app/controllers/renalware/events/types_controller.rb
@@ -3,45 +3,49 @@ require_dependency "renalware/events"
 module Renalware
   module Events
     class TypesController < BaseController
-      before_action :load_event_type, only: [:edit, :update]
-
       def new
-        @event_type = Type.new
-        authorize @event_type
+        event_type = Type.new
+        authorize event_type
+        render locals: { event_type: event_type }
       end
 
       def create
-        @event_type = Type.new(event_params)
-        authorize @event_type
-
-        if @event_type.save
-          redirect_to events_types_path,
-            notice: t(".success", model_name: "event type")
+        event_type = Type.new(event_params)
+        authorize event_type
+        if event_type.save
+          redirect_to events_types_path, notice: success_msg_for("event type")
         else
-          flash[:error] = t(".failed", model_name: "event type")
-          render :new
+          flash[:error] = failed_msg_for("event type")
+          render :new, locals: { event_type: event_type }
         end
       end
 
       def index
-        @event_types = Type.all
-        authorize @event_types
+        event_types = Type.all
+        authorize event_types
+        render locals: { event_types: event_types }
+      end
+
+      def edit
+        event_type = load_and_authorize_event_type
+        render locals: { event_type: event_type }
       end
 
       def update
-        if @event_type.update(event_params)
-          redirect_to events_types_path,
-            notice: t(".success", model_name: "event type")
+        event_type = load_and_authorize_event_type
+        if event_type.update(event_params)
+          redirect_to events_types_path, notice: success_msg_for("event type")
         else
-          flash[:error] = t(".failed", model_name: "event type")
-          render :edit
+          flash[:error] = failed_msg_for("event type")
+          render :edit, locals: { event_type: event_type }
         end
       end
 
       def destroy
-        authorize Type.destroy(params[:id])
-        redirect_to events_types_path,
-          notice: t(".success", model_name: "event type")
+        event_type = load_and_authorize_event_type
+        authorize event_type
+        event_type.destroy
+        redirect_to events_types_path, notice: success_msg_for("event type")
       end
 
       private
@@ -50,9 +54,10 @@ module Renalware
         params.require(:events_type).permit(:name, :deleted_at)
       end
 
-      def load_event_type
-        @event_type = Type.find(params[:id])
-        authorize @event_type
+      def load_and_authorize_event_type
+        event_type = Type.find(params[:id])
+        authorize event_type
+        event_type
       end
     end
   end
