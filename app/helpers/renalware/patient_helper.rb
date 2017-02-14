f20dcb08a96bdea7b36ba0c49398aebe866efa7a
Tim Crowe <tim@woodpigeon.com>
2017-02-14 18:57:48 +0000

    Highlight the currently active module in the LH patient nav

--- app/helpers/renalware/patient_helper.rb
+++ app/helpers/renalware/patient_helper.rb
@@ -17,5 +17,17 @@ module Renalware
         yield
       end
     end
+
+    def patient_menu_item(title:, path:, active_when_controller_matches:, enabled: true)
+      klasses = %w(link)
+      klasses << "active" if current_controller_matches(active_when_controller_matches)
+      content_tag :li, class: klasses.join(" ") do
+        link_to_if(enabled, title, path)
+      end
+    end
+
+    def current_controller_matches(regex)
+      regex.match(params[:controller]).present?
+    end
   end
 end
