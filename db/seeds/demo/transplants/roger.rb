08cb706de51d363b1607a9b453610e78b74c6684
Tim Crowe <tim@woodpigeon.com>
2017-03-02 17:51:02 +0000

    Updated transplant seeds
    - Added new created_by_id and updated_by_id cols to transplant_recipient_workups seeds
    - Formatted SQL lines so none are over 100 chars to aid readability

--- db/seeds/demo/transplants/roger.rb
+++ db/seeds/demo/transplants/roger.rb
@@ -10,29 +10,105 @@ module Renalware
 
   log "SQL INSERT Transplant Registration for Roger RABBIT" do
 
-  # Execute a sql statement
-    connection.execute("INSERT INTO transplant_registrations (id, patient_id, referred_on, assessed_on, entered_on, contact, notes, document, created_at, updated_at) VALUES (1, 1, '2016-12-06', '2017-02-13', '2017-02-15', 'Sister: JESSICA RABBIT 0123 777 888 999', 'Extremely anxious', '{\"crf\": {\"latest\": {\"result\": \"67\", \"recorded_on\": \"2017-02-15\"}, \"highest\": {\"result\": \"93\", \"recorded_on\": \"2017-02-01\"}}, \"hla\": {\"a\": {\"value_1\": \"ASDF\", \"value_2\": \"JLHJK\"}, \"b\": {\"value_1\": \"ASFA\", \"value_2\": \"DFSDA\"}, \"cw\": {\"value_1\": \"ADFA\", \"value_2\": \"DSFFS\"}, \"dq\": {\"value_1\": \"SASDF\", \"value_2\": \"DFSFS\"}, \"dr\": {\"value_1\": \"DFSA\", \"value_2\": \"FDSDF\"}, \"drq\": {\"value_1\": \"FDAFS\", \"value_2\": \"WEWRE\"}, \"drw\": {\"value_1\": \"FADSFA\", \"value_2\": \"FSFSD\"}, \"type\": \"DFAJ;LSKJ;FKJAS;DKJF;LASJ;FDKJAS;K\", \"recorded_on\": \"2017-02-13\"}, \"codes\": {\"uk_transplant_centre_code\": \"X123\", \"uk_transplant_patient_recipient_number\": \"987654\"}, \"organs\": {\"rejection_risk\": \"standard\", \"transplant_type\": \"kidney_pancreas\", \"pancreas_only_type\": \"solid_organ\", \"also_listed_for_kidney_only\": \"no\", \"to_be_listed_for_other_organs\": \"no\", \"received_previous_kidney_or_pancreas_grafts\": \"no\"}, \"transplant\": {\"blood_group\": {\"value\": \"A\"}, \"sens_status\": \"SADF;LJ;K\", \"nb_of_previous_grafts\": 0}, \"nhb_consent\": {\"value\": \"no\", \"full_name\": \"JANE SUMISU\", \"consented_on\": \"2017-02-21\"}}', '2017-02-23 13:34:24.618171', '2017-02-23 13:34:24.618171');")
-
+    connection.execute(<<-SQL)
+      INSERT INTO transplant_registrations
+      (patient_id, referred_on, assessed_on, entered_on,
+      contact, notes, document, created_at, updated_at)
+      VALUES
+      (1, '2016-12-06', '2017-02-13', '2017-02-15',
+      'Sister: JESSICA RABBIT 0123 777 888 999', 'Extremely anxious',
+      '{"crf": {"latest": {"result": "67", "recorded_on": "2017-02-15"},
+      "highest": {"result": "93", "recorded_on": "2017-02-01"}},
+      "hla": {"a": {"value_1": "ASDF", "value_2": "JLHJK"},
+      "b": {"value_1": "ASFA", "value_2": "DFSDA"},
+      "cw": {"value_1": "ADFA", "value_2": "DSFFS"},
+      "dq": {"value_1": "SASDF", "value_2": "DFSFS"},
+      "dr": {"value_1": "DFSA", "value_2": "FDSDF"},
+      "drq": {"value_1": "FDAFS", "value_2": "WEWRE"},
+      "drw": {"value_1": "FADSFA", "value_2": "FSFSD"},
+      "type": "DFAJ;LSKJ;FKJAS;DKJF;LASJ;FDKJAS;K", "recorded_on": "2017-02-13"},
+      "codes": {"uk_transplant_centre_code": "X123",
+      "uk_transplant_patient_recipient_number": "987654"},
+      "organs": {"rejection_risk": "standard", "transplant_type": "kidney_pancreas",
+      "pancreas_only_type": "solid_organ", "also_listed_for_kidney_only": "no",
+      "to_be_listed_for_other_organs": "no",
+      "received_previous_kidney_or_pancreas_grafts": "no"},
+      "transplant": {"blood_group": {"value": "A"},
+      "sens_status": "SADF;LJ;K", "nb_of_previous_grafts": 0},
+      "nhb_consent": {"value": "no", "full_name": "JANE SUMISU",
+      "consented_on": "2017-02-21"}}', '2017-02-23 13:34:24.618171',
+      '2017-02-23 13:34:24.618171');
+    SQL
   end
 
   log "SQL INSERT Transplant Registration Status for Roger RABBIT" do
 
-    connection.execute("INSERT INTO transplant_registration_statuses (id, registration_id, description_id, started_on, terminated_on, created_by_id, updated_by_id, created_at, updated_at) VALUES (1, 1, 1, '2017-02-23', NULL, 9, 9, '2017-02-23 13:34:24.626362', '2017-02-23 13:34:24.626362');")
-
+    connection.execute(<<-SQL)
+      INSERT INTO transplant_registration_statuses
+      (id, registration_id, description_id, started_on, terminated_on, created_by_id,
+      updated_by_id, created_at, updated_at)
+      VALUES
+      (1, 1, 1, '2017-02-23', NULL, 9, 9,
+      '2017-02-23 13:34:24.626362', '2017-02-23 13:34:24.626362');
+    SQL
   end
 
   log "SQL INSERT Transplant Recipient Workup for Roger RABBIT" do
 
-  # Execute a sql statement
-    connection.execute("INSERT INTO transplant_recipient_workups (id, patient_id, document, created_at, updated_at) VALUES (1, 1, '{\"scores\": {\"prisma\": 3, \"karnofsky\": 56}, \"consent\": {\"value\": \"full\", \"full_name\": \"JANE SUMISU\", \"consented_on\": \"2017-02-22\"}, \"hla_data\": \"FASDF;KJASFDAHSDFKSHFDHASOUOW\", \"education\": {\"cancer\": \"yes\", \"followup\": \"yes\", \"infection\": \"yes\", \"procedure\": \"yes\", \"rejection\": \"yes\", \"recurrence\": \"yes\", \"success_rate\": \"yes\", \"waiting_list\": \"yes\", \"drugs_longterm\": \"yes\", \"drugs_shortterm\": \"yes\", \"transport_benefits\": \"yes\"}, \"examination\": {\"heart_sounds\": \"NAD\", \"carotid_bruit\": {\"left\": \"no\", \"right\": \"no\"}, \"femoral_bruit\": {\"left\": \"no\", \"right\": \"no\"}, \"femoral_pulse\": {\"left\": \"yes\", \"right\": \"yes\"}, \"dorsalis_pedis_pulse\": {\"left\": \"yes\", \"right\": \"yes\"}, \"posterior_tibial_pulse\": {\"left\": \"yes\", \"right\": \"yes\"}}, \"historicals\": {\"tb\": \"no\", \"dvt\": \"no\", \"reflux\": \"yes\", \"recurrent_utis\": \"no\", \"family_diabetes\": \"no\", \"pregnancies_count\": 0, \"neurogenic_bladder\": \"no\"}, \"cervical_smear\": {\"result\": \"\", \"recorded_on\": \"\"}, \"marginal_consent\": {\"value\": \"yes\", \"full_name\": \"JANE SUMISU\", \"consented_on\": \"2017-02-22\"}}', '2017-02-23 13:36:44.271472', '2017-02-23 13:36:44.271472');")
-
+    # Execute a sql statement
+    connection.execute(<<-SQL)
+      INSERT INTO transplant_recipient_workups
+      (patient_id, document, created_at, updated_at, created_by_id, updated_by_id)
+      VALUES (1, '{"scores": {"prisma": 3, "karnofsky": 56},
+      "consent": {"value": "full", "full_name": "JANE SUMISU",
+      "consented_on": "2017-02-22"}, "hla_data": "FASDF;KJASFDAHSDFKSHFDHASOUOW",
+      "education": {"cancer": "yes", "followup": "yes", "infection": "yes",
+      "procedure": "yes", "rejection": "yes", "recurrence": "yes",
+      "success_rate": "yes", "waiting_list": "yes", "drugs_longterm": "yes",
+      "drugs_shortterm": "yes", "transport_benefits": "yes"},
+      "examination": {"heart_sounds": "NAD",
+      "carotid_bruit": {"left": "no", "right": "no"},
+      "femoral_bruit": {"left": "no", "right": "no"},
+      "femoral_pulse": {"left": "yes", "right": "yes"},
+      "dorsalis_pedis_pulse": {"left": "yes", "right": "yes"},
+      "posterior_tibial_pulse": {"left": "yes", "right": "yes"}},
+      "historicals": {"tb": "no", "dvt": "no", "reflux": "yes",
+      "recurrent_utis": "no", "family_diabetes": "no", "pregnancies_count": 0,
+      "neurogenic_bladder": "no"},
+      "cervical_smear": {"result": "", "recorded_on": ""},
+      "marginal_consent": {"value": "yes", "full_name": "JANE SUMISU",
+      "consented_on": "2017-02-22"}}',
+      '2017-02-23 13:36:44.271472', '2017-02-23 13:36:44.271472', 1,1);
+    SQL
   end
 
 log "SQL INSERT Transplant Recipient Operation for Roger RABBIT" do
 
-  # Execute a sql statement
-    connection.execute("INSERT INTO transplant_recipient_operations (id, patient_id, performed_on, theatre_case_start_time, donor_kidney_removed_from_ice_at, operation_type, hospital_centre_id, kidney_perfused_with_blood_at, cold_ischaemic_time, warm_ischaemic_time, notes, document, created_at, updated_at) VALUES (1, 1, '2017-02-23', '08:15:00', '2017-02-23 07:07:00', 'kidney_pancreas', 51, '2017-02-23 09:15:00', 5100, 5520, 'Uneventful Tx.', '{\"donor\": {\"age\": {\"unit\": \"years\", \"amount\": 36}, \"hla\": \"DASFS\", \"type\": \"cadaver\", \"gender\": \"male\", \"born_on\": \"1980-03-13\", \"asystolic\": \"yes\", \"cmv_status\": \"unknown\", \"blood_group\": {\"value\": \"B\"}, \"kidney_side\": \"left\", \"hla_mismatch\": \"WWERW\", \"kidney_weight\": 234, \"ethnic_category\": \"other_european\", \"ukt_notified_at\": \"2017-02-22T06:07:00.000+00:00\", \"ukt_donor_number\": \"54321\", \"blood_group_rhesus\": \"positive\", \"organ_donor_register_checked\": \"yes\"}, \"bk_virus\": {\"notes\": \"BKV notes here\", \"results\": \"NEGATIVE\", \"tested_on\": \"2017-02-22\"}, \"recipient\": {\"cmv_status\": \"negative\", \"blood_group\": {\"value\": \"B\"}, \"last_dialysis_on\": \"2017-02-21\", \"operation_number\": 12345}, \"cadaveric_donor\": {\"death_certified_at\": \"2017-02-22T19:19:00.000+00:00\", \"ukt_cause_of_death\": \"trauma_rta_pushbike\", \"cadaveric_donor_type\": \"non_heart_beating\", \"ukt_cause_of_death_other\": \"\", \"warm_ischaemic_time_in_minutes\": null}, \"donor_specific_antibodies\": {\"notes\": \"DSA notes here\", \"results\": \"NEGATIVE\", \"tested_on\": \"2017-02-22\"}}', '2017-02-23 13:41:34.499713', '2017-02-23 13:41:34.499713');")
+    connection.execute(<<-SQL)
+      INSERT INTO transplant_recipient_operations
+      (patient_id, performed_on, theatre_case_start_time, donor_kidney_removed_from_ice_at,
+      operation_type, hospital_centre_id, kidney_perfused_with_blood_at, cold_ischaemic_time,
+      warm_ischaemic_time, notes, document, created_at, updated_at)
+      VALUES (1, '2017-02-23', '08:15:00', '2017-02-23 07:07:00', 'kidney_pancreas', 51,
+      '2017-02-23 09:15:00', 5100, 5520, 'Uneventful Tx.', '{"donor":
+      {"age": {"unit": "years", "amount": 36}, "hla": "DASFS", "type":
+      "cadaver", "gender": "male", "born_on": "1980-03-13", "asystolic": "yes",
+      "cmv_status": "unknown", "blood_group": {"value": "B"}, "kidney_side": "left",
+      "hla_mismatch": "WWERW", "kidney_weight": 234, "ethnic_category": "other_european",
+      "ukt_notified_at": "2017-02-22T06:07:00.000+00:00", "ukt_donor_number": "54321",
+      "blood_group_rhesus": "positive", "organ_donor_register_checked": "yes"},
+      "bk_virus": {"notes": "BKV notes here", "results": "NEGATIVE",
+      "tested_on": "2017-02-22"}, "recipient": {"cmv_status": "negative",
+      "blood_group": {"value": "B"}, "last_dialysis_on": "2017-02-21",
+      "operation_number": 12345},
+      "cadaveric_donor": {"death_certified_at": "2017-02-22T19:19:00.000+00:00",
+      "ukt_cause_of_death": "trauma_rta_pushbike",
+      "cadaveric_donor_type": "non_heart_beating",
+      "ukt_cause_of_death_other": "", "warm_ischaemic_time_in_minutes": null},
+      "donor_specific_antibodies": {"notes": "DSA notes here", "results": "NEGATIVE",
+      "tested_on": "2017-02-22"}}', '2017-02-23 13:41:34.499713',
+      '2017-02-23 13:41:34.499713');
+    SQL
 
   end
-
 end
