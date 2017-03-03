df0c1f4e572e91657e7aa35fb6ebcf84de33f914
Paul Nordstrom August <paul@nuagist.es>
2017-03-03 12:45:48 -0500

    randomise Worryboard 'created_at'

--- db/seeds/demo/worryboard/seeds.rb
+++ db/seeds/demo/worryboard/seeds.rb
@@ -2,7 +2,7 @@ module Renalware
 
   # add random patients plus RABBITs
   users = User.all.to_a
-
+  dates = (1..365).to_a
   log "Adding Patients to Worryboard" do
     Patient.transaction do
       patients = Patient.all
@@ -12,7 +12,8 @@ module Renalware
         if i % 10 == 0 or patient.family_name == "RABBIT"
           Renalware::Patients::Worry.new(
             patient: patient,
-            by: users.sample).save!
+            by: users.sample,
+            created_at: dates.sample.days.ago).save!
         end
       end
     end
