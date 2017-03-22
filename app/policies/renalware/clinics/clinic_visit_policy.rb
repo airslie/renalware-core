0235cc707cc14a2413126148ab7d25ec870f5898
Tim Crowe <tim@woodpigeon.com>
2017-03-22 11:44:59 +0000

    Only allow clinic visit deletion within 24hours of creation

--- app/policies/renalware/clinics/clinic_visit_policy.rb
+++ app/policies/renalware/clinics/clinic_visit_policy.rb
@@ -3,6 +3,10 @@ require_dependency "renalware/clinics"
 module Renalware
   module Clinics
     class ClinicVisitPolicy < BasePolicy
+      def destroy?
+        return false unless record.persisted?
+        record.created_at > Renalware.config.new_clinic_visit_deletion_window.ago
+      end
     end
   end
 end
---
+++ spec/policies/renalware/clinics/clinic_visits_policy_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper"
+
+module Renalware
+  module Clinics
+    describe ClinicVisitPolicy, type: :policy do
+      subject { described_class }
+      let(:user) { User.new }
+      let(:clinic_visit) { ClinicVisit.new }
+
+      permissions :destroy? do
+        context "for an unsaved clinic visit" do
+          it "is not permitted" do
+            expect(subject).to_not permit(user, clinic_visit)
+          end
+        end
+
+        context "for a saved clinic visit" do
+          before do
+            allow(clinic_visit).to receive(:persisted?).and_return(true)
+            Renalware.configure{ |config| config.new_clinic_visit_deletion_window = 24.hours }
+          end
+
+          it "is not permitted if creation date not is within the deletion window" do
+            allow(clinic_visit).to receive(:created_at).and_return(Time.zone.now - 25.hours)
+            expect(subject).to_not permit(user, clinic_visit)
+          end
+
+          it "is permitted if creation date is within the deletion window" do
+            allow(clinic_visit).to receive(:created_at).and_return(Time.zone.now - 23.hours)
+            expect(subject).to permit(user, clinic_visit)
+          end
+        end
+      end
+    end
+  end
+end
