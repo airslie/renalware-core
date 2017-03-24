47c661a5f5dfc0600d79962b5b6b3b172e5dd829
Paul Nordstrom August <paul@nuagist.es>
2017-03-24 09:39:22 -0400

    Fix `recipient_operation_spec` (remove donor age calculation)

--- spec/models/renalware/transplants/recipient_operation_spec.rb
+++ spec/models/renalware/transplants/recipient_operation_spec.rb
@@ -1,6 +1,4 @@
 require "rails_helper"
-require "age_calculator"
-require "renalware/automatic_age_calculator"
 
 module Renalware
   module Transplants
@@ -19,20 +17,6 @@ module Renalware
       it { is_expected.to validate_timeliness_of(:theatre_case_start_time) }
 
       subject { build(:transplant_recipient_operation) }
-
-      describe "#before_validation" do
-        let(:calculator) { double.as_null_object }
-        let(:age) { Age.new }
-
-        it "computes the age of the donor" do
-          expect(Renalware::AutomaticAgeCalculator).to receive(:new).and_return(calculator)
-          expect(calculator).to receive(:compute).and_return(age)
-
-          subject.valid?
-
-          expect(subject.document.donor.age).to eq(age)
-        end
-      end
 
       describe "#cold_ischaemic_time_formatted=" do
         it "stores duration in seconds" do
