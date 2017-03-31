b073891c078c3ee49b7ace512c62ae76dbbf15c6
Tim Crowe <tim@woodpigeon.com>
2017-03-31 17:07:30 +0100

    Remove dead space from snippet seeds

--- db/seeds/demo/snippets/seeds.rb
+++ db/seeds/demo/snippets/seeds.rb
@@ -3,19 +3,20 @@ module Renalware
   log "Adding Sample Snippets" do
 
     x = User.count
-    #for DNA snippets -- so not every user has one
+    # For DNA snippets -- so not every user has one
     y = x/2
 
     modals = %w(Nephrology Transplant HD PD Access)
-    body_text_1 = "Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-        nisi ut aliquip ex ea commodo consequat."
 
-    body_text_2 = "Duis aute irure dolor in reprehenderit in voluptate
-     velit esse cillum dolore eu fugiat nulla pariatur.
-     Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-     officia deserunt mollit anim id est laborum."
+    body_text_1 = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, "\
+                  "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "\
+                  "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris "\
+                  "nisi ut aliquip ex ea commodo consequat."
+
+    body_text_2 = "Duis aute irure dolor in reprehenderit in voluptate "\
+                  "velit esse cillum dolore eu fugiat nulla pariatur. "\
+                  "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui "\
+                  "officia deserunt mollit anim id est laborum."
 
     x.times do |i|
       clinic = modals.sample
@@ -44,9 +45,9 @@ module Renalware
         title: "#{clinic} Clinic DNA",
         last_used_on: i.days.ago,
         times_used: 10,
-        body: "This patient did not attend the #{clinic} Clinic ullamco
-        laboris nisi ut aliquip ex ea commodo consequat.
-        Duis aute irure dolor in reprehenderit in voluptate")
+        body: "This patient did not attend the #{clinic} Clinic ullamco "\
+              "laboris nisi ut aliquip ex ea commodo consequat. "\
+              "Duis aute irure dolor in reprehenderit in voluptate")
     end
   end
 end
