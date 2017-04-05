0d500f6db34b4e91ec3093f8c3373bfabcbf49e4
Tim Crowe <tim@woodpigeon.com>
2017-04-05 13:59:38 +0100

    Display errors correctly if error occurs rendering an rtf

--- app/controllers/renalware/errors_controller.rb
+++ app/controllers/renalware/errors_controller.rb
@@ -3,11 +3,11 @@ module Renalware
     layout "renalware/layouts/error"
 
     def not_found
-      render status: 404
+      render status: 404, formats: [:html]
     end
 
     def internal_server_error
-      render status: 500
+      render status: 500, formats: [:html]
     end
 
     def generate_test_internal_server_error
