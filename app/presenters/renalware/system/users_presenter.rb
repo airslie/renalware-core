5840040d90798d9fa779adf1a4e947e76a162d74
Tim Crowe <tim@woodpigeon.com>
2017-04-30 22:49:26 +0100

    Make current user first in list when selecting letter author

---
+++ app/presenters/renalware/system/users_presenter.rb
@@ -0,0 +1,24 @@
+module Renalware
+  module System
+    class UsersPresenter
+
+      def list_for_dropdown(user_to_promote_to_top: nil)
+        return users if user_to_promote_to_top.blank?
+        matching_user = users.detect{ |user| user.id == user_to_promote_to_top.id }
+        make_user_first_in_list(matching_user) if matching_user.present?
+        users
+      end
+
+      private
+
+      def make_user_first_in_list(user)
+        users.delete(user)
+        users.unshift(user)
+      end
+
+      def users
+        @users ||= Renalware::User.select(:id, :family_name, :given_name).ordered.to_a
+      end
+    end
+  end
+end
