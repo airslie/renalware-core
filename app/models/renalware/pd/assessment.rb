aec75cfc6a8b323dbb8209eadd992f88401cdedb
Paul Nordstrom August <paul@nuagist.es>
2017-05-31 16:37:54 +0200

    Add additional Document attributes

--- app/models/renalware/pd/assessment.rb
+++ app/models/renalware/pd/assessment.rb
@@ -15,15 +15,39 @@ module Renalware
       attr_reader :ignore_me # see html form for explanation of this non-persistent attribute
 
       class Document < Document::Embedded
+        attribute :assessed_on, Date
+        attribute :assessor, String
         attribute :had_home_visit, ::Document::Enum, enums: %i(yes no)
         attribute :home_visit_on, Date
-        attribute :housing_type, ::Document::Enum, enums: %i(patient council)
-        attribute :occupant_notes, String
+        attribute :housing_type, ::Document::Enum, enums: %i(flat house bungalow)
+        attribute :occupant_notes
         attribute :exchange_area, String
         attribute :handwashing, String
         attribute :fluid_storage, String
         attribute :bag_warming, String
-        attribute :delivery_frequency, String
+        attribute :delivery_interval, String
+        attribute :needs_rehousing, ::Document::Enum, enums: %i(yes no)
+        attribute :rehousing_notes
+        attribute :social_worker, ::Document::Enum, enums: %i(yes no)
+        attribute :seen_video, ::Document::Enum, enums: %i(yes no)
+        attribute :can_open_bag, ::Document::Enum, enums: %i(yes no)
+        attribute :can_lift_bag, ::Document::Enum, enums: %i(yes no)
+        attribute :eyesight, String
+        attribute :hearing, String
+        attribute :dexterity, String
+        attribute :motivation, String
+        attribute :language, String
+        attribute :other_notes
+        attribute :suitable_for_pd, ::Document::Enum, enums: %i(yes no)
+        attribute :system_choice, ::Document::Enum, enums: %i(Homechoice Solo Staysafe)
+        attribute :insertion_discussed, ::Document::Enum, enums: %i(yes no)
+        attribute :method_chosen, ::Document::Enum, enums: %i(PERC_LA IOT_GA)
+        attribute :access_clinic_referral, ::Document::Enum, enums: %i(yes no)
+        attribute :access_clinic_on, Date
+        attribute :abdo_assessor, String
+        attribute :bowel_disease, ::Document::Enum, enums: %i(yes no)
+        attribute :bowel_disease_notes
+        attribute :added_comments
       end
       has_document
 
