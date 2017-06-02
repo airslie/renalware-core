809cf2ef2df759979386a11c96aa40c7c977773d
Tim Crowe <tim@woodpigeon.com>
2017-06-02 15:48:54 +0100

    Disable all items bar Workup on Tx Rc summary unless they have the Translpant modality

--- app/models/renalware/transplants/patient.rb
+++ app/models/renalware/transplants/patient.rb
@@ -8,11 +8,13 @@ module Renalware
               class_name: "DonorStage"
 
       def has_ever_been_a_donor?
-        modality_descriptions.exists?(type: "Renalware::Transplants::DonorModalityDescription")
+        donor_modality = "Renalware::Transplants::DonorModalityDescription"
+        @has_ever_been_a_donor ||= modality_descriptions.exists?(type: donor_modality)
       end
 
       def has_ever_been_a_recipient?
-        modality_descriptions.exists?(type: "Renalware::Transplants::RecipientModalityDescription")
+        recipient_modality = "Renalware::Transplants::RecipientModalityDescription"
+        @has_ever_been_a_recipient ||= modality_descriptions.exists?(type: recipient_modality)
       end
     end
   end
