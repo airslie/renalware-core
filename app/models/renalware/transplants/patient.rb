05426ca682fa3ef51f0da3987f580a3d74392bbb
Tim Crowe <tim@woodpigeon.com>
2017-06-07 12:00:01 +0100

    Add missing modality warning when disabling meu items for that reason

--- app/models/renalware/transplants/patient.rb
+++ app/models/renalware/transplants/patient.rb
@@ -3,18 +3,20 @@ require_dependency "renalware/transplants"
 module Renalware
   module Transplants
     class Patient < ActiveType::Record[Renalware::Patient]
-      has_one :current_donor_stage,
-              -> { current },
-              class_name: "DonorStage"
+      has_one :current_donor_stage, -> { current }, class_name: "DonorStage"
 
       def has_ever_been_a_donor?
-        donor_modality = "Renalware::Transplants::DonorModalityDescription"
-        @has_ever_been_a_donor ||= modality_descriptions.exists?(type: donor_modality)
+        @has_ever_been_a_donor ||= begin
+          donor_modality = "Renalware::Transplants::DonorModalityDescription"
+          modality_descriptions.exists?(type: donor_modality)
+        end
       end
 
       def has_ever_been_a_recipient?
-        recipient_modality = "Renalware::Transplants::RecipientModalityDescription"
-        @has_ever_been_a_recipient ||= modality_descriptions.exists?(type: recipient_modality)
+        @has_ever_been_a_recipient ||= begin
+          recipient_modality = "Renalware::Transplants::RecipientModalityDescription"
+          modality_descriptions.exists?(type: recipient_modality)
+        end
       end
     end
   end
