b703c0039f26b4f3565e7566caf04bac764c1dc8
Paul Nordstrom August <paul@nuagist.es>
2017-06-07 12:57:08 -0400

    Update training session model

--- app/models/renalware/pd/training_session.rb
+++ app/models/renalware/pd/training_session.rb
@@ -11,20 +11,21 @@ module Renalware
       extend Enumerize
 
       belongs_to :patient, class_name: "Renalware::PD::Patient", touch: true
+      belongs_to :training_site, class_name: "Renalware::PD::TrainingSite"
 
       attr_reader :ignore_me # see html form for explanation of this non-persistent attribute
+
+      validates :training_site_id, presence: true
 
       class Document < Document::Embedded
         attribute :started_on, Date
         attribute :trainer, String
-        attribute :training_site, ::Document::Enum # Defined in i18n as may vary
         attribute :training_type, ::Document::Enum # Defined in i18n as may vary
         attribute :training_duration, String
         attribute :outcome, ::Document::Enum, enums: %i(successful limited_success unsuccessful)
         attribute :training_comments
 
         validates :started_on, presence: true
-        validates :training_site, presence: true
         validates :training_type, presence: true
 
       end
