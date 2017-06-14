161172e4f84cfd5a85573c319b6251953ec04b1e
Paul Nordstrom August <paul@nuagist.es>
2017-06-14 12:24:08 -0400

    (Greatly) improve seed file
    based on TC's great biopsy seed

--- spec/dummy/db/seeds/demo/clinical/body_compositions.rb
+++ spec/dummy/db/seeds/demo/clinical/body_compositions.rb
@@ -1,27 +1,39 @@
 module Renalware
-
-  rabbit = Patient.find_by(family_name: "RABBIT", given_name: "Roger")
-
   log "Adding Body Composition Measurement for Roger RABBIT" do
-    Clinical::BodyComposition.create!(
-      patient_id: rabbit.to_param,
-      assessed_on: 2.days.ago.to_date,
-      created_by_id: Renalware::User.first.id,
-      updated_by_id: Renalware::User.last.id,
-      assessor_id: Renalware::User.first.id,
-      overhydration: 34.1,
-      volume_of_distribution: 52.1,
-      total_body_water: 23.1,
-      extracellular_water: 63.1,
-      intracellular_water: 23.1,
-      lean_tissue_index: 78.1,
-      fat_tissue_index: 32.1,
-      lean_tissue_mass: 32.1,
-      fat_tissue_mass: 42.1,
-      adipose_tissue_mass: 15.1,
-      body_cell_mass: 53.1,
-      quality_of_reading: 65.123,
-      notes: "Lorem rabbitum body composition notes"
-    )
+
+    class CreateBodyComposition
+      def initialize
+        @rabbit = Patient.find_by(family_name: "RABBIT", given_name: "Roger")
+        @user_id = Renalware::User.last.id
+      end
+
+      def call(on: Time.now - 2.weeks,
+               notes: "Lorem ipusm")
+        Clinical::BodyComposition.new(
+          patient_id: @rabbit.id,
+          notes: Faker::Lorem.sentence,
+          assessed_on: on,
+          created_by_id: @user_id,
+          updated_by_id: @user_id,
+          assessor_id: @user_id,
+          overhydration: rand(-20..20).round(1),
+          volume_of_distribution: rand(50.0..70.0).round(1),
+          total_body_water: rand(50.0..70.0).round(1),
+          extracellular_water: rand(50.0..70.0).round(1),
+          intracellular_water: rand(50.0..70.0).round(1),
+          lean_tissue_index: rand(50.0..70.0).round(1),
+          fat_tissue_index: rand(50.0..70.0).round(1),
+          lean_tissue_mass: rand(50.0..70.0).round(1),
+          fat_tissue_mass: rand(50.0..70.0).round(1),
+          adipose_tissue_mass: rand(50.0..70.0).round(1),
+          body_cell_mass: rand(50.0..70.0).round(1),
+          quality_of_reading: rand(50.000..100.000).round(3),
+        ).save!
+      end
+    end
+
+    CreateBodyComposition.new.call(on: Time.now - 2.months)
+    CreateBodyComposition.new.call(on: Time.now - 1.month)
+    CreateBodyComposition.new.call(on: Time.now - 1.week)
   end
 end
