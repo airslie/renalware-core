1f30c4b3a989a6e01a26b9db7a54dc8b615481d3
Paul Nordstrom August <paul@nuagist.es>
2017-06-14 17:02:26 -0400

    enable 'assign_modality'

--- app/models/renalware/clinical/body_composition.rb
+++ app/models/renalware/clinical/body_composition.rb
@@ -9,8 +9,11 @@ module Renalware
 
       belongs_to :patient, class_name: "Renalware::Clinical::Patient"
       belongs_to :assessor, class_name: "User", foreign_key: "assessor_id"
+      belongs_to :modality_description, class_name: "Modalities::Description"
 
       has_paper_trail class_name: "Renalware::Clinical::Version"
+
+      before_create :assign_modality
 
       scope :ordered, -> { order(assessed_on: :desc, created_at: :desc) }
 
@@ -29,6 +32,12 @@ module Renalware
       validates :body_cell_mass, presence: true, numeric_inclusion: { in: 0..150 }
       validates :quality_of_reading, presence: true, numeric_inclusion: { in: 50..100 }
       validates :assessed_on, presence: true, timeliness: { type: :date, allow_blank: false }
+
+      private
+
+      def assign_modality
+        self.modality_description = patient.modality_description
+      end
     end
   end
 end
---
+++ app/models/renalware/clinical/modality_description.rb
@@ -0,0 +1,8 @@
+require_dependency "renalware/clinical"
+
+module Renalware
+  module Clinical
+    class ModalityDescription < Modalities::Description
+    end
+  end
+end
