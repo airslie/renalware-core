da0292cef7c980f885832ca52b034426593c4e59
Tim Crowe <tim@woodpigeon.com>
2017-06-28 11:44:51 +0100

    Access Profile and Procedure tweaks
    - Remove abbreviation from end of Accesss Profile Type options
    - In Access Summary curr profile remove the abbreviation in ()s
    -  In Access Summary procedures list keep abbreviation at end of procedure
    - in Access Procedure form change Access Type label to Access procedure

--- app/models/renalware/accesses/type.rb
+++ app/models/renalware/accesses/type.rb
@@ -4,11 +4,12 @@ module Renalware
   module Accesses
     class Type < ApplicationRecord
       validates :name, presence: true
+      delegate :to_s, to: :name
 
       scope :ordered, -> { order(:name) }
       scope :having_abbreviation, -> { where.not(abbreviation: nil) }
 
-      def to_s
+      def long_name
         abbrev = abbreviation && " (#{abbreviation})"
         "#{name}#{abbrev}"
       end
