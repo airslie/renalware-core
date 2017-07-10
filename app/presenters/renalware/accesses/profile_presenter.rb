884ffbe95981ca3b03f19947b5acbb66d2629964
Tim Crowe <tim@woodpigeon.com>
2017-07-10 11:41:22 +0100

    Display access plan on HD Summary

--- app/presenters/renalware/accesses/profile_presenter.rb
+++ app/presenters/renalware/accesses/profile_presenter.rb
@@ -16,6 +16,20 @@ module Renalware
       def side
         super.try(:text)
       end
+
+      def plan_type
+        current_plan.plan_type.to_s
+      end
+
+      def plan_date
+        current_plan.created_at
+      end
+
+      private
+
+      def current_plan
+        @current_plan ||= patient.current_plan || NullObject.instance
+      end
     end
   end
 end
