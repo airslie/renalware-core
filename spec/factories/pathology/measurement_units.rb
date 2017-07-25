64333dabc477014544b5fc399768042219595ea8
Tim Crowe <tim@woodpigeon.com>
2017-07-25 16:00:59 +0100

    Fix specs and features for pathology measurement units

---
+++ spec/factories/pathology/measurement_units.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  sequence(:measurement_unit) { |n| "10(#{n})/L" }
+  factory :pathology_measurement_unit,
+          class: "Renalware::Pathology::MeasurementUnit" do
+    name { generate(:measurement_unit) }
+    description ""
+  end
+end
--- spec/factories/pathology/observation_descriptions.rb
+++ spec/factories/pathology/observation_descriptions.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :pathology_observation_description,
       class: "Renalware::Pathology::ObservationDescription" do
     code "WBR"
+    association :measurement_unit, factory: :pathology_measurement_unit
   end
 end
