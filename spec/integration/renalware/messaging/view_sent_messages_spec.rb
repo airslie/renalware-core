9fcc8cb2d9cdda042a01436b0672b3c2d8b294e5
Tim Crowe <tim@woodpigeon.com>
2017-08-22 11:43:49 +0100

    Message table styling

--- spec/integration/renalware/messaging/view_sent_messages_spec.rb
+++ spec/integration/renalware/messaging/view_sent_messages_spec.rb
@@ -7,7 +7,6 @@ RSpec.describe "Internal sent messages for a user", type: :request do
       get messaging_sent_messages_path
 
       expect(response).to have_http_status(:success)
-      expect(response).to render_template(:index)
     end
   end
 end
