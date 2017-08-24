0f9fe8b8a3f33fa13de2bcc1d22781bba53b4379
Tim Crowe <tim@woodpigeon.com>
2017-08-24 11:53:06 +0100

    Autofocus the recipients dropdown in message form
    Autofocus the recipients dropdown in new message form on initial load and after a validation error.

---
+++ app/assets/javascripts/renalware/messaging.js
@@ -0,0 +1,15 @@
+var Renalware = typeof Renalware === 'undefined' ? {} : Renalware;
+
+Renalware.Messaging = (function() {
+  var focusRecipientSearchInput = function() {
+    $('#message_recipient_ids').select2('open').select2('close');
+  };
+
+  return {
+    init: function () {
+      focusRecipientSearchInput()
+    }
+  };
+})();
+
+$(document).one('opened.fndtn.reveal', '#send-message-modal', Renalware.Messaging.init);
