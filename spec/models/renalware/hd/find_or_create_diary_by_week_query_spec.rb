b578f574b336761dcf5ddf6a9f00e553cdc84631
Tim Crowe <tim@woodpigeon.com>
2017-09-16 13:31:06 +0100

    Remove before(:all) causing random spec error

--- spec/models/renalware/hd/find_or_create_diary_by_week_query_spec.rb
+++ spec/models/renalware/hd/find_or_create_diary_by_week_query_spec.rb
@@ -2,28 +2,26 @@ require "rails_helper"
 
 module Renalware
   describe HD::FindOrCreateDiaryByWeekQuery do
-    before(:all) do
-      @unit = create(:hospital_unit)
-      @user = create(:user)
-    end
     let(:week_period){ WeekPeriod.from_date(Time.zone.today) }
+    let(:user) { create(:user) }
+    let(:unit) { create(:hospital_unit) }
 
     it "finds an existing diary for the given week period" do
       master_diary = create(
         :hd_master_diary,
-        hospital_unit_id: @unit.id,
-        by: @user
+        hospital_unit_id: unit.id,
+        by: user
       )
       created_diary = create(
         :hd_weekly_diary,
         master_diary: master_diary,
-        hospital_unit_id: @unit.id,
+        hospital_unit_id: unit.id,
         week_number: week_period.week_number,
         year: week_period.year,
-        by: @user)
+        by: user)
 
-      found_diary = described_class.new(by: @user,
-                                        unit_id: @unit.id,
+      found_diary = described_class.new(by: user,
+                                        unit_id: unit.id,
                                         week_period: week_period
                                         ).call
 
@@ -31,14 +29,14 @@ module Renalware
     end
 
     it "creates a new diary if one does not exists for the given week period" do
-      diary = described_class.new(by: @user,
-                                  unit_id: @unit.id,
+      diary = described_class.new(by: user,
+                                  unit_id: unit.id,
                                   week_period: week_period
                                   ).call
 
       expect(diary.week_number).to eq(week_period.week_number)
       expect(diary.year).to eq(week_period.year)
-      expect(diary.hospital_unit_id).to eq(@unit.id)
+      expect(diary.hospital_unit_id).to eq(unit.id)
     end
   end
 end
