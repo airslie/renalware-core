cb1fe41eb6b8d97b0a2d7df2550f593747c985e7
Tim Crowe <tim@woodpigeon.com>
2017-09-18 14:18:54 +0100

    Add patient filtering to electronic-cc pages
    Filter by name, nhs number, and local hospital numbers

--- app/controllers/renalware/letters/electronic_receipts_controller.rb
+++ app/controllers/renalware/letters/electronic_receipts_controller.rb
@@ -16,16 +16,17 @@ module Renalware
         }
       end
 
-      # GET all unread electronic ccs
+      # GET all unread electronic CCs
       def unread
         render_receipts(received_receipts.unread)
       end
 
-      # GET all read electronic ccs
+      # GET all read electronic CCs
       def read
         render_receipts(received_receipts.read)
       end
 
+      # GET all electronic CCs sent by the current user
       def sent
         render_receipts(sent_receipts)
       end
@@ -33,10 +34,30 @@ module Renalware
       private
 
       def render_receipts(receipts)
+        search_form = build_search_form_object
+        receipts = filter_receipts(receipts, search_form).ordered.page(page).per(per_page)
         authorize receipts
+
         render locals: {
-          receipts: present_receipts(receipts)
+          receipts: present_receipts(receipts),
+          search_form: search_form
         }
+      end
+
+      def build_search_form_object
+        Patients::SearchForm.new(
+          term: search_term,
+          url: request.path
+        )
+      end
+
+      def filter_receipts(receipts, search_form)
+        matching_patients = Patients::SearchQuery.new(term: search_form.term).call
+        receipts.joins(letter: [:patient]).merge(matching_patients)
+      end
+
+      def search_term
+        params.fetch(:patient_search, {}).fetch(:term, nil)
       end
 
       def receipt
@@ -44,11 +65,11 @@ module Renalware
       end
 
       def received_receipts
-        ElectronicReceipt.where(recipient_id: current_user.id).ordered.page(page).per(per_page)
+        ElectronicReceipt.where(recipient_id: current_user.id)
       end
 
       def sent_receipts
-        ElectronicReceipt.sent_by(current_user.id).ordered.page(page).per(per_page)
+        ElectronicReceipt.sent_by(current_user.id)
       end
 
       def present_receipts(receipts)
