ec59ea4d870b4cf1c0e685de0131ab9f9cee7450
Tim Crowe <tim@woodpigeon.com>
2017-09-18 14:13:11 +0100

    Add reusable patient search components
    A queryobject, formobject and partial that work together for adding patient filtering to an html page.

---
+++ app/models/renalware/patients/search_form.rb
@@ -0,0 +1,17 @@
+require_dependency "renalware/patients"
+
+# A form object for use behind a form (see to_partial_path) which can add a patient filter
+# to a page. See Letters/ElectronicReceiptsController for an example.
+# Works in concert with Patients::SearchQuery.
+module Renalware
+  module Patients
+    class SearchForm
+      include ActiveModel::Model
+      attr_accessor :term, :url
+
+      def to_partial_path
+        "renalware/patients/search_form"
+      end
+    end
+  end
+end
