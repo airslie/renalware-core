9f7fa3301dfea2a3e15c53ae4ff6d98c06ed804d
Tim Crowe <tim@woodpigeon.com>
2017-09-20 17:02:00 +0100

    Add Admission Requests into the top Patients menu

--- spec/integration/renalware/admissions/create_admission_request_spec.rb
+++ spec/integration/renalware/admissions/create_admission_request_spec.rb
@@ -2,9 +2,11 @@ require "rails_helper"
 
 feature "Create an Admission Request", type: :feature, js: true do
   scenario "Creating a new request from the patient LH menu" do
+
     login_as_clinician
     patient = create(:patient)
     reason = create(:admissions_request_reason, description: "AKI")
+    expect(Renalware::Admissions::Request.count).to eq(0)
     clinical_profile_path = patient_clinical_profile_path(patient)
     dialog_title = "Request Admission"
 
@@ -19,8 +21,7 @@ feature "Create an Admission Request", type: :feature, js: true do
 
     expect(page).to have_content(dialog_title)
 
-    # 2. Try filling out without a reason and we should get an error displayed in the dialog
-
+    # 1. Try filling out without a reason and we should get an error displayed in the dialog
     click_on("Create")
     expect(page).to have_content(t_validation(:reason_id, :blank))
 
@@ -28,7 +29,8 @@ feature "Create an Admission Request", type: :feature, js: true do
     select reason.description, from: "Reason"
     click_on("Create")
 
-    expect(page).to have_content(dialog_title)
+    expect(page).to_not have_content(dialog_title)
+    expect(Renalware::Admissions::Request.count).to eq(1)
   end
 
   def t_validation(attribute, validation)
