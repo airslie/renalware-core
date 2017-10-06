43ad5aee4f60dc65ca347b002d6ee9d45fb1abda
Tim Crowe <tim@woodpigeon.com>
2017-10-06 16:46:33 +0100

    Fixed spec format error

--- spec/factories/hd/diurnal_period_codes.rb
+++ spec/factories/hd/diurnal_period_codes.rb
@@ -2,8 +2,17 @@ FactoryGirl.define do
   factory :hd_diurnal_period_code, class: "Renalware::HD::DiurnalPeriodCode" do
     initialize_with { Renalware::HD::DiurnalPeriodCode.find_or_create_by(code: code) }
     code "am"
-    trait :am { code "am" }
-    trait :pm { code "pm" }
-    trait :eve { code "eve" }
+
+    trait :am do
+      code "am"
+    end
+
+    trait :pm do
+      code "pm"
+    end
+
+    trait :eve do
+      code "eve"
+    end
   end
 end
