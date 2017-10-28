e534a69499633c598a4377d5fbb6c9871e6325cd
Paul Nordstrom August <paul@nuagist.es>
2017-10-28 09:57:23 +0100

    Remove local modality descr from seeds/default/
    These will be seeded in the local migration process

--- db/seeds/default/modalities/descriptions.rb
+++ db/seeds/default/modalities/descriptions.rb
@@ -6,18 +6,5 @@ module Renalware
     Transplants::RecipientModalityDescription.find_or_create_by!(name: "Transplant")
     PD::ModalityDescription.find_or_create_by!(name: "PD")
     HD::ModalityDescription.find_or_create_by!(name: "HD")
-    [
-      "LCC",
-      "LOST",
-      "Nephrology",
-      "Potential LD",
-      "Supportive Care",
-      "Transfer Out",
-      "Transplant",
-      "vCKD",
-      "Waiting List"
-    ].each do |modal_name|
-      Modalities::Description.find_or_create_by!(name: modal_name)
-    end
   end
 end
