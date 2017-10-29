06e762674cf119ff9bde753dc95f6cb0007ce752
Tim Crowe <tim@woodpigeon.com>
2017-10-29 18:42:48 +0000

    Sort lhs and rhs in test to avoid intermittent order errors

--- features/support/worlds/medications/patients.rb
+++ features/support/worlds/medications/patients.rb
@@ -15,8 +15,8 @@ module World
 
       def expect_prescriptions_to_be(table)
         expect(@prescriptions.size).to eq(table.hashes.size)
-        actual_names = @prescriptions.map { |prescription| prescription.patient.to_s }
-        expected_names = table.hashes.map{ |h| h["patient"] }
+        actual_names = @prescriptions.map { |prescription| prescription.patient.to_s }.sort
+        expected_names = table.hashes.map{ |h| h["patient"] }.sort
         expect(actual_names).to eq(expected_names)
       end
 
