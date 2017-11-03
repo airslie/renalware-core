6a04f3d505084e8d54820b49ae0a252d6b134b53
Tim Crowe <tim@woodpigeon.com>
2017-11-03 18:00:59 +0000

    Consult AKI Nurse toggle WIP

--- app/helpers/renalware/dropdown_button_helper.rb
+++ app/helpers/renalware/dropdown_button_helper.rb
@@ -5,5 +5,27 @@ module Renalware
         link_to_if(enabled, title, path)
       end
     end
+
+    # rubocop:disable Metrics/MethodLength
+    def dropdown_btn_icon_link(enabled: true, icon:, title:, path:, **options)
+      content_tag(:li, class: "#{'disabled' unless enabled}") do
+        capture do
+          if enabled
+            capture do
+              link_to(path, options) do
+                concat "<i class='fa fa-link-annotation #{icon}'></i>".html_safe
+                concat title
+              end
+            end
+          else
+            capture do
+              concat "<i class='fa fa-link-annotation #{icon}'></i>".html_safe
+              concat title
+            end
+          end
+        end
+      end
+    end
+    # rubocop:enable Metrics/MethodLength
   end
 end
