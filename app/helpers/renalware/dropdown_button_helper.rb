b91af96936888798a0d9ca542a4a1987be01e854
Tim Crowe <tim@woodpigeon.com>
2017-11-05 18:06:47 +0000

    Tidy up dropdown button helper

--- app/helpers/renalware/dropdown_button_helper.rb
+++ app/helpers/renalware/dropdown_button_helper.rb
@@ -1,31 +1,9 @@
+require "dropdown_button_item"
+
 module Renalware
   module DropdownButtonHelper
-    def dropdown_btn_item(enabled: true, title:, path:)
-      content_tag(:li, class: "#{'disabled' unless enabled}") do
-        link_to_if(enabled, title, path)
-      end
+    def dropdown_btn_item(**options)
+      DropdownButtonItem.new(options).to_html
     end
-
-    # rubocop:disable Metrics/MethodLength
-    def dropdown_btn_icon_link(enabled: true, icon:, title:, path:, **options)
-      content_tag(:li, class: "#{'disabled' unless enabled}") do
-        capture do
-          if enabled
-            capture do
-              link_to(path, options) do
-                concat "<i class='fa fa-link-annotation #{icon}'></i>".html_safe
-                concat title
-              end
-            end
-          else
-            capture do
-              concat "<i class='fa fa-link-annotation #{icon}'></i>".html_safe
-              concat title
-            end
-          end
-        end
-      end
-    end
-    # rubocop:enable Metrics/MethodLength
   end
 end
