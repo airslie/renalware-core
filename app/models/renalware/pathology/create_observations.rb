25004977ebf890c85f03b37ddf0e411a6b1f066a
Tim Crowe <tim@woodpigeon.com>
2017-11-13 16:39:05 +0000

    Mirth integration changes
    - Do not create patient who are not found
    - Only update pathology for exsiting patients

--- app/models/renalware/pathology/create_observations.rb
+++ app/models/renalware/pathology/create_observations.rb
@@ -1,19 +1,21 @@
 require_dependency "renalware/pathology"
 
+#
+# Create pathology observations for an existing patient from HL7 message content.
+#
 module Renalware
   module Pathology
     class CreateObservations
       def call(params)
-        observation_params = params.fetch(:observation_request)
         patient = find_patient(params.fetch(:patient_id))
-
+        observation_params = params.fetch(:observation_request)
         patient.observation_requests.create!(observation_params)
       end
 
       private
 
       def find_patient(id)
-        ::Renalware::Pathology::Patient.find(id)
+        ::Renalware::Pathology::Patient.find_by(id: id) || NullObject.instance
       end
     end
   end
