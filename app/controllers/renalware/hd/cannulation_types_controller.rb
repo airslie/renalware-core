d1594d243e8d7ada9027d0295ba5737dc7c5eb0c
Tim Crowe <tim@woodpigeon.com>
2017-11-15 21:31:12 +0000

    Remove @vars from cannulation_types

--- app/controllers/renalware/hd/cannulation_types_controller.rb
+++ app/controllers/renalware/hd/cannulation_types_controller.rb
@@ -3,38 +3,45 @@ require_dependency "renalware/hd/base_controller"
 module Renalware
   module HD
     class CannulationTypesController < BaseController
-      before_action :load_cannulation_type, only: [:edit, :update]
 
       def new
-        @cannulation_type = CannulationType.new
-        authorize @cannulation_type
+        cannulation_type = CannulationType.new
+        authorize cannulation_type
+        render_new(cannulation_type)
       end
 
       def create
-        @cannulation_type = CannulationType.new(cannulation_type_params)
-        authorize @cannulation_type
+        cannulation_type = CannulationType.new(cannulation_type_params)
+        authorize cannulation_type
 
-        if @cannulation_type.save
+        if cannulation_type.save
           redirect_to hd_cannulation_types_path,
             notice: t(".success", model_name: "cannulation type")
         else
           flash.now[:error] = t(".failed", model_name: "cannulation type")
-          render :new
+          render_new(cannulation_type)
         end
       end
 
       def index
-        @cannulation_types = CannulationType.all
-        authorize @cannulation_types
+        cannulation_types = CannulationType.all
+        authorize cannulation_types
+        render locals: { cannulation_types: cannulation_types }
+      end
+
+      def edit
+        cannulation_type = find_and_authorize_cannulation_type
+        render_edit(cannulation_type)
       end
 
       def update
-        if @cannulation_type.update(cannulation_type_params)
+        cannulation_type = find_and_authorize_cannulation_type
+        if cannulation_type.update(cannulation_type_params)
           redirect_to hd_cannulation_types_path,
             notice: t(".success", model_name: "cannulation type")
         else
           flash.now[:error] = t(".failed", model_name: "cannulation type")
-          render :edit
+          render_edit(cannulation_type)
         end
       end
 
@@ -46,13 +53,22 @@ module Renalware
 
       private
 
+      def render_new(cannulation_type)
+        render :new, locals: { cannulation_type: cannulation_type }
+      end
+
+      def render_edit(cannulation_type)
+        render :edit, locals: { cannulation_type: cannulation_type }
+      end
+
       def cannulation_type_params
         params.require(:hd_cannulation_type).permit(:name)
       end
 
-      def load_cannulation_type
-        @cannulation_type = CannulationType.find(params[:id])
-        authorize @cannulation_type
+      def find_and_authorize_cannulation_type
+        CannulationType.find(params[:id]).tap do |cannulation_type|
+          authorize cannulation_type
+        end
       end
     end
   end
