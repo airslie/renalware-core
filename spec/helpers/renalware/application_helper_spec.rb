06d5dacf3050cde6473e82013742c398da67efab
Tim Crowe <tim@woodpigeon.com>
2017-11-23 13:27:52 +0000

    Helper specs aren’t really legacy so unflagged as such

---
+++ spec/helpers/renalware/application_helper_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+describe Renalware::ApplicationHelper, type: :helper do
+  describe ".default_patient_link" do
+    it "creates a link going to the patient's clinical summary" do
+      patient = build_stubbed(:patient)
+
+      expect(helper.default_patient_link(patient))
+        .to match(/#{patient_clinical_summary_path(patient)}/)
+    end
+  end
+
+  describe ".flash_messages" do
+    it "returns alert, notice and success flash message" do
+      allow(helper).to receive(:flash).and_return(
+        {
+          alert: [:alert_message],
+          notice: [:notice_message],
+          success: [:success_message],
+          timedout: [true]
+        }
+      )
+      expect(helper.flash_messages).to eq(
+        {
+          alert: [:alert_message],
+          notice: [:notice_message],
+          success: [:success_message]
+        }
+      )
+    end
+  end
+end
