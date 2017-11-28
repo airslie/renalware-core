3ac2e93ba82c0ac74c625a51698d7c745764dd79
Tim Crowe <tim@woodpigeon.com>
2017-11-28 11:42:01 +0000

    Low Clearance MDM patients list

--- app/controllers/renalware/renal/low_clearance/mdm_patients_controller.rb
+++ app/controllers/renalware/renal/low_clearance/mdm_patients_controller.rb
@@ -5,7 +5,7 @@ module Renalware
     module LowClearance
       class MDMPatientsController < Renalware::MDMPatientsController
         def index
-          render_index(query: MDMPatientsQuery.new(q: params[:q]),
+          render_index(query: query,
                        page_title: t(".page_title"),
                        view_proc: ->(patient) { patient_renal_low_clearance_mdm_path(patient) })
         end
--- app/models/renalware/renal/low_clearance/mdm_patients_query.rb
+++ app/models/renalware/renal/low_clearance/mdm_patients_query.rb
@@ -5,12 +5,14 @@ module Renalware
     module LowClearance
       class MDMPatientsQuery
         include ModalityScopes
-        attr_reader :q, :relation
+        MODALITY_NAMES = ["LCC"].freeze
+        attr_reader :q, :relation, :named_filter
 
         # modality_names: eg "HD" or "PD"
-        def initialize(relation: Patient.all, q:)
+        def initialize(relation: Patient.all, named_filter: nil, q: {})
           @q = q
           @relation = relation
+          @named_filter = named_filter || :none
         end
 
         def call
@@ -21,7 +23,11 @@ module Renalware
           @search ||= begin
             relation
               .extending(Scopes)
+              .extending(ModalityScopes)
+              .extending(NamedFilterScopes)
+              .with_current_modality_matching(MODALITY_NAMES)
               .with_current_key_pathology
+              .public_send(named_filter.to_s)
               .search(q)
             # .order("pathology_current_key_observations.hgb_result asc")
           end
@@ -30,6 +36,32 @@ module Renalware
         module Scopes
           def with_current_key_pathology
             includes(:current_key_observation_set) # .joins(:current_key_observation)
+          end
+        end
+
+        module NamedFilterScopes
+          def none
+            self # NOOP
+          end
+
+          def on_worryboard
+            joins("RIGHT OUTER JOIN patient_worries ON patient_worries.patient_id = patients.id")
+          end
+
+          def tx_candidates
+            self
+          end
+
+          def urea
+            self
+          end
+
+          def hgb_low
+            self
+          end
+
+          def hgb_high
+            self
           end
         end
       end
