2f06b36470898f4eb6555f924e005941963430f5
Tim Crowe <tim@woodpigeon.com>
2017-12-14 16:44:07 +0000

    Add tests for trigger on pathology_observations
    Test it inserts/updates rows in pathology_current_observation_sets correctly

--- spec/models/renalware/pathology/current_observation_set_spec.rb
+++ spec/models/renalware/pathology/current_observation_set_spec.rb
@@ -1,3 +1,4 @@
+# rubocop:disable Metrics/ModuleLength
 require "rails_helper"
 
 module Renalware
@@ -39,30 +40,119 @@ module Renalware
         end
       end
 
-      # describe "#all" do
-      #   it "returns only the most recent path results" do
-      #     patient = create_patient
-      #     descriptions = create_descriptions(%w(x y z))
-      #     travel_to(Time.zone.now) do
-      #       # current
-      #       create_observations(patient,
-      #                           descriptions,
-      #                           observed_at: Time.zone.now,
-      #                           result: 99)
-      #       # !! not current
-      #       create_observations(patient,
-      #                           descriptions,
-      #                           observed_at: Time.zone.now - 1.day,
-      #                           result: 1)
+      describe "the TRIGGER on pathology_observations that updates current_observation_set" do
+        context "when a patient has no current_observation_set yet" do
+          it "creating an observation adds it to the current_observation_set jsonb hash" do
+            time = Time.zone.now
+            patient = create(:pathology_patient)
+            expect{
+              create_hgb_observation(patient: patient, observed_at: time, result: 123.1)
+            }.to change{ CurrentObservationSet.count }.by(1)
+            # trigger has now updated the current_observation_set
 
-      #       obs = patient.current_observations.all
+            obs_set = CurrentObservationSet.find_by(patient_id: patient.id)
+            hgb = obs_set.values[:HGB]
+            expect_obs_to_match(hgb, "123.1", time)
+          end
+        end
 
-      #       expect(obs.length).to eq(3)
-      #       expect(obs.map(&:observed_at).uniq).to eq([Time.zone.now])
-      #       expect(obs.map(&:result).uniq).to eq(["99"])
-      #     end
-      #  end
-      # end
+        context "when a patient has a current_observation_set with an entry for HGB" do
+          context "when a new HGB observation is created (importantly with a newer date)" do
+            it "updates the values hash with the new result and time" do
+              time = Time.zone.now
+              patient = create(:pathology_patient)
+              obs = create_hgb_observation(patient: patient, observed_at: time, result: 111.1)
+
+              obs_set = CurrentObservationSet.find_by(patient_id: patient.id)
+              hgb = obs_set.values[:HGB]
+              expect_obs_to_match(hgb, 111.1, time)
+
+              new_time = time + 1.hour
+              expect{
+                obs = create_hgb_observation(patient: patient, observed_at: new_time, result: 222.2)
+              }.to change{ CurrentObservationSet.count }.by(0)
+              # trigger has now updated the current_observation_set
+
+              new_hgb = obs_set.reload.values[:HGB]
+              expect(new_hgb).not_to eq(hgb)
+              expect_obs_to_match(new_hgb, obs.result, new_time)
+            end
+          end
+
+          context "when another HGB observation is created but with an OLDER date" do
+            it "does not update the values hash" do
+              time = Time.zone.now
+              patient = create(:pathology_patient)
+              obs = create_hgb_observation(patient: patient, observed_at: time, result: 111.1)
+              # trigger has now updated the current_observation_set
+
+              obs_set = CurrentObservationSet.find_by(patient_id: patient.id)
+              hgb = obs_set.values[:HGB]
+              expect_obs_to_match(hgb, obs.result, time)
+
+              expect{
+                obs = create_hgb_observation(
+                  patient: patient,
+                  observed_at:  time - 1.hour, # an older date,
+                  result: 222.2
+                )
+              }.to change{ CurrentObservationSet.count }.by(0)
+
+              expect(obs_set.reload.values[:HGB]).to eq(hgb)
+            end
+          end
+
+          context "when another HGB observation is updated and has a NEWER date" do
+            it "updates the values hash with the new result and time" do
+              time = Time.zone.now
+              patient = create(:pathology_patient)
+              obs = create_hgb_observation(patient: patient, observed_at: time, result: 111.1)
+              # trigger has now updated the current_observation_set
+
+              new_time = time + 1.hour
+              obs.update!(observed_at: new_time, result: 222.2)
+
+              obs.reload
+              obs_set = CurrentObservationSet.find_by(patient_id: patient.id)
+              hgb = obs_set.values[:HGB]
+              expect_obs_to_match(hgb, obs.result, new_time)
+            end
+          end
+
+          context "when an HGB observation is updated with the same date but different result" do
+            it "updates the values hash with the new result and time" do
+              time = Time.zone.now
+              patient = create(:pathology_patient)
+              obs = create_hgb_observation(patient: patient, observed_at: time, result: 111.1)
+              # trigger has now updated the current_observation_set
+
+              obs.update!(result: 222.2)
+
+              obs_set = CurrentObservationSet.find_by(patient_id: patient.id)
+              hgb = obs_set.values[:HGB]
+              expect_obs_to_match(hgb, obs.result, time)
+            end
+          end
+        end
+      end
+
+      def create_hgb_observation(patient:, **observation_options)
+        request = create(:pathology_observation_request, patient: patient)
+        create(
+          :pathology_observation,
+          request: request,
+          description: create(:pathology_observation_description, code: "HGB"),
+          **observation_options
+        )
+      end
+
+      # rubocop:disable Rails/TimeZone
+      def expect_obs_to_match(obs, result, time)
+        observed_at = I18n.l(Time.parse(obs[:observed_at]))
+        expect(obs[:result]).to eq(result.to_s)
+        expect(observed_at).to eq(I18n.l(time))
+      end
+      # rubocop:enable Rails/TimeZone
     end
   end
 end
