87f522f0270fab35e7bbd0d6f9cb95c066462d46
Tim Crowe <tim@woodpigeon.com>
2017-12-19 14:40:37 +0000

    Add missing specs

---
+++ spec/integration/renalware/system/mock_errors_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+feature "Producing a mock error so we can test error reporting", type: :request do
+  describe "index" do
+    it "raises a divide by zero error and thus returns a 500 http error" do
+      expect {
+        get generate_test_internal_server_error_path
+      }.to raise_error(RuntimeError)
+    end
+  end
+end
