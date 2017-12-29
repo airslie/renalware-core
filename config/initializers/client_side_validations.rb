1ec173faa7e0c97d1d94d86b1c779be97e7aff3a
Tim Crowe <tim@woodpigeon.com>
2017-12-29 21:20:46 +0000

    Update client_side_validations gem and others
    - client_side_validations
    - chosen-rails
    - delayed_job
    - paper_trail
    - trix
    - uglifier

--- config/initializers/client_side_validations.rb
+++ config/initializers/client_side_validations.rb
@@ -1,23 +1,21 @@
+# frozen_string_literal: true
 # ClientSideValidations Initializer
 
-# Disabled validators. The uniqueness validator is disabled by default for security issues.
-# Enable it on your own responsibility!
-# ClientSideValidations::Config.disabled_validators = [:uniqueness]
+# Disabled validators
+# ClientSideValidations::Config.disabled_validators = []
 
 # Uncomment to validate number format with current I18n locale
 # ClientSideValidations::Config.number_format_with_locale = true
 
-# Uncomment the following block if you want each input field to have the validation
-# messages attached.
+# Uncomment the following block if you want each input field to have the validation messages attached.
 #
 # Note: client_side_validation requires the error to be encapsulated within
 # <label for="#{instance.send(:tag_id)}" class="message"></label>
 #
-# ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
-#   unless html_tag =~ /^<label/
-#     %{<div class="field_with_errors">#{html_tag}<label for="#{instance.send(:tag_id)}"
-#     class="message">#{instance.error_message.first}</label></div>}.html_safe
+# ActionView::Base.field_error_proc = proc do |html_tag, instance|
+#   if html_tag =~ /^<label/
+#     %(<div class="field_with_errors">#{html_tag}</div>).html_safe
 #   else
-#     %{<div class="field_with_errors">#{html_tag}</div>}.html_safe
+#     %(<div class="field_with_errors">#{html_tag}<label for="#{instance.send(:tag_id)}" class="message">#{instance.error_message.first}</label></div>).html_safe
 #   end
 # end
