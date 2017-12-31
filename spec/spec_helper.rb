94bddb003198ffd78928459c5243a667743d9363
Tim Crowe <tim@woodpigeon.com>
2017-12-31 23:26:46 +0000

    Enforce RSpec expect syntax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -41,6 +41,10 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+
   config.filter_run :focus
   config.run_all_when_everything_filtered = true
 
