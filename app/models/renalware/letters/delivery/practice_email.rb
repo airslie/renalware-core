3ce38de07678bbb1d46d67cdd203406aefe21376
Tim Crowe <tim@woodpigeon.com>
2018-01-05 11:51:40 +0000

    Add PracticeEmail helper class

---
+++ app/models/renalware/letters/delivery/practice_email.rb
@@ -0,0 +1,27 @@
+require "renalware/letters"
+require "attr_extras"
+
+module Renalware
+  module Letters
+    module Delivery
+      # Utility class to help us resolve the correct email address when sending to the practice.
+      # If we allow_external_mail then we use the practice email, other wise we use the email
+      # of the last user to update the letter (useful for testing, ie you get the GP email if you
+      # approve the letter) or a system default for testing if the user has no email address.
+      class PracticeEmail
+        pattr_initialize :letter
+        delegate :patient, to: :letter
+        delegate :practice, to: :patient
+
+        def address
+          if Renalware.config.allow_external_mail
+            practice.email # nil is acceptable
+          else
+            user_email = letter.updated_by&.email
+            user_email.presence || Renalware.config.fallback_email_address_for_test_messages
+          end
+        end
+      end
+    end
+  end
+end
