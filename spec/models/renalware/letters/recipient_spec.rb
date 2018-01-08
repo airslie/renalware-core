68f5e62cb1fe7c2e9cc8e782be7159e91f626532
Tim Crowe <tim@woodpigeon.com>
2018-01-08 14:35:58 +0000

    Add heroku default ENV vars for emails and letters

--- spec/models/renalware/letters/recipient_spec.rb
+++ spec/models/renalware/letters/recipient_spec.rb
@@ -3,6 +3,7 @@ require "rails_helper"
 module Renalware
   module Letters
     RSpec.describe Recipient, type: :model do
+      #
       it { is_expected.to respond_to(:emailed_at) }
       it { is_expected.to respond_to(:printed_at) }
 
