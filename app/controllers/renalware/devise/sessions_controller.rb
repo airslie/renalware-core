5fa8c51548f3464505a1ee3834a735b9d5e0dc97
Tim Crowe <tim@woodpigeon.com>
2018-01-08 10:54:39 +0000

    A further fix to resolve devise login redirect issue

--- app/controllers/renalware/devise/sessions_controller.rb
+++ app/controllers/renalware/devise/sessions_controller.rb
@@ -20,7 +20,8 @@ module Renalware
     end
 
     # Important note: Since Devise 4.4.0 the gem's SessionsController#create will do an implicit
-    # check to see if the resource (User signing-in) is valid. If the user is not valid it now
+    # check to see if the resource (User signing-in) is valid (or rather, I think it checks for the
+    # presence of anything in resource.errors). If the user is not valid it now
     # skips the redirect to the path specified in .after_sign_in_path_for.
     # The result for us was that the user _could_ log on, but they stayed on the login screen
     # (the appearance of the menu indicated they were in fact logged in). The redirect was not
@@ -35,7 +36,16 @@ module Renalware
     # Note that ideally we should move signature, professional position etc to a Profile model
     # and then we could remove the conditional validation.
     def create
-      super { |resource| resource.skip_validation = true }
+      super do |resource|
+        # This first line clears any existing errors that prevent Devise redirecting on successful
+        # login. It does not negate any password validations however. Its just a workaround to get
+        # the redirect to work.
+        resource.errors.clear
+        # This second line means resource.valid? returns true ie conditional on: :update validations
+        # are skipped. This line is not necessary (clearing the error above actually solves the
+        # problem) but does no harm and is belt and braces.
+        resource.skip_validation = true
+      end
     end
 
     private
