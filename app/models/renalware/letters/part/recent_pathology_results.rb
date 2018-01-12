96d8a1a9dc36c02224610adcd3b09886c8e2f986
Tim Crowe <tim@woodpigeon.com>
2018-01-12 10:44:20 +0000

    Improve letter pathology formatting
    - Use OBX code not name
    - Improved grouping by date

--- app/models/renalware/letters/part/recent_pathology_results.rb
+++ app/models/renalware/letters/part/recent_pathology_results.rb
@@ -12,27 +12,19 @@ module Renalware
         "renalware/letters/parts/recent_pathology_results"
       end
 
-      def recent_pathology_results
+      def results
         @recent_pathology_results ||= begin
-          letter.pathology_snapshot
-          group_snapshot_by_code_and_date(letter.pathology_snapshot)
+          snapshot = letter.pathology_snapshot
+          return if snapshot.blank?
+          group_snapshot_by_code_and_date(snapshot)
         end
       end
 
       private
 
-      # class SnapshotGroupedByDate
-      #   pattr_initialize :snapshot
-
-      #   def to_s
-      #   end
-      # end
-
       def group_snapshot_by_code_and_date(snapshot)
         snapshot = snapshot_ordered_by_obx_codes_relevant_to_letters(snapshot)
-
         snapshot_grouped_by_date = group_snapshot_by_date(snapshot)
-
         format_pathology_string(snapshot_grouped_by_date)
       end
 
@@ -54,7 +46,7 @@ module Renalware
             h[date] = []
           end
 
-          h[date] << "#{obx_code_to_name_map[code]} #{obs[:result]}"
+          h[date] << "#{code} #{obs[:result]}"
         end
       end
 
@@ -63,18 +55,7 @@ module Renalware
         grouped_snapshot.each do |date, observations|
           str << "#{I18n.l(date&.to_date)}: #{observations.join(', ')}; "
         end
-        str
-      end
-
-      def obx_code_to_name_map
-        @obx_code_to_name_map ||= begin
-          Pathology::ObservationDescription.where(
-            code: RelevantObservationDescription.codes
-          ).select(:code, :name)
-           .each_with_object(HashWithIndifferentAccess.new) do |obx, hash|
-            hash[obx.code] = obx.name
-          end
-        end
+        str.strip
       end
     end
   end
