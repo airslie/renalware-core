dbe242da41fa9f422ebceaf4363578e1e5497027
Tim Crowe <tim@woodpigeon.com>
2018-01-15 14:26:22 +0000

    Add MDM link to Tx Recip Summary

---
+++ spec/integration/renalware/transplants/recipient_dashboard_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+RSpec.describe "Viewing the Recipient Summary (Dashboard)", type: :feature do
+  let(:modality_description) { create(:modality_description, :transplant) }
+
+  context "when the patient has the tx recipient modality" do
+    scenario "user follows the MDM link" do
+      user = login_as_clinical
+      patient = create(:transplant_patient, family_name: "Rabbit", local_patient_id: "KCH12345")
+      Renalware::Modalities::ChangePatientModality
+        .new(patient: patient, user: user)
+        .call(description: modality_description, started_on: Time.zone.now)
+
+      visit patient_transplants_recipient_dashboard_path(patient)
+      within ".page-actions" do
+        click_on "MDM"
+      end
+
+      expect(page.current_path).to eq(patient_transplants_mdm_path(patient))
+    end
+  end
+end
