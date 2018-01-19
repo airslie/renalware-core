fdfc39ced307e83355c5ca789e49b8ecede81d59
Tim Crowe <tim@woodpigeon.com>
2018-01-19 08:13:58 +0000

    Add job for saving a pdf letter to a file
    Used by the host app when orchestrating EPR

---
+++ spec/jobs/renalware/letters/save_pdf_letter_to_file_job_spec.rb
@@ -0,0 +1,37 @@
+require "rails_helper"
+require_dependency "renalware/letters"
+
+describe Renalware::Letters::SavePdfLetterToFileJob do
+  include LettersSpecHelper
+  subject(:job) { described_class.new }
+
+  let(:patient) do
+    create(
+      :letter_patient,
+      local_patient_id: "123",
+      family_name: "O'Farrel-Boson"
+    )
+  end
+
+  let(:letter) do
+    create_letter(
+      state: :approved,
+      to: :patient,
+      issued_on: Date.parse("2017-12-01"),
+      patient: patient
+    )
+  end
+
+  describe "#perform" do
+    it "creates the specific file with the pdf letter content" do
+      file_path = Renalware::Engine.root.join("tmp", "test.pdf")
+
+      allow_any_instance_of(Renalware::Letters::LetterPresenter)
+        .to receive(:to_html).and_return("test")
+
+      job.perform(letter: letter, file_path: file_path)
+
+      expect(File.exist?(file_path)).to be_truthy
+    end
+  end
+end
