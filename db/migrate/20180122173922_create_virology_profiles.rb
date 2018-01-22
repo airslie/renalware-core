aa4c70f3df5c4f3bb7aa2fa9993a8344cd52bf0b
Tim Crowe <tim@woodpigeon.com>
2018-01-22 18:20:19 +0000

    Add Virology

---
+++ db/migrate/20180122173922_create_virology_profiles.rb
@@ -0,0 +1,6 @@
+class CreateVirologyProfiles < ActiveRecord::Migration[5.1]
+  def change
+    create_table :virology_profiles do |t|
+    end
+  end
+end
---
+++ spec/models/renalware/virology/profile_spec.rb
@@ -0,0 +1,7 @@
+require "rails_helper"
+
+RSpec.describe Renalware::Virology::Profile do
+  it "exists" do
+    subject
+  end
+end
