a4579d6493b2bcaf3d6c3c100480cd4221150df4
Tim Crowe <tim@woodpigeon.com>
2018-01-23 11:42:55 +0000

    Add Event::Investigation

---
+++ app/models/renalware/events/investigation.rb
@@ -0,0 +1,8 @@
+require_dependency "renalware/events"
+
+module Renalware
+  module Events
+    class Investigation < Event
+    end
+  end
+end
---
+++ spec/models/renalware/events/investigation_spec.rb
@@ -0,0 +1,5 @@
+require "rails_helper"
+
+describe Renalware::Events::Investigation, type: :model do
+  it { is_expected.to be_nil }
+end
