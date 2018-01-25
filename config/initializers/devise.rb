ee3a05ba492530b666725da05eed2c0368af2080
Tim Crowe <tim@woodpigeon.com>
2018-01-25 07:41:46 +0000

    Use config default from email address in Devise emails

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -11,7 +11,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = "please-change-me-at-config-initializers-devise@example.com"
+  config.mailer_sender = Renalware.config.default_from_email_address
 
   config.parent_controller = "ApplicationController"
   config.router_name = :renalware
