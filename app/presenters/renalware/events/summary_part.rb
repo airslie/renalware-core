5d10c8a6e9ac684bb7a1e677dfef459a4e6d2490
Tim Crowe <tim@woodpigeon.com>
2018-02-16 23:24:11 +0000

    Cache events on Clinical summary

--- app/presenters/renalware/events/summary_part.rb
+++ app/presenters/renalware/events/summary_part.rb
@@ -3,8 +3,8 @@ require_dependency "renalware/events"
 module Renalware
   module Events
     class SummaryPart < Renalware::SummaryPart
-      def current_events
-        @current_events ||= begin
+      def recent_events
+        @recent_events ||= begin
           Events::Event.includes([:created_by, :event_type])
                        .for_patient(patient)
                        .limit(Renalware.config.clinical_summary_max_events_to_display)
@@ -12,11 +12,15 @@ module Renalware
         end
       end
 
-      def current_events_count
+      def recent_events_count
         title_friendly_collection_count(
-          actual: current_events.size,
+          actual: recent_events.size,
           total: patient.summary.events_count
         )
+      end
+
+      def cache_key
+        Events::Event.for_patient(patient).maximum(:updated_at)
       end
 
       def to_partial_path
