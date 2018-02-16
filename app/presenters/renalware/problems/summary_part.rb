9a7e44f908ade66d1a83b7c192e23c33da857bd2
Tim Crowe <tim@woodpigeon.com>
2018-02-16 22:53:10 +0000

    Cache clinical summary problem list
    And move grid instruction to css so the partial is more flexible.

--- app/presenters/renalware/problems/summary_part.rb
+++ app/presenters/renalware/problems/summary_part.rb
@@ -3,13 +3,13 @@ require_dependency "renalware/problems"
 module Renalware
   module Problems
     class SummaryPart < Renalware::SummaryPart
-      def current_problems
-        @current_problems ||= patient.problems.current.ordered
+      def problems
+        @problems ||= patient.problems.ordered
       end
 
-      # def cache_key
-      #   current_problems.maximum(:updated_at)
-      # end
+      def cache_key
+        problems.maximum(:updated_at)
+      end
 
       def to_partial_path
         "renalware/problems/problems/summary_part"
