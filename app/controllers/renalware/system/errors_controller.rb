65aec74bc62497a123b51f3d44f9901e555fbf1c
Tim Crowe <tim@woodpigeon.com>
2018-02-19 17:13:11 +0000

    Add padding to test error to check party_foul title length
    This is to test our assertion that party_foul is posting titles longer than 256 chars.

--- app/controllers/renalware/system/errors_controller.rb
+++ app/controllers/renalware/system/errors_controller.rb
@@ -13,7 +13,9 @@ module Renalware
 
       def generate_test_internal_server_error
         raise "This is an intentionally raised error - please ignore it. " \
-              "It is used only to test system integration"
+              "It is used only to test system integration. " \
+              "The rest of this messages is padding to test that the title is truncated to 256 " \
+              "characters#{'.' * 100}"
       end
     end
   end
