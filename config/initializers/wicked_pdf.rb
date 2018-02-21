2c7b2df11ad00212b99120e382b9cc32ddd52b42
Tim Crowe <tim@woodpigeon.com>
2018-02-21 20:43:44 +0000

    Housekeeping

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -14,7 +14,6 @@ WickedPdf.config = {
   # exe_path: '/usr/local/bin/wkhtmltopdf',
   #   or
   # exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
-
   # Layout file to be used for all PDFs
   # (but can be overridden in `render :pdf` calls)
   # layout: 'pdf.html',
