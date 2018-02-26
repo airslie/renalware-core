5cfc6a50a13b31010e667df676dc9e47283af101
Tim Crowe <tim@woodpigeon.com>
2018-02-26 11:13:33 +0000

    Add tests to ensure OBR placer order number is stored correctly
    I.e. check we are not resolving the wrong OBR placer order number

--- spec/integration/renalware/feeds/hl7_end_to_end_spec.rb
+++ spec/integration/renalware/feeds/hl7_end_to_end_spec.rb
@@ -8,10 +8,10 @@ RSpec.describe "HL7 message handling end to end" do
         PID|||Z999990^^^PAS Number||RABBIT^JESSICA^^^MS||19880924|F|||18 RABBITHOLE ROAD^LONDON^^^SE8 8JR|||||||||||||||||||
         PV1||Inpatient|NIBC^^^^^^^^|||||MID^KINGS MIDWIVES||||||||||NHS|HXF888888^^^Visit Number|||||||||
         ORC|RE|^PCS|09B0099478^LA||CM||||200911111841|||MID^KINGS MIDWIVES|||||||
-        OBR|1|123456^PCS|09B0099478^LA|FBC^FULL BLOOD COUNT^MB||200911111841|200911111841|||||||200911111841|B^Blood|MID^KINGS MIDWIVES||09B0099478||||200911121646||HM|F||||||||||||||||||
+        OBR|1|PLACER_ORDER_NO_1^PCS|09B0099478^LA|FBC^FULL BLOOD COUNT^MB||200911111841|200911111841|||||||200911111841|B^Blood|MID^KINGS MIDWIVES||09B0099478||||200911121646||HM|F||||||||||||||||||
         OBX|1|TX|WBC^WBC^MB||6.09|10\\S\\12/L|||||F|||200911112026||BBKA^Donald DUCK|
         OBX|2|TX|RBC^RBC^MB||4.00|10\\S\\9/L|||||F|||200911112026||BBKA^Donald DUCK|
-        OBR|2|111111111^PCS|100000000^LA|RLU^RENAL/LIVER/UREA^HM||201801251204|201801250541||||||.|201801250541|B^Blood|RABRO^Rabbit, Roger||100000000||||201801251249||HM|F
+        OBR|2|PLACER_ORDER_NO_2^PCS|100000000^LA|RLU^RENAL/LIVER/UREA^HM||201801251204|201801250541||||||.|201801250541|B^Blood|RABRO^Rabbit, Roger||100000000||||201801251249||HM|F
         OBX|1|NM|NA^Sodium^HM||136|mmol/L|||||F|||201801251249||XXXXXVC01^BHI Authchecker
         OBX|2|NM|POT^Potassium^HM||4.7|mmol/L|||||F|||201801251249||XXXXXVC01^BHI Authchecker
         OBX|3|NM|URE^Urea^HM||6.6|mmol/L|||||F|||201801251249||XXXXXVC01^BHI Authchecker
@@ -39,6 +39,7 @@ RSpec.describe "HL7 message handling end to end" do
       expect(patient.observation_requests.count).to eq(2)
       request_fbc = patient.observation_requests.first
       expect(request_fbc.description.code).to eq("FBC")
+      expect(request_fbc.requestor_order_number).to eq("PLACER_ORDER_NO_1")
       expect(request_fbc.observations.count).to eq(2)
 
       expect(request_fbc.observations.first).to have_attributes(
@@ -48,6 +49,7 @@ RSpec.describe "HL7 message handling end to end" do
       )
 
       request_rlu = patient.observation_requests.last
+      expect(request_rlu.requestor_order_number).to eq("PLACER_ORDER_NO_2")
       expect(request_rlu.description.code).to eq("RLU")
       expect(request_rlu.observations.count).to eq(4)
 
