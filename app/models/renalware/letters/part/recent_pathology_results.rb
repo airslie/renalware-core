ed67be8e21e88f494b6f7d2e05edf9d212057830
Tim Crowe <tim@woodpigeon.com>
2018-03-22 13:12:07 +0000

    Fix CRE not showing in letters
    Issue to do with dates and and date order affecting output.
    First attempt to fix works but has scuppered the required order so I need to track down what that should be ie with regrard to grouping obx precedence and date order.

--- app/models/renalware/letters/part/recent_pathology_results.rb
+++ app/models/renalware/letters/part/recent_pathology_results.rb
@@ -15,8 +15,8 @@ module Renalware
       end
 
       def results
-        @recent_pathology_results ||= begin
-          snapshot = letter.pathology_snapshot
+        @results ||= begin
+          snapshot = letter.pathology_snapshot&.dup
           return if snapshot.blank?
           group_snapshot_by_code_and_date(snapshot)
         end
@@ -25,37 +25,58 @@ module Renalware
       private
 
       def group_snapshot_by_code_and_date(snapshot)
-        snapshot = snapshot_ordered_by_obx_codes_relevant_to_letters(snapshot)
-        snapshot_grouped_by_date = group_snapshot_by_date(snapshot)
-        format_pathology_string(snapshot_grouped_by_date)
+        hash = map_snapshot_into_new_hash_keyed_by_observation_date(snapshot)
+        hash = sort_each_dates_codes_by_obx_precedence(hash)
+        format_pathology_string(hash)
       end
 
-      def snapshot_ordered_by_obx_codes_relevant_to_letters(snapshot)
-        RelevantObservationDescription.codes.each_with_object({}) do |code, hash|
-          hash[code.to_sym] = snapshot[code.to_sym]
+      # We start with e.g.
+      # {
+      #  "NA": {
+      #    "result": "137",
+      #    "observed_at": "2018-03-12T13:56:00"
+      #  }
+      #  "ABC": {
+      #    "result": "123",
+      #    "observed_at": "2018-03-12T13:56:00"
+      #  },...
+      # }
+      # and map to:
+      # {
+      #   "2018-03-12": {
+      #     "NA": "137",
+      #     "ABC": "123"
+      #   }
+      # }
+      def map_snapshot_into_new_hash_keyed_by_observation_date(snapshot)
+        snapshot.each_with_object({}) do |arr, hash|
+          key, value = arr
+          result = value[:result]
+          observed_at_date = Time.zone.parse(value[:observed_at]).to_date
+          date_key = I18n.l(observed_at_date)
+          hash[date_key] ||= {}
+          hash[date_key][key] = result
         end
       end
 
-      def group_snapshot_by_date(snapshot)
-        current_date = NullObject.instance
-        snapshot.each_with_object({}) do |observation, h|
-          code, obs = observation
-          next if obs.nil?
-          date = Time.zone.parse(obs[:observed_at])
-
-          if date != current_date
-            current_date = date
-            h[date] = []
+      def sort_each_dates_codes_by_obx_precedence(hash)
+        # Sort hash by date so we get e.g.
+        #   {"02-Feb-2018"=>{:CCA=>"2.24", :CRE=>"221", :PHOS=>"0.85"}}
+        hash = hash.sort_by{ |key, _v| Date.parse(key) }.reverse # newest first
+        # now sort each dates codes by OBX precedence
+        hash.each_with_object({}) do |arr, new_hash|
+          key, results = arr
+          new_hash[key] = results.sort_by do |obx_code_key, _result|
+            RelevantObservationDescription.codes.find_index(obx_code_key.to_s.upcase)
           end
-
-          h[date] << "#{code} #{obs[:result]}"
         end
       end
 
       def format_pathology_string(grouped_snapshot)
         str = ""
         grouped_snapshot.each do |date, observations|
-          str += "#{I18n.l(date&.to_date)}: #{observations.join(', ')}; "
+          observations_string = observations.map{ |x| x.join(" ") }.join(", ")
+          str += "#{I18n.l(date&.to_date)}: #{observations_string}; "
         end
         str.strip
       end
