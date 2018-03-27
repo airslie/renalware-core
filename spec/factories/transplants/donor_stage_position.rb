d8d186d7ec98dae72b45ee124cc17ee166617852
Tim Crowe <tim@woodpigeon.com>
2018-03-27 16:15:02 +0100

    Use a random string rather than Faker::Lorem.word in specs
    Faker::Lorem.word is causing random ‘Name already taken’ failures on CI because when it serves up a duplicate

--- spec/factories/transplants/donor_stage_position.rb
+++ spec/factories/transplants/donor_stage_position.rb
@@ -2,6 +2,6 @@
 
 FactoryBot.define do
   factory :donor_stage_position, class: "Renalware::Transplants::DonorStagePosition" do
-    name { Faker::Lorem.word }
+    name { SecureRandom.hex(20) }
   end
 end
