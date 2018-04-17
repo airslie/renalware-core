d44db5081a1811065eacf59fbf14f76e84bc0e88
Tim Crowe <tim@woodpigeon.com>
2018-04-17 10:21:42 +0100

    Improve with_profile scope
    Join onto hd_profile -> site so we can use that in ransack searches/sorting

--- app/models/renalware/hd/patient.rb
+++ app/models/renalware/hd/patient.rb
@@ -8,7 +8,14 @@ module Renalware
       has_one :hd_profile, class_name: "Profile"
       has_one :hd_preference_set, class_name: "PreferenceSet"
       has_many :hd_sessions, class_name: "Session"
-      scope :with_profile, -> { includes(:hd_profile) }
+      scope :with_profile, lambda {
+        includes(hd_profile: :hospital_unit)
+        .joins(<<-SQL)
+          LEFT OUTER JOIN hd_profiles ON hd_profiles.patient_id = patients.id
+          LEFT OUTER JOIN hospital_units ON hd_profiles.hospital_unit_id = hospital_units.id
+        SQL
+        .where("hd_profiles.deactivated_at is NULL")
+      }
 
       def treated?
         modality_descriptions.exists?(type: "Renalware::HD::ModalityDescription")
