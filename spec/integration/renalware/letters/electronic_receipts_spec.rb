d8b5948eb0e43b4a37cfccf01cb968367d8528fe
Tim Crowe <tim@woodpigeon.com>
2018-04-18 09:42:09 +0100

    Add Electronic CC specs
    To test correct messages listed on read and unread tabs

--- spec/integration/renalware/letters/electronic_receipts_spec.rb
+++ spec/integration/renalware/letters/electronic_receipts_spec.rb
@@ -2,62 +2,135 @@
 
 require "rails_helper"
 
-RSpec.describe "View a user's read/unread electronic ccs", type: :request do
+RSpec.describe "View a user's read/unread electronic ccs", type: :feature do
   include LettersSpecHelper
+  let(:patient_family_name) { "GRAVES" }
+  let(:primary_care_physician) { create(:letter_primary_care_physician) }
+  let(:user_who_is_not_me) { create(:user) }
 
-  def create_receipt(patient_family_name, body, read: false, to: @current_user)
+  let(:completed_letter) { crete_letter_with_state(:completed) }
+  let(:approved_letter) { crete_letter_with_state(:approved) }
+  let(:pending_review_letter) { crete_letter_with_state(:pending_review) }
+
+  let(:unread_rcpt_for_approved_letter_sent_to_me) do
+    create_receipt(read: false, to: me, letter: approved_letter)
+  end
+  let(:unread_rcpt_for_completed_letter_sent_to_me) do
+    create_receipt(read: false, to: me, letter: completed_letter)
+  end
+  let(:unread_rcpt_for_completed_letter_sent_to_other) do
+    create_receipt(read: false, to: user_who_is_not_me, letter: completed_letter)
+  end
+  let(:unread_rcpt_for_pending_letter_sent_to_me) do
+    create_receipt(read: false, to: me, letter: pending_review_letter)
+  end
+  let(:read_rcpt_for_approved_letter_sent_to_me) do
+    create_receipt(read: true, to: me, letter: approved_letter)
+  end
+  let(:read_rcpt_for_completed_letter_sent_to_me) do
+    create_receipt(read: true, to: me, letter: completed_letter)
+  end
+  let(:read_rcpt_for_completed_letter_sent_to_other) do
+    create_receipt(read: true, to: user_who_is_not_me, letter: completed_letter)
+  end
+
+  def crete_letter_with_state(state)
     patient = create(
       :letter_patient,
-      primary_care_physician: create(:letter_primary_care_physician),
-      family_name: patient_family_name
+      primary_care_physician: primary_care_physician,
+      family_name: SecureRandom.hex(10).upcase
     )
-    approved_letter = create_letter(
+    create_letter(
       to: :patient,
-      state: :approved,
+      state: state,
       patient: patient,
       description: "xxx",
-      body: body
+      body: SecureRandom.hex(10)
     )
+  end
+
+  def create_receipt(read: false, to: @current_user, letter:)
     create(
       :letter_electronic_receipt,
-      letter: approved_letter,
+      letter: letter,
       recipient: to,
       read_at: read ? Time.zone.now : nil
     )
   end
 
+  def me
+    @current_user
+  end
+
+  before do
+    @current_user = login_as_clinical
+  end
+
   describe "GET unread" do
-    it "responds successfully" do
-      other_user = create(:user)
-      body = SecureRandom.hex(10)
-      create_receipt("GRAVES", body, read: false, to: @current_user)
-      create_receipt("GRAVES", "This one is not for my eyes", read: false, to: other_user)
+    it "displays only unread letters which are approved or completed and sent to me" do
+      all_receipts = [
+        unread_rcpt_for_approved_letter_sent_to_me,
+        unread_rcpt_for_completed_letter_sent_to_me,
+        unread_rcpt_for_pending_letter_sent_to_me,
+        unread_rcpt_for_completed_letter_sent_to_other,
+        read_rcpt_for_approved_letter_sent_to_me,
+        read_rcpt_for_completed_letter_sent_to_me
+      ]
 
-      get unread_letters_electronic_receipts_path
+      expected_visible_receipts = [
+        unread_rcpt_for_approved_letter_sent_to_me,
+        unread_rcpt_for_completed_letter_sent_to_me
+      ]
 
-      expect(response).to have_http_status(:success)
-      expect(response).to render_template(:unread)
-      expect(response.body).to include("GRAVES")
-      expect(response.body).to include(body)
-      expect(response.body).not_to include("This one is not for my eyes")
+      visit unread_letters_electronic_receipts_path
+
+      all_receipts.each do |receipt|
+        selector = "#electronic-receipt-#{receipt.id}"
+        if expected_visible_receipts.include?(receipt)
+          expect(page).to have_css(selector)
+        else
+          expect(page).not_to have_css(selector)
+        end
+      end
     end
   end
 
   describe "GET read" do
     it "responds successfully" do
-      get read_letters_electronic_receipts_path
+      all_receipts = [
+        unread_rcpt_for_approved_letter_sent_to_me,
+        unread_rcpt_for_completed_letter_sent_to_me,
+        unread_rcpt_for_pending_letter_sent_to_me,
+        read_rcpt_for_approved_letter_sent_to_me,
+        read_rcpt_for_completed_letter_sent_to_me,
+        read_rcpt_for_completed_letter_sent_to_other
+      ]
 
-      expect(response).to have_http_status(:success)
-      expect(response).to render_template(:read)
+      expected_visible_receipts = [
+        read_rcpt_for_approved_letter_sent_to_me,
+        read_rcpt_for_completed_letter_sent_to_me
+      ]
+
+      visit read_letters_electronic_receipts_path
+
+      all_receipts.each do |receipt|
+        selector = "#electronic-receipt-#{receipt.id}"
+        if expected_visible_receipts.include?(receipt)
+          expect(page).to have_css(selector)
+        else
+          expect(page).not_to have_css(selector)
+        end
+      end
     end
   end
 
   describe "GET sent" do
     it "responds successfully" do
-      get sent_letters_electronic_receipts_path
+      visit sent_letters_electronic_receipts_path
 
-      expect(response).to have_http_status(:success)
-      expect(response).to render_template(:sent)
+      expect(page.status_code).to eq(200)
+
+      # TODO: Test content
     end
   end
 end
