7782cf7685a8dee56975864b9893ee251a64b535
Tim Crowe <tim@woodpigeon.com>
2018-05-08 09:52:37 +0100

    Ensure a unique requestor order number

--- app/models/renalware/pathology/observation_request.rb
+++ app/models/renalware/pathology/observation_request.rb
@@ -17,6 +17,7 @@ module Renalware
       validates :patient, presence: true
       validates :requestor_name, presence: true
       validates :requested_at, presence: true
+      validates :requestor_order_number, presence: true, uniqueness: true
 
       scope :ordered, -> { order(requested_at: :desc) }
 
