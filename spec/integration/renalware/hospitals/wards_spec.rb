f586d7dc084a822cd5f0e8ba59a9e686b49e5bda
Tim Crowe <tim@woodpigeon.com>
2018-05-16 09:27:12 +0100

    Ward management WIP

--- spec/integration/renalware/hospitals/wards_spec.rb
+++ spec/integration/renalware/hospitals/wards_spec.rb
@@ -3,7 +3,7 @@
 require "rails_helper"
 
 RSpec.describe "Hospital Wards", type: :request do
-  describe "GET index json" do
+  describe "GET index JSON" do
     it "returns wards for a hospital unit, useful for example for inserting into a wards select " \
        "when the parent unit dropdown has changed" do
 
@@ -21,6 +21,18 @@ RSpec.describe "Hospital Wards", type: :request do
           { id: ward_b.id, name: ward_b.name }
         ]
       )
+    end
+  end
+
+  describe "GET index HTML" do
+    it "displays a list of wards for a hospital" do
+      unit = create(:hospital_unit)
+      # ward_a = create(:hospital_ward, name: "WardA", hospital_unit: unit)
+      # ward_b = create(:hospital_ward, name: "WardB", hospital_unit: unit)
+
+      get hospitals_unit_wards_path(unit.id)
+
+      expect(response).to be_successful
     end
   end
 end
