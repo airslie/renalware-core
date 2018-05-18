bf456db164c2aacff4e435824a9c7599d02b728d
Tim Crowe <tim@woodpigeon.com>
2018-05-18 14:16:51 +0100

    Fix and test consults error when searching by eg ‘rabbit r’
    Was raising an ambiguous column error on family_name as we were also including the creator (user) and both `patients` and `users` have a family_name column.
    Had previously removed the created_by user from the consults table so no longwe neded to include them in the query anyway.
    Fixes renalware-kch 392

---
+++ spec/integration/renalware/admissions/consults_filtering_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+module Renalware
+  RSpec.describe "Filtering consults", type: :feature do
+    let(:consult_site1) { create(:admissions_consult_site, name: "Site1") }
+    let(:consult_site2) { create(:admissions_consult_site, name: "Site2") }
+    let(:hospital_unit) { create(:hospital_unit, unit_code: "HospUnit1") }
+    let(:hospital_ward) { create(:hospital_ward, name: "Ward1", hospital_unit: hospital_unit) }
+
+    context "when searching by family name and part of given name e.g. Rabbit R" do
+      it "succeeds without a 'family_name' is ambiguous' error" do
+        create(
+          :admissions_consult,
+          patient: create(:patient, family_name: "Rabbit", given_name: "Ronald"),
+          started_on: Time.zone.today
+        )
+        login_as_clinical
+
+        visit admissions_consults_path
+
+        within ".filters" do
+          fill_in "Hosp/NHS no or name", with: "rabbit r"
+          click_on "Filter"
+        end
+
+        within ".admissions-consults-table tbody" do
+          expect(page).to have_content("RABBIT, Ronald")
+        end
+      end
+    end
+  end
+end
