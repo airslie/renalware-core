c01093f919db6aa09ca1bde6ad5de9c29c3c43b3
Tim Crowe <tim@woodpigeon.com>
2018-05-21 13:28:18 +0100

    List Wards

--- app/controllers/renalware/hospitals/wards_controller.rb
+++ app/controllers/renalware/hospitals/wards_controller.rb
@@ -7,13 +7,17 @@ module Renalware
     class WardsController < BaseController
       def index
         authorize Ward, :index?
-        unit = Unit.find(params[:unit_id])
         wards = Ward.where(hospital_unit_id: unit.id).order(name: :asc).select(:id, :name)
         respond_to do |format|
-          format.json do
-            render json: wards.to_json
-          end
+          format.json { render json: wards.to_json }
+          format.html { render locals: { wards: wards } }
         end
+      end
+
+      private
+
+      def unit
+        @unit ||= Unit.find(params[:unit_id])
       end
     end
   end
