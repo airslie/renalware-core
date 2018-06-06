c282b89c3ba7dc03a6df19b25f8ed831ae734616
Tim Crowe <tim@woodpigeon.com>
2018-06-06 16:05:57 +0100

    Fix issue in HD Session dry weight issue
    On signing off an HD Session it should capture (assign to itself) the latest dry weight at that point in time, used when generating audit info.
    A bug meant it was always using the first recorded dry weight, not the latest

--- spec/models/renalware/hd/save_session_spec.rb
+++ spec/models/renalware/hd/save_session_spec.rb
@@ -55,6 +55,47 @@ module Renalware
           obj.call(params: { type: "Renalware::HD::Session::DNA" })
           expect(enqueued_jobs.size).to eq(1)
         end
+
+        it "assigns the most recent hd dry weight to the session" do
+          patient = create(:hd_patient)
+          create(:dry_weight, patient_id: patient.id, weight: 100.0, assessed_on: 1.year.ago)
+          latest_dw = create(
+            :dry_weight,
+            patient_id: patient.id,
+            weight: 103.0,
+            assessed_on: 1.day.ago
+          )
+          create(:dry_weight, patient_id: patient.id, weight: 99.0, assessed_on: 2.days.ago)
+
+          # Build session params but make sure there is no dry_weight_id in there
+          # as this is not usually in the params passed to SaveSession, which will lookup the latest
+          # dry_weight itself. Passing it here ends up overwriting the looked-up weight.
+          session_attributes = build(
+            :hd_closed_session,
+            hospital_unit_id: create(:hospital_unit).id
+          ).attributes
+          session_attributes.delete("dry_weight_id")
+
+          obj = SaveSession.new(patient: patient, current_user: user)
+
+          # We'll assign something to this in the save_success method below
+          saved_session = nil
+
+          # Create a listener to receive broadcast success or failure from SaveSession
+          MyListener = Class.new do
+            define_method :save_success, ->(session){ saved_session = session }
+            define_method :save_failure, ->(_session){ fail("should not get here") }
+          end
+          obj.subscribe MyListener.new
+
+          # Do the save
+          obj.call(
+            params: session_attributes.update(type: "Renalware::HD::Session::Open"),
+            signing_off: true
+          )
+
+          expect(saved_session.dry_weight).to eq(latest_dw)
+        end
       end
     end
   end
