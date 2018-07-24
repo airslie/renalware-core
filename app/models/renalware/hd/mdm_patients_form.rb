0b9eb569e79c12d66e29452166dc1d870c4512f4
Tim Crowe <tim@woodpigeon.com>
2018-07-24 13:52:44 +0100

    HD schedule filter on HD MDM Patients list

---
+++ app/models/renalware/hd/mdm_patients_form.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module Renalware
+  module HD
+    class MDMPatientsForm
+    end
+  end
+end
---
+++ spec/models/renalware/hd/mdm_patients_form_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+module Renalware::HD
+  RSpec.describe MDMPatientsForm, type: :model do
+    describe "options" do
+      it "" do
+      end
+    end
+  end
+end
