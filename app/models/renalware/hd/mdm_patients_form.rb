f028c771a18b6457401183924a8fe484858e171a
Tim Crowe <tim@woodpigeon.com>
2018-07-25 08:54:34 +0100

    HD schedule filter on HD MDM Patients list WIP

--- app/models/renalware/hd/mdm_patients_form.rb
+++ app/models/renalware/hd/mdm_patients_form.rb
@@ -3,6 +3,25 @@
 module Renalware
   module HD
     class MDMPatientsForm
+      include ActiveModel::Model
+      include Virtus::Model
+
+      attribute :hospital_unit_id, Integer
+      attribute :schedule
+
+      def options
+        opts = {
+          hospital_unit_id_eq: hospital_unit_id
+        }
+        if schedule.to_i > 0
+          opts[:schedule_defnition_id_eq] = schedule.to_i
+        else
+          case schedule
+          when "any_mtw" then opts[:schedule_defnition_id_eq] = [1, 2, 3]
+          end
+        end
+        opts
+      end
     end
   end
 end
--- spec/models/renalware/hd/mdm_patients_form_spec.rb
+++ spec/models/renalware/hd/mdm_patients_form_spec.rb
@@ -4,8 +4,31 @@ require "rails_helper"
 
 module Renalware::HD
   RSpec.describe MDMPatientsForm, type: :model do
-    describe "options" do
-      it "" do
+    describe "#options" do
+      describe "hospital_unit_id" do
+        it "maps to a ransack predicate" do
+          options = described_class.new(hospital_unit_id: 1).options
+          expect(options[:hospital_unit_id_eq]).to eq(1)
+        end
+      end
+
+      describe "schedule" do
+        context "when a schedule definition id eg 'M T W am' was chosen" do
+          it "maps to a ransack predicate so we can query for patients having the matching "\
+             "schedule definition" do
+            options = described_class.new(schedule: "1").options
+            expect(options[:schedule_definition_id_eq]).to eq(1)
+          end
+        end
+
+        context "when a named filter is passed" do
+          describe "any_mtw" do
+            it "maps to a ransack predicate" do
+              options = described_class.new(schedule: "any_mtw").options
+              expect(options[:schedule_definition_id_in]).to eq([1, 2, 3])
+            end
+          end
+        end
       end
     end
   end
