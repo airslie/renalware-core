b9b58cf43247248bdfffcc22ba2dc260451732c8
Tim Crowe <tim@woodpigeon.com>
2018-07-31 12:15:02 +0100

    Add external and Renalware session ids to HD Transmission Log

--- spec/dummy/db/seeds/hd/provider_units.rb
+++ spec/dummy/db/seeds/hd/provider_units.rb
@@ -4,6 +4,10 @@ module Renalware
   log "Adding HD Providers" do
     unit = Hospitals::Unit.find_by(unit_code: "MOUNT")
     provider = HD::Provider.find_by(name: "Diaverum")
-    HD::ProviderUnit.find_or_create_by!(hospital_unit: unit, hd_provider: provider)
+    HD::ProviderUnit.find_or_create_by!(
+      hospital_unit: unit,
+      hd_provider: provider,
+      providers_reference: "1831"
+    )
   end
 end
