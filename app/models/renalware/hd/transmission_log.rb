fec5744b24ad51eafc61148d85eb1a7d4a72fcf4
Tim Crowe <tim@woodpigeon.com>
2018-07-31 12:15:58 +0100

    Link to Renalware Session id in logs table

--- app/models/renalware/hd/transmission_log.rb
+++ app/models/renalware/hd/transmission_log.rb
@@ -8,6 +8,8 @@ module Renalware
     # and the outgoing data back to that provider, for example HL7 messages.
     class TransmissionLog < ApplicationRecord
       belongs_to :hd_provider_unit, class_name: "ProviderUnit"
+      belongs_to :patient, class_name: "Renalware::Patient"
+      belongs_to :session
       validates :direction, presence: true
       validates :format, presence: true
       has_many :children,
