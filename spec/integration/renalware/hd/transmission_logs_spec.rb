7740f6115fbd333fb648bf35bf3dfc2213de9814
Tim Crowe <tim@woodpigeon.com>
2018-08-02 17:09:49 +0100

    Fix transmission logs spec

--- spec/integration/renalware/hd/transmission_logs_spec.rb
+++ spec/integration/renalware/hd/transmission_logs_spec.rb
@@ -6,18 +6,18 @@ RSpec.describe "View transmission logs", type: :feature do
   describe "GET index" do
     it "displays a paginated list of transmission logs, most recent first" do
       login_as_admin
-      log_1 = create(:hd_transmission_log, :incoming_xml, payload: "log_1")
-      create(:hd_transmission_log, :outgoing_hl7, payload: "log_2")
+      create(:hd_transmission_log, :incoming_xml, filepath: "log_1.txt")
+      log_2 = create(:hd_transmission_log, :outgoing_hl7, filepath: "log_2.txt")
 
       visit hd_transmission_logs_path(per_page: 1)
 
       expect(page).to have_content("HD Transmission Logs")
 
       within ".hd_transmission_logs" do
-        expect(page).to have_content("log_1")
-        expect(page).to have_content(log_1.direction)
-        expect(page).to have_content(log_1.format)
-        expect(page).not_to have_content("log_2")
+        expect(page).to have_content("log_2")
+        expect(page).to have_content(log_2.direction)
+        expect(page).to have_content(log_2.format)
+        expect(page).not_to have_content("log_1")
         expect(page).to have_content("Next ")
       end
     end
