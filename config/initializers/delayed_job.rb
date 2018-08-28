c6f6f364e197ca1e82174078fb1259fc566ca3df
Tim Crowe <tim@woodpigeon.com>
2018-08-28 20:37:23 +0100

    Change default delayed_job parameters
    - Read ahead 10 jobs not 5
    - Sleep for 60 seconds not 10 when no jobs found

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -4,3 +4,5 @@ Delayed::Worker.logger = Logger.new(Rails.root.join("log", "delayed_job.log"))
 Delayed::Worker.destroy_failed_jobs = false
 Delayed::Worker.max_attempts = 10
 Delayed::Worker.max_run_time = 4.hours
+Delayed::Worker.read_ahead = 10
+Delayed::Worker.sleep_delay = 60
