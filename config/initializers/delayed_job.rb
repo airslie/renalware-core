705b5f1da7a98b41be064fa6049cea60fdfa56b8
Tim Crowe <tim@woodpigeon.com>
2018-08-28 20:55:10 +0100

    Optimised HL7 FeedJob delayed_job configuration

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -2,7 +2,8 @@
 
 Delayed::Worker.logger = Logger.new(Rails.root.join("log", "delayed_job.log"))
 Delayed::Worker.destroy_failed_jobs = false
-Delayed::Worker.max_attempts = 10
+Delayed::Worker.max_attempts = 10 # overridden e.g. in FeedJob
 Delayed::Worker.max_run_time = 4.hours
 Delayed::Worker.read_ahead = 10
-Delayed::Worker.sleep_delay = 60
+Delayed::Worker.sleep_delay = 15 # Also affects how long it takes delayed_job to stop on Crl+C
+Delayed::Worker.default_priority = 5 # use a lower number to get preferential treatment
