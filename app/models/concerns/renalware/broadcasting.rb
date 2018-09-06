d28bd317a66bd5a62b22badca3da21e7b1ae278d
Tim Crowe <tim@woodpigeon.com>
2018-09-06 10:07:59 +0100

    Support async wisper subscribers via delayed_job
    If you add a subscriber to the broadcast_subscription_map by using the following syntax
    ```
    Renalware.configure do |config|
      config.broadcast_subscription_map = {
    "Renalware::Modalities::ChangePatientModality" => [
    Renalware::Broadcasting::Subscriber.new("Renalware::Patients::DummyListener", async: true), …]
    }
    ```
    Then the the async: true means the subscriber will receive the publisher’s broadcast message asynchronously via delayed_job. This is useful where we want subscribers to handle messages out of band so that
    - if they error they can retry and do not interfere synchronous code that spawaned them
    - we don’t need the pattern where we have a subscriber that receives a message and them creates a job - this way does both
    - allows us to build a more loosely coupled system
    
    Note we need to be mindful of database transactions when processing message asynchronously.

--- app/models/concerns/renalware/broadcasting.rb
+++ app/models/concerns/renalware/broadcasting.rb
@@ -7,6 +7,28 @@ module Renalware
     include Wisper::Publisher
     extend ActiveSupport::Concern
 
+    class Subscriber
+      attr_reader :klass_name, :async
+      alias_method :async?, :async
+
+      def initialize(klass_name, async: false)
+        @klass_name = klass_name
+        @async = async
+      end
+
+      # Note that when using the wisper-activejob gem (so we can use delayed_job for instance)
+      # we subscribe the class not an instance, and the subscriber must have class
+      # methods matching the event names it wants to handle.
+      # https://github.com/krisleech/wisper-activejob
+      def instance
+        async? ? klass : klass.new
+      end
+
+      def klass
+        klass_name.to_s.constantize
+      end
+    end
+
     # Subscribes any listeners configured in Renalware.config.broadcast_subscription_map
     # to the current instance.
     #
@@ -25,10 +47,12 @@ module Renalware
     # See https://github.com/krisleech/wisper
     #
     def broadcasting_to_configured_subscribers
-      subscribers = Renalware.config.broadcast_subscription_map[self.class.name]
-      Array(subscribers).each{ |listener|
-        subscribe(listener.to_s.constantize.new)
-      }
+      subscribers = Array(Renalware.config.broadcast_subscription_map[self.class.name])
+      subscribers.each do |subscriber|
+        # Support String subscribers eg a simple class name as well as Subscriber instances.
+        subscriber = Subscriber.new(subscriber) unless subscriber.respond_to?(:klass)
+        subscribe(subscriber.instance, async: subscriber.async?)
+      end
       self
     end
   end
--- config/initializers/renalware.rb
+++ config/initializers/renalware.rb
@@ -2,8 +2,26 @@
 
 require_dependency "renalware"
 
-# New subscription registry - previous implementation does not work across threads
-
+# New subscription registry - previous implementation does not work across threads.
+# Each key in the map (hash) is the name of a class that broadacasts/publishes messages.
+# Entries in the array (value) for that key are classes which subscribe to events in the
+# publishing class. If you want a subscriber to listen asynchronously for events via ActiveJob,
+# use an Subscriber instance like so
+# "Renalware::Modalities::ChangePatientModality" => [
+#   Renalware::Broadcasting::Subscriber.new("Renalware::Patients::DummyListener", async: true),
+#   ...
+# ]
+# TODO: Ideally we would like an API something like this:
+#   Renalware.configure do |config|
+#     config.broadcast_subscription_map.configure do |publishers|
+#       publishers[Renalware::Modalities::ChangePatientModality] do |publisher|
+#         publisher.add_subscriber(Renalware::X)
+#         publisher.add_subscriber(Renalware::y, async: true)
+#       end
+#     end
+#   end
+# As it stands its a bit too easy for the exposed subscription map hash to be overwritten by a host
+# application with confusing and undesirable outcomes.
 Renalware.configure do |config|
   config.broadcast_subscription_map = {
     "Renalware::Modalities::ChangePatientModality" => [
