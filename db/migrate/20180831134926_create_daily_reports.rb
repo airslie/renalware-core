b5e56ed465964105d8de7edd56cf7e97a465db16
Tim Crowe <tim@woodpigeon.com>
2018-09-24 16:36:58 +0100

    Put reporting_daily views into the correct schema

--- db/migrate/20180831134926_create_daily_reports.rb
+++ db/migrate/20180831134926_create_daily_reports.rb
@@ -1,5 +1,10 @@
 class CreateDailyReports < ActiveRecord::Migration[5.1]
   def change
+    reversible do |direction|
+      direction.up { connection.execute("SET SEARCH_PATH=renalware,public;") }
+      direction.down { connection.execute("SET SEARCH_PATH=renalware,public;") }
+    end
+
     create_view :reporting_daily_pathology
     create_view :reporting_daily_letters
   end
