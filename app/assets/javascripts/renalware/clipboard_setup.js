62b3bf9e609a568b63bb5b6fd53835c150c2f23c
Tim Crowe <tim@woodpigeon.com>
2018-10-01 14:11:07 +0100

    Copy patient identifier to clipboard when clicked on

--- app/assets/javascripts/renalware/clipboard_setup.js
+++ app/assets/javascripts/renalware/clipboard_setup.js
@@ -14,4 +14,33 @@ $(document).ready(function(){
       $.post(invocation_url);
     }
   });
+
+
+  // Adapted from https://alligator.io/js/copying-to-clipboard/
+  // When a user clicks on a .clipboardable, copy the contents to the clipboard
+  // and toggle a 'success' class for shot while to provide visual feedback.
+  $(".clipboardable").on('click', function(e) {
+    var selection = window.getSelection();
+    var range = document.createRange();
+    range.selectNodeContents(this);
+    selection.removeAllRanges();
+    selection.addRange(range);
+
+    console.log(range);
+
+    try {
+      document.execCommand('copy');
+      selection.removeAllRanges();
+
+      var original = this.textContent;
+      this.classList.add('success');
+
+      setTimeout(() => {
+        this.classList.remove('success');
+      }, 1200);
+    } catch(e) {
+      // for now do nothing as this is not a crucial error
+      // but we could display a js modal error dialog
+    }
+  });
 });
