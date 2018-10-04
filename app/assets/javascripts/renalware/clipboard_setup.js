786565a339332d2820f3e3536204bd57834a9bb1
Tim Crowe <tim@woodpigeon.com>
2018-10-04 14:48:17 +0100

    Removed js copy to clipboard for now as UI not right
    For now in Chrome and Firefox only it selects the entire hosp/nhs number (using css) when clicked on making it easy to then copy the number.
    Later we will add some js to add the similar functionality to IE/Egde.
    I have removed the automatic copy to clipboard as felt the UI was not quite right it it deselects anything manually selected when clicked on.
    Not a big job to get this right but leaving for now - this PR as it stands will provides some help for Chrome users.

--- app/assets/javascripts/renalware/clipboard_setup.js
+++ app/assets/javascripts/renalware/clipboard_setup.js
@@ -19,6 +19,13 @@ $(document).ready(function(){
   // Adapted from https://alligator.io/js/copying-to-clipboard/
   // When a user clicks on a .clipboardable, copy the contents to the clipboard
   // and toggle a 'success' class for shot while to provide visual feedback.
+  /*
+  Have disabled this for now until we can get the UI a little better. Atm if the user
+  drags to select the text, we remove their selection here - a bit confusing for them.
+  We maybe should highlight the element text on a click and then copy, but leave the
+  text highlighted so the usr can manually copy if they don't realise it has been done for them.
+  And allow them to highlight manually. Or provide a Copy button which pops up or something.
+
   $(".clipboardable").on('click', function(e) {
     var selection = window.getSelection();
     var range = document.createRange();
@@ -43,4 +50,5 @@ $(document).ready(function(){
       // but we could display a js modal error dialog
     }
   });
+  */
 });
