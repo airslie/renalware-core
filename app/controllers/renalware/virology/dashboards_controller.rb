8119f477a939269ac7269b61d16c2ca78434b69a
Tim Crowe <tim@woodpigeon.com>
2018-10-15 11:50:38 +0100

    Use the virology dashboard policy
    Rather then falling back to the patient policy

--- app/controllers/renalware/virology/dashboards_controller.rb
+++ app/controllers/renalware/virology/dashboards_controller.rb
@@ -6,7 +6,7 @@ module Renalware
   module Virology
     class DashboardsController < BaseController
       def show
-        authorize patient
+        authorize [:renalware, :virology, :dashboard], :show?
         render locals: {
           patient: patient,
           vaccinations: Vaccination.for_patient(patient)
