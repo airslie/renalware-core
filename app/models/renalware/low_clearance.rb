002fe4db1faa172d921c670cae51b175f8f18c63
Tim Crowe <tim@woodpigeon.com>
2018-10-22 14:32:39 +0100

    Add Tx Candidates tab to LCC MDM patients list

--- app/models/renalware/low_clearance.rb
+++ app/models/renalware/low_clearance.rb
@@ -4,7 +4,7 @@ require_dependency "renalware"
 
 module Renalware
   module LowClearance
-    MDM_FILTERS = %w(urea hgb_low hgb_high on_worryboard supportive_care).freeze
+    MDM_FILTERS = %w(urea hgb_low hgb_high on_worryboard supportive_care tx_candidates).freeze
 
     def self.table_name_prefix
       "low_clearance_"
