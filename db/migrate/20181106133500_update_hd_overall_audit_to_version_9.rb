fb993b5ef8a1a6ab50f1bf238340472d924666f8
Tim Crowe <tim@woodpigeon.com>
2018-11-19 12:19:05 +0000

    Fix migration error

--- db/migrate/20181106133500_update_hd_overall_audit_to_version_9.rb
+++ db/migrate/20181106133500_update_hd_overall_audit_to_version_9.rb
@@ -1,6 +1,12 @@
 class UpdateHDOverallAuditToVersion9 < ActiveRecord::Migration[5.1]
-  update_view :reporting_hd_overall_audit,
-              materialized: true,
-              version: 9,
-              revert_to_version: 8
+  include MigrationHelper
+
+  def change
+    within_renalware_schema do
+      update_view :reporting_hd_overall_audit,
+                  materialized: true,
+                  version: 9,
+                  revert_to_version: 8
+    end
+  end
 end
