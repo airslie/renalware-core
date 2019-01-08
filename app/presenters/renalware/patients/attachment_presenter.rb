d3f47b19bc94d88d509505c0f4bbcb3bc30b614f
Tim Crowe <tim@woodpigeon.com>
2019-01-08 17:39:34 +0000

    Support adding file attachments to a patient
    - Add Attachment and AttachmentType classes and tables
    - Add Patients::AttachmentsController for uploading new files
    - Add Linked Files LH gutter item
    - Add count of attachments to patient summaries SQL view and display the count in LH gutter
    - Add UI to manage attachments, with filtering
    - Validate presence of attachment name
    - Validate uploaded files are no bigger than the configured value - The setting in
      Renalware.config.max_file_upload_size determines the maximum size. This is set to 10MB by default.
      but can be overridden in the host application.
    - Handle externally stored Patients::Attachments
      If attachment#attachment_type.store_file_externally == true then we validate than exteral_location
      has been entered.
      If attachment#attachment_type.store_file_externally == false then we validate that a file has been
      uploaded to the app.
    - View uploaded attachments - Use just the path when redirecting to active_storage/blobs to avoid us
      having to set default_url_options[:host]
    - Add dummy seed data for attachment types

---
+++ app/presenters/renalware/patients/attachment_presenter.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require_dependency "renalware/patients"
+
+module Renalware
+  module Patients
+    class AttachmentPresenter < SimpleDelegator
+      delegate :byte_size, :content_type, to: :blob
+
+      def size
+        "#{byte_size / 1000} KB" if file.attached?
+      end
+
+      def blob
+        file.attached? ? file.blob : NullObject.instance
+      end
+    end
+  end
+end
--- spec/integration/renalware/patients/alerts_spec.rb
+++ spec/integration/renalware/patients/alerts_spec.rb
@@ -9,17 +9,13 @@ describe "Managing alerts", type: :request do
   describe "POST create" do
     context "with valid attributes and no urgency" do
       it "creates a new alert" do
-        headers = {
-          "HTTP_REFERER" => "/",
-          "ACCEPT" => "application/javascript"
-        }
         params = {
           patients_alert: {
             notes: "An alert"
           }
         }
 
-        post(patient_alerts_path(patient), params: params, headers: headers)
+        post(patient_alerts_path(patient, format: :js), params: params)
         expect(response).to be_successful
 
         alert = Renalware::Patients::Alert.find_by(patient_id: patient.id)
@@ -79,10 +75,6 @@ describe "Managing alerts", type: :request do
 
     context "with invalid attributes" do
       it "returns 44 validation error and does not create a new alert" do
-        headers = {
-          "HTTP_REFERER" => "/",
-          "ACCEPT" => "application/javascript"
-        }
         params = {
           patients_alert: {
             urgent: true,
@@ -90,7 +82,7 @@ describe "Managing alerts", type: :request do
           }
         }
 
-        post(patient_alerts_path(patient), params: params, headers: headers)
+        post(patient_alerts_path(patient, format: :js), params: params)
 
         expect(response).to have_http_status(:unprocessable_entity)
         alert = Renalware::Patients::Alert.find_by(patient_id: patient.id)
@@ -107,12 +99,7 @@ describe "Managing alerts", type: :request do
     end
 
     it "soft deletes the alert" do
-      headers = {
-        "HTTP_REFERER" => "/",
-        "ACCEPT" => "application/javascript"
-      }
-
-      delete patient_alert_path(patient, alert), headers: headers
+      delete patient_alert_path(patient, alert, format: :js)
 
       expect(response).to be_successful
       expect(Renalware::Patients::Alert).not_to exist(id: alert.id)
@@ -121,12 +108,8 @@ describe "Managing alerts", type: :request do
     it "does not baulk if the alert has already been deleted" do
       alert.destroy!
       expect(Renalware::Patients::Alert).not_to exist(id: alert.id)
-      headers = {
-        "HTTP_REFERER" => "/",
-        "ACCEPT" => "application/javascript"
-      }
 
-      delete patient_alert_path(patient, alert), headers: headers
+      delete patient_alert_path(patient, alert, format: :js)
 
       expect(response).to be_successful
       expect(Renalware::Patients::Alert).not_to exist(id: alert.id)
