48abf651b44d68277771e674eee90a099518fc82
Tim Crowe <tim@woodpigeon.com>
2019-01-08 17:39:34 +0000

    Support adding file attachments to a patient
    - Add Attachment and AttachmentType classes and tables
    - Add Patients::AttachmentsController for uploading new files
    - Add Linked Files LH gutter item
    - Add count of attachments to patient summaries SQL view and display the count in LH gutter
    - Add UI to manage attachments, with filtering
    - Validate presence of attachment name
    - Validate uploaded files are no bigger than the configured value - The setting in
      Renalware.config.max_file_upload_size determines the maximum size. This is set to 10MB by default.
      but can be overridden in the host application.
    - Handle externally stored Patients::Attachments
      If attachment#attachment_type.store_file_externally == true then we validate than exteral_location
      has been entered.
      If attachment#attachment_type.store_file_externally == false then we validate that a file has been
      uploaded to the app.
    - View uploaded attachments - Use just the path when redirecting to active_storage/blobs to avoid us
      having to set default_url_options[:host]
    - Add dummy seed data for attachment types

--- app/models/renalware/patients/attachment.rb
+++ app/models/renalware/patients/attachment.rb
@@ -32,6 +32,14 @@ module Renalware
       validate :validate_uploaded_file_size
       validate :validate_presence_of_file_if_attachment_type_indicates_it_should_be_uploaded
 
+      # The user may have selected the file to upload, then changed the atachment_type to one that
+      # has store_file_externally = true, thus hiding the file input, but the file still gets sent.
+      def discard_uploaded_file_if_attachment_type_suggests_external_storage
+        if file.attached? && attachment_type.store_file_externally?
+          file.purge
+        end
+      end
+
       private
 
       def validate_uploaded_file_size
