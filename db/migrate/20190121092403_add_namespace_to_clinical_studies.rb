7a667a12b2ed0b70b30bf6b010d8198461dc98be
Tim Crowe <tim@woodpigeon.com>
2019-01-22 16:49:36 +0000

    Changes to support custom studies defined in a host app
    [skip ci]

--- db/migrate/20190121092403_add_namespace_to_clinical_studies.rb
+++ db/migrate/20190121092403_add_namespace_to_clinical_studies.rb
@@ -1,5 +1,7 @@
 class AddNamespaceToClinicalStudies < ActiveRecord::Migration[5.2]
   def change
-    add_column :research_studies, :namespace, :string, index: true
+    within_renalware_schema do
+      add_column :research_studies, :namespace, :string, index: true
+    end
   end
 end
