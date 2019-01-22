fbd361ad0153c7bb0be76a04a71568ee6d1643ab
Tim Crowe <tim@woodpigeon.com>
2019-01-22 16:49:36 +0000

    Changes to support custom studies defined in a host app
    [skip ci]

--- db/migrate/20190107163734_add_deleted_at_to_research_memberships.rb
+++ db/migrate/20190107163734_add_deleted_at_to_research_memberships.rb
@@ -1,6 +1,8 @@
 class AddDeletedAtToResearchMemberships < ActiveRecord::Migration[5.2]
   def change
-    add_column :research_investigatorships, :deleted_at, :datetime
-    add_index :research_investigatorships, :deleted_at
+    within_renalware_schema do
+      add_column :research_investigatorships, :deleted_at, :datetime
+      add_index :research_investigatorships, :deleted_at
+    end
   end
 end
