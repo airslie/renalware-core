da5c53a1627b239761d861904261c002e4e8d497
Tim Crowe <tim@woodpigeon.com>
2019-01-22 16:49:36 +0000

    Changes to support custom studies defined in a host app
    [skip ci]

--- db/migrate/20190121135548_add_document_to_research_tables.rb
+++ db/migrate/20190121135548_add_document_to_research_tables.rb
@@ -1,10 +1,12 @@
 class AddDocumentToResearchTables < ActiveRecord::Migration[5.2]
   def change
-    add_column :research_studies, :document, :jsonb
-    add_index :research_studies, :document, using: :gin
-    add_column :research_participations, :document, :jsonb
-    add_index :research_participations, :document, using: :gin
-    add_column :research_investigatorships, :document, :jsonb
-    add_index :research_investigatorships, :document, using: :gin
+    within_renalware_schema do
+      add_column :research_studies, :document, :jsonb
+      add_index :research_studies, :document, using: :gin
+      add_column :research_participations, :document, :jsonb
+      add_index :research_participations, :document, using: :gin
+      add_column :research_investigatorships, :document, :jsonb
+      add_index :research_investigatorships, :document, using: :gin
+    end
   end
 end
