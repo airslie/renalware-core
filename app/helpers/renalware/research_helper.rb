d973024d73b40e0fa0c29bb9724bc1f6481948de
Tim Crowe <tim@woodpigeon.com>
2019-01-29 14:30:32 +0000

    Heroic support

--- app/helpers/renalware/research_helper.rb
+++ app/helpers/renalware/research_helper.rb
@@ -2,10 +2,19 @@
 
 module Renalware
   module ResearchHelper
-    def path_to_research_document_form_partial(namespace, type = nil)
-      return if namespace.blank?
+    # Given an STI class name like "Renalware::Heroic::Research::Study" we extract the
+    # namespace so we can build derive the partial path for a type like :participaton, e.g.
+    # "Renalware::Heroic::Research::Participation"
+    #
+    # Example usage:
+    # path_to_research_document_form_partial(
+    #   "Renalware::Heroic::Research::Study",
+    #   :participation
+    # )
+    def path_to_research_document_form_partial(study_class, type = nil)
+      return if study_class.blank?
 
-      partial_path = "#{namespace&.underscore}/#{type}/form"
+      partial_path = "#{study_class.deconstantize.underscore}/#{type}/form"
       partial_path&.gsub("::", "/")&.gsub("//", "/")&.gsub("//", "/")
     end
   end
