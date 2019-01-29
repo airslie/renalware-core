b2c8a4cb396aac15fb106ff25a65e6f79efc5669
Tim Crowe <tim@woodpigeon.com>
2019-01-29 14:30:32 +0000

    Heroic support

--- db/seeds/default/practices/practices.rb
+++ db/seeds/default/practices/practices.rb
@@ -3,18 +3,20 @@
 module Renalware
   log "Adding NHS Practices\n" do
     sample_status = "SAMPLE ONLY"
+    uk = System::Country.find_by!(alpha2: "GB")
     Patients::Practice.transaction do
-      #NOTE: use '_sample' file for demo/devel
+      # NOTE: use '_sample' file for demo/devel
       CSV.foreach(File.join(File.dirname(__FILE__), "nhs_practices_sample.csv"), headers: true) do |row|
         practice = Patients::Practice.find_or_initialize_by(code: row["code"])
         practice.name = row["name"]
-        unless practice.address.present?
+        if practice.address.blank?
           practice.build_address(
             organisation_name: row["name"],
             postcode: row["postcode"],
             street_1: row["street_1"],
             street_2: row["street_2"],
-            town: row["town"]
+            town: row["town"],
+            country_id: uk.id
           )
         end
         practice.save!
