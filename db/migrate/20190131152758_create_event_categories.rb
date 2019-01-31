1b6667ba8bec1aefc9899ee3d570c26da624e95b
Tim Crowe <tim@woodpigeon.com>
2019-01-31 17:51:08 +0000

    Seed a default ‘General’ event

--- db/migrate/20190131152758_create_event_categories.rb
+++ db/migrate/20190131152758_create_event_categories.rb
@@ -2,10 +2,18 @@ class CreateEventCategories < ActiveRecord::Migration[5.2]
   def change
     create_table :event_categories do |t|
       t.string :name, null: false, index: { unique: true }
+      t.integer :position, null: false, default: 10
       t.datetime :deleted_at, index: true
       t.timestamps null: false
     end
 
-    add_reference :event_types, :category, references: :event_categories, index: true, null: true
+    add_reference(
+      :event_types,
+      :category,
+      references: :event_categories,
+      index: true,
+      null: true,
+      foreign_key: { to_table: :event_categories }
+    )
   end
 end
