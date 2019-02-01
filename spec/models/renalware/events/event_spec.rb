bff8b7f4299cd0ec52282b61c270a8759472f750
Tim Crowe <tim@woodpigeon.com>
2019-02-01 14:00:31 +0000

    Allow editing and soft-deleting an event if policy permits
    - Allow editing an event if policy permits
    - Allow soft-deleting an event if policy permits
    - Add versioning to Events on update and destroy

--- spec/models/renalware/events/event_spec.rb
+++ spec/models/renalware/events/event_spec.rb
@@ -6,13 +6,16 @@ module Renalware::Events
   describe Event, type: :model do
     subject(:event) { described_class.new }
 
+    it_behaves_like "a Paranoid model"
     it_behaves_like "an Accountable model"
 
     it :aggregate_failures do
+      is_expected.to be_versioned
       is_expected.to validate_presence_of(:patient)
       is_expected.to validate_presence_of(:date_time)
       is_expected.to validate_presence_of(:event_type_id)
       is_expected.to respond_to(:type)
+      is_expected.to respond_to(:deleted_at)
       is_expected.to belong_to(:patient).touch(true)
       is_expected.to validate_timeliness_of(:date_time)
     end
