41b3a7956acce2be6b6157a796b42697acbf35fc
Tim Crowe <tim@woodpigeon.com>
2019-02-01 11:13:37 +0000

    Implement event category -> type selection in events filter and new event form

--- db/seeds/default/events/event_types.rb
+++ db/seeds/default/events/event_types.rb
@@ -2,13 +2,15 @@
 
 module Renalware
   log "Adding Default Event Types e.g. Biopsy, Swabs" do
-
     file_path = File.join(File.dirname(__FILE__), "event_types.csv")
 
     CSV.foreach(file_path, headers: true) do |row|
-      Events::Type.find_or_create_by!(name: row["name"],
-                                      event_class_name: row["event_class_name"],
-                                      slug: row["slug"])
+      Events::Type.find_or_create_by!(
+        name: row["name"],
+        event_class_name: row["event_class_name"],
+        slug: row["slug"],
+        category_id: row["category_id"]
+      )
     end
   end
 end
