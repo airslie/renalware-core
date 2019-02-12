975a6b489a606a90f331b07f9f6f079d5e7493ad
Tim Crowe <tim@woodpigeon.com>
2019-02-12 11:18:57 +0000

    Prefix urls in top menu with renalware.*
    This allows these to continue working when rendering views inside another engine. Until we prefix all routes or mix renalware routes using a Mapper class for instance, if we are rendering a view in engine x then eg patients_path will render as /x/patients rather than /patients. Prefixing the url helpers used in the top menu prevents this error for now.

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  mount Renalware::Engine => "/"
+  mount Renalware::Engine => "/", as: :renalware
   resources :dummies, only: :index
 end
