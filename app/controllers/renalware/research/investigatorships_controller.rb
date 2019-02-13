c3250cf38e3141bd513db2b3121bd8619f371c63
Tim Crowe <tim@woodpigeon.com>
2019-02-13 11:09:56 +0000

    Add started_on and left_on to investigatorships

--- app/controllers/renalware/research/investigatorships_controller.rb
+++ app/controllers/renalware/research/investigatorships_controller.rb
@@ -75,7 +75,7 @@ module Renalware
       def investigatorship_params
         params
           .require(:investigatorship)
-          .permit(:user_id, document: {})
+          .permit(:user_id, :started_on, :left_on, document: {})
       end
 
       def render_new(investigatorship)
