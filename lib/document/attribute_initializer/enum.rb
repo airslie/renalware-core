cf5faaf9a7c15e91a3c48dcc776f9eb8cb788d61
Tim Crowe <tim@woodpigeon.com>
2019-02-15 19:19:01 +0000

    Support Enumerize .. multiple: true
    Which will store an array of enums

--- lib/document/attribute_initializer/enum.rb
+++ lib/document/attribute_initializer/enum.rb
@@ -10,9 +10,17 @@ module Document
 
       private
 
+      # Be sure to pass any enumerize options in to klass.enumerize, for example to support
+      # multiple: true which will results in an array of enums stored.
+      #
+      # Example usage for multiple: true
+      #
+      #  Class X
+      #    attribute :roles, ::Document::Enum, enumerize: { multiple: true }
+      #  end
       def configure_enums
         enums = options[:enums] || Document::Enum.default_enums(klass.model_name, name)
-        klass.enumerize name, in: enums
+        klass.enumerize name, in: enums, **(options[:enumerize] || {})
       end
     end
   end
