699e405083bf28dbb33fa0d96699dc9f04935276
Tim Crowe <tim@woodpigeon.com>
2019-02-26 12:47:18 +0000

    Make sure pundit policy_scope(Patient) is called - WIP

--- app/models/renalware/patients/patient_search.rb
+++ app/models/renalware/patients/patient_search.rb
@@ -3,8 +3,8 @@
 module Renalware
   module Patients
     class PatientSearch
-      def self.call(params)
-        Renalware::Patient
+      def self.call(params, scope = Renalware::Patient)
+        scope
           .includes(:hospital_centre, current_modality: [:description])
           .ransack(params[:patient_search]).tap do |search|
             search.sorts = %w(family_name_case_insensitive given_name)
