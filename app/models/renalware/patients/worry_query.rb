cf51d6c6069d05f6aae68a3f37f98b623afc0f85
Tim Crowe <tim@woodpigeon.com>
2019-02-26 12:43:51 +0000

    Adapt WorryQuery to accept patient_scope

--- app/models/renalware/patients/worry_query.rb
+++ app/models/renalware/patients/worry_query.rb
@@ -5,22 +5,24 @@ require_dependency "renalware/patients"
 module Renalware
   module Patients
     class WorryQuery
-      attr_reader :query_params
+      attr_reader :query_params, :patient_scope
 
-      def initialize(query_params)
+      def initialize(query_params:, patient_scope: Patient.all)
         @query_params = query_params
         @query_params[:s] = "date_time DESC" if @query_params[:s].blank?
+        @patient_scope = patient_scope
       end
 
       def call
         search
           .result
+          .eager_load(:patient)
           .includes(:created_by, patient: { current_modality: [:description] })
           .order(created_at: :asc)
       end
 
       def search
-        @search ||= Worry.ransack(query_params)
+        @search ||= Worry.merge(patient_scope).ransack(query_params)
       end
     end
   end
