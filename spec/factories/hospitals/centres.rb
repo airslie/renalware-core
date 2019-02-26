97ef4106b8c9f3cca4b0eb074eee7e8c309cf8ee
Tim Crowe <tim@woodpigeon.com>
2019-02-26 18:01:33 +0000

    Add host_site? boolean to hospital_centres
    This can only be set on one centre and indicates the site/centre where Renalware is deployed. This is used in the PatientPolicy when determining which patients a user can view

--- spec/factories/hospitals/centres.rb
+++ spec/factories/hospitals/centres.rb
@@ -2,7 +2,11 @@
 
 FactoryBot.define do
   factory :hospital_centre, class: "Renalware::Hospitals::Centre" do
+    initialize_with do
+      Renalware::Hospitals::Centre.find_or_create_by!(code: code, name: name, host_site: host_site)
+    end
     name { "King's College Hospital" }
     code { "RJZ" }
+    host_site { true }
   end
 end
