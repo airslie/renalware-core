6af5deca54a035d12ac59e9ec740becfdc76ba1c
Tim Crowe <tim@woodpigeon.com>
2019-02-28 11:30:55 +0000

    Events table changes
    - add sorting to events table when show in the events view (not clinical summary)
    - only display updated_at/by if different from updated_at  > created_at
    - remove caching from events summary part for now

--- app/presenters/renalware/renal/clinical_summary_presenter.rb
+++ app/presenters/renalware/renal/clinical_summary_presenter.rb
@@ -9,12 +9,12 @@ module Renalware
       rattr_initialize :patient
 
       # Host application may override the order or add other summary presenters
-      def summary_parts(current_user)
+      def summary_parts(current_user, params = {})
         Renalware
           .config
           .page_layouts[:clinical_summary]
           .map(&:constantize)
-          .map { |klass| klass.new(patient, current_user) }
+          .map { |klass| klass.new(patient, current_user, params: params) }
           .select(&:render?)
       end
     end
