898fe80101afe0c4a40c1dfb01b7dac3da941322
Tim Crowe <tim@woodpigeon.com>
2019-02-28 18:58:45 +0000

    Only an investigator can add, edit or delete participants

--- spec/factories/research/investigatorships.rb
+++ spec/factories/research/investigatorships.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
   factory :research_investigatorship, class: "Renalware::Research::Investigatorship" do
     association :study, factory: :research_study
     association :user
+    started_on { "201-01-01" }
     association :created_by, factory: :user
     association :updated_by, factory: :user
   end
