902f8e385f023e2dbc398cf2680a5834e5494442
Tim Crowe <tim@woodpigeon.com>
2019-02-28 18:58:45 +0000

    Only an investigator can add, edit or delete participants

--- spec/integration/renalware/research/edit_participation_spec.rb
+++ spec/integration/renalware/research/edit_participation_spec.rb
@@ -2,28 +2,51 @@
 
 require "rails_helper"
 
-describe "Edit a patient's participation in a study", type: :system, js: true do
+describe "Edit a patient's participation in a study", type: :system do
   include AjaxHelpers
 
-  it "Updating a participation" do
-    user = login_as_admin
-    participation = create(:research_participation, by: user)
-    visit research_study_participations_path(participation.study)
+  context "when the user is an investogator in the study" do
+    it "Updating a participation" do
+      user = login_as_admin
+      participation = create(:research_participation, by: user)
+      create(
+        :research_investigatorship,
+        user: user,
+        study: participation.study,
+        by: user,
+        started_on: "2018-01-01"
+      )
 
-    within ".study-participations-table" do
-      click_on "Edit"
+      visit research_study_participations_path(participation.study)
+
+      within ".study-participations-table" do
+        click_on "Edit"
+      end
+
+      expect(page).to have_current_path(
+        edit_research_study_participation_path(participation.study, participation)
+      )
+
+      # Add a left_on date
+      fill_in "Left on", with: "2019-02-01"
+      click_on "Save"
+
+      expect(page).to have_current_path(research_study_participations_path(participation.study))
+      expect(page).to have_content(participation.patient.to_s)
+      expect(page).to have_content("01-Feb-2019")
     end
+  end
 
-    expect(page).to have_current_path(
-      edit_research_study_participation_path(participation.study, participation)
-    )
+  context "when the user not an investogator in the study" do
+    it "they cannot see the edit option" do
+      user = login_as_admin
+      participation = create(:research_participation, by: user)
 
-    # Add a left_on date
-    fill_in "Left on", with: "2019-02-01"
-    click_on t("btn.save")
+      visit research_study_participations_path(participation.study)
 
-    expect(page).to have_current_path(research_study_participations_path(participation.study))
-    expect(page).to have_content(participation.patient.to_s)
-    expect(page).to have_content("01-Feb-2019")
+      within ".study-participations-table" do
+        expect(page).not_to have_content "Edit"
+      end
+    end
   end
 end
