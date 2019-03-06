9a3098a59b90ae9fd0247f65cb8c384eb549b8c1
Tim Crowe <tim@woodpigeon.com>
2019-03-06 16:44:35 +0000

    Restrict research actions by permission
    - only a super admin or study admin can delete a particpant
    - only a super admin or study admin can add/edit/delete investigators
    - improve rendering of action links in tables, removing the need to call `= pipe_separator` by using CSS and an .actions class on the `td`

--- app/policies/renalware/research/investigatorship_policy.rb
+++ app/policies/renalware/research/investigatorship_policy.rb
@@ -5,6 +5,29 @@ require_dependency "renalware/research"
 module Renalware
   module Research
     class InvestigatorshipPolicy < ResearchPolicy
+      def destroy?
+        return if record.study.blank?
+
+        user_is_super_admin? || user_is_a_manager_in_this_study?
+      end
+      alias edit? destroy?
+      alias new? destroy?
+      alias create? destroy?
+      alias update? destroy?
+
+      private
+
+      def user_is_an_investigator_in_this_study?
+        investigator_ids.pluck(:user_id).include?(user.id)
+      end
+
+      def user_is_a_manager_in_this_study?
+        investigator_ids.where(manager: true).pluck(:user_id).include?(user.id)
+      end
+
+      def investigator_ids
+        record.study.investigatorships
+      end
     end
   end
 end
