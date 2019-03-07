5c23c3007c576d3f11d11360fa44f68ccd4dc9f9
Tim Crowe <tim@woodpigeon.com>
2019-03-07 16:12:17 +0000

    Add private boolean to research study

--- spec/dummy/db/structure.sql
+++ spec/dummy/db/structure.sql
@@ -7530,7 +7530,8 @@ CREATE TABLE renalware.research_studies (
     application_url character varying,
     namespace character varying,
     type character varying,
-    document jsonb
+    document jsonb,
+    private boolean DEFAULT false NOT NULL
 );
 
 
@@ -15904,6 +15905,13 @@ CREATE INDEX index_research_studies_on_leader ON renalware.research_studies USIN
 
 
 --
+-- Name: index_research_studies_on_private; Type: INDEX; Schema: renalware; Owner: -
+--
+
+CREATE INDEX index_research_studies_on_private ON research_studies USING btree (private);
+
+
+--
 -- Name: index_research_studies_on_updated_by_id; Type: INDEX; Schema: renalware; Owner: -
 --
 
@@ -20049,6 +20057,7 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20190225103005'),
 ('20190226162607'),
 ('20190306121545'),
+('20190307123232'),
 ('20190315125638'),
 ('20190322120025'),
 ('20190325134823'),
