a29f75d357a1b21fa8a9d9212bc1a8a34186b294
Tim Crowe <tim@woodpigeon.com>
2019-03-09 20:55:51 +0000

    Rubocop housekeeping

--- spec/models/renalware/patients/attachment_spec.rb
+++ spec/models/renalware/patients/attachment_spec.rb
@@ -15,6 +15,7 @@ module Renalware
 
       describe "#file via ActiveStorage" do
         subject(:attachment) { create(:patient_attachment, :with_file) }
+
         let(:user) { attachment.updated_by }
 
         it "can accept an uploaded file" do
