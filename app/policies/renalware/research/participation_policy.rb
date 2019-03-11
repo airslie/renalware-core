b3b899710c8916f5f5d297578b58c592612eaa38
Tim Crowe <tim@woodpigeon.com>
2019-03-11 12:19:22 +0000

    Refactor research policies

--- app/policies/renalware/research/participation_policy.rb
+++ app/policies/renalware/research/participation_policy.rb
@@ -18,20 +18,6 @@ module Renalware
 
         user_is_super_admin? || user_is_a_manager_in_this_study?
       end
-
-      private
-
-      def user_is_an_investigator_in_this_study?
-        investigator_ids.pluck(:user_id).include?(user.id)
-      end
-
-      def user_is_a_manager_in_this_study?
-        investigator_ids.where(manager: true).pluck(:user_id).include?(user.id)
-      end
-
-      def investigator_ids
-        record.study.investigatorships
-      end
     end
   end
 end
