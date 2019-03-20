56572bfbeac7bd17845428a9b2d95d580f80269e
Tim Crowe <tim@woodpigeon.com>
2019-03-20 14:30:21 +0000

    Reduce the number of rows of seed data
    We need to stay below 10,000 to apease Heroku.

--- demo_data/seeds.rb
+++ demo_data/seeds.rb
@@ -7,10 +7,12 @@ module Renalware
 
   ms = Benchmark.ms do
     # Load default seeds
+    PaperTrail.enabled = false
     Renalware::Engine.load_seed
 
     Renalware.log_section "Seeding demo renalware-core data"
     require_relative "./seeds/seeds.rb"
+    PaperTrail.enabled = true
   end
 
   log_section "Database seeding completed in #{ms / 1000}s"
