36425a4042de7a37a6d0f16e7ada56ba6c455996
Tim Crowe <tim@woodpigeon.com>
2019-03-22 18:06:16 +0000

    Refactor MessageProcessor to aid clarity

--- app/models/renalware/feeds/message_processor.rb
+++ app/models/renalware/feeds/message_processor.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_dependency "renalware/feeds"
+require "attr_extras"
 
 module Renalware
   module Feeds
@@ -9,33 +10,48 @@ module Renalware
     #
     class MessageProcessor
       include Broadcasting
+      attr_reader :raw_message, :hl7_message, :feed_message
 
-      # For SubscriptionRegistry only, may not be needed as also inc in Broadcasting module
-      include Wisper::Publisher
-
-      # rubocop:disable Metrics/MethodLength
+      # We want to wrap message processing in a transaction because if message processing
+      # fails we don't want to leave an unprocessed message in the feed_messages table.
+      # If we did, and the same FeedJob retires a few minutes later, if will try to save to
+      # feed_messages with the same MD5 body_hash (the message is identical to one already saved)
+      # resulting in unique key violation.
+      # Using a transaction here prevents any orphaned records if there is an error.
+      # However we should be aware that any listeners raising an error will prevent successful
+      # in all other listeners. So a listener should be careful to catch errors and not re-raise
+      # them, or use the :message_processed message (lower down) which is safer.
       def call(raw_message)
-        feed_message = nil
+        @raw_message = raw_message
 
-        # We want to wrap message processing in a transaction because if message processing
-        # fails we don't want to leave an unprocessed message in the feed_messages table.
-        # If we did, and the same FeedJob retires a few minutes later, if will try to save to
-        # feed_messages with the same MD5 body_hash (the message is identical to one already saved)
-        # resulting in unique key violation.
-        # Using a transaction here prevents any orphaned records if there is an error.
-        # However we should be aware that any listeners raising an error will prevent successful
-        # in all other listeners. So a listener should be careful to catch errors and not re-raise
-        # them, or use the :message_processed message (lower down) which is safer.
+        parse_raw_message_into_hl7_object
+
         ActiveRecord::Base.transaction do
-          hl7_message = build_hl7_object_from(raw_message)
-          feed_message = persist_message(hl7_message)
-          broadcast(
-            :message_arrived,
-            hl7_message: hl7_message,
-            feed_message: feed_message
-          )
+          create_feed_message_using_raw_message_and_basic_extracted_patient_data
+          allow_listeners_to_process_the_message
         end
 
+        allow_listeners_to_post_process_the_message
+      rescue StandardError => exception
+        notify_exception(exception)
+        raise exception
+      end
+
+      private
+
+      def notify_exception(exception)
+        Engine.exception_notifier.notify(exception)
+      end
+
+      def allow_listeners_to_process_the_message
+        broadcast(
+          :message_arrived,
+          hl7_message: hl7_message,
+          feed_message: feed_message
+        )
+      end
+
+      def allow_listeners_to_post_process_the_message
         # Another event, this one letting anyone interested know that a message been successfully
         # processed. They might want to forward the message on somewhere else for instance.
         # Think Diaverum.
@@ -53,12 +69,9 @@ module Renalware
         notify_exception(e)
         raise e
       end
-      # rubocop:enable Metrics/MethodLength
 
-      private
-
-      def build_hl7_object_from(raw_message)
-        MessageParser.parse(raw_message)
+      def parse_raw_message_into_hl7_object
+        @hl7_message = MessageParser.parse(raw_message)
       end
 
       # If the incoming message has already been processed we should not be processing it again.
@@ -66,12 +79,8 @@ module Renalware
       # same message payload is saved twice - in this case we exit #call early, the broadcast
       # is not issued and therefore the message is not processed. The message will go back into
       # the delayed_job queue and retry, failing until it finally gives up!
-      def persist_message(hl7_message)
-        PersistMessage.new.call(hl7_message)
-      end
-
-      def notify_exception(exception)
-        Engine.exception_notifier.notify(exception)
+      def create_feed_message_using_raw_message_and_basic_extracted_patient_data
+        @feed_message = PersistMessage.new.call(hl7_message)
       end
     end
   end
