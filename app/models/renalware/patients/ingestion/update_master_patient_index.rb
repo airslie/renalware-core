98589762da87dc39c17f17e12c952ae1758e1e92
Tim Crowe <tim@woodpigeon.com>
2019-03-27 16:37:57 +0000

    Test that we update gp and practice in master patient index

--- app/models/renalware/patients/ingestion/update_master_patient_index.rb
+++ app/models/renalware/patients/ingestion/update_master_patient_index.rb
@@ -7,21 +7,26 @@ module Renalware
   module Patients
     module Ingestion
       class UpdateMasterPatientIndex
-        pattr_initialize :hl7_message
+        pattr_initialize :hl7_message, :by
+        attr_reader :rw_patient
         delegate :patient_identification, to: :hl7_message
 
-        def self.call(hl7_message)
-          new(hl7_message).call
+        def self.call(hl7_message, by)
+          new(hl7_message, by).call
         end
 
         def call
           return unless hl7_message.adt?
 
+          @rw_patient = find_patient_in_renalware
           update_or_create_abridged_patient
+          update_primary_care_physician
+          update_practice
         end
 
         private
 
+        # rubocop:disable Metrics/AbcSize
         def update_or_create_abridged_patient
           find_or_initialize_abridged_patient.update!(
             given_name: patient_identification.given_name,
@@ -29,13 +34,41 @@ module Renalware
             sex: patient_identification.sex,
             title: patient_identification.title,
             born_on: patient_identification.born_on,
-            died_at: patient_identification.died_at
+            died_at: patient_identification.died_at,
+            patient_id: rw_patient.id,
+            practice_code: hl7_message.practice_code,
+            gp_code: hl7_message.gp_code
           )
         end
+        # rubocop:enable Metrics/AbcSize
 
         def find_or_initialize_abridged_patient
           Patients::Abridgement.find_or_initialize_by(
             hospital_number: patient_identification.internal_id
+          )
+        end
+
+        def find_patient_in_renalware
+          ::Renalware::Patient.find_by(
+            local_patient_id: patient_identification.internal_id
+          ) || NullObject.instance
+        end
+
+        def update_primary_care_physician
+          return unless rw_patient && hl7_message.gp_code
+
+          rw_patient.update!(
+            primary_care_physician: PrimaryCarePhysician.find_by(code: hl7_message.gp_code),
+            by: by
+          )
+        end
+
+        def update_practice
+          return unless rw_patient && hl7_message.practice_code
+
+          rw_patient.update!(
+            practice: Practice.find_by(code: hl7_message.practice_code),
+            by: by
           )
         end
       end
--- spec/models/renalware/patients/ingestion/update_master_patient_index_spec.rb
+++ spec/models/renalware/patients/ingestion/update_master_patient_index_spec.rb
@@ -7,15 +7,30 @@ module Renalware::Patients
     include HL7Helpers
     subject(:service) { described_class }
 
+    let(:by) { create(:user) }
+
+    def hl7_data(**args)
+      OpenStruct.new(
+        {
+          hospital_number: "A123",
+          family_name: "new_family_name",
+          given_name: "new_given_name",
+          born_on: Time.zone.parse("2002-02-01").to_date,
+          died_at: Time.zone.parse("2003-03-02").to_date,
+          gp_code: "G123",
+          practice_code: "P456"
+        }.update(**args)
+      )
+    end
+
     describe "#call" do
       context "when an ORU pathology message" do
         it "does not update the master patient index" do
-          patient = build_stubbed(:patient)
-          hl7_message = hl7_message_from_file("ORU_R01.hl7", patient)
+          hl7_message = hl7_message_from_file("ORU_R01.hl7", hl7_data)
           expect(hl7_message).to be_oru
 
           expect {
-            service.call(hl7_message)
+            service.call(hl7_message, by)
           }.not_to change(Abridgement, :count)
         end
       end
@@ -24,43 +39,75 @@ module Renalware::Patients
         context "when the patient does not exist in Renalware" do
           context "when the patient does not exist in the master patient index" do
             it "adds the patient to the master patient index" do
-              patient = build_stubbed(:patient)
-              hl7_message = hl7_message_from_file("ADT_A31.hl7", patient)
+              hl7_message = hl7_message_from_file("ADT_A31.hl7", hl7_data)
               expect(hl7_message).to be_adt
 
               expect {
-                service.call(hl7_message)
+                service.call(hl7_message, by)
               }.to change(Abridgement, :count).by(1)
             end
           end
 
           context "when the patient already exist in the master patient index" do
-            it "updates the patient to the master patient index" do
+            it "updates the patient in the master patient index" do
               abridged_patient = create(
                 :abridged_patient,
                 hospital_number: "A123",
                 born_on: "1900-01-01"
               )
-              patient = build_stubbed(
-                :patient,
-                local_patient_id: "A123",
-                family_name: "new_family_name",
-                given_name: "new_given_name",
-                born_on: "2002-02-01",
-                died_on: "2003-03-02"
-              )
-              hl7_message = hl7_message_from_file("ADT_A31.hl7", patient)
+              hl7_message = hl7_message_from_file("ADT_A31.hl7", hl7_data)
               expect(hl7_message).to be_adt
 
-              expect { service.call(hl7_message) }.not_to change(Abridgement, :count)
+              expect { service.call(hl7_message, by) }.not_to change(Abridgement, :count)
 
-              expect(abridged_patient.reload).to have_attributes(
-                hospital_number: "A123",
-                family_name: "new_family_name",
-                given_name: "new_given_name",
-                born_on: Time.zone.parse("2002-02-01").to_date,
-                died_at: Time.zone.parse("2003-03-02").to_date
+              expect(abridged_patient.reload).to have_attributes(hl7_data.to_h)
+            end
+          end
+
+          context "when the patient exists in Renalware" do
+            context "when the patient does not exist in the master patient index" do
+              it "adds the patient to the master patient index and updates the patient_id "\
+                 "to point to the renalware patients" do
+                rw_patient = create(:patient, local_patient_id: "A123")
+
+                hl7_message = hl7_message_from_file("ADT_A31.hl7", hl7_data)
+                expect(hl7_message).to be_adt
+
+                expect { service.call(hl7_message, by) }.to change(Abridgement, :count).by(1)
+
+                abridgement = Abridgement.find_by!(hospital_number: "A123")
+                expect(abridgement.patient_id).to eq(rw_patient.id)
+              end
+            end
+
+            it "updates the primary_care_physician on the Renalware patient" do
+              data = hl7_data
+              primary_care_physician = create(:primary_care_physician, code: data.gp_code)
+              rw_patient = create(
+                :patient,
+                local_patient_id: data.hospital_number,
+                primary_care_physician: nil
               )
+              hl7_message = hl7_message_from_file("ADT_A31.hl7", data)
+
+              service.call(hl7_message, by)
+
+              expect(rw_patient.reload.primary_care_physician).to eq(primary_care_physician)
+            end
+
+            it "updates the practice on the Renalware patient" do
+              data = hl7_data
+              practice = create(:practice, code: data.practice_code)
+              rw_patient = create(
+                :patient,
+                local_patient_id: data.hospital_number,
+                practice: nil
+              )
+              hl7_message = hl7_message_from_file("ADT_A31.hl7", data)
+
+              service.call(hl7_message, by)
+
+              expect(rw_patient.reload.practice).to eq(practice)
             end
           end
         end
