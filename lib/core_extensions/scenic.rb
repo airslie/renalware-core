f5ff7fa2760369dc964f997b4c208549e128217e
Tim Crowe <tim@woodpigeon.com>
2019-03-29 09:42:03 +0000

    Fix issue resolving scenic views using engineer

--- lib/core_extensions/scenic.rb
+++ lib/core_extensions/scenic.rb
@@ -1,19 +1,4 @@
 # frozen_string_literal: true
 
-require_dependency "scenic"
-
-# Override Scenic's definition#full_path so it will resolve db/views as whatever is in
-# config.paths["db/views"] if present, otherwise default to the original implementation.
-# Note his does not support having or merging views in both engine and the host app (as yet).
-module ResolveSceneicViewsInEngineOnly
-  def full_path
-    app = Rails.application
-    if defined?(app) && app && app.config.paths["db/views"].present?
-      File.join(app.config.paths["db/views"].to_a.first, filename)
-    else
-      super
-    end
-  end
-end
-
-Scenic::Definition.prepend(ResolveSceneicViewsInEngineOnly)
+# See engineer/datanase/scenic where we override Scenic's definition#to_sql so we can search
+# among any engine/app db/views paths in order to find the requested sql definition.
