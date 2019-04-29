0107d4400b102a03e63c9ae1ff143f600a52d910
Tim Crowe <tim@woodpigeon.com>
2019-04-29 11:06:23 +0100

    Clinic Visit editability changes
    Edit changes
    - A clinic visit can only be edited with 7 days of creation
    - A clinic visit can only be edited by the creator or a super admin
    
    Delete changes
    - A clinic visit can only be deleted by the creator or a super admin

--- app/policies/renalware/clinics/clinic_visit_policy.rb
+++ app/policies/renalware/clinics/clinic_visit_policy.rb
@@ -8,7 +8,21 @@ module Renalware
       def destroy?
         return false unless record.persisted?
 
-        record.created_at > Renalware.config.new_clinic_visit_deletion_window.ago
+        record.created_at > Renalware.config.new_clinic_visit_deletion_window.ago &&
+          (user_created_record? || user_is_super_admin?)
+      end
+
+      def edit?
+        return false unless record.persisted?
+
+        record.created_at > Renalware.config.new_clinic_visit_edit_window.ago &&
+          (user_created_record? || user_is_super_admin?)
+      end
+
+      private
+
+      def user_created_record?
+        record.created_by_id == user.id
       end
     end
   end
