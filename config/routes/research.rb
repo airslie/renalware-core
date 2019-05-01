8db20c708f5b4f59184ffa4411be0ab25f9fccad
Tim Crowe <tim@woodpigeon.com>
2019-05-01 14:11:16 +0100

    Fix merge issues

--- config/routes/research.rb
+++ config/routes/research.rb
@@ -3,6 +3,7 @@
 namespace :research do
   resources :studies do
     resources :participations, controller: :participations
+    resources :investigatorships, controller: :investigatorships
     resources :memberships, controller: :memberships
   end
 end
