8aa56783a9c9ce8a91fc380af2681ef707747888
Tim Crowe <tim@woodpigeon.com>
2019-06-05 14:27:46 +0100

    Add a service object to prepare ukrdc tables
    If the ukrdc_prepare_tables() SQL function is defined, call it (to generate ukrdc_prepared_* tables)

---
+++ spec/models/renalware/ukrdc/prepare_tables_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+module Renalware
+  describe UKRDC::PrepareTables do
+    let(:connection) { ActiveRecord::Base.connection }
+
+    describe ".call" do
+      context "when the ukrdc_prepare_tables function does not exist" do
+        it "does not error but returns false" do
+          expect(described_class.call).to be_falsey
+        end
+      end
+
+      context "when the ukrdc_prepare_tables function exists, letting a site prepare various "\
+              "tables and save them as eg ukrdc_prepared_hd_profiles" do
+        it "calls the SQL function" do
+          # We can assert this by making the function raise an exception
+          connection.execute(<<-SQL)
+            create function renalware.ukrdc_prepare_tables() RETURNS void
+            LANGUAGE plpgsql AS $$
+            BEGIN
+            RAISE EXCEPTION 'this will blow up';
+            END; $$;
+          SQL
+
+          expect {
+            described_class.call
+          }.to raise_error ActiveRecord::StatementInvalid
+        end
+
+        it "returns true" do
+          connection.execute(
+            "create function renalware.ukrdc_prepare_tables() RETURNS void "\
+            "LANGUAGE plpgsql AS $$ BEGIN END; $$;"
+          )
+
+          expect(described_class.call).to be_truthy
+        end
+      end
+    end
+  end
+end
