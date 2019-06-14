7d6f7fccd078fac88d1ebb141a91b2ce7548cfaf
Tim Crowe <tim@woodpigeon.com>
2019-06-14 12:55:36 +0100

    Remove concept of access procedures from seeds
    There are only access types now.

--- db/seeds/default/accesses/access_types.rb
+++ db/seeds/default/accesses/access_types.rb
@@ -2,13 +2,23 @@
 
 module Renalware
   log "Adding Access Types" do
-
     file_path = File.join(File.dirname(__FILE__), "access_types.csv")
 
     CSV.foreach(file_path, headers: true) do |row|
-      Accesses::Type.find_or_create_by!(code: row["code"]) do |type|
-        type.name = row["name"]
-        type.abbreviation = row["abbreviation"]
+      if row["rr02_code"].present?
+        rr02_code = row["rr02_code"]
+        abbreviation = rr02_code
+      end
+
+      if row["rr41_code"].present?
+        rr41_code = row["rr41_code"]
+        abbreviation = "#{rr02_code} #{rr41_code}"
+      end
+
+      Accesses::Type.find_or_create_by!(name: row["name"]) do |type|
+        type.rr02_code = rr02_code
+        type.rr41_code = rr41_code
+        type.abbreviation = abbreviation
       end
     end
   end
--- db/seeds/default/accesses/seeds.rb
+++ db/seeds/default/accesses/seeds.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 require_relative "./access_sites"
-require_relative "./access_types_procedures"
+require_relative "./access_types"
 require_relative "./access_plan_types"
 require_relative "./access_pd_catheter_insertion_techniques"
