5a9777521fc97410cd5836bb074a7e9dfef3d565
Tim Crowe <tim@woodpigeon.com>
2019-07-04 12:53:23 +0100

    Fix timeline generation issues

--- spec/models/renalware/ukrdc/treatment_timeline/remap_model_table_names_to_their_prepared_equivalents_spec.rb
+++ spec/models/renalware/ukrdc/treatment_timeline/remap_model_table_names_to_their_prepared_equivalents_spec.rb
@@ -14,11 +14,11 @@ module Renalware
         create(:hd_profile, patient: patient, by: user)
       end
 
-      it "only allows certain procsses to use it" do
+      it "only allows certain processes to use it" do
         stub_const("Renalware::UKRDC::TreatmentTimeline::"\
                    "RemapModelTableNamesToTheirPreparedEquivalents::ALLOWED_PROCESSES", [])
         expect {
-          described_class.call
+          described_class.new.call
         }.to raise_error(RemapModelTableNamesToTheirPreparedEquivalents::ExecutionNotAllowedError)
       end
 
@@ -27,7 +27,10 @@ module Renalware
           create_an_hd_profile
           table_name_before = klass.table_name
 
-          described_class.call(klass)
+          described_class.new.call(klass) do
+            expect(klass.table_name).to eq(table_name_before)
+            expect(klass.count).to eq(1)
+          end
 
           expect(klass.table_name).to eq(table_name_before)
           expect(klass.count).to eq(1)
@@ -35,8 +38,9 @@ module Renalware
       end
 
       context "when a prepared equivalent exists" do
-        it "changes the underlying class_name" do
+        it "changes the underlying class_name for the duration of the block" do
           create_an_hd_profile
+          table_name_before = Renalware::HD::Profile.table_name
           table_name_after = "ukrdc_prepared_hd_profiles"
           # Create the prepared table with 0 rows, so we will know if we have the right table
           connection.execute(<<-SQL)
@@ -44,10 +48,13 @@ module Renalware
             select * into #{table_name_after} from hd_profiles limit 0;
           SQL
 
-          described_class.call(klass)
+          described_class.new.call(klass) do
+            expect(klass.table_name).to eq(table_name_after)
+            expect(klass.count).to eq(0) # prooves we are looking at the right table
+          end
 
-          expect(klass.table_name).to eq(table_name_after)
-          expect(klass.count).to eq(0) # prooves we are looking at the right table
+          expect(klass.table_name).to eq(table_name_before)
+          expect(klass.count).to eq(1)
         end
       end
     end
