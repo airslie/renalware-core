461a5c5d7e420d9ef0817f9d3b10f1e14989299b
Tim Crowe <tim@woodpigeon.com>
2019-07-19 10:07:27 +0100

    Fix extration of rr41 from session#access_type_abbreviation
    session#access_type_abbreviation is a space-separated concatenation of rr02 and rr41 so we were taking the last word. However rr41 can be missing, so only extract rr41 is there are >1 words in access_type_abbreviation

--- spec/presenters/renalware/hd/session_presenter_spec.rb
+++ spec/presenters/renalware/hd/session_presenter_spec.rb
@@ -30,6 +30,7 @@ module Renalware
           it "returns the difference between before and after measurements rounded to 1 DP" do
             session.document.observations_before.weight = 101.1
             session.document.observations_after.weight = 100.0
+
             expect(presenter.change_in(:weight)).to eq(-1.1)
           end
         end
@@ -38,6 +39,7 @@ module Renalware
           it "returns the difference between before and after measurements" do
             session.document.observations_before.pulse = 70
             session.document.observations_after.pulse = 80
+
             expect(presenter.change_in(:pulse)).to eq 10
           end
         end
@@ -69,6 +71,7 @@ module Renalware
       describe "#after_measurement_for" do
         it "returns the post-dialysis measurement" do
           session.document.observations_after.weight = 101.1
+
           expect(presenter.after_measurement_for(:weight)).to eq 101.1
         end
       end
@@ -76,19 +79,82 @@ module Renalware
       describe "#access_side_rr40_code" do
         it "maps left to L" do
           session.document.info.access_side = "left"
+
           expect(presenter.access_side_rr40_code).to eq("L")
         end
 
         it "maps right to R" do
           session.document.info.access_side = "right"
+
           expect(presenter.access_side_rr40_code).to eq("R")
         end
 
         it "maps anything else (ie unknown) to U " do
           [nil, "", "xyz"].each do |side|
             session.document.info.access_side = side
+
             expect(presenter.access_side_rr40_code).to eq("U")
           end
+        end
+      end
+
+      describe "#access_rr02_code" do
+        subject { presenter.access_rr02_code }
+        # We store in the session only the access abbreviation of rr02 + rr41
+        # To resolve rr02 we assume it is the first word in the abbreviation.
+
+        context "when access_abbreviation is two words" do
+          before { session.document.info.access_type_abbreviation = "rr02 rr41" }
+
+          it { is_expected.to eq("rr02") }
+        end
+
+        context "when access_abbreviation is one word ie just the rr02 code" do
+          before { session.document.info.access_type_abbreviation = "rr02" }
+
+          it { is_expected.to eq("rr02") }
+        end
+
+        context "when access_abbreviation is nil" do
+          before { session.document.info.access_type_abbreviation = nil }
+
+          it { is_expected.to be_nil }
+        end
+
+        context "when access_abbreviation is an empty string" do
+          before { session.document.info.access_type_abbreviation = "" }
+
+          it { is_expected.to be_nil }
+        end
+      end
+
+      describe "#access_rr41_code" do
+        subject { presenter.access_rr41_code }
+        # We store in the session only the access abbreviation of rr02 + rr41
+        # To resolve rr02 we assume it is the last word in the abbreviation if there are >1 words.
+
+        context "when access_abbreviation is two words" do
+          before { session.document.info.access_type_abbreviation = "rr02 rr41" }
+
+          it { is_expected.to eq("rr41") }
+        end
+
+        context "when access_abbreviation is one word ie just the rr02 code" do
+          before { session.document.info.access_type_abbreviation = "rr02" }
+
+          it { is_expected.to be_nil }
+        end
+
+        context "when access_abbreviation is nil" do
+          before { session.document.info.access_type_abbreviation = nil }
+
+          it { is_expected.to be_nil }
+        end
+
+        context "when access_abbreviation is an empty string" do
+          before { session.document.info.access_type_abbreviation = "" }
+
+          it { is_expected.to be_nil }
         end
       end
     end
