67b1471611edd1ff7a214983b3290c34c1746be3
Tim Crowe <tim@woodpigeon.com>
2019-09-11 12:26:33 +0100

    Fix HD prescription administrations on the heroic branch

--- app/controllers/renalware/hd/prescription_administration_authorisations_controller.rb
+++ app/controllers/renalware/hd/prescription_administration_authorisations_controller.rb
@@ -6,6 +6,7 @@ module Renalware
   module HD
     class PrescriptionAdministrationAuthorisationsController < BaseController
       skip_after_action :verify_authorized
+      skip_after_action :verify_policy_scoped
 
       def create
         user = User.find(auth_params[:id])
