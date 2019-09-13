fd9f3ae1a1a6fabd605f1c07277d63d2732d2984
Tim Crowe <tim@woodpigeon.com>
2019-09-13 12:31:28 +0100

    Fix UKRDC transplant treatments

---
+++ app/models/renalware/ukrdc/treatment_timeline/transplant/generator.rb
@@ -0,0 +1,113 @@
+# frozen_string_literal: true
+
+require_dependency "renalware/ukrdc"
+require "attr_extras"
+
+# QBL   TXT   Description
+# "20"  "20"  "Transplant ; Cadaver donor"
+# "21"  "21"  "Transplant ; Live related - sibling"
+# "74"  "74"  "Transplant ; Live related - father"
+# "75"  "75"  "Transplant ; Live related - mother"
+# "77"  "77"  "Transplant ; Live related - child"
+# "23"  "23"  "Transplant ; Live related - other"
+# "24"  "24"  "Transplant ; Live genetically unrelated"
+# "25"  "25"  "Transplant ; Cadaver donor + transp other organ"
+# "26"  "26"  "Transplant ; Live donor + transplant other organ"
+# "27"  "27"  "Transplant ; Live donor non-UK transplant"
+# "28"  "28"  "Transplant ; non-heart-beating donor"
+# "29"  "29"  "Transplant ; type unknown"
+module Renalware
+  module UKRDC
+    module TreatmentTimeline
+      module Transplant
+        class Generator
+          pattr_initialize :modality
+          delegate :patient, to: :modality
+
+          UKRR_TXT = {
+            cadaver: 20,
+            unknown: 29,
+            live_related_other: 23,
+            live_related_sibling: 21,
+            live_related_father: 74,
+            live_related_mother: 75,
+            live_related_child: 77,
+            non_heart_beating: 28,
+            live_unrelated: 24
+          }.freeze
+
+          # We need to create a treatment record to match the UKRDC code expectations
+          # based on donor relationship.
+          # Otherwise Default to 29 Transplant ; type unknown
+          def call
+            create_treatment
+          end
+
+          private
+
+          def create_treatment
+            operation
+            Treatment.create!(
+              patient: modality.patient,
+              clinician: modality.created_by,
+              started_on: modality.started_on,
+              modality_id: modality.id,
+              modality_description_id: modality.description_id,
+              ended_on: modality.ended_on,
+              modality_code: ukrr_modality_code
+            )
+          end
+
+          def unknown_transplant_ukrr_modality_code
+            ukrr_modality_code_for_txt(UKRR_TXT[:unknown])
+          end
+
+          def ukrr_modality_code
+            return unknown_transplant_ukrr_modality_code if operation.nil?
+            return unknown_transplant_ukrr_modality_code if donor_type.nil?
+
+            if donor_type == :live_related
+              ukrr_modality_code_for_live_donor_relationship
+            else
+              ukrr_modality_code_for_txt(UKRR_TXT[donor_type])
+            end
+          end
+
+          def donor_type
+            operation.document.donor.type&.to_sym
+          end
+
+          def donor_relationship
+            operation.document.donor.relationship&.to_sym
+          end
+
+          # rubocop:disable Style/StringLiterals
+          def ukrr_modality_code_for_live_donor_relationship
+            key = :"live_related_#{donor_relationship || 'other'}"
+            ukrr_modality_code_for_txt(UKRR_TXT.fetch(key))
+          end
+          # rubocop:enable Style/StringLiterals
+
+          def ukrr_modality_code_for_txt(txt_code)
+            UKRDC::ModalityCode.find_by(txt_code: txt_code)
+          end
+
+          # Try and select an operation within the start and end dates of the modality
+          def operation
+            @operation ||= begin
+              Renalware::Transplants::RecipientOperation
+                .for_patient(patient)
+                .where(
+                  "performed_on >= ? and performed_on <= ?",
+                  modality.started_on,
+                  modality.ended_on || Time.zone.today
+                )
+                .order(performed_on: :desc)
+                .first
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/models/renalware/ukrdc/treatment_timeline/transplant/generator_spec.rb
@@ -0,0 +1,174 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+# rubocop:disable Metrics/LineLength
+module Renalware
+  module UKRDC
+    describe TreatmentTimeline::Transplant::Generator do
+      include PatientsSpecHelper
+      subject(:generator) { described_class.new(modality) }
+
+      let(:user) { create(:user) }
+      let!(:unknown_ukrdc_modality_code) { create(:ukrdc_modality_code, :type_unknown) }
+      let!(:cadaver_ukrdc_modality_code) { create(:ukrdc_modality_code, :cadaver) }
+      let!(:nhb_ukrdc_modality_code) { create(:ukrdc_modality_code, :non_heart_beating) }
+      let!(:live_related_sibling_modality_code) { create(:ukrdc_modality_code, :live_related_sibling) }
+      let!(:live_related_father_modality_code) { create(:ukrdc_modality_code, :live_related_father) }
+      let!(:live_related_mother_modality_code) { create(:ukrdc_modality_code, :live_related_mother) }
+      let!(:live_related_child_modality_code) { create(:ukrdc_modality_code, :live_related_child) }
+      let!(:live_related_other_modality_code) { create(:ukrdc_modality_code, :live_related_other) }
+      let!(:live_unrelated_ukrdc_modality_code) { create(:ukrdc_modality_code, :live_unrelated) }
+      let(:tx_mod_desc) { create(:transplant_modality_description) }
+
+      context "when the patient has the RW Transplant modality" do
+        let(:patient) { create(:transplant_patient) }
+        let(:modality) do
+          set_modality(
+            patient: patient,
+            modality_description: tx_mod_desc,
+            by: user
+          )
+        end
+
+        before { modality }
+
+        context "when the patient has no recipient operations" do
+          it "creates a Treatment with type Tx unknown" do
+            expect {
+              generator.call
+            }.to change(UKRDC::Treatment, :count).by(1)
+
+            expect(UKRDC::Treatment.first).to have_attributes(
+              patient_id: patient.id,
+              hd_profile_id: nil,
+              modality_code: unknown_ukrdc_modality_code,
+              modality_description_id: tx_mod_desc.id,
+              started_on: modality.started_on,
+              ended_on: modality.ended_on,
+              modality_id: modality.id
+            )
+          end
+        end
+
+        context "when the patient has a recipient operation outside the Tx modality start/end" do
+          subject { UKRDC::Treatment.first.modality_code }
+
+          before do
+            create_operation(type: :cadaver, performed_on: modality.started_on - 1.day)
+            generator.call
+          end
+
+          it { is_expected.to eq(unknown_ukrdc_modality_code) }
+        end
+
+        context "when the patient has a recipient operation inside the modality's date span" do
+          context "when the donor is a cadaver" do
+            subject { UKRDC::Treatment.first.modality_code }
+
+            before do
+              create_operation(type: :cadaver)
+              generator.call
+            end
+
+            it { is_expected.to eq(cadaver_ukrdc_modality_code) }
+          end
+
+          context "when the donor is non heart beating" do
+            subject { UKRDC::Treatment.first.modality_code }
+
+            before do
+              create_operation(type: :non_heart_beating)
+              generator.call
+            end
+
+            it { is_expected.to eq(nhb_ukrdc_modality_code) }
+          end
+
+          context "when the donor is non live_unrelated" do
+            subject { UKRDC::Treatment.first.modality_code }
+
+            before do
+              create_operation(type: :live_unrelated)
+              generator.call
+            end
+
+            it { is_expected.to eq(live_unrelated_ukrdc_modality_code) }
+          end
+
+          context "when the donor is a live_related and relationship is unset" do
+            subject { UKRDC::Treatment.first.modality_code }
+
+            before do
+              create_operation(type: :live_related)
+              generator.call
+            end
+
+            it { is_expected.to eq(live_related_other_modality_code) }
+          end
+
+          context "when the donor is a live_related sibling" do
+            subject { UKRDC::Treatment.first.modality_code }
+
+            before do
+              create_operation(type: :live_related, relationship: :sibling)
+              generator.call
+            end
+
+            it { is_expected.to eq(live_related_sibling_modality_code) }
+          end
+
+          context "when the donor is a live_related father" do
+            subject { UKRDC::Treatment.first.modality_code }
+
+            before do
+              create_operation(type: :live_related, relationship: :father)
+              generator.call
+            end
+
+            it { is_expected.to eq(live_related_father_modality_code) }
+          end
+
+          context "when the donor is a live_related mother" do
+            subject { UKRDC::Treatment.first.modality_code }
+
+            before do
+              create_operation(type: :live_related, relationship: :mother)
+              generator.call
+            end
+
+            it { is_expected.to eq(live_related_mother_modality_code) }
+          end
+
+          context "when the donor is a live_related child" do
+            subject { UKRDC::Treatment.first.modality_code }
+
+            before do
+              create_operation(type: :live_related, relationship: :child)
+              generator.call
+            end
+
+            it { is_expected.to eq(live_related_child_modality_code) }
+          end
+        end
+      end
+
+      def create_operation(
+        type:,
+        performed_on: modality.started_on,
+        relationship: nil
+      )
+        create(
+          :transplant_recipient_operation,
+          patient: patient,
+          performed_on: performed_on
+        ).tap do |op|
+          op.document.donor.type = type
+          op.document.donor.relationship = relationship
+          op.save!
+        end
+      end
+    end
+  end
+end
+# rubocop:enable Metrics/LineLength
