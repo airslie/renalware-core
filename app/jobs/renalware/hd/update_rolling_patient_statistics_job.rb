9ec37f373ce0d8d8a4c7a4322448b72f964b0e1f
Tim Crowe <tim@woodpigeon.com>
2019-09-23 18:21:13 +0100

    Do not keep failed UpdateRollingPatientStatisticsJobs
    Because this job will be triggered again the next time an HD Sessions is created, it is not crucial to keep each event around - ie they have a short shelf-life. For this reason we only retry 3 times then delete failed jobs.
    Note after migration at a site, the last 12 HD Sessions for a patient might trigger various errors as they have not been created through Renalware, so it is expected for some of these jobs to fail in the first month or so.

--- app/jobs/renalware/hd/update_rolling_patient_statistics_job.rb
+++ app/jobs/renalware/hd/update_rolling_patient_statistics_job.rb
@@ -2,16 +2,26 @@
 
 # When executed this job updates rolling statistics for a patient's
 # last past 12 HD sessions.
-
+# Because this job will be triggered again the next time an HD Sessions is
+# created, it is not crucial to keep each event around - ie they have a short
+# shelf-life. For this reason we only retry 3 times then delete failed jobs.
 module Renalware
   module HD
     class UpdateRollingPatientStatisticsJob < ApplicationJob
       queue_as :hd_patient_statistics
-      queue_with_priority 1
+      queue_with_priority 4
 
       # :reek:UtilityFunction
       def perform(patient)
         UpdateRollingPatientStatistics.new(patient: patient).call
+      end
+
+      def max_attempts
+        3
+      end
+
+      def destroy_failed_jobs?
+        true
       end
     end
   end
