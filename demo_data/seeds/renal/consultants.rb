32a2b29634f004a8284a65a28ae44e2faf400956
Tim Crowe <tim@woodpigeon.com>
2019-09-26 15:56:39 +0100

    Fix renal consulltant seeds

--- demo_data/seeds/renal/consultants.rb
+++ demo_data/seeds/renal/consultants.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Renalware
-  log "Adding Clinic Consultants" do
+  log "Adding Renal Consultants" do
     Renal::Consultant.find_or_create_by(code: "A", name: "Dr Jonathon Strange")
     Renal::Consultant.find_or_create_by(code: "B", name: "Melissa Montefiori")
   end
