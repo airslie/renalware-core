e134d72edc1458e299dba23cef7f03f5bc4f3f96
Tim Crowe <tim@woodpigeon.com>
2019-10-29 11:25:22 +0000

    Resolve rebase issues

--- app/controllers/renalware/pd/dashboards_controller.rb
+++ app/controllers/renalware/pd/dashboards_controller.rb
@@ -5,6 +5,8 @@ require_dependency "renalware/pd"
 module Renalware
   module PD
     class DashboardsController < PD::BaseController
+      skip_after_action :verify_policy_scoped
+
       def show
         authorize patient
         render :show, locals: {
