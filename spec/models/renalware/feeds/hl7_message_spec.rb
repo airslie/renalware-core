1eedda95565dcc5bb586b0da17911646e4c0ae3d
Tim Crowe <tim@woodpigeon.com>
2019-11-04 14:01:18 +0000

    Handle persisting feed messages containing no patient identifying info
    E.g.  MFN^M02

--- spec/models/renalware/feeds/hl7_message_spec.rb
+++ spec/models/renalware/feeds/hl7_message_spec.rb
@@ -164,5 +164,31 @@ module Renalware::Feeds
         end
       end
     end
+
+    context "when the message type is MFN^M02" do
+      let(:raw_message) do
+        <<-RAW.strip_heredoc
+          MSH|^~\&|ADT|iSOFT Engine|eGate|Kings|20191030155640||MFN^M02|1861609776|P|2.3|||AL|AL
+          MFI|STF|PIMS|UPD|20191030155640|20191030155640|NE
+          MFE|MAD|1861609776|20191030155640|193814
+          STF|193814|C1119528^^^^MAINCODE~XXX^^^^DG~C1119528^^^^GMC|Xxx^Xxxx^^^Mr|CONLT|UNKNOWN||A|100^Trauma and Orthopaedic~102^Fracture||020 0000 000^PHONE|X Hospital NHS Foundation Trust^Somewhere^London, Greater London^^N1 1AAS^UK^BUSIN|20120912000000
+          PRA|200000|XYZ^XX Hospital NHS Trust^TRUST|||100^NAT^MAIN~TRAUMA^DG^MAIN~01^ABC^MAIN~FRAC^DG^SEC1~020^LOCAL^SEC1|||20120912
+        RAW
+      end
+
+      describe "#message_type" do
+        subject { decorator.message_type }
+
+        it { is_expected.to eq("MFN") }
+      end
+
+      describe "patient_identification" do
+        subject(:pi) { decorator.patient_identification }
+
+        it "returns a blank internal_id because none is available" do
+          expect(pi.internal_id).to be_nil
+        end
+      end
+    end
   end
 end
