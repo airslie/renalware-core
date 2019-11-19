b3f9f2b15ecce57d36266f71d62410e5ef389584
Tim Crowe <tim@woodpigeon.com>
2019-11-19 10:42:18 +0000

    Remove webpacker
    Have switched to Sprockets 4 as a way to start using es6 and stimulus in Renalware. The addition of webpacker in the engine is a pain and I think in any case we need to be using something like rollup.js to build an npm (or github package) js bundle to include in the host app rather that the hackery that webpacker-in-an-engine requires e.g. injecting the engines pack into the host at compile time.

--- lib/renalware.rb
+++ lib/renalware.rb
@@ -4,17 +4,4 @@ require "renalware/engine"
 require "renalware/configuration"
 
 module Renalware
-  # Configure Webpacker instance.
-  # See https://github.com/rails/webpacker/blob/master/docs/engines.md
-  class << self
-    def webpacker
-      @webpacker ||= begin
-        root_path = Pathname.new(File.join(__dir__, ".."))
-        ::Webpacker::Instance.new(
-          root_path: root_path,
-          config_path: root_path.join("config/webpacker.yml")
-        )
-      end
-    end
-  end
 end
