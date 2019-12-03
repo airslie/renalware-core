1dbec704daf75b7e48f6508fad00951ce1b562d4
Tim Crowe <tim@woodpigeon.com>
2019-12-03 19:28:40 +0000

    Fix an IE11 js bug in tab toggling

--- app/assets/javascripts/renalware/components/tabs.js
+++ app/assets/javascripts/renalware/components/tabs.js
@@ -2,8 +2,8 @@ function initTabs() {
   $(".sub-nav.with-tabs dl a").on("click", function(e) {
     e.preventDefault();
     var anchor = e.target
-    var dl = anchor.closest("dl");
-    var dd = anchor.closest("dd");
+    var dl = $(anchor).closest("dl");
+    var dd = $(anchor).closest("dd");
     var idToDeactivate = $("dd.active a", dl).attr("href");
     $("dd.active", dl).removeClass("active")
     $(dd).addClass("active");
