bb8e8a4f6c0ae90e6b5312920185098434b6b228
Tim Crowe <tim@woodpigeon.com>
2020-01-06 22:22:16 +0000

    Ensure user email addresses are case-insensitive
    - Enforce case-insensitive email and username at the database level using functional indexes eg lower(email)
    - Introduce case insensitivity at the application level in Devise, so that Devise will automatically downcase the email when creating or updating a user record.

--- db/migrate/20170526060804_enable_uuid_extension.rb
+++ db/migrate/20170526060804_enable_uuid_extension.rb
@@ -2,6 +2,6 @@ class EnableUuidExtension < ActiveRecord::Migration[5.0]
   # Enable the pg extension that will allow us to have automatically
   # generated uuids in the format f8c9ffd6-a234-4729-bd2a-68379df315fb
   def change
-    enable_extension 'uuid-ossp'
+    enable_extension "uuid-ossp"
   end
 end
