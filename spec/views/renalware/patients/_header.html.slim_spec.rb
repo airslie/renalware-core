78681c8856f0fa6701fb63ea1bf1b8f860a753d9
Tim Crowe <tim@woodpigeon.com>
2020-02-04 19:08:53 +0000

    Temporarily exclude turnip features
    In an attempt to solve failing tests on CI

--- spec/views/renalware/patients/_header.html.slim_spec.rb
+++ spec/views/renalware/patients/_header.html.slim_spec.rb
@@ -2,12 +2,7 @@
 
 require "rails_helper"
 
-# Removed this test as something environmental on CI is causing line 6 to complain that 
-# PatientsSpecHelper is not defined, even if it is included with
-#  include PatientsSpecHelper
-# Nothing else has changed to provoke this. All PRs seemd to start failing together on this test.
 describe "renalware/patients/_header" do
-  include PatientsSpecHelper
   helper(Renalware::ApplicationHelper, Renalware::PatientHelper)
 
   it "includes the correctly formatted NHS number" do
