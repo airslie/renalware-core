6e3fad35455cffdc6a802d3613b9ab6f8fe4543a
Tim Crowe <tim@woodpigeon.com>
2020-03-08 20:49:29 +0000

    Switch to using the npm version of trix
    Previously we were using a forked version of maclover7/trix
    in order for the trix-rails gem to work with Rails 5.2.
    Here we switch to the npm package in order to reduce
    complexity and remove the need for our fork.

---
+++ app/inputs/trix_editor_input.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# See https://github.com/kylefox/trix/blob/master/lib/trix/simple_form/trix_editor_input.rb
+class TrixEditorInput < SimpleForm::Inputs::Base
+  def input(_wrapper_options)
+    trix_options = options.slice(:spellcheck, :toolbar, :tabindex, :input, :class)
+    editor_options = { input: input_class, class: "trix-content" }.merge(trix_options)
+
+    editor_tag = template.content_tag("trix-editor", "", editor_options)
+    hidden_field = @builder.hidden_field(attribute_name, input_html_options)
+
+    template.content_tag("div", hidden_field + editor_tag, class: "trix-editor-wrapper")
+  end
+end
