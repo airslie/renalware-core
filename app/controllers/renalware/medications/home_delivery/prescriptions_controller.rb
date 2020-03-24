bdc2947ef4882a752f5bb76d8a5676e46f65f6ae
Tim Crowe <tim@woodpigeon.com>
2020-03-24 17:26:16 +0000

    Filter home delivery prescriptions by modality

--- app/controllers/renalware/medications/home_delivery/prescriptions_controller.rb
+++ app/controllers/renalware/medications/home_delivery/prescriptions_controller.rb
@@ -9,13 +9,33 @@ module Renalware
         include Pagy::Backend
 
         def index
-          pagy, prescriptions = pagy(
-            Medications::Delivery::PrescriptionsDueForDeliveryQuery.new(
-              drug_type_code: params[:named_filter]
-            ).call
+          form = SearchForm.new(search_params)
+          query = Medications::Delivery::PrescriptionsDueForDeliveryQuery.new(
+            drug_type_code: params[:named_filter],
+            modality_description_id: form.modality_description_id,
+            query: params[:q]
           )
+          pagy, prescriptions = pagy(query.call)
           authorize prescriptions
-          render :index, locals: { prescriptions: prescriptions, pagy: pagy }
+          render :index, locals: {
+            prescriptions: prescriptions,
+            pagy: pagy,
+            query: query.search,
+            form: form
+          }
+        end
+
+        class SearchForm
+          include ActiveModel::Model
+          include Virtus::Model
+
+          attribute :modality_description_id, Integer
+        end
+
+        def search_params
+          return {} unless params.key?(:search)
+
+          params.require(:search).permit(:modality_description_id)
         end
       end
     end
--- app/models/renalware/medications/delivery/prescriptions_due_for_delivery_query.rb
+++ app/models/renalware/medications/delivery/prescriptions_due_for_delivery_query.rb
@@ -8,18 +8,28 @@ module Renalware
       # Find current home delivery prescriptions with a next_delivery_date falling
       # within the specified range.
       class PrescriptionsDueForDeliveryQuery
-        attr_reader :drug_type_code, :from, :to
+        attr_reader :drug_type_code, :from, :to, :query, :modality_description_id
 
-        def initialize(drug_type_code: nil, from: nil, to: nil)
+        def initialize(
+          drug_type_code: nil,
+          from: nil,
+          to: nil,
+          modality_description_id: nil,
+          query: {}
+        )
           @from = (from || 4.weeks.ago).beginning_of_day
           @to = (to || 2.weeks.since).end_of_day
           @drug_type_code = drug_type_code
+          @query = query
+          @modality_description_id = modality_description_id
         end
 
         def call
-          query = Medications::Prescription
+          query = search.result
             .current
-            .eager_load(drug: [:drug_types])
+            .includes(:patient)
+            .eager_load(drug: [:classifications, :drug_types])
+            .joins("inner join patient_current_modalities pcm on pcm.patient_id = patients.id")
             .where(provider: :home_delivery)
             .where(next_delivery_date: (from..to))
             .order(next_delivery_date: :asc)
@@ -27,7 +37,15 @@ module Renalware
           if drug_type_code.present?
             query = query.where("lower(drug_types.code) = ?", drug_type_code)
           end
+
+          if modality_description_id.present?
+            query = query.where("pcm.modality_description_id = ?", modality_description_id)
+          end
           query
+        end
+
+        def search
+          @search ||= Medications::Prescription.ransack(query)
         end
       end
     end
