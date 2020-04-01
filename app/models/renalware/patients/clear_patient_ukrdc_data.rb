984433a65d4520a1bb2ea58998fa9cbda6ceeca7
Tim Crowe <tim@woodpigeon.com>
2020-04-01 16:14:12 +0100

    Ignore validation errors when clearing RPV data at Death
        When a patient is given the Death modality a Wisper
        listener catches the broadcast death modality change
        and sets send_to_rpv = false (and related columns).
        Handle the case where an error occurs at this point
        for example because the patient has an invalid email
        and cannot be saved, by skipping validation callbacks.
        Its bad practice but this case is a pragmatic solution.

--- app/models/renalware/patients/clear_patient_ukrdc_data.rb
+++ app/models/renalware/patients/clear_patient_ukrdc_data.rb
@@ -12,12 +12,17 @@ module Renalware
         new(**args).call
       end
 
+      # Note that we want to avoid any patient validation errors (email is invalid etc)
+      # at this important stage, so we use validate: false. While we validate patient data
+      # entered thorugh the UI, its possible that some migrated data might be invalid
+      # and would cause an error to be raised during a callback in save!
       def call
         patient.send_to_rpv = false
         patient.rpv_decision_on = Time.zone.today
         patient.rpv_recorded_by = by.to_s
         patient.skip_death_validations = true
-        patient.save_by!(by)
+        patient.by = by
+        patient.save!(validate: false)
       end
     end
   end
---
+++ spec/models/renalware/patients/clear_patient_ukrdc_data_spec.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+module Renalware
+  module Patients
+    describe ClearPatientUKRDCData do
+      describe "#call" do
+        it "clears the RPV status if the modality description type is death" do
+          travel_to(Time.zone.now) do
+            patient = create(:patient, send_to_rpv: true, rpv_decision_on: 1.year.ago)
+            user = create(:user)
+
+            described_class.call(patient: patient, by: user)
+
+            patient.reload
+            expect(patient.send_to_rpv).to eq(false)
+            expect(patient.rpv_decision_on).to eq(Time.zone.today)
+            expect(patient.rpv_recorded_by).to eq(user.to_s)
+          end
+        end
+
+        context "when the patient has data that would cause a validation error" do
+          it "saves anyway, skipping validations, because a bad email address for example "\
+             "should not prevent this action" do
+            user = create(:user)
+            patient = build(:patient, send_to_rpv: true, email: "invalid-email-address", by: user)
+
+            # Check this would normally result in a validation error
+            expect { patient.save! }.to raise_error(ActiveRecord::RecordInvalid)
+
+            # Now save without validation so we have an invalid persisted record to deal with
+            patient.save!(validate: false)
+
+            # calling the service object should not raise an error!
+            expect {
+              described_class.call(patient: patient, by: user)
+            }.not_to raise_error
+
+            patient.reload
+            expect(patient.send_to_rpv).to eq(false)
+            expect(patient.rpv_decision_on).to eq(Time.zone.today)
+            expect(patient.rpv_recorded_by).to eq(user.to_s)
+          end
+        end
+      end
+    end
+  end
+end
