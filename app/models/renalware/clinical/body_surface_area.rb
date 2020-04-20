c2a03528945aa7577b33e3e7b7ffdddb722810d5
Tim Crowe <tim@woodpigeon.com>
2020-04-20 13:11:50 +0100

    Refactor body seruface area calculator
    As height is always required in a clinic visit, and
    defaults to the last measured height in any case -
    and rarely changes -  I think it is safe to just use
    the height and weight measurements from
    the most recent clinic visit where a weight was
    recorded.

--- app/models/renalware/clinical/body_surface_area.rb
+++ app/models/renalware/clinical/body_surface_area.rb
@@ -24,22 +24,23 @@ module Renalware
       end
 
       def most_recent_height
-        @most_recent_height ||= most_recent(:height)
+        most_recent_visit_where_weight_was_measured&.height.to_f
       end
 
       def most_recent_weight
-        @most_recent_weight ||= most_recent(:weight)
+        most_recent_visit_where_weight_was_measured&.weight.to_f
       end
 
-      def most_recent(measurement)
-        Clinics::ClinicVisit
-          .for_patient(patient)
-          .where("#{measurement} > 0")
-          .order(date: :desc, created_at: :desc)
-          .limit(1)
-          .pluck(measurement.to_sym)
-          .first
-          .to_f
+      def most_recent_visit_where_weight_was_measured
+        @most_recent_visit_where_weight_was_measured ||= begin
+          Clinics::ClinicVisit
+            .for_patient(patient)
+            .select(:weight, :height)
+            .where("weight > 0")
+            .order(date: :desc, created_at: :desc)
+            .limit(1)
+            .first
+        end
       end
     end
   end
--- spec/models/renalware/clinical/body_surface_area_spec.rb
+++ spec/models/renalware/clinical/body_surface_area_spec.rb
@@ -16,18 +16,22 @@ module Renalware
           end
         end
 
-        context "when height and weight are available in recent clinic visits" do
+        context "when height and weight are available in a recent clinic visit" do
           it "returns the correct calculated BSA rounded to default 3 dp" do
-            create(:clinic_visit, patient: patient, height: 1.23, weight: nil)
-            create(:clinic_visit, patient: patient, height: 1.23, weight: 100.99, date: 1.day.ago)
+            # The top most recent visit will be ignored as we only look for the most recent visit
+            # where weight was recorded - so in this case the visit 10 days ago
+            create(:clinic_visit, patient: patient, height: 1.21, weight: nil, date: 1.day.ago)
+            create(:clinic_visit, patient: patient, height: 1.23, weight: 100.99, date: 10.days.ago)
+
+            result = described_class.new(patient: patient).calculate
 
             # BSA = 0.007184*Weight^0.425*(Height)^0.725 eg 0.059342499887066046
-            expect(described_class.new(patient: patient).calculate).to eq(0.059)
+            expect(result).to eq(0.059)
           end
 
           it "returns the correct calculated BSA rounded to the requested dp" do
-            create(:clinic_visit, patient: patient, height: 1.23, weight: nil)
             create(:clinic_visit, patient: patient, height: 1.23, weight: 100.99, date: 1.day.ago)
+
             result = described_class.new(patient: patient, dp: 2).calculate
 
             expect(result).to eq(0.06)
