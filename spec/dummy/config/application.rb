8fa47887e829a7adb95ad69eba1dea8ac8257ad0
Tim Crowe <tim@woodpigeon.com>
2020-05-19 12:15:16 +0100

    Don't use zeitwerk in the dummy app

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -12,6 +12,7 @@ module Dummy
     config.cache_store = :file_store, Rails.root.join("tmp", "cache") # capistrano symmlinked
     config.active_record.time_zone_aware_types = [:datetime]
     config.active_storage.service = :local
+    config.autoloader = :classic
 
     # Important!!
     # Unless set to :all, pg extensions are not put into structure.sql so certain
