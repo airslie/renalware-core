12aaba722b29fc1dcdb8550366c59eb5e3f35835
Tim Crowe <tim@woodpigeon.com>
2020-06-12 15:42:04 +0100

    Revert trix rubocop changes
    Using tag.xx rather than content_tag(:xx) breaks things

--- app/inputs/trix_editor_input.rb
+++ app/inputs/trix_editor_input.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: true
 
+# rubocop:disable Rails/ContentTag
 # See https://github.com/kylefox/trix/blob/master/lib/trix/simple_form/trix_editor_input.rb
 class TrixEditorInput < SimpleForm::Inputs::Base
   def input(_wrapper_options)
     trix_options = options.slice(:spellcheck, :toolbar, :tabindex, :input, :class, :data)
     editor_options = { input: input_class, class: "trix-content" }.merge(trix_options)
 
-    editor_tag = template.tag("trix-editor", "", editor_options)
+    editor_tag = template.content_tag("trix-editor", "", editor_options)
     hidden_field = @builder.hidden_field(attribute_name, input_html_options)
 
-    template.tag.div(hidden_field + editor_tag, class: "trix-editor-wrapper")
+    template.content_tag("div", hidden_field + editor_tag, class: "trix-editor-wrapper")
   end
 end
+# rubocop:enable Rails/ContentTag
