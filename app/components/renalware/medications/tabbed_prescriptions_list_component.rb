05d32408b0c2437f7b3d0f058c2c183cf293ce0b
Tim Crowe <tim@woodpigeon.com>
2020-10-07 19:36:36 +0100

    Add Bone prescriptions to the tabs in an MDM
    In the prescriptions in each MDM (show) view, add a 'Bone Meds'
    tab to display prescriptions having a drug type of
    'Bone/Calcium/Phosphate'.
    
    Implement as a new ViewComponent that can take a group of prescriptions
    options and render each as a tab and its tabular data.
    
    Prescription Tabs WIP
    
    Prescription BOn tabs wip

---
+++ app/components/renalware/medications/tabbed_prescriptions_list_component.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+module Renalware
+  module Medications
+    # Accepts a group of prescriptions -  see example usage - and renders a tabs for each of the
+    # group names with their corresponding tablular content.
+    # Various options - see PrescriptionGroup - drive what columns are shown.
+    #
+    # Example usage:
+    #
+    # = render Renalware::Medications::TabbedPrescriptionsListComponent.new(
+    #   [
+    #      { title: "Current", prescriptions: list1, show_administer_on_hd: true },
+    #      { title: "EPO", prescriptions: list2, show_terminated_on: true }
+    #   ]
+    class TabbedPrescriptionsListComponent < ApplicationComponent
+      include DrugsHelper
+      attr_reader :groups
+
+      class PrescriptionGroup
+        attr_reader_initialize [
+          :title,
+          :prescriptions,
+          show_terminated_on: true,
+          show_administer_on_hd: true,
+          show_drug_types: false
+        ]
+
+        # The markup expects each prescription to have been decorated with a PresriptionPresenter.
+        # However there may be cases where they might have been, so we check by
+        # (looking for a method which is defined by the presenter) and decorate them if necessary.
+        def prescriptions
+          @prescriptions.map do |presc|
+            presc.respond_to?(:drug_type_names) ? presc : PrescriptionPresenter.new(presc)
+          end
+        end
+      end
+
+      # Map the incoming array of hashes to an array of PrescriptionGroup objects to make
+      # interrogation easier in the html.
+      def initialize(prescription_groups)
+        @groups = Array(prescription_groups).map { |options| PrescriptionGroup.new(options) }
+        super
+      end
+    end
+  end
+end
--- spec/dummy/db/seeds/drugs/drug_types.rb
+++ spec/dummy/db/seeds/drugs/drug_types.rb
@@ -1,19 +1,22 @@
 # frozen_string_literal: true
 
+# rubocop:disable Style/WordArray
 module Renalware
   log "Adding Drug Types" do
     Drugs::Drug.transaction do
-      %w(
-        Antibiotic
-        ESA
-        Immunosuppressant
-        Peritonitis
-        Controlled
-      ).each do |drug_type|
-        Drugs::Type.find_or_create_by!(code: drug_type.downcase) do |type|
-          type.name = drug_type
+      [
+        ["Antibiotic", "antibiotic"],
+        ["ESA", "esa"],
+        ["Immunosuppressant", "immunosuppressant"],
+        ["Peritonitis", "peritonitis"],
+        ["Controlled", "controlled"],
+        ["Bone/Calcium/Phosphate", "bone/Ca/PO4"]
+      ].each do |drug_type|
+        Drugs::Type.find_or_create_by!(code: drug_type[1]) do |type|
+          type.name = drug_type[0]
         end
       end
     end
   end
 end
+# rubocop:enable Style/WordArray
