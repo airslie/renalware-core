89b1af37d9a38d7e20f3702b9d605a71ceb86fc4
Tim Crowe <tim@woodpigeon.com>
2020-10-09 13:34:38 +0100

    Add a list of users that all users can see
    This allows user to find eg the professional position
     or email address of a user.

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -7,3 +7,8 @@
 # Foundation extra: Add nav, nav_js and combo_nav_js helpers and templates for Foundation pagination
 # See https://ddnexus.github.io/pagy/extras/foundation
 require "pagy/extras/foundation"
+
+# Allow the client to request a custom number of items per page with an optional selector UI.
+# It is useful with APIs or higly user-customizable apps.
+# Usefule for testing pagination from testing by supplying eg :items => 1
+require "pagy/extras/items"
