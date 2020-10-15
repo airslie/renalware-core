5a96537cb1c03033f38c6a57f5dac841c846054e
Tim Crowe <tim@woodpigeon.com>
2020-10-15 17:27:14 +0100

    Add colour, position and weigthing to drug types
    To prepare the ground for making drug types more data-driven and
    to remove the hardcoding around eg esa and immunosuppresants.
    
    - position is to drive display order wherever a list of drug types is shown
    - color is a css colour eg '#abc123' used to help identify a drug's type when shown in tables and as a lozenge/badge background colour wherever a drug type name is displayed.
    - weighting is used to ensure that if a drug has say 2 types - 'ESA' and 'Some Other Less Significant Drug Type', and both have a colour, the colour of 'ESA' in this instance will trump the other, since it has greater clinical significance.

--- app/helpers/renalware/drugs_helper.rb
+++ app/helpers/renalware/drugs_helper.rb
@@ -11,6 +11,12 @@ module Renalware
       )
     end
 
+    # Now that we have a drug_types.colour column e.g. '#aaa' we can use here.
+    # Where the drug belongs to 2 drug types, we need to be sure to choose the colour of the most
+    # clinically significant drug type - eg 'ESA' wins over 'My other drug type'.
+    # This should be done by ordering a drugs types and taking the one with the highest value
+    # in the #weighting column - i.e. ESA will have a a large value in this column to ensure it
+    # trumps other types and its colour is used.
     def drug_types_colour_tag(drug_types)
       drug_names = drug_types.map(&:name)
       return "esa" if drug_names.include?("ESA")
