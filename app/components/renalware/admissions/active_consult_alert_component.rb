b63ea2c895f9c88ec6642e110edd583b95cb4bfc
Tim Crowe <tim@woodpigeon.com>
2020-10-19 19:18:46 +0100

    Display an alert if the patient has an active consult
    If the patient has one or more active consults, display
    an alert badge on the patient's pages saying
    `Active Alert` with a link to the consult.
    If the patient has >1 active consults, which is currently possible,
    display a link to the most recent one only.

---
+++ app/components/renalware/admissions/active_consult_alert_component.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Renalware
+  module Admissions
+    # rubocop:disable Rails/Pick
+    class ActiveConsultAlertComponent < ApplicationComponent
+      pattr_initialize [:current_user, :patient!]
+
+      def consult_id
+        @consult_id ||= begin
+          Consult
+            .where(patient_id: patient.id, ended_on: nil)
+            .order(started_on: :desc)
+            .pluck(:id)
+            .first
+        end
+      end
+
+      def render?
+        consult_id.present?
+      end
+    end
+    # rubocop:enable Rails/Pick
+  end
+end
---
+++ spec/components/renalware/admissions/active_consult_alert_component_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Renalware::Admissions::ActiveConsultAlertComponent, type: :component do
+  context "when the patient has no active consults" do
+    it "renders nothing" do
+      patient = create(:patient)
+      create(
+        :admissions_consult,
+        patient: patient,
+        started_on: 2.days.ago,
+        ended_on: 1.day.ago
+      )
+
+      component = described_class.new(patient: patient)
+      expect(component.render?).to eq(false)
+    end
+  end
+
+  context "when the patient has >1 active consults" do
+    it "renders an alert linking to the latest one" do
+      patient = create(:patient)
+      admission = create(
+        :admissions_consult,
+        patient: patient,
+        started_on: 1.day.ago,
+        ended_on: nil
+      )
+      create(
+        :admissions_consult,
+        patient: patient,
+        started_on: 10.days.ago,
+        ended_on: nil
+      )
+
+      component = described_class.new(patient: patient)
+      render_inline(described_class.new(patient: patient))
+
+      expect(component.render?).to eq(true)
+      expect(page).to have_content("Active Consult")
+      expect(page).to have_link(nil, href: edit_admissions_consult_path(id: admission.id))
+    end
+  end
+end
