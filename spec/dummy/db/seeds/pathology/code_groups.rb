79f1539b11534643e67b1affe81e4edc9d242391
Tim Crowe <tim@woodpigeon.com>
2020-10-22 18:24:32 +0100

    Seed pathology code groups

--- spec/dummy/db/seeds/pathology/code_groups.rb
+++ spec/dummy/db/seeds/pathology/code_groups.rb
@@ -9,11 +9,39 @@ module Renalware
         description: "Pathology relating to serum taken during a PD PET test." \
                      "position_within_subgroup determines the order inputs are "\
                      "populated in the PET pathology form.",
-        codes: %w(URE CRE PGLU NA POT)
+        subgroups: %w(URE CRE PGLU NA POT)
       },
       hd_session_form_recent: {
         description: "Recent pathology shown on the HD Sessions PDF print-out",
-        codes: %w(HGB PLT CRP POT CCA FER)
+        subgroups: [
+          %w(HGB PLT CRP POT CCA FER)
+        ]
+      },
+      letters: {
+        description: "Pathology shown on clinical letters",
+        subgroups: [
+          %w(HGB WBC PLT),
+          %w(URE),
+          %w(CRE EGFR),
+          %w(NA POT),
+          %w(BIC),
+          %w(CCA PHOS),
+          %w(PTHI),
+          %w(ALB),
+          %w(BIL AST ALP GGT),
+          %w(HBA),
+          %w(CHOL)
+        ]
+      },
+      default: {
+        description: "Default codes used for example in historical, recent and current pathology",
+        subgroups: [
+          [
+            %w(FOL ESR CRP FER HGB B12 MCV MCH HYPO WBC LYM NEUT PLT RETA),
+            %w(URAT ALB TP GLO URE CRE EGFR NA POT BIC CCA PHOS PTHI ACR),
+            %w(BIL ALT AST ALP GGT BGLU HBA HBAI CHOL HDL LDL TRIG TSH CK URR CRCL UREP AL)
+          ]
+        ]
       }
     }
 
@@ -26,12 +54,14 @@ module Renalware
         grp.updated_by = user
       end
 
-      Array(options[:codes]).each_with_index do |code, index|
-        membership = group.memberships.build
-        membership.observation_description = Pathology::ObservationDescription.find_by(code: code)
-        membership.subgroup = 1
-        membership.position_within_subgroup = index + 1
-        membership.save_by! user
+      Array(options[:subgroups]).each_with_index do |codes, subgrp_number|
+        Array(codes).each_with_index do |code, position|
+          membership = group.memberships.build
+          membership.observation_description = Pathology::ObservationDescription.find_by(code: code)
+          membership.subgroup = subgrp_number + 1
+          membership.position_within_subgroup = position + 1
+          membership.save_by! user
+        end
       end
     end
   end
