ec94d9f9c0e57c3c43ef974564389bd8817dcbc7
Tim Crowe <tim@woodpigeon.com>
2020-10-26 13:44:14 +0000

    Fix migration version errors

--- db/migrate/20201020171139_update_low_clearance_mdm_patients_view.rb
+++ db/migrate/20201020171139_update_low_clearance_mdm_patients_view.rb
@@ -1,4 +1,4 @@
-class UpdateLowClearanceMDMPatientsView < ActiveRecord::Migration[6.0]
+class UpdateLowClearanceMDMPatientsView < ActiveRecord::Migration[5.2]
   def change
     within_renalware_schema do
       update_view :low_clearance_mdm_patients, version: 2, revert_to_version: 1
--- db/migrate/20201021154809_update_transplant_mdm_patients_view.rb
+++ db/migrate/20201021154809_update_transplant_mdm_patients_view.rb
@@ -1,4 +1,4 @@
-class UpdateTransplantMDMPatientsView < ActiveRecord::Migration[6.0]
+class UpdateTransplantMDMPatientsView < ActiveRecord::Migration[5.2]
   def change
     within_renalware_schema do
       update_view :transplant_mdm_patients, version: 2, revert_to_version: 1
