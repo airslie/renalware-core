43a30b56fab4969d81ae22bdfa41cef68b99eeff
Tim Crowe <tim@woodpigeon.com>
2020-11-22 14:00:57 +0000

    Rename the test PathologyListener constant
    It is named incorrectly and will not load in the zeitwerk world.

--- spec/dummy/app/models/pathology_listener.rb
+++ spec/dummy/app/models/pathology_listener.rb
@@ -5,18 +5,14 @@
 # This listener had been wired to receive the letter_approved broadcast events in the
 # broadcast_subscription_map. See config/initializers/renalware.rb
 #
-module Renalware
-  module Pathology
-    class Listener
-      # The observation_request is about to saved.
-      def before_observation_request_persisted(obr_attributes)
-        # ...
-      end
+class PathologyListener
+  # The observation_request is about to saved.
+  def before_observation_request_persisted(obr_attributes)
+    # ...
+  end
 
-      # The observation_request has just been saved.
-      def after_observation_request_persisted(observation_request)
-        # ...
-      end
-    end
+  # The observation_request has just been saved.
+  def after_observation_request_persisted(observation_request)
+    # ...
   end
 end
