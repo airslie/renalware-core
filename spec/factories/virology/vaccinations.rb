6d4781ab7915efc5dffb2a5f37b5b37208ea2fa3
Tim Crowe <tim@woodpigeon.com>
2020-11-23 18:35:42 +0000

    Housekeeping - remove empty blocks

--- spec/factories/virology/vaccinations.rb
+++ spec/factories/virology/vaccinations.rb
@@ -3,6 +3,6 @@
 FactoryBot.define do
   factory :vaccination, class: "Renalware::Virology::Vaccination" do
     event_type factory: :vaccination_event_type
-    document {}
+    document { {} }
   end
 end
