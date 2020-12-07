f68196b74abc730caeecdd930d45d681642e06fa
Tim Crowe <tim@woodpigeon.com>
2020-12-07 20:47:24 +0000

    Enabled feed inputs in dev

--- spec/dummy/app/assets/javascripts/application.js
+++ spec/dummy/app/assets/javascripts/application.js
@@ -13,8 +13,8 @@
 //= require_tree .
 
 Renalware.Configuration.init({
-  disable_inputs_controlled_by_tissue_typing_feed: true,
-  disable_inputs_controlled_by_demographics_feed: true
+  disable_inputs_controlled_by_tissue_typing_feed: false,
+  disable_inputs_controlled_by_demographics_feed: false
 });
 
 // Running integration tests, we turn off animations to reduce the risk of timing errors.
