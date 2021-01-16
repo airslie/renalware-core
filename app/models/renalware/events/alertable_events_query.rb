dd0e641f68643eb81d2bea70b1206d73946f1c30
Tim Crowe <tim@woodpigeon.com>
2021-01-16 11:34:11 +0000

    Add EventTypeAlertTrigger model and query object

---
+++ app/models/renalware/events/alertable_events_query.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require_dependency "renalware/events"
+
+module Renalware
+  module Events
+    # Query object that returns, for a patient, the most recent matching event (if any) for each
+    # event_type_alert_trigger row in the databse. The results are used to display
+    # alerts in the UI.
+    # For example given a vaccination Event::Type and an EventTypeAlertTrigger
+    # which is configured to find any vaccination event with the word "covid" in
+    # anywhere in the document, this query will return the most matching event.
+    # It is possible to have mltiple triggers rows for the same event type, so for eample one
+    # could display triggers for the most recent covid vaccination, and the most recent HBV
+    # vaccination.
+    class AlertableEventsQuery
+      def self.call(patient:)
+        Event
+          .for_patient(patient)
+          .joins(event_type: :alert_triggers)
+          .select("DISTINCT ON (events.patient_id, event_type_alert_triggers.id) events.*")
+          .where("events.document::text ilike '%' || when_event_document_contains || '%'")
+          .order("events.patient_id, event_type_alert_triggers.id, events.created_at desc")
+      end
+    end
+  end
+end
---
+++ db/migrate/20210115181817_create_event_type_alert_triggers.rb
@@ -0,0 +1,14 @@
+class CreateEventTypeAlertTriggers < ActiveRecord::Migration[6.0]
+  def change
+    within_renalware_schema do
+      create_table(
+        :event_type_alert_triggers,
+        comment: "Matching alerts are displayed on patient pages"
+      ) do |t|
+        t.references :event_type, foreign_key: true, null: false
+        t.text :when_event_document_contains
+        t.timestamps null: false
+      end
+    end
+  end
+end
