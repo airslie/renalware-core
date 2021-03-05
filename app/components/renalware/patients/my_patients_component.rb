72a7fa6bf7f34a3a6bf6bebea1f25965c3c57b64
Tim Crowe <tim@woodpigeon.com>
2021-03-05 16:09:12 +0000

    Add a MyPatients component to dashboard
    If the current user is the named nurse for
    any patients, display the list of patients under the heading
    My Patients on the dasboard.
    If there are no patients where the current user is
    a named nurse then do not render the component at all.

---
+++ app/components/renalware/patients/my_patients_component.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+module Renalware
+  module Patients
+    class MyPatientsComponent < ApplicationComponent
+      include ApplicationHelper
+      include Pagy::Backend
+      include Pagy::Frontend
+
+      delegate :default_patient_link, to: :helpers
+      pattr_initialize [:current_user!]
+
+      def render?
+        patients.count > 0
+      end
+
+      def pagination
+        load_patients unless @pagination
+        @pagination
+      end
+
+      def patients
+        load_patients unless @patients
+        @patients
+      end
+
+      def formatted_title(title)
+        if pagination.items < pagination.count
+          "#{title} (#{pagination.items} of #{pagination.count})"
+        else
+          "#{title} (#{pagination.count})"
+        end
+      end
+
+      private
+
+      def load_patients
+        @pagination, @patients = pagy(scope, items: 10, link_extra: "data-remote='true'")
+      end
+
+      def scope
+        Patient
+          .where(named_nurse: current_user)
+          .order(updated_at: :desc)
+      end
+    end
+  end
+end
---
+++ spec/components/renalware/patients/my_patients_component_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Renalware::Patients::MyPatientsComponent, type: :component do
+  subject(:component) { described_class.new(current_user: user) }
+
+  let(:user) { create(:user) }
+
+  context "when the user is not a named nurse for any patients" do
+    it "does not render the component at all" do
+      render_inline(component)
+
+      expect(page.text).to be_blank
+    end
+  end
+
+  context "when the user is a named nurse" do
+    it "renders the user's patients" do
+      my_patient1 = create(:patient, by: user, named_nurse: user, family_name: "MyPatient1")
+      my_patient2 = create(:patient, by: user, named_nurse: user, family_name: "MyPatient2")
+      not_my_patient = create(:patient, by: user, named_nurse: nil, family_name: "Zzz")
+
+      render_inline(component)
+
+      expect(page).to have_content("My Patients (2)")
+      expect(page).to have_content(my_patient1.family_name.upcase)
+      expect(page).to have_content(my_patient2.family_name.upcase)
+      expect(page).not_to have_content(not_my_patient.family_name.upcase)
+    end
+  end
+end
