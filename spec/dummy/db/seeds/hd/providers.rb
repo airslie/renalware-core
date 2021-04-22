474d0249dd1d9d4e35f8a3a50205b4385b53968e
Tim Crowe <tim@woodpigeon.com>
2021-04-22 14:33:36 +0100

    Seed Nikkiso HD provider

--- spec/dummy/db/seeds/hd/providers.rb
+++ spec/dummy/db/seeds/hd/providers.rb
@@ -4,5 +4,6 @@ module Renalware
   log "Adding HD Providers" do
     HD::Provider.find_or_create_by!(name: "Diaverum")
     HD::Provider.find_or_create_by!(name: "Fresenius")
+    HD::Provider.find_or_create_by!(name: "Nikkiso")
   end
 end
