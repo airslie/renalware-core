959d2225b9b574163c6163481fc968137246264f
Tim Crowe <tim@woodpigeon.com>
2021-05-19 18:11:47 +0100

    Use red to highlight missing ESRF date in patient header
    If the patient's modality is hd, pd or transplant, display a red 'Missing' warning if the esrf_on is blank in their renal profile.
    For other modalities, display the esrf_on date if there is one, but if it is missing, just hide the ESRF: label altogether.

---
+++ app/components/renalware/renal/esrf_date_component.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+module Renalware
+  module Renal
+    # Encapusaltes the UI display of a patient's ESRF date for example in the
+    # mini profile at the top of patient pages.
+    # If the patient has a current modality in the speficied set then always
+    # display the ESRF: label and the esrf_on date, but if the date is missing
+    # then add a css class to make e.g. red, so that it becomes a 'nag'.
+    # If the patient has any other modality, only display the ESRF if one is
+    # present.
+    class ESRFDateComponent < ApplicationComponent
+      rattr_initialize [:patient!]
+      MODALITY_CODES_REQUIRING_AN_ESRF_DATE = %w(pd hd transplant).freeze
+
+      def render?
+        patient_should_have_an_esrf_date? || esrf_on.present?
+      end
+
+      def value
+        esrf_on.present? ? I18n.l(esrf_on) : "Missing"
+      end
+
+      def css_class
+        esrf_on.present? ? nil : "patient-missing-data"
+      end
+
+      def esrf_on
+        @esrf_on ||= Renal.cast_patient(patient)&.profile&.esrf_on
+      end
+
+      def modality_code
+        @modality_code ||= patient.current_modality&.description&.code
+      end
+
+      def patient_should_have_an_esrf_date?
+        MODALITY_CODES_REQUIRING_AN_ESRF_DATE.include?(modality_code)
+      end
+    end
+  end
+end
---
+++ spec/components/renalware/renal/esrf_date_component_spec.rb
@@ -0,0 +1,102 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+# rubocop:disable RSpec/PredicateMatcher
+describe Renalware::Renal::ESRFDateComponent, type: :component do
+  let(:esrf_on) { nil }
+  let(:profile) { Renalware::Renal::Profile.new(esrf_on: esrf_on) }
+  let(:patient) do
+    Renalware::Renal::Patient.new(profile: profile).tap do |pat|
+      # Make url generation work for patients
+      allow(pat).to receive(:to_param).and_return("1")
+    end
+  end
+
+  def change_current_modality(patient:, code:)
+    modality_desc = instance_double(Renalware::Modalities::Description, code: code)
+    modality = instance_double(Renalware::Modalities::Modality, description: modality_desc)
+    allow(patient).to receive(:current_modality).and_return(modality)
+  end
+
+  context "when the patient's current modality expects an ESFR date to be present" do
+    %w(hd pd transplant).each do |modality_code|
+      before { change_current_modality(patient: patient, code: modality_code) }
+
+      context "when the ESRF date is missing" do
+        it "displays a warning" do
+          component = described_class.new(patient: patient)
+
+          render_inline(component).to_html
+
+          expect(page).to have_content "ESRF:Missing"
+        end
+      end
+
+      context "when the ESRF date is present" do
+        let(:esrf_on) { "2018-01-01" }
+
+        it "renders the date" do
+          component = described_class.new(patient: patient)
+
+          render_inline(component).to_html
+
+          expect(page).to have_content "ESRF:01-Jan-2018"
+        end
+      end
+    end
+  end
+
+  context "when the patient's current modality does not expect an ESFR date" do
+    %w(akcc).each do |modality_code|
+      before { change_current_modality(patient: patient, code: modality_code) }
+
+      context "when the ESRF date is missing" do
+        let(:esrf_on) { nil }
+
+        it "renders nothing" do
+          component = described_class.new(patient: patient)
+
+          expect(component.render?).to be_falsey
+        end
+      end
+
+      context "when the ESRF date is present" do
+        let(:esrf_on) { "2018-01-01" }
+
+        it "renders the date" do
+          component = described_class.new(patient: patient)
+
+          render_inline(component).to_html
+
+          expect(page).to have_content "ESRF:01-Jan-2018"
+        end
+      end
+    end
+  end
+
+  context "when the patient has no current modality" do
+    context "when the ESRF date is missing" do
+      let(:esrf_on) { nil }
+
+      it "renders nothing" do
+        component = described_class.new(patient: patient)
+
+        expect(component.render?).to be_falsey
+      end
+    end
+
+    context "when the ESRF date is present" do
+      let(:esrf_on) { "2018-01-01" }
+
+      it "renders the date" do
+        component = described_class.new(patient: patient)
+
+        render_inline(component).to_html
+
+        expect(page).to have_content "ESRF:01-Jan-2018"
+      end
+    end
+  end
+end
+# rubocop:enable RSpec/PredicateMatcher
