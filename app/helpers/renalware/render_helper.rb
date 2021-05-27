36655ff82aaefa7fd8cd8434c7e3b539728dbf2a
Nihad Abbasov <nihad@42na.in>
2021-05-27 13:39:30 +0300

    Fix keyword arguments

--- app/helpers/renalware/render_helper.rb
+++ app/helpers/renalware/render_helper.rb
@@ -2,7 +2,7 @@
 
 module Renalware
   module RenderHelper
-    def render_if_exists(path_to_partial, **args)
+    def render_if_exists(path_to_partial, args = {})
       render(path_to_partial, args)
     rescue ActionView::MissingTemplate
       nil
