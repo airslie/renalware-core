fd17a671b709d065a85ef959abfc161dd4ec241b
Tim Crowe <tim@woodpigeon.com>
2021-07-23 10:45:59 +0100

    Enable webmock correctly in cucumber features
    Was previously not set up correctly and so was allowing outgoing connections

--- features/support/env.rb
+++ features/support/env.rb
@@ -23,13 +23,14 @@ include Renalware::Engine.routes.url_helpers
 require "cucumber/rails"
 require "capybara-screenshot/cucumber"
 require "rspec/rails"
-require "webmock"
+require "webmock/cucumber"
 
 require "factory_bot"
 require "./spec/support/factory_bot"
 FactoryBot.find_definitions
 
-WebMock.disable!
+WebMock.disable_net_connect!(allow_localhost: true)
+
 # Capybara defaults to CSS3 selectors rather than XPath.
 # If you"d prefer to use XPath, just uncomment this line and adjust any
 # selectors in your step definitions to use the XPath syntax.
