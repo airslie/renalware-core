3fc4367b43b8d1e2b2553a43c9f2c2d3eec9f2eb
Tim Crowe <tim@woodpigeon.com>
2021-10-30 16:12:58 +0100

    Allow read-only admin access to letter descriptions

--- app/policies/renalware/letters/description_policy.rb
+++ app/policies/renalware/letters/description_policy.rb
@@ -6,11 +6,15 @@ module Renalware
   module Letters
     class DescriptionPolicy < BasePolicy
       def index?
+        user_is_any_admin?
+      end
+      alias show? index?
+
+      def new?
         user_is_super_admin?
       end
-      alias new? index?
-      alias create? index?
-      alias sort? index?
+      alias create? new?
+      alias sort? new?
 
       def edit?
         return false if record.deleted?
