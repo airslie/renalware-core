b8902f7c07f68532891b06f65bef7c02236cd5d8
Tim Crowe <tim@woodpigeon.com>
2022-01-21 11:45:17 +0000

    Use data to drive Event edit/delete policies
    Add new columns to event_types to indicate various permissions for events of
    a certain type:
    - whether a superadmin can always edit/delete an event
    - the window of time (integer hours) within which admins can
      edit/delete an event
    - the window of time (integer hours) within which the event
      author/creator can edit/delete an event
    
    Allow these values to be configured under Admin -> Event Types.
    Apply the new data-driven event policy wherever displaying events.

---
+++ db/migrate/20220120172755_add_editable_window_columns_to_event_types.rb
@@ -0,0 +1,24 @@
+class AddEditableWindowColumnsToEventTypes < ActiveRecord::Migration[5.2]
+  def change
+    within_renalware_schema do
+      change_table :event_types do |t|
+        t.boolean :superadmin_can_always_change, default: true, null: false
+        t.integer(
+          :author_change_window_hours,
+          default: 0,
+          null: false,
+          comment: "Period post-creation within which an event of this type "\
+                   "can be edited by the author"
+        )
+        t.integer(
+          :admin_change_window_hours,
+          default: 0,
+          null: false,
+          comment: "Period post-creation within which an event of this type "\
+                   "can be edited by an admin (or superadmin if superadmin_can_always_change "\
+                   "is false"
+        )
+      end
+    end
+  end
+end
