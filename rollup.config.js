9cd5c1a89293bd5fcbcb084e84f85fe9942d6c9a
Tim Crowe <tim@woodpigeon.com>
2022-06-10 12:09:30 +0100

    Reference package.json 'main' in rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -4,13 +4,14 @@ import resolve from "@rollup/plugin-node-resolve"
 import commonjs from "@rollup/plugin-commonjs" // required for uglify
 import babel from "rollup-plugin-babel"
 import inject from "@rollup/plugin-inject" // allow referencing jquery in stimulus controllers
+import pkg from "./package.json"
 
 // Had to add context window to avoid rollup converting self to undefined in stimulus
 export default {
   context: "window",
   input: "app/javascript/renalware/application.js",
   output: {
-    file: "app/assets/javascripts/renalware/built.js",
+    file: pkg.main,
     format: "esm",
     //format: "es",
     inlineDynamicImports: true,
