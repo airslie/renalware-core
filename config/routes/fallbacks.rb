6b9766a73d9df1c23f3e3bdb258deab13b775138
Tim Crowe <tim@woodpigeon.com>
2022-08-05 18:13:07 +0100

    Return a 404 on eg /robots123.txt requests
    Azure healthcheck sends requests like '/robots3455.txt' and if we raise an error it pollutes the
    logs so instead handle these by redirecting to the errors controller which will return
    a 404 (and display a 404 page)

--- config/routes/fallbacks.rb
+++ config/routes/fallbacks.rb
@@ -10,3 +10,8 @@ get "/patients/:id/transplants/donor",
     to: redirect("/patients/%{id}/transplants/donor/dashboard")
 get "/patients/:id/transplants/recipient",
     to: redirect("/patients/%{id}/transplants/recipient/dashboard")
+
+# Azure healthcheck sends requests like '/robots3455.txt' and if we raise an error it pollutes the
+# logs so instead handle these by redirecting to the errors controller which will return
+# a 404 (and display a 404 page)
+get "/robots:id.txt", to: "system/errors#not_found", constraints: { id: /\d+/ }
