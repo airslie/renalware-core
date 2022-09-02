2dcdf32a94e8d8d7d1f09dbfcc87a11fe3a5286e
Tim Crowe <tim@woodpigeon.com>
2022-09-02 17:57:00 +0100

    Add comparison operator to BloodPressure

--- app/documents/renalware/blood_pressure.rb
+++ app/documents/renalware/blood_pressure.rb
@@ -18,5 +18,21 @@ module Renalware
     def blank?
       systolic.blank? && diastolic.blank?
     end
+
+    # Compare BPs by first comparing systolic values. If they are equal, compare by diastolic.
+    # Missing values default to a large number so they will come e.g last if #min is called
+    # on an array of BloodPressures.
+    def <=>(other)
+      dia = diastolic.presence || 10000
+      sys = systolic.presence || 10000
+      other_dia = other.diastolic.presence || 10000
+      other_sys = other.systolic.presence || 10000
+
+      if sys == other_sys
+        dia <=> other_dia
+      else
+        sys <=> other_sys
+      end
+    end
   end
 end
