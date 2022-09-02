dc1f5dcc1603cf226b2a1f8bdbcfa992103ab5bf
Tim Crowe <tim@woodpigeon.com>
2022-09-02 17:55:13 +0100

    Add missing patient visibility includes

--- app/controllers/renalware/patients/worryboard_controller.rb
+++ app/controllers/renalware/patients/worryboard_controller.rb
@@ -5,11 +5,15 @@ require_dependency "renalware/patients"
 module Renalware
   module Patients
     class WorryboardController < BaseController
+      include Renalware::Concerns::PatientVisibility
       include Renalware::Concerns::Pageable
 
       def show
         authorize Worry, :index?
-        query = WorryQuery.new(query_params)
+        query = WorryQuery.new(
+          query_params: query_params, 
+          patient_scope: patient_scope 
+        )
         worries = query.call.page(page).per(per_page)
         render locals: {
           query: query.search,
--- app/controllers/renalware/transplants/live_donors_controller.rb
+++ app/controllers/renalware/transplants/live_donors_controller.rb
@@ -5,10 +5,14 @@ require_dependency "renalware/transplants"
 module Renalware
   module Transplants
     class LiveDonorsController < BaseController
+      include Renalware::Concerns::PatientVisibility
       include Renalware::Concerns::Pageable
 
       def index
-        query = LiveDonorsQuery.new(params[:q])
+        query = LiveDonorsQuery.new(
+          params: params[:q], 
+          relation: policy_scope(Transplants::Patient.all)
+        )
         live_donors = query.call.page(page).per(per_page || 50)
 
         authorize live_donors
