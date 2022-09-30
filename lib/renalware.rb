ac9aa4a8cfd87495f13eabcdcf51784f70a91c6f
Martin Tomov <mtomov@outlook.com>
2022-09-30 18:19:53 +0100

    Enable Zeitwerk code loader

--- lib/renalware.rb
+++ lib/renalware.rb
@@ -1,15 +1,8 @@
 # frozen_string_literal: true
 
-# require "zeitwerk"
-
-# loader = Zeitwerk::Loader.for_gem
-# loader.setup
-
 require "renalware/version"
 require "renalware/engine"
 require "renalware/configuration"
 
 module Renalware
 end
-
-# loader.eager_load
