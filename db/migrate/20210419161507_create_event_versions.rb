d03deb9dfcad0fcb48f372882323bb4a62193a11
Tim Crowe <tim@woodpigeon.com>
2022-10-03 16:49:24 +0100

    Allow for a pre-existing event_versions table

--- db/migrate/20210419161507_create_event_versions.rb
+++ db/migrate/20210419161507_create_event_versions.rb
@@ -1,7 +1,10 @@
 class CreateEventVersions < ActiveRecord::Migration[5.2]
+  disable_ddl_transaction!
+
   def change
     within_renalware_schema do
-      create_table :event_versions do |t|
+      # Because BLT already have this object, we check for its existence before creating
+      create_table :event_versions, if_not_exists: true do |t|
         t.string   :item_type, null: false
         t.integer  :item_id,   null: false
         t.string   :event,     null: false
@@ -10,7 +13,24 @@ class CreateEventVersions < ActiveRecord::Migration[5.2]
         t.jsonb    :object_changes
         t.datetime :created_at
       end
-      add_index :event_versions, [:item_type, :item_id]
+
+      reversible do |dir|
+        dir.up   {
+          safety_assured do
+            # Because BLT already have this object, we check for its existence before creating
+            execute(<<-SQL.squish)
+              CREATE INDEX CONCURRENTLY IF NOT EXISTS
+                index_event_versions_on_item_type_and_item_id
+                ON renalware.event_versions USING btree (item_type, item_id);
+            SQL
+          end
+        }
+        dir.down {
+          # It is safer to leave the index intact because
+          # - it was already extant at BLT and we do not want to remove it if rolling back
+          # - it take a long time to create
+        }
+      end
     end
   end
 end
