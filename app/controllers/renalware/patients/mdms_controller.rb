a45b18e79ecb41255fbdb3e7d65ad72361ff1c90
Tim Crowe <tim@woodpigeon.com>
2022-10-12 16:52:07 +0100

    Fix incorrect MDM paths
    Some beta MDM patient paths where going to patient_transplants_mdm_path

--- app/controllers/renalware/patients/mdms_controller.rb
+++ app/controllers/renalware/patients/mdms_controller.rb
@@ -22,11 +22,13 @@ module Renalware
         sql_view_klass = Reporting::SqlView.new(view_name).klass
         sql_view_klass.reset_column_information
         search = sql_view_klass.ransack(params[:q])
-        view_proc = ->(patient) { patient_transplants_mdm_path(patient_id: patient.secure_id) }
+        view_proc = lambda { |patient|
+          send(:"patient_#{scope_name}_mdm_path", patient_id: patient.secure_id)
+        }
         pagy, patients = pagy(search.result)
         options = MDMListOptions.new(
           search: search,
-          rows: patients.load, # search.result.page(page).per(5).load,
+          rows: patients.load,
           current_view: current_view,
           view_proc: view_proc,
           pagination: pagy
@@ -36,6 +38,16 @@ module Renalware
       # rubocop:enable Metrics/MethodLength
 
       private
+
+      # Some 'mdm' entries in system.view_metadata have a #scope that does not yet have its own
+      # MDM patient view so we use a default scope for when building the route to the mdm view.
+      def scope_name
+        default_scope = "low_clearance"
+        case current_view.scope
+        when "akcc", "supportive_care", "dietetic" then default_scope
+        else current_view.scope
+        end
+      end
 
       def view_name
         "#{current_view.schema_name}.#{current_view.view_name}"
