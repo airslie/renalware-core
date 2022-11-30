f82634677c06655fc4852b42707f183944f1a1ca
Martin Tomov <mtomov@outlook.com>
2022-11-30 12:10:07 +0000

    ♻️ Improve Letter topics dropdown
    Goal
    ---
    
    Reduce logic in JS controller - `select_update_frame`,
    so that it can be reused for other similar use cases
    
    Move the dropdown collection to a tested presenter.

---
+++ app/presenters/renalware/letters/topics_presenter.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Renalware
+  module Letters
+    class TopicsPresenter
+      def self.list_for_dropdown(url)
+        Topic.ordered.select(:id, :text).map do |topic|
+          [
+            topic.text,
+            topic.id,
+            {
+              data: {
+                frame_url: url.sub("REPLACE_ID", topic.id.to_s)
+              }
+            }
+          ]
+        end
+      end
+    end
+  end
+end
---
+++ spec/presenters/renalware/letters/topics_presenter_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+module Renalware
+  module Letters
+    describe TopicsPresenter do
+      let(:insta) { ContactPresenter.new }
+
+      let(:contact) { build(:letter_contact) }
+
+      describe ".list_for_dropdown" do
+        let!(:topic) { create(:letter_topic, text: "Topic") }
+
+        it "returns a list with data attributes for frame url" do
+          result = described_class.list_for_dropdown("http://url/REPLACE_ID")
+
+          expect(result.size).to eq 1
+
+          option = result[0]
+          expect(option[0]).to eq "Topic"
+          expect(option[1]).to eq topic.id
+          expect(option[2][:data][:frame_url]).to eq "http://url/#{topic.id}"
+        end
+      end
+    end
+  end
+end
