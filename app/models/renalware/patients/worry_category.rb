5c96ee6ec4cbcb082634bb503a755e861da2bbfd
Tim Crowe <tim@woodpigeon.com>
2023-01-31 19:23:25 +0000

    HD Session form PDF (protocol) changes
    - Display worry category and notes on HD protocol: if a worry is present
      display e.g. "Yes (category name) notes and everything truncated to 30 chara..."
      otherwise "No"
    - Add solution flow rate from HD Profile to HD Protocol

--- app/models/renalware/patients/worry_category.rb
+++ app/models/renalware/patients/worry_category.rb
@@ -6,6 +6,10 @@ module Renalware
       include Accountable
       acts_as_paranoid
       validates :name, presence: true, uniqueness: true
+
+      def to_s
+        name
+      end
     end
   end
 end
---
+++ app/presenters/renalware/patients/worry_presenter.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module Renalware
+  module Patients
+    class WorryPresenter
+      pattr_initialize :worry
+
+      # If worry is present will return eg
+      # "Yes (category name) notes and everything truncated to 30 chara..."
+      def summary(truncate_at: 30)
+        if worry.present?
+          [
+            "Yes",
+            category_name,
+            worry.notes
+          ].compact.join(" ").truncate(truncate_at)
+        else
+          "No"
+        end
+      end
+
+      private
+
+      def category_name
+        return if worry.worry_category.nil?
+
+        "(#{worry.worry_category})"
+      end
+    end
+  end
+end
