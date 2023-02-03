a37319f4a532f0fcd21ca1a406b79af9a5487d7f
Tim Crowe <tim@woodpigeon.com>
2023-02-03 11:58:32 +0000

    Remove redundant DumbDelegator class
    Now that we are using the DumbDelegator gem.
    However I think we should remove the DumbDelegator gem and default to using delegate_missing_to as it is one less gem and the intention is a bit clearer. Its does mean we also have to delegate to_s manually however as this method is not really 'missing'!

--- app/presenters/renalware/letters/contact_presenter.rb
+++ app/presenters/renalware/letters/contact_presenter.rb
@@ -2,7 +2,11 @@
 
 module Renalware
   module Letters
-    class ContactPresenter < DumbDelegator
+    class ContactPresenter
+      pattr_initialize :contact
+      delegate_missing_to :contact
+      delegate :to_s, to: :contact
+
       def description_name
         description.unspecified? ? other_description : description.to_s
       end
