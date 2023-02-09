db6f5c2f68a7595389e23af99b60e69b86843b8f
Tim Crowe <tim@woodpigeon.com>
2023-02-09 15:43:40 +0000

    Introduce an icon helper to render inline svgs

---
+++ app/helpers/renalware/icon_helper.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+module Renalware
+  module IconHelper
+    INLINE_SVG_ICON_SIZE = {
+      xs: " h-3 w-3",
+      sm: " h-4 w-4",
+      md: " h-5 w-5",
+      lg: " h-6 w-6",
+      xl: " h-7 w-7"
+    }.freeze
+
+    # A generic icon helper rendering an inlince svg icon file using inline_svg_tag
+    # Example usage in a view:
+    # = inline_icon(:exclamation)
+    # = inline_icon(:exclamation, size: :lg)
+    # = inline_icon(:exclamation, class: "fill-red-400 stoke-red-100 inline-block")
+    # See `#inline_svg_tag` for acceptable options.
+    def inline_icon(name, size: :sm, **options)
+      options[:class] ||= ""
+      options[:class] ||= " stroke-current fill-current"
+      options[:class] += INLINE_SVG_ICON_SIZE.fetch(size.to_sym, "")
+      inline_svg_tag("renalware/icons/#{name}.svg", **options)
+    end
+
+    # Display a checked icon.
+    def inline_checked_icon(**options)
+      options[:class] ||= "inline-block fill-green-600"
+      inline_icon("check-circle", size: :md, **options) # check-circle is an odd size hence :md
+    end
+
+    # Display an unchecked icon
+    def inline_unchecked_icon(**options)
+      options[:class] ||= "inline-block stroke-gray-400"
+      inline_icon("circle", **options)
+    end
+
+    # Display a checked icon if boolean value is true, otherwise display an unchecked icon.
+    def inline_check_icon(checked, **options)
+      checked ? inline_checked_icon(**options) : inline_unchecked_icon(**options)
+    end
+  end
+end
