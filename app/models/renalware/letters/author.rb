f0aabc21eb730252b97b493222113940fd350c2b
Tim Crowe <tim@woodpigeon.com>
2023-02-12 08:34:39 +0000

    Fix error when an author views their patients
    Specify the foreign_key when using ActiveType model in a relationship.

--- app/models/renalware/letters/author.rb
+++ app/models/renalware/letters/author.rb
@@ -3,7 +3,9 @@
 module Renalware
   module Letters
     class Author < ActiveType::Record[Renalware::User]
-      has_many :letters, dependent: :restrict_with_exception
+      # rubocop:disable Rails/RedundantForeignKey
+      has_many :letters, dependent: :restrict_with_exception, foreign_key: :author_id
+      # rubocop:enable Rails/RedundantForeignKey
     end
   end
 end
