38d1eff7c1e63f9ae65d4c389b160d9c8cbbbacc
Tim Crowe <tim@woodpigeon.com>
2023-02-15 12:43:52 +0000

    Add Renal Reg outcome code and text to Deaths::Location

--- spec/dummy/db/seeds/deaths/locations.rb
+++ spec/dummy/db/seeds/deaths/locations.rb
@@ -2,8 +2,17 @@
 
 module Renalware
   log "Death locations" do
-    %w(Home Hospital Hospice Abroad Other).each do |name|
-      Deaths::Location.create!(name: name)
+    [
+      ["Home", 11, "Current Home"],
+      ["Nursing Home", 12, "Nursing Home"],
+      ["Hospice", 13, "Hospice"],
+      ["Hospital", 14, "Hospital"],
+      ["Other", 14, "Other"]
+    ].each do |opts|
+      Deaths::Location.find_or_create_by!(name: opts[0]) do |dl|
+        dl.rr_outcome_code = opts[1]
+        dl.rr_outcome_text = opts[2]
+      end
     end
   end
 end
