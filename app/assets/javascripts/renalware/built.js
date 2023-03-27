2c001b240c0439dd12bbc7ca9df69fa0cc973a5a
Martin Tomov <mtomov@outlook.com>
2023-03-27 11:48:02 +0100

    Address: Placeholder capital letters, size of flash message text
    & remove bg colour on tables

--- app/assets/javascripts/renalware/built.js
+++ app/assets/javascripts/renalware/built.js
@@ -1,3 +1,5 @@
+
+(function(l, r) { if (!l || l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (self.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(self.document);
 var commonjsGlobal = typeof globalThis !== 'undefined' ? globalThis : typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : {};
 
 var check = function (it) {
@@ -44164,3 +44166,4 @@ var index = /*#__PURE__*/Object.freeze({
 	createConsumer: createConsumer,
 	getConfig: getConfig
 });
+//# sourceMappingURL=built.js.map
