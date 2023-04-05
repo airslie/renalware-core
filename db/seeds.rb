b11bc747b6014ed3f020b4f6d561352eaf2fafe5
Martin Tomov <mtomov@outlook.com>
2023-04-05 22:29:05 +0100

    Use a simpler SeedsHelper require instead of SeedStep
    * can still call run a single seed file only by requiring it,
       ex:
    
    ```ruby
    require Renalware::Engine.root.join("spec/dummy/db/seeds/drugs/dmd/dmd_seeds")
    ```

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,8 @@ require_relative "./seeds/seeds_helper"
 
 PaperTrail.enabled = false
 
+include SeedsHelper
+
 # Seed the database with data common to all installations.
 # Site specific data should be seeded from the host application.
 require_relative "./seeds/default/seeds"
-
-SeedStep.seed_steps.each do |klass|
-  log klass.to_s.demodulize.titleize do
-    klass.new.call
-  end
-end
