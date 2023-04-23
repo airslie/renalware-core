eb89e66e193ec9663a83c8ebe5cd8f0a6233e7d6
Martin Tomov <mtomov@outlook.com>
2023-04-23 10:31:00 +0100

    DMDMigration::TradeFamilyMigrator -> handle trade family name "non-match"

--- app/models/renalware/drugs/dmd_migration/trade_family_migrator.rb
+++ app/models/renalware/drugs/dmd_migration/trade_family_migrator.rb
@@ -9,9 +9,12 @@ module Renalware
           dmd_matches = DMDMatch.where(approved_trade_family_match: true).where.not(trade_family_name: nil)
 
           dmd_matches.each do |dmd_match|
+            new_trade_family_id = trade_family_ids_by_name[dmd_match.trade_family_name]
+            next if new_trade_family_id.nil?
+
             Medications::Prescription.where(trade_family_id: nil)
               .where(drug_id: dmd_match.drug_id)
-              .update_all(trade_family_id: trade_family_ids_by_name[dmd_match.trade_family_name])
+              .update_all(trade_family_id: new_trade_family_id)
           end
         end
       end
