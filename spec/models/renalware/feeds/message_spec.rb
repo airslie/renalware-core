4be5f425fecc3dd469c21a3dbb4f1cd763d2885e
Tim Crowe <tim@woodpigeon.com>
2023-05-03 17:30:55 +0100

    Add enum columns to feed_messages
    To reduce space on disk used by the tables ~3GB per 10 million rows. Also makes things tidier.
    
    This is the first step and in another release we will drop the old event_code column after data has manually been moved.

--- spec/models/renalware/feeds/message_spec.rb
+++ spec/models/renalware/feeds/message_spec.rb
@@ -10,6 +10,7 @@ module Renalware
       is_expected.to validate_presence_of(:body)
       is_expected.to respond_to(:body_hash)
       is_expected.to have_db_index(:body_hash).unique(true)
+      is_expected.to have_db_index([:message_type, :event_type])
     end
   end
 end
