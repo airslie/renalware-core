bb5543791285504e3a5eded39cdaef2df1e38897
Tim Crowe <tim@woodpigeon.com>
2023-05-11 14:31:39 +0100

    Add a Transplant Review event

---
+++ app/models/renalware/transplants/review.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Renalware
+  module Transplants
+    class Review < Events::Event
+      belongs_to :patient, class_name: "Renalware::Patient"
+
+      def partial_for(partial_type)
+        File.join("renalware/transplants/reviews", partial_type)
+      end
+    end
+  end
+end
