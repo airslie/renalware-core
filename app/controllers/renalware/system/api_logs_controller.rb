eb803a8f27a1876f99e177d28e79aa6f069f3816
Tim Crowe <tim@woodpigeon.com>
2023-05-17 21:47:33 +0100

    An an API Logs superadmin view
    To aid debugging

---
+++ app/controllers/renalware/system/api_logs_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Renalware
+  module System
+    class APILogsController < BaseController
+      include Pagy::Backend
+
+      def index
+        authorize System::Message, :new?
+        pagy, api_logs =  pagy(APILog.all.order(created_at: :desc))
+        render locals: { api_logs: api_logs, pagy: pagy }
+      end
+    end
+  end
+end
