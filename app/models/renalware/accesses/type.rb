f29eef20383f1ff8f83bb226ea4ca078028f040e
Tim Crowe <tim@woodpigeon.com>
2023-06-08 16:54:16 +0100

    Rrestrict HD Session access type choice by hd_vascular: true
    Add a new column to access_types called hd_vascular which defaults to true.
    
    When loading ther Access Type dropdown in the HD Session form, only display where access types.hd_vascular = true

--- app/models/renalware/accesses/type.rb
+++ app/models/renalware/accesses/type.rb
@@ -8,7 +8,11 @@ module Renalware
 
       scope :ordered, -> { order(:name) }
       scope :having_abbreviation, -> { where.not(abbreviation: nil) }
-      scope :relevant_to_access_profiles, -> { where.not(rr02_code: nil).ordered }
+      scope :relevant_to_access_profiles, lambda {
+        where(hd_vascular: true)
+          .where.not(rr02_code: nil)
+          .ordered
+      }
 
       def long_name
         abbrev = abbreviation && " (#{abbreviation})"
