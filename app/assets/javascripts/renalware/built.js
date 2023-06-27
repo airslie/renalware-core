7152d05a46bf48fb3ba2b9faafc84e7801b07b07
Tim Crowe <tim@woodpigeon.com>
2023-06-27 14:39:04 +0100

    Remove livereload for now
    Using as is it gets into the ./built.js and then into build targets

--- app/assets/javascripts/renalware/built.js
+++ app/assets/javascripts/renalware/built.js
@@ -1,5 +1,3 @@
-
-(function(l, r) { if (!l || l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (self.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(self.document);
 var commonjsGlobal = typeof globalThis !== 'undefined' ? globalThis : typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : {};
 
 var check = function (it) {
--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,9 @@
 // rollupjs setup adapted from the approach used by ActiveStorage
-var devEnv = (process.env.NODE_ENV || "development") == "development";
 
 import resolve from "@rollup/plugin-node-resolve"
 import commonjs from "@rollup/plugin-commonjs" // required for uglify
 import babel from "rollup-plugin-babel"
 import pkg from "./package.json"
-import livereload from "rollup-plugin-livereload"
 
 const watchDirectories = [
   pkg.main,
@@ -32,15 +30,7 @@ export default {
   plugins: [
     babel(),
     resolve(),
-    commonjs(),
-    process.env.ROLLUP_WATCH && devEnv &&
-      livereload({
-        watch: watchDirectories,
-        // If true, set `config.assets.digest = false` in development.rb
-        applyCSSLive: false,
-        extraExts: ["slim"],
-        debug: false,
-      }),
+    commonjs()
   ],
   external: [],
 }
