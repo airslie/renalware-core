017490865669ae9f656de92ecb7cd22c63c4ae56
Tim Crowe <tim@woodpigeon.com>
2023-10-08 07:40:03 +0100

    Add an ENV-driven config to disable the DMD sync job

--- app/models/renalware/drugs/dmd/synchroniser_job.rb
+++ app/models/renalware/drugs/dmd/synchroniser_job.rb
@@ -8,6 +8,8 @@ module Renalware
     # @see Synchronisers::FullSynchroniser
     class SynchroniserJob < ApplicationJob
       def perform_now
+        return if Renalware.config.disable_dmd_synchroniser_job
+
         System::APILog.with_log "Drugs::DMD::SynchroniserJob" do
           Synchronisers::FullSynchroniser.new.call
         end
