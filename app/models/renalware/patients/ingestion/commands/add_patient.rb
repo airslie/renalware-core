dde582519932d852fe1a72e23b74c98c9c956942
Tim Crowe <tim@woodpigeon.com>
2023-10-30 21:15:25 +0000

    Rollback previous attempt to handle an AJ race condition
    Previously tried to do a search for the patient if it has been created by another thread in between our thread not-finding it and trying to create it. My solution was a bit misguided as in practice, at the point of rescuing the error, the transaction is already aborted - so we just end up with a different error. Will rely on a retry_on handler (#4523) instead as the race condition occurs fairly infrequently

--- app/models/renalware/patients/ingestion/commands/add_patient.rb
+++ app/models/renalware/patients/ingestion/commands/add_patient.rb
@@ -1,14 +1,11 @@
 # frozen_string_literal: true
 
-# rubocop:disable Style/RaiseArgs
 module Renalware
   module Patients
     module Ingestion
       module Commands
         class AddPatient < Command
-          class PossibleRaceConditionCreatingPatientError < StandardError; end
-
-          def self.call(...) = new(...).call
+          include Callable
 
           def initialize(message, mapper_factory: MessageMappers::Patient)
             @mapper_factory = mapper_factory
@@ -28,34 +25,20 @@ module Renalware
 
           def add_patient_if_not_exists
             patient = find_patient
-            return patient if patient.present? || ENV.key?("ADT_SKIP_UPDATE_PATIENT")
+            return patient if patient.present?
+            return patient if ENV.key?("ADT_SKIP_UPDATE_PATIENT")
 
-            patient = try_create_patient
-            BroadcastPatientAddedEvent.call(patient) if patient.new_record?
+            patient = mapper_factory.new(message, patient).fetch
+            patient.by = SystemUser.find
+            new_record = patient.new_record?
+            patient.save!
+
+            BroadcastPatientAddedEvent.call(patient) if new_record
+
             patient
-          end
-
-          def try_create_patient
-            begin
-              patient = mapper_factory.new(message, patient).fetch
-              patient.by = SystemUser.find
-              patient.save!
-              patient
-            rescue ActiveRecord::RecordNotUnique => e
-              # A few moments ago could we not find the patient and now we and now we can't create
-              # it due to a RecordNotUnique error... this can only (?) mean there has been race
-              # condition to create the patient due to two messages arriving hot on each other's
-              # heels... so try to find the patient again and if for any reason we can't then raise
-              # a descriptive error.
-              patient = find_patient
-              raise PossibleRaceConditionCreatingPatientError.new(e) if patient.blank?
-            ensure
-              patient
-            end
           end
         end
       end
     end
   end
 end
-# rubocop:enable Style/RaiseArgs
