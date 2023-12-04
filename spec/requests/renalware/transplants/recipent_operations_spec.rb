328764c6395c8dbfa7ce4ba9989dff940010bf84
Tim Crowe <tim@woodpigeon.com>
2023-12-04 14:50:00 +0000

    Fix error showing recipient transplant operation
    ie when clicking on the transplant operation date in the Tx dashboard.

---
+++ spec/requests/renalware/transplants/recipent_operations_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe "Transplant Recipient Operation" do
+  let(:patient) { create(:transplant_patient, family_name: "Rabbit", local_patient_id: "KCH12345") }
+
+  describe "GET show" do
+    it "responds successfully" do
+      operation = create(:transplant_recipient_operation, patient: patient)
+
+      get patient_transplants_recipient_operation_path(patient, operation)
+
+      expect(response).to be_successful
+    end
+  end
+end
