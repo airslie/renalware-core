363d1e69917a0ef14f4a688da522fdcbc4f86a95
Tim Crowe <tim@woodpigeon.com>
2024-02-09 11:03:47 +0000

    Display L or D against prescription dates in HD protocol
    When displaying administer_on_hd prescriptions on HD protocol (printable session form), if the prescription is stat then display the prescribed_date with a (D) suffix, otherwise for any non-stat drug that has been given before, display the date with (L) suffix meaning Last Given. Display no date for ungiven non-stat drugs.

--- spec/presenters/renalware/medications/prescription_presenter_spec.rb
+++ spec/presenters/renalware/medications/prescription_presenter_spec.rb
@@ -5,7 +5,13 @@ require "rails_helper"
 module Renalware
   module Medications
     describe PrescriptionPresenter do
-      let(:instance) {      described_class.new(prescription) }
+      let(:instance) { described_class.new(prescription) }
+      let(:user1) {
+        create(:user, password: "Renalware123!", password_confirmation: "Renalware123!")
+      }
+      let(:user2) {
+        create(:user, password: "Renalware123!", password_confirmation: "Renalware123!")
+      }
 
       describe "#frequency" do
         let(:prescription) { Prescription.new(frequency: "test", frequency_comment: "abc") }
@@ -17,6 +23,59 @@ module Renalware
         end
 
         it { is_expected.to eq "TEST abc" }
+      end
+
+      describe "#last_given_or_due_date_with_indicator" do
+        context "when hd and stat and not yet given" do
+          it "displays date with suffix (D) meaning due" do
+            prescription = Prescription.new(
+              administer_on_hd: true,
+              stat: true,
+              prescribed_on: "2020-01-01"
+            )
+
+            expect(described_class.new(prescription).last_given_or_due_date_with_indicator)
+              .to eq("01-Jan-2020 (D)")
+          end
+        end
+
+        context "when hd and NOT stat and not yet given" do
+          it "displays nothing" do
+            prescription = Prescription.new(
+              administer_on_hd: true,
+              stat: false,
+              prescribed_on: "2020-01-01",
+              patient: build(:patient)
+            )
+
+            expect(described_class.new(prescription).last_given_or_due_date_with_indicator)
+              .to be_blank
+          end
+        end
+
+        context "when hd and NOT stat and has been given before" do
+          it "displays date with suffix (L) meaning Last Given" do
+            prescription = create(
+              :prescription,
+              administer_on_hd: true,
+              stat: false,
+              prescribed_on: "2020-01-01"
+            )
+            HD::PrescriptionAdministration.create!(
+              prescription: prescription,
+              administered: true,
+              administered_by: user1,
+              witnessed_by: user2,
+              administered_by_password: "Renalware123!",
+              witnessed_by_password: "Renalware123!",
+              recorded_on: "2021-01-01",
+              by: user1
+            )
+
+            expect(described_class.new(prescription).last_given_or_due_date_with_indicator)
+              .to be_blank
+          end
+        end
       end
     end
   end
