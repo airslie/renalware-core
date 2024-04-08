55d3c57c05a94b0f7a90403ca0d9db32da402075
Tim Crowe <tim@woodpigeon.com>
2024-04-08 10:09:43 +0100

    Hide pagination links if there is only one page

--- app/controllers/renalware/pd/adequacy_completions_controller.rb
+++ app/controllers/renalware/pd/adequacy_completions_controller.rb
@@ -9,7 +9,7 @@ module Renalware
         query = AdequacyResult.joins(:patient).order(performed_on: :desc).ransack(params[:q])
         pagy, results = pagy(query.result)
         authorize results
-        render locals: { pagination: pagy, results: results, query: query }
+        render locals: { pagy: pagy, results: results, query: query }
       end
     end
   end
--- app/controllers/renalware/pd/pet_completions_controller.rb
+++ app/controllers/renalware/pd/pet_completions_controller.rb
@@ -9,7 +9,7 @@ module Renalware
         query = PETResult.joins(:patient).order(performed_on: :desc).ransack(params[:q])
         pagy, results = pagy(query.result)
         authorize results
-        render locals: { pagination: pagy, results: results, query: query }
+        render locals: { pagy: pagy, results: results, query: query }
       end
     end
   end
