1f16cc5a3000613f6d6fa5ac41d5d3716e497cae
Tim Crowe <tim@woodpigeon.com>
2024-05-15 08:55:55 +0100

    Do not validate prescriptions when terminating on death modality

--- spec/models/renalware/medications/terminate_all_patient_prescriptions_spec.rb
+++ spec/models/renalware/medications/terminate_all_patient_prescriptions_spec.rb
@@ -8,10 +8,11 @@ module Renalware::Medications
       user = create(:user)
       patient = create(:patient, by: user)
 
-      # Current prescriptions
+      # Create two current prescriptions
       create(:prescription, patient: patient)
       create(:prescription, patient: patient)
 
+      # This prescription is not current - it has already been terminated.
       previously_terminated_prescription = create(
         :prescription,
         patient: patient,
@@ -22,11 +23,30 @@ module Renalware::Medications
              terminated_on: "2018-01-01",
              by: user)
 
-      expect(patient.prescriptions.current.count).to eq(2)
+      # This current prescription is in the future with a future termination date also eg like a
+      # future stat drug
+      future_terminated_prescription = create(
+        :prescription,
+        patient: patient,
+        prescribed_on: 1.month.from_now
+      )
+      create(:prescription_termination,
+             prescription: future_terminated_prescription,
+             terminated_on: 2.months.from_now,
+             by: user)
+
+      # This current one is to be prescribed in the future, no termination date.
+      _future_unterminated_prescription = create(
+        :prescription,
+        patient: patient,
+        prescribed_on: 1.year.from_now
+      )
+
+      expect(patient.prescriptions.current.count).to eq(4)
 
       expect {
         described_class.call(patient: patient, by: user)
-      }.to change(patient.prescriptions.current, :count).by(-2)
+      }.to change(patient.prescriptions.current, :count).by(-4)
 
       # The termination date of this one should be unchanged
       expect(previously_terminated_prescription.reload.terminated_on.to_s).to eq("2018-01-01")
