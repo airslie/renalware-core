de603bd86c59a2637c06071a02e7125d41d497d2
Tim Crowe <tim@woodpigeon.com>
2024-05-23 16:16:39 +0100

    Change research_participations.external_id column from int to text
    To allow for external patient identifiers like "444-12345"

---
+++ db/migrate/20240523145856_change_research_participation_external_id_type.rb
@@ -0,0 +1,29 @@
+class ChangeResearchParticipationExternalIdType < ActiveRecord::Migration[7.1]
+  def change
+    within_renalware_schema do
+      reversible do |direction|
+        direction.up do
+          safety_assured do
+            add_column :research_participations,
+                       :external_id_deprecated,
+                       :integer,
+                       if_not_exists: true,
+                       comment: "Backup of external_id taken #{Time.zone.now} before changing " \
+                                "its type from int to text"
+            execute(<<~SQL.squish)
+              update research_participations set external_id_deprecated = external_id
+            SQL
+            change_column :research_participations, :external_id, :text
+          end
+        end
+        direction.down do
+          safety_assured do
+            change_column :research_participations,
+                          :external_id,
+                          "integer USING external_id::integer"
+          end
+        end
+      end
+    end
+  end
+end
