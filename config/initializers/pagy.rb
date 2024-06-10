dbb5fdc7dab2380783ad32274e4776da1c142acf
Tim Crowe <tim@woodpigeon.com>
2024-06-10 14:19:27 +0100

    Handle Pagy::OverflowError and display last page
    If a page number that does not exists, display the last page.
    https://github.com/ddnexus/pagy/blob/master/docs/extras/overflow.md

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -4,10 +4,16 @@
 # See here for all options: https://github.com/ddnexus/pagy/blob/master/lib/config/pagy.rb
 #
 
-Pagy::DEFAULT[:items] = 25
-Pagy::DEFAULT[:size] = [1, 4, 4, 1]
+# Allow easy handling of overflowing pages (i.e. requested page > count).
+# It internally rescues Pagy::OverflowError exceptions offering the following ready to use
+# behaviors/modes: :empty_page, :last_page, and :exception.
+require "pagy/extras/overflow"
 
 # Allow the client to request a custom number of items per page with an optional selector UI.
-# It is useful with APIs or higly user-customizable apps.
+# It is useful with APIs or highly user-customizable apps.
 # Useful for testing pagination from testing by supplying eg :items => 1
 require "pagy/extras/items"
+
+Pagy::DEFAULT[:items] = 25
+Pagy::DEFAULT[:size] = [1, 4, 4, 1]
+Pagy::DEFAULT[:overflow] = :last_page
