3fb66eeca4290d5c74dba9d5426c48bdd3060e19
Tim Crowe <tim@woodpigeon.com>
2024-07-02 17:44:25 +0100

    Correct l to L litre ukrdc measurement unit

--- spec/factories/medications/medication_route.rb
+++ spec/factories/medications/medication_route.rb
@@ -3,19 +3,26 @@
 FactoryBot.define do
   factory :medication_route, class: "Renalware::Medications::MedicationRoute" do
     initialize_with do
-      Renalware::Medications::MedicationRoute.find_or_create_by!(code: code, name: name)
+      Renalware::Medications::MedicationRoute
+        .find_or_create_by!(code: code, name: name) do |route|
+          route.rr_code = Renalware::Medications::MedicationRoute.rr22_code_for("Other")
+        end
     end
+
     code { "PO" }
     name { "Oral" }
+    rr_code { "1" }
 
     trait :po do
       code { "PO" }
       name { "Oral" }
+      rr_code { "1" }
     end
 
     trait :other do
       code { "Other" }
       name { "Other" }
+      rr_code { "1" }
     end
   end
 end
