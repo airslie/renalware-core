443c18ed2375604281e71bcf892d24341240f0fc
Tim Crowe <tim@woodpigeon.com>
2024-08-16 11:34:01 +0100

    Add a blank PDF to use as a replacement MESH letter

---
+++ app/models/renalware/letters/formats/pdf/blank_replacement.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module Renalware
+  module Letters
+    module Formats
+      module Pdf
+        class BlankReplacement
+          include Prawn::View
+
+          def initialize
+            @document = Prawn::Document.new(
+              page_size: "A4",
+              page_layout: :portrait
+            )
+            build
+          end
+
+          private
+
+          attr_reader :document
+
+          def build
+            move_down 100
+            text "Original letter has been removed.", align: :center, size: 12
+            self
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/models/renalware/letters/formats/pdf/blank_replacement_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require "pdf/reader"
+
+module Renalware::Letters::Formats::Pdf
+  describe BlankReplacement do
+    include LettersSpecHelper
+
+    describe "#render" do
+      it "generates a PDF stating the original letter has been replaced with blank this one" do
+        io = StringIO.new(described_class.new.render)
+
+        PDF::Reader.open(io) do |reader|
+          expect(reader.page_count).to eq(1)
+          expect(reader.pages[0]).to have_pdf_page_text("Original letter has been removed")
+        end
+      end
+    end
+  end
+end
