ade68aa2d2bfda6b755c440a9314b9f5c322f8fb
Tim Crowe <tim@woodpigeon.com>
2024-11-04 09:27:25 +0000

    Upgrade to Ruby 3.3.5

--- config/initializers/null_object.rb
+++ config/initializers/null_object.rb
@@ -5,6 +5,8 @@ module Renalware
     config.black_hole
     config.define_explicit_conversions
     config.singleton
+    def to_int = 0
+
     # Note adding `predicates_return false` causes failures e.g. with I18n.l(<null>)
     # config.predicates_return false
   end
