bf3cb4bed978eb9099a005931807f5204b78950a
Tim Crowe <tim@woodpigeon.com>
2024-11-18 08:22:34 +0000

    Add application presenter class
    Not yet used anywhere.

--- demo/app/models/application_record.rb
+++ demo/app/models/application_record.rb
@@ -2,4 +2,12 @@
 
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.decorate
+    "#{name}Presenter".constantize.decorate(self)
+  end
+
+  def decorate
+    "#{self.class.name}Presenter".constantize.decorate(self)
+  end
 end
