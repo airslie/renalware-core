f30b54fa98c44fc64c81947985a33d532da83481
Tim Crowe <tim@woodpigeon.com>
2024-11-29 11:27:53 +0000

    Reinstate snippet invocations
    Stimulus SnippetsController now POSTS to an invocationUrl supplied via outlets API by the 'child' SnippetController, in order to increment snippet usage count.

--- packs/authoring/spec/requests/renalware/authoring/snippet_invocations_spec.rb
+++ packs/authoring/spec/requests/renalware/authoring/snippet_invocations_spec.rb
@@ -6,11 +6,11 @@ describe "Invoking a snippet (indicating it has been used)" do
       user = Renalware::Authoring.cast_user(@current_user)
       snippet = create(:snippet, author: user, times_used: 0)
 
-      post(authoring.snippet_invocations_path(snippet, format: :js))
+      post(authoring.snippet_invocations_path(snippet, format: :json))
 
       expect(response).to be_successful
       expect(snippet.reload.times_used).to eq(1)
-      expect(response.media_type).to eq("text/javascript")
+      expect(response.media_type).to eq("application/json")
     end
   end
 end
