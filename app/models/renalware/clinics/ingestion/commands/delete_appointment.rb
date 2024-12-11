17542ff1f9c764b7626886fbd615f221725414dd
Tim Crowe <tim@woodpigeon.com>
2024-12-11 13:53:46 +0000

    Handle SIU^S15 delete appointment

--- app/models/renalware/clinics/ingestion/commands/delete_appointment.rb
+++ app/models/renalware/clinics/ingestion/commands/delete_appointment.rb
@@ -8,14 +8,16 @@ module Renalware
           include Callable
 
           pattr_initialize :message
-          delegate :patient_identification, :pv1, to: :message
-          delegate :clinic, :visit_number, to: :pv1
+          delegate :patient_identification, :pv1, :sch, to: :message
+          delegate :clinic, to: :pv1
 
           def call
             return unless patient && appointment
 
             appointment.destroy
           end
+
+          private
 
           def patient
             @patient ||= Feeds::PatientLocator.call(
@@ -26,6 +28,10 @@ module Renalware
 
           def appointment
             @appointment ||= Appointment.find_by(patient: patient, visit_number: visit_number)
+          end
+
+          def visit_number
+            message.siu? ? sch.visit_number : pv1.visit_number
           end
         end
       end
--- spec/requests/renalware/clinics/ingestion/siu_s15_appointment_cancellation_spec.rb
+++ spec/requests/renalware/clinics/ingestion/siu_s15_appointment_cancellation_spec.rb
@@ -11,7 +11,7 @@ describe "SIU^S15 - Notification of Appointment Cancellation" do
 
   #   MSH|^~\&|BLT_TIE|BLT|RENALWARE|MSE|20241107153747||SIU^S15|Q166123117|P|2.4
   #   SCH|149549027|||||6080524|Nephrology Haemodialysis F/Up||60|MINUTES|^^60^20241113100000^20241113110000||||||||||||||Cancelled
-  #   PID|1||10769859^^^RNJ 5C4 MRN^MRN||MOLLY^RENALTESTFIVE^^^^^CURRENT||19870101|2|||51 Sidney Street^^^LONDON^E1 2GB^^HOME^^||07898989898^MOBILE~0209999998^HOME~testemail@test.co^EMAIL|""^BUSINESS||""||13401074||||N||||||||N
+  #   PID|1||10769859^^^KCH^MRN||MOLLY^RENALTESTFIVE^^^^^CURRENT||19870101|2|||51 Sidney Street^^^LONDON^E1 2GB^^HOME^^||07898989898^MOBILE~0209999998^HOME~testemail@test.co^EMAIL|""^BUSINESS||""||13401074||||N||||||||N
   #   PD1|||THE CHRISP STREET HTH CTR^^F84062|G9401882^SMALL^AM^^^^^^EXTID
   #   PV1|1|O|RNJ Renal SBH^^^RNJ BARTS^^AMB^RNJ KGV|""|||C3590850|G9401882||361||||""||||PREADMIT|24301156^^^RNJATTNUM^VISITID|||||||||||||||||""|""||RNJ BARTS||||||
   #   RGS|1
@@ -21,6 +21,65 @@ describe "SIU^S15 - Notification of Appointment Cancellation" do
   include HL7Helpers
   include PatientsSpecHelper
 
-  it "todo"
+  let(:visit_number) { "123" }
+
+  let(:raw_hl7) do
+    hl7 = <<-HL7
+      MSH|^~\&|BLT_TIE|BLT|RENALWARE|MSE|20241107153747||SIU^S15|Q166123117|P|2.4
+      SCH|#{visit_number}|||||6080524|Nephrology Haemodialysis F/Up||60|MINUTES|^^60^20241113100000^20241113110000||||||||||||||Cancelled
+      PID|1||10769859^^^KCH^MRN||MOLLY^RENALTESTFIVE^^^^^CURRENT||19870101|2|||51 Sidney Street^^^LONDON^E1 2GB^^HOME^^||07898989898^MOBILE~0209999998^HOME~testemail@test.co^EMAIL|""^BUSINESS||""||13401074||||N||||||||N
+      PD1|||THE CHRISP STREET HTH CTR^^F84062|G9401882^SMALL^AM^^^^^^EXTID
+      PV1|1|O|RNJ Renal SBH^^^RNJ BARTS^^AMB^RNJ KGV|""|||C3590850|G9401882||361||||""||||PREADMIT|24301156^^^RNJATTNUM^VISITID|||||||||||||||||""|""||RNJ BARTS||||||
+      RGS|1
+      AIG|1||RNJ Chester, Dr Brook - Nurse 2
+      AIL|1||RNJ Renal SBH^^^RNJ BARTS^^AMB^RNJ KGV
+    HL7
+    hl7.gsub(/^ */, "")
+  end
+
+  def create_patient
+    create(
+      :clinics_patient,
+      local_patient_id: "10769859",
+      given_name: "MOLLY",
+      family_name: "RENALTESTFIVE",
+      born_on: Date.parse("19870101")
+    ).tap do |pat|
+      pat.current_address.update!(postcode: "RG7 0JB")
+    end
+  end
+
+  context "when the appointment exists" do
+    it "hard deletes it" do
+      patient = create_patient
+
+      Renalware::Clinics::Appointment.create!(
+        visit_number: visit_number,
+        starts_at: Time.zone.parse("20241105101500"),
+        ends_at: Time.zone.parse("20241105104500"),
+        patient: patient,
+        consultant: create(:consultant, code: "CONSULTANT1", name: "Dr A B"),
+        clinic: create(:clinic, code: "CLINIC1")
+      )
+
+      msg = hl7_message_from_raw_string(raw_hl7)
+
+      expect {
+        Renalware::Clinics::Ingestion::Commands::DeleteAppointment.call(msg)
+      }.to change(Renalware::Clinics::Appointment, :count).by(-1)
+    end
+  end
+
+  context "when the appointment does not exist" do
+    it "does nothing" do
+      create_patient
+
+      msg = hl7_message_from_raw_string(raw_hl7)
+
+      expect {
+        Renalware::Clinics::Ingestion::Commands::DeleteAppointment.call(msg)
+      }.not_to change(Renalware::Clinics::Appointment, :count)
+    end
+  end
 end
 # rubocop:enable Layout/LineLength
