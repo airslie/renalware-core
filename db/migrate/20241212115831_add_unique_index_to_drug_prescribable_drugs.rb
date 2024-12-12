41794fd723e3dabed9effd34dc92bdaafd9ab6c8
Tim Crowe <tim@woodpigeon.com>
2024-12-12 12:25:30 +0000

    Refresh prescribable_drugs materialised view after a drug edit
    If a drug is edited in the backend UI, including adding/removing a trade family or drug type, or a drug is added or deleted, refresh the drugs_prescribable_drugs materialized view - concurrently to avoid locking the table (requires a new unique index) - to make the changes surface wherever drugs are selected in the UI.

---
+++ db/migrate/20241212115831_add_unique_index_to_drug_prescribable_drugs.rb
@@ -0,0 +1,12 @@
+class AddUniqueIndexToDrugPrescribableDrugs < ActiveRecord::Migration[7.2]
+  def change
+    within_renalware_schema do
+      safety_assured do
+        add_index :drug_prescribable_drugs,
+                  :compound_id,
+                  unique: true,
+                  comment: "Unique idx on this materialized view enables us to refresh concurrently"
+      end
+    end
+  end
+end
