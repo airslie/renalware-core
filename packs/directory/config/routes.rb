05b0c472a7a42daaf880974f3bd7a468a8a1c036
Tim Crowe <tim@woodpigeon.com>
2024-12-24 23:11:02 +0000

    Update rails to 8.0.1

--- packs/directory/config/routes.rb
+++ packs/directory/config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Renalware::Directory::Engine.routes.draw do
-  resources :people, except: [:delete] do
+  resources :people, except: [:destroy] do
     collection do
       get :search
     end
