36c1581efdca7f4be69abd198b5dfe39c3d90ed4
Tim Crowe <tim@woodpigeon.com>
2025-03-12 21:57:30 +0000

    Downgrade migration versions from 8 to 7
    To support BLT still on 7.x

--- db/migrate/20250228173152_correct_tristate_booleans.rb
+++ db/migrate/20250228173152_correct_tristate_booleans.rb
@@ -1,4 +1,4 @@
-class CorrectTristateBooleans < ActiveRecord::Migration[8.0]
+class CorrectTristateBooleans < ActiveRecord::Migration[7.0]
   def change
     within_renalware_schema do
       safety_assured do
