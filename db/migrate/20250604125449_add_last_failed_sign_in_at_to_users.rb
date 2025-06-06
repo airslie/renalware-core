565df96c680bc949229186978fe41f945df554e8
Phil Thompson <34678+PhilT@users.noreply.github.com>
2025-06-06 15:43:23 +0100

    Ensure DB migration runs in the Renalware schema on Rails 7

--- db/migrate/20250604125449_add_last_failed_sign_in_at_to_users.rb
+++ db/migrate/20250604125449_add_last_failed_sign_in_at_to_users.rb
@@ -1,5 +1,7 @@
-class AddLastFailedSignInAtToUsers < ActiveRecord::Migration[8.0]
+class AddLastFailedSignInAtToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :last_failed_sign_in_at, :datetime
+    within_renalware_schema do
+      add_column :users, :last_failed_sign_in_at, :datetime
+    end
   end
 end
