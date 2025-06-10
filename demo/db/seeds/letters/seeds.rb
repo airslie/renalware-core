4f0e0449bb77dd67d54d9a9a9b387e8465a0075e
Tim Crowe <tim@woodpigeon.com>
2025-06-10 13:12:12 +0100

    Do not seed letter_mailshot_patients_where_surname_starts_with_r view

--- demo/db/seeds/letters/seeds.rb
+++ demo/db/seeds/letters/seeds.rb
@@ -5,7 +5,7 @@ require_relative "contacts"
 require_relative "rabbit"
 
 # Create demo SQL view to demostrate mailshots
-ActiveRecord::Base.connection.execute(<<-SQL.squish)
-  create or replace view renalware.letter_mailshot_patients_where_surname_starts_with_r
-  as select id as patient_id from patients where family_name like 'R%';
-SQL
+# ActiveRecord::Base.connection.execute(<<-SQL.squish)
+#   create or replace view renalware.letter_mailshot_patients_where_surname_starts_with_r
+#   as select id as patient_id from patients where family_name like 'R%';
+# SQL
