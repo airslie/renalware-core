535402cd7a4b5937df71adcf0da82b7b2f67101b
Phil Thompson <34678+PhilT@users.noreply.github.com>
2025-07-08 16:31:20 +0100

    More comments

--- app/helpers/renalware/component_helper.rb
+++ app/helpers/renalware/component_helper.rb
@@ -1,4 +1,6 @@
 module Renalware::ComponentHelper
+  # Work out the component class name from the model class
+  # e.g. Renalware::Events::SimpleEvent -> Renalware::Events::Detail::SimpleEvent
   def render_toggled_cell(record)
     class_parts = record.class.name.split("::")
     class_name = class_parts.pop
