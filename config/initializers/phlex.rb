ab03776fb4fa7b152b8eada0932f9ed9ca699325
Phil Thompson <34678+PhilT@users.noreply.github.com>
2025-07-08 16:26:56 +0100

    Add some comments

--- config/initializers/phlex.rb
+++ config/initializers/phlex.rb
@@ -22,4 +22,6 @@ Rails.autoloaders.main.push_dir(ENGINE.join("app/views"), namespace: Views)
 Rails.autoloaders.main.push_dir(ENGINE.join("app/components/shared"), namespace: Shared)
 Rails.autoloaders.main.push_dir(ENGINE.join("app/components/renalware"), namespace: Renalware)
 
+# Squashes out the Shared part of the namespace so that shared components can be called from the
+# Renalware module without the fully qualified name: E.g. DescriptionListItem instead of Shared::DescriptionListItem
 Rails.autoloaders.main.collapse(ENGINE.join("app/components/shared/*"))
