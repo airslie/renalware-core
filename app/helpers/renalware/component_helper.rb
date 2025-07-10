72bb15e68e13c7baf2931d6c2bf2a8e14daf93eb
Phil Thompson <34678+PhilT@users.noreply.github.com>
2025-07-10 20:24:37 +0100

    When used outside Phlex this was causing a not found error

--- app/helpers/renalware/component_helper.rb
+++ app/helpers/renalware/component_helper.rb
@@ -1,5 +1,5 @@
 module Renalware::ComponentHelper
   def render_toggled_cell(record)
-    render NameService.from_model(record, to: "Detail", keep_class: true).new(record)
+    render Renalware::NameService.from_model(record, to: "Detail", keep_class: true).new(record)
   end
 end
