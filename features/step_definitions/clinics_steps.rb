2e44a7c470618403fde26f006d228306a58aa3ff
Phil Thompson <34678+PhilT@users.noreply.github.com>
2025-07-14 16:45:28 +0100

    Migrate recording_a_clinic_visit.feature to a system spec

--- features/step_definitions/clinics_steps.rb
+++ features/step_definitions/clinics_steps.rb
@@ -4,28 +4,12 @@ Given /^Clinics$/ do |clinics|
   end
 end
 
-Given /Patty has a recorded clinic visit/ do
-  @clinic_visit = create_clinic_visit(@patty, @clyde || Renalware::User.first)
-end
-
 Given /the following consultants:/ do |table|
   table.raw.flatten.each do |name|
     FactoryBot.create(:consultant, name: name, code: SecureRandom.uuid)
   end
 end
 
-When /Clyde records Patty's clinic visit/ do
-  @clinic_visit = record_clinic_visit(@patty, @clyde)
-end
-
-When /Clyde updates Patty's clinic visit/ do
-  update_clinic_visit(@clinic_visit, @patty, @clyde)
-end
-
-Then /Patty's clinic visit should exist/ do
-  expect_clinic_visit_to_exist(@patty)
-end
-
-Then /Patty's clinic visit should be updated/ do
-  expect_clinic_visit_to_be_updated(@clinic_visit)
+Given /Patty has a recorded clinic visit/ do
+  @clinic_visit = create_clinic_visit(@patty, @clyde || Renalware::User.first)
 end
--- spec/system/renalware/clinics/visits_spec.rb
+++ spec/system/renalware/clinics/visits_spec.rb
@@ -1,14 +1,77 @@
-describe "Global Clinic Visits list" do
-  describe "GET index" do
+describe "Clinic Visits" do
+  let(:clinician) { create(:user, :clinical) }
+  let!(:clinic) { create(:clinic) }
+  let!(:patient) { create(:clinics_patient, by: clinician, nhs_number: "2717073604") }
+
+  before { login_as clinician }
+
+  describe "Global Clinic Visits list" do
     it "responds successfully" do
-      user = login_as_clinical
-      patient = create(:clinics_patient, by: user, nhs_number: "2717073604")
-      create(:clinic_visit, patient: patient, by: user)
+      create(:clinic_visit, patient: patient, by: clinician)
 
       visit clinic_visits_path
 
       expect(page.status_code).to eq(200)
       expect(page).to have_content("271 707 3604")
+    end
+  end
+
+  describe "Recording a clinic visit", :js do
+    it "creates a clinic visit" do
+      visit new_patient_clinic_visit_path(patient_id: patient)
+
+      within(".document") { fill_in "Date", with: "20-07-2015" }
+      slim_select "Access", from: "Clinic"
+
+      within ".document" do
+        fill_in "Height", with: "1.78"
+        fill_in "Weight", with: "82.5"
+        fill_in "Pulse", with: "100"
+        fill_in "Temperature", with: "37.3"
+        fill_in "Blood Pressure", with: "110/75"
+        fill_in "Standing BP", with: "107/71"
+        find("trix-editor").set("Notes")
+        find("textarea[name='clinic_visit[admin_notes]']").set("Admin notes")
+
+        click_on t("btn.create")
+      end
+
+      expect(page).to have_content(
+        "20-Jul-2015\tNo\t#{clinic.description}\t1.78\t82.5\t26.0\t110/75\t107/71\t100\t37.3"
+      )
+    end
+  end
+
+  describe "Updating a clinic visit", :js do
+    let(:patient) { clinic_visit.patient }
+    let!(:clinic_visit) { create(:clinic_visit, clinic:, by: clinician) }
+    let(:today) { l(Date.current) }
+
+    it "updates a clinic visit" do
+      visit edit_patient_clinic_visit_path(
+        patient_id: patient,
+        id: clinic_visit.id
+      )
+
+      fill_in "Height", with: "1.71"
+      fill_in "Weight", with: "75"
+      fill_in "Pulse", with: "101"
+      fill_in "Temperature", with: "37.7"
+      fill_in "Blood Pressure", with: "128/95"
+      fill_in "Standing BP", with: "124/92"
+      find("trix-editor").set("Updated notes")
+      fill_in "clinic_visit[admin_notes]", with: "Updated admin notes"
+
+      submit_form
+
+      expect(page).to have_content(
+        "#{today}\tNo\t#{clinic.description}\t1.71\t75.0\t25.6\t128/95\t124/92\t101\t37.7"
+      )
+
+      all("a.toggler")[1].click
+
+      expect(page).to have_content "Updated notes"
+      expect(page).to have_content "Updated admin notes"
     end
   end
 end
