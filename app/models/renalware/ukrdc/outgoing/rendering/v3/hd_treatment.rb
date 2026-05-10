9a97af792cc426c49c05f081bdc2cf3230f868a1
Tim Crowe <tim@woodpigeon.com>
2026-05-10 21:35:23 +0100

    Fix QBL05 mapping in v3 UKRDC

--- app/models/renalware/ukrdc/outgoing/rendering/v3/hd_treatment.rb
+++ app/models/renalware/ukrdc/outgoing/rendering/v3/hd_treatment.rb
@@ -8,6 +8,12 @@ module Renalware
           # Handles rendering an HD Treatment (aka modality). We defer to the base Treatment
           # class but pass in some extra arguments to the ctor.
           class HDTreatment < Treatment
+            UNIT_TYPE_RR8_MAP = {
+              hospital: "HOSP",
+              satellite: "SATL",
+              home: "HOME"
+            }.freeze
+
             def initialize(treatment:)
               encounter_number = [
                 treatment.modality_id,
@@ -17,8 +23,13 @@ module Renalware
               super(
                 treatment:,
                 encounter_number:,
-                attributes: { "QBL05" => treatment.hospital_unit&.unit_type_rr8 }
+                attributes: { "QBL05" => unit_type_rr8_for(treatment.hospital_unit) }
               )
+            end
+
+            # Map unit_type to its equivalent Renal Registry RR8 code.
+            def unit_type_rr8_for(unit)
+              UNIT_TYPE_RR8_MAP[unit&.unit_type&.to_sym]
             end
           end
         end
