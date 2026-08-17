39bcc9012ad9c53e226d42d87258a4b75250643b
Tim Crowe <tim@woodpigeon.com>
2026-08-17 17:43:22 +0100

    Make gpg spec more reliable

--- spec/lib/gpg_encrypt_file_spec.rb
+++ spec/lib/gpg_encrypt_file_spec.rb
@@ -29,6 +29,11 @@ describe GpgEncryptFile do
       "/tmp/patient record.xml"
     ]
   end
+  let(:gpg_command) { instance_double(GpgCommand, to_a: command) }
+
+  before do
+    allow(GpgCommand).to receive(:new).with(file:, options:).and_return(gpg_command)
+  end
 
   it "passes each GPG option as a separate subprocess argument" do
     allow(Open3).to receive(:popen3).with(*command).and_yield(nil, nil, StringIO.new(""))
@@ -46,20 +51,21 @@ describe GpgEncryptFile do
   end
 
   describe GpgCommand, "creating the keyring" do
+    subject(:gpg_command) { described_class.allocate }
+
+    before do
+      gpg_command.instance_variable_set(:@file, file)
+      gpg_command.instance_variable_set(:@options, options)
+    end
+
+    it "builds the encryption command as separate arguments" do
+      expect(gpg_command.to_a).to eq(command)
+    end
+
     it "passes each import option as a separate subprocess argument" do
-      command = described_class.allocate
-      command.instance_variable_set(:@file, Pathname("/tmp/patient.xml"))
-      command.instance_variable_set(
-        :@options,
-        GpgOptions.new(
-          recipient: "Patient View",
-          destination_folder: Pathname("/tmp"),
-          public_key_name: "patient view.asc"
-        )
-      )
       allow(Open3).to receive(:capture3)
 
-      command.send(:create_keyring)
+      gpg_command.send(:create_keyring)
 
       expect(Open3).to have_received(:capture3).with(
         "gpg",
@@ -67,7 +73,7 @@ describe GpgEncryptFile do
         "--no-default-keyring",
         "--keyring",
         Rails.root.join("config/gpg/ukrdc_keyring.gpg").to_s,
-        Rails.root.join("config/gpg/public_keys/patient view.asc").to_s
+        Rails.root.join("config/gpg/public_keys/patient-view.asc").to_s
       )
     end
   end
